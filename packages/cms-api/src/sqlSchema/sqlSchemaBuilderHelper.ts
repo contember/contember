d97950d7cf1b725bc5d40f7c1e95fab2df03e891
David Matejka <matej21@matej21.cz>
2018-07-23 11:01:34 +0200

    sql schema builder refactoring

---
+++ packages/cms-api/src/sqlSchema/sqlSchemaBuilderHelper.ts
@@ -0,0 +1,13 @@
+import { Schema } from "../schema/model"
+import buildSqlSchema from "./sqlSchemaBuilder";
+
+
+const getSql = (schema: Schema): string => {
+  const builderClass = require("node-pg-migrate/dist/migration-builder").default
+  const migrationBuilder = new builderClass({}, {
+    query: null,
+    select: null,
+  })
+  buildSqlSchema(schema, migrationBuilder)
+  return (migrationBuilder as any).getSql()
+}

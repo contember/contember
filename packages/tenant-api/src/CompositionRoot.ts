673b2df47f98910d4e6749bff71a82ff04c70697
Jan Tvrdik <jan.tvrdik@gmail.com>
2018-07-30 06:15:08 +0200

    tenant-api: refactor CompositionRoot to new Container class

--- packages/tenant-api/src/CompositionRoot.ts
+++ packages/tenant-api/src/CompositionRoot.ts
@@ -1,123 +1,73 @@
 import * as knex from 'knex'
 import {ApolloServer} from 'apollo-server'
-import {DocumentNode} from 'graphql'
 import typeDefs from './schema/tenant.graphql'
-import Resolver from './resolvers/Resolver'
 import SignInMutationResolver from './resolvers/mutation/SignInMutationResolver'
 import KnexConnection from './core/knex/KnexConnection'
 import Env from './Env'
 import QueryHandler from './core/query/QueryHandler'
 import KnexQueryable from './core/knex/KnexQueryable'
-import QueryHandlerAccessor from './core/query/QueryHandlerAccessor'
 import SignInManager from './model/service/SignInManager'
 import SignUpManager from './model/service/SignUpManager'
 import SignUpMutationResolver from './resolvers/mutation/SignUpMutationResolver'
 import MeQueryResolver from './resolvers/query/MeQueryResolver'
 import ProjectMemberManager from './model/service/ProjectMemberManager'
-import AddProjectMemberMutationResolver from './resolvers/mutation/AddProjectMemberMutationResolver'
 import ApiKeyManager from './model/service/ApiKeyManager'
+import Container from './core/dic/Container'
 
 export default class CompositionRoot {
   composeServer(env: Env): ApolloServer {
-    const db = this.createDatabaseConnection(env)
-    const queryHandler = this.createQueryHandler(db)
+    const container = new Container.Builder({})
+      .addService('env', () => env)
 
-    const apiKeyManager = this.createApiKeyManager(queryHandler, db)
-    const signUpManager = this.createSignUpManager(queryHandler, db)
-    const signInManager = this.createSignInManager(queryHandler, apiKeyManager)
-    const projectMemberManager = this.createProjectMemberManager(queryHandler, db)
+      .addService('knexConnection', ({env}) => {
+        return new KnexConnection(knex({
+          debug: true,
+          client: 'pg',
+          connection: {
+            host: env.DB_HOST,
+            port: env.DB_PORT,
+            user: env.DB_USER,
+            password: env.DB_PASSWORD,
+            database: env.DB_DATABASE,
+          }
+        }))
+      })
+      .addService('queryHandler', ({knexConnection}) => {
+        const handler = new QueryHandler(new KnexQueryable(knexConnection, {
+          get(): QueryHandler<KnexQueryable> {
+            return handler
+          }
+        }))
 
-    const meQueryResolver = this.createMeQueryResolver(queryHandler)
-    const signUpMutationResolver = this.createSignUpMutationResolver(signUpManager, queryHandler)
-    const signInMutationResolver = this.createSignInMutationResolver(signInManager, queryHandler)
-    const addProjectMemberMutationResolver = this.createAddProjectMemberMutationResolver(projectMemberManager)
-
-    const resolver = this.createResolver(
-      meQueryResolver,
-      signUpMutationResolver,
-      signInMutationResolver,
-      addProjectMemberMutationResolver,
-    )
-
-    return this.createServer([typeDefs], resolver)
-  }
-
-  private createServer(typeDefs: Array<DocumentNode>, resolvers: Resolver): ApolloServer {
-    return new ApolloServer({
-      typeDefs,
-      resolvers,
-    })
-  }
-
-  private createResolver(meQuery: MeQueryResolver, signUp: SignUpMutationResolver, signIn: SignInMutationResolver, addProjectMember: AddProjectMemberMutationResolver): Resolver {
-    return {
-      Query: {
-        me: meQuery.me.bind(meQuery),
-      },
-      Mutation: {
-        signUp: signUp.signUp.bind(signUp),
-        signIn: signIn.signIn.bind(signIn),
-        addProjectMember: addProjectMember.addProjectMember.bind(addProjectMember)
-      },
-    }
-  }
-
-  private createAddProjectMemberMutationResolver(projectMemberManager: ProjectMemberManager): AddProjectMemberMutationResolver {
-    return new AddProjectMemberMutationResolver(projectMemberManager)
-  }
-
-  private createSignInMutationResolver(signInManager: SignInManager, queryHandler: QueryHandler<KnexQueryable>): SignInMutationResolver {
-    return new SignInMutationResolver(signInManager, queryHandler);
-  }
-
-  private createSignUpMutationResolver(signUpManager: SignUpManager, queryHandler: QueryHandler<KnexQueryable>): SignUpMutationResolver {
-    return new SignUpMutationResolver(signUpManager, queryHandler)
-  }
-
-  private createMeQueryResolver(queryHandler: QueryHandler<KnexQueryable>): MeQueryResolver {
-    return new MeQueryResolver(queryHandler)
-  }
-
-  private createProjectMemberManager(queryHandler: QueryHandler<KnexQueryable>, db: KnexConnection): ProjectMemberManager {
-    return new ProjectMemberManager(queryHandler, db)
-  }
-
-  private createSignInManager(queryHandler: QueryHandler<KnexQueryable>, apiKeyManager: ApiKeyManager): SignInManager {
-    return new SignInManager(queryHandler, apiKeyManager)
-  }
-
-  private createSignUpManager(queryHandler: QueryHandler<KnexQueryable>, db: KnexConnection): SignUpManager {
-    return new SignUpManager(queryHandler, db)
-  }
-
-  private createApiKeyManager(queryHandler: QueryHandler<KnexQueryable>, db: KnexConnection): ApiKeyManager {
-    return new ApiKeyManager(queryHandler, db)
-  }
-
-  private createQueryHandler(db: KnexConnection): QueryHandler<KnexQueryable> {
-    const accessor = new class implements QueryHandlerAccessor<KnexQueryable> {
-      get(): QueryHandler<KnexQueryable> {
         return handler
-      }
-    };
+      })
 
-    const queryable = new KnexQueryable(db, accessor)
-    const handler = new QueryHandler<KnexQueryable>(queryable)
+      .addService('apiKeyManager', ({queryHandler, knexConnection}) => new ApiKeyManager(queryHandler, knexConnection))
+      .addService('signUpManager', ({queryHandler, knexConnection}) => new SignUpManager(queryHandler, knexConnection))
+      .addService('signInManager', ({queryHandler, apiKeyManager}) => new SignInManager(queryHandler, apiKeyManager))
+      .addService('projectMemberManager', ({queryHandler, knexConnection}) => new ProjectMemberManager(queryHandler, knexConnection))
 
-    return handler
-  }
+      .addService('meQueryResolver', ({queryHandler}) => new MeQueryResolver(queryHandler))
+      .addService('signUpMutationResolver', ({signUpManager, queryHandler}) => new SignUpMutationResolver(signUpManager, queryHandler))
+      .addService('signInMutationResolver', ({signInManager, queryHandler}) => new SignInMutationResolver(signInManager, queryHandler))
+      .addService('addProjectMemberMutationResolver', ({queryHandler, knexConnection}) => new ProjectMemberManager(queryHandler, knexConnection))
 
-  private createDatabaseConnection(env: Env): KnexConnection {
-    return new KnexConnection(knex({
-      debug: true,
-      client: 'pg',
-      connection: {
-        host: env.DB_HOST,
-        port: env.DB_PORT,
-        user: env.DB_USER,
-        password: env.DB_PASSWORD,
-        database: env.DB_DATABASE,
-      }
-    }))
+      .addService('resolvers', ({meQueryResolver, signUpMutationResolver, signInMutationResolver, addProjectMemberMutationResolver}) => {
+        return {
+          Query: {
+            me: meQueryResolver.me.bind(meQueryResolver),
+          },
+          Mutation: {
+            signUp: signUpMutationResolver.signUp.bind(signUpMutationResolver),
+              signIn: signInMutationResolver.signIn.bind(signInMutationResolver),
+              addProjectMember: addProjectMemberMutationResolver.addProjectMember.bind(addProjectMemberMutationResolver)
+          },
+        }
+      })
+
+      .addService('server', ({resolvers}) => new ApolloServer({typeDefs, resolvers}))
+      .build()
+
+    return container.get('server')
   }
 }

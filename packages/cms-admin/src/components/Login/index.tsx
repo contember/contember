dcad396d7b987f72373212ec8d903b5a8c2f22e3
Vojta Staněk <stanekv01@gmail.com>
2018-08-07 17:05:12 +0200

    FE: Login and SignUp page

---
+++ packages/cms-admin/src/components/Login/index.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react'
+import { InputGroup, FormGroup, Card, Elevation, H1, Button } from '@blueprintjs/core'
+import { LoginModel } from '../../model/LoginModel'
+
+interface State {
+	email: string
+	password: string
+	loading: boolean
+	error: boolean
+	message?: string
+}
+
+export default class Login extends React.Component<{}, State> {
+	state: State = {
+		email: '',
+		password: '',
+		loading: false,
+		error: false
+	}
+
+	loginModel = LoginModel.create()
+
+	render() {
+		return (
+			<Card elevation={Elevation.ONE}>
+				<H1>Login</H1>
+				<form
+					onSubmit={async e => {
+						e.preventDefault()
+						this.setState({ loading: true })
+						const result = await this.loginModel.logIn(this.state.email, this.state.password)
+						this.setState({ loading: false, error: !result.ok, message: result.message })
+					}}
+				>
+					<FormGroup label="Email">
+						<InputGroup
+							value={this.state.email}
+							autoComplete="username"
+							type="email"
+							disabled={this.state.loading}
+							onChange={(e: React.FormEvent<HTMLInputElement>) => this.setState({ email: e.currentTarget.value })}
+						/>
+					</FormGroup>
+					<FormGroup label="Password">
+						<InputGroup
+							type="password"
+							autoComplete="current-password"
+							value={this.state.password}
+							disabled={this.state.loading}
+							onChange={(e: React.FormEvent<HTMLInputElement>) => this.setState({ password: e.currentTarget.value })}
+						/>
+					</FormGroup>
+					<Button type="submit" disabled={this.state.loading}>
+						Login
+					</Button>
+				</form>
+			</Card>
+		)
+	}
+}

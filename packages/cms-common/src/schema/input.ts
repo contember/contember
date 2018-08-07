c0a03804b6c9872b498151db3b056bf2a26ca389
Vojta Staněk <stanekv01@gmail.com>
2018-08-07 15:29:47 +0200

    Prettier: Fix common and client

--- packages/cms-common/src/schema/input.ts
+++ packages/cms-common/src/schema/input.ts
@@ -1,140 +1,140 @@
 namespace Input {
-  export interface Object<E = never> {
-    [key: string]: ColumnValue<E>
-  }
+	export interface Object<E = never> {
+		[key: string]: ColumnValue<E>
+	}
 
-  export interface List<E = never> extends Array<ColumnValue<E>> {}
+	export interface List<E = never> extends Array<ColumnValue<E>> {}
 
-  export type PrimaryValue<E = never> = string | number | E
+	export type PrimaryValue<E = never> = string | number | E
 
-  export type AtomicValue<E = never> = PrimaryValue<E> | null | boolean
-  export type ColumnValue<E = never> = AtomicValue<E> | Object<E> | List<E>
+	export type AtomicValue<E = never> = PrimaryValue<E> | null | boolean
+	export type ColumnValue<E = never> = AtomicValue<E> | Object<E> | List<E>
 
-  export type GenericValueLike<T> = T | PromiseLike<T> | (() => T | PromiseLike<T>)
+	export type GenericValueLike<T> = T | PromiseLike<T> | (() => T | PromiseLike<T>)
 
-  export type ColumnValueLike<E = never> = GenericValueLike<ColumnValue<E>>
+	export type ColumnValueLike<E = never> = GenericValueLike<ColumnValue<E>>
 
-  export interface ConnectRelationInput<E = never> {
-    connect: UniqueWhere<E>
-  }
+	export interface ConnectRelationInput<E = never> {
+		connect: UniqueWhere<E>
+	}
 
-  export interface CreateRelationInput<E = never> {
-    create: CreateDataInput<E>
-  }
+	export interface CreateRelationInput<E = never> {
+		create: CreateDataInput<E>
+	}
 
-  export interface DisconnectSpecifiedRelationInput<E = never> {
-    disconnect: UniqueWhere<E>
-  }
+	export interface DisconnectSpecifiedRelationInput<E = never> {
+		disconnect: UniqueWhere<E>
+	}
 
-  export interface DeleteSpecifiedRelationInput<E = never> {
-    delete: UniqueWhere<E>
-  }
+	export interface DeleteSpecifiedRelationInput<E = never> {
+		delete: UniqueWhere<E>
+	}
 
-  export interface UpdateSpecifiedRelationInput<E = never> {
-    update: { where: UniqueWhere<E>; data: UpdateDataInput<E> }
-  }
+	export interface UpdateSpecifiedRelationInput<E = never> {
+		update: { where: UniqueWhere<E>; data: UpdateDataInput<E> }
+	}
 
-  export interface UpsertSpecifiedRelationInput<E = never> {
-    upsert: { where: UniqueWhere<E>; update: UpdateDataInput<E>; create: CreateDataInput<E> }
-  }
+	export interface UpsertSpecifiedRelationInput<E = never> {
+		upsert: { where: UniqueWhere<E>; update: UpdateDataInput<E>; create: CreateDataInput<E> }
+	}
 
-  export interface DisconnectRelationInput {
-    disconnect: true
-  }
+	export interface DisconnectRelationInput {
+		disconnect: true
+	}
 
-  export interface UpdateRelationInput<E = never> {
-    update: UpdateDataInput<E>
-  }
+	export interface UpdateRelationInput<E = never> {
+		update: UpdateDataInput<E>
+	}
 
-  export interface DeleteRelationInput {
-    delete: true
-  }
+	export interface DeleteRelationInput {
+		delete: true
+	}
 
-  export interface UpsertRelationInput<E = never> {
-    upsert: { update: UpdateDataInput<E>; create: CreateDataInput<E> }
-  }
+	export interface UpsertRelationInput<E = never> {
+		upsert: { update: UpdateDataInput<E>; create: CreateDataInput<E> }
+	}
 
-  export interface CreateDataInput<E = never> {
-    [column: string]: ColumnValue<E> | CreateOneRelationInput<E> | CreateManyRelationInput<E>
-  }
+	export interface CreateDataInput<E = never> {
+		[column: string]: ColumnValue<E> | CreateOneRelationInput<E> | CreateManyRelationInput<E>
+	}
 
-  export type CreateOneRelationInput<E = never> = ConnectRelationInput<E> | CreateRelationInput<E>
+	export type CreateOneRelationInput<E = never> = ConnectRelationInput<E> | CreateRelationInput<E>
 
-  export type CreateManyRelationInput<E = never> = CreateOneRelationInput<E>[]
+	export type CreateManyRelationInput<E = never> = CreateOneRelationInput<E>[]
 
-  export interface UpdateDataInput<E = never> {
-    [column: string]: ColumnValue<E> | UpdateOneRelationInput<E> | UpdateManyRelationInput<E>
-  }
+	export interface UpdateDataInput<E = never> {
+		[column: string]: ColumnValue<E> | UpdateOneRelationInput<E> | UpdateManyRelationInput<E>
+	}
 
-  export interface UpdateInput<E = never> {
-    where: UniqueWhere<E>
-    data: UpdateDataInput<E>
-  }
+	export interface UpdateInput<E = never> {
+		where: UniqueWhere<E>
+		data: UpdateDataInput<E>
+	}
 
-  export interface CreateInput<E = never> {
-    data: CreateDataInput<E>
-  }
+	export interface CreateInput<E = never> {
+		data: CreateDataInput<E>
+	}
 
-  export interface DeleteInput<E = never> {
-    where: UniqueWhere<E>
-  }
+	export interface DeleteInput<E = never> {
+		where: UniqueWhere<E>
+	}
 
-  export interface UniqueQueryInput<E = never> {
-    where: UniqueWhere<E>
-  }
+	export interface UniqueQueryInput<E = never> {
+		where: UniqueWhere<E>
+	}
 
-  export interface ListQueryInput<E = never> {
-    where?: Where<E>
-  }
+	export interface ListQueryInput<E = never> {
+		where?: Where<E>
+	}
 
-  export type UpdateOneRelationInput<E = never> =
-    | CreateRelationInput<E>
-    | ConnectRelationInput<E>
-    | DeleteRelationInput
-    | DisconnectRelationInput
-    | UpdateRelationInput<E>
-    | UpsertRelationInput<E>
+	export type UpdateOneRelationInput<E = never> =
+		| CreateRelationInput<E>
+		| ConnectRelationInput<E>
+		| DeleteRelationInput
+		| DisconnectRelationInput
+		| UpdateRelationInput<E>
+		| UpsertRelationInput<E>
 
-  export type UpdateManyRelationInput<E = never> = Array<
-    | CreateRelationInput<E>
-    | ConnectRelationInput<E>
-    | DeleteSpecifiedRelationInput<E>
-    | DisconnectSpecifiedRelationInput<E>
-    | UpdateSpecifiedRelationInput<E>
-    | UpsertSpecifiedRelationInput<E>
-  >
+	export type UpdateManyRelationInput<E = never> = Array<
+		| CreateRelationInput<E>
+		| ConnectRelationInput<E>
+		| DeleteSpecifiedRelationInput<E>
+		| DisconnectSpecifiedRelationInput<E>
+		| UpdateSpecifiedRelationInput<E>
+		| UpsertSpecifiedRelationInput<E>
+	>
 
-  export interface Condition<T> {
-    and?: Array<Condition<T>>
-    or?: Array<Condition<T>>
-    not?: Condition<T>
+	export interface Condition<T> {
+		and?: Array<Condition<T>>
+		or?: Array<Condition<T>>
+		not?: Condition<T>
 
-    eq?: T
-    null?: boolean
-    notEq?: T
-    in?: T[]
-    notIn?: T[]
-    lt?: T
-    lte?: T
-    gt?: T
-    gte?: T
-  }
+		eq?: T
+		null?: boolean
+		notEq?: T
+		in?: T[]
+		notIn?: T[]
+		lt?: T
+		lte?: T
+		gt?: T
+		gte?: T
+	}
 
-  export interface UniqueWhere<E = never> {
-    [field: string]: PrimaryValue<E>
-  }
+	export interface UniqueWhere<E = never> {
+		[field: string]: PrimaryValue<E>
+	}
 
-  export interface ComposedWhere<E = never> {
-    and?: Where<E>[]
-    or?: Where<E>[]
-    not?: Where<E>
-  }
+	export interface ComposedWhere<E = never> {
+		and?: Where<E>[]
+		or?: Where<E>[]
+		not?: Where<E>
+	}
 
-  interface FieldWhere<E = never> {
-    [name: string]: Condition<ColumnValue<E>> | Where<ColumnValue<E>>
-  }
+	interface FieldWhere<E = never> {
+		[name: string]: Condition<ColumnValue<E>> | Where<ColumnValue<E>>
+	}
 
-  export type Where<E = never> = ComposedWhere<E> & FieldWhere<E>
+	export type Where<E = never> = ComposedWhere<E> & FieldWhere<E>
 }
 
 export default Input
--- packages/cms-common/src/schema/model.ts
+++ packages/cms-common/src/schema/model.ts
@@ -1,166 +1,166 @@
 namespace Model {
-  export interface Entity {
-    name: string
-    pluralName: string
-    primary: string
-    primaryColumn: string
-    tableName: string
-    fields: { [name: string]: Column | AnyRelation }
-    unique: Array<{ fields: string[]; name: string }>
-  }
+	export interface Entity {
+		name: string
+		pluralName: string
+		primary: string
+		primaryColumn: string
+		tableName: string
+		fields: { [name: string]: Column | AnyRelation }
+		unique: Array<{ fields: string[]; name: string }>
+	}
 
-  export interface Column {
-    name: string
-    default?: any | (() => any)
-    type: string
-    columnName: string
-    nullable: boolean
-    options?: {
-      [name: string]: any
-    }
-  }
+	export interface Column {
+		name: string
+		default?: any | (() => any)
+		type: string
+		columnName: string
+		nullable: boolean
+		options?: {
+			[name: string]: any
+		}
+	}
 
-  export interface ColumnVisitor<T> {
-    visitColumn(entity: Entity, column: Column): T
-  }
+	export interface ColumnVisitor<T> {
+		visitColumn(entity: Entity, column: Column): T
+	}
 
-  export interface RelationVisitor<T> {
-    visitRelation(entity: Entity, relation: Relation, targetEntity: Entity, targetRelation: Relation | null): T
-  }
+	export interface RelationVisitor<T> {
+		visitRelation(entity: Entity, relation: Relation, targetEntity: Entity, targetRelation: Relation | null): T
+	}
 
-  export type FieldVisitor<T> = ColumnVisitor<T> &
-    (RelationVisitor<T> | RelationByTypeVisitor<T> | RelationByGenericTypeVisitor<T>)
+	export type FieldVisitor<T> = ColumnVisitor<T> &
+		(RelationVisitor<T> | RelationByTypeVisitor<T> | RelationByGenericTypeVisitor<T>)
 
-  export interface RelationByTypeVisitor<T> {
-    visitManyHasOne(
-      entity: Entity,
-      relation: ManyHasOneRelation,
-      targetEntity: Entity,
-      targetRelation: OneHasManyRelation | null
-    ): T
+	export interface RelationByTypeVisitor<T> {
+		visitManyHasOne(
+			entity: Entity,
+			relation: ManyHasOneRelation,
+			targetEntity: Entity,
+			targetRelation: OneHasManyRelation | null
+		): T
 
-    visitOneHasMany(
-      entity: Entity,
-      relation: OneHasManyRelation,
-      targetEntity: Entity,
-      targetRelation: ManyHasOneRelation
-    ): T
+		visitOneHasMany(
+			entity: Entity,
+			relation: OneHasManyRelation,
+			targetEntity: Entity,
+			targetRelation: ManyHasOneRelation
+		): T
 
-    visitOneHasOneOwner(
-      entity: Entity,
-      relation: OneHasOneOwnerRelation,
-      targetEntity: Entity,
-      targetRelation: OneHasOneInversedRelation | null
-    ): T
+		visitOneHasOneOwner(
+			entity: Entity,
+			relation: OneHasOneOwnerRelation,
+			targetEntity: Entity,
+			targetRelation: OneHasOneInversedRelation | null
+		): T
 
-    visitOneHasOneInversed(
-      entity: Entity,
-      relation: OneHasOneInversedRelation,
-      targetEntity: Entity,
-      targetRelation: OneHasOneOwnerRelation
-    ): T
+		visitOneHasOneInversed(
+			entity: Entity,
+			relation: OneHasOneInversedRelation,
+			targetEntity: Entity,
+			targetRelation: OneHasOneOwnerRelation
+		): T
 
-    visitManyHasManyOwner(
-      entity: Entity,
-      relation: ManyHasManyOwnerRelation,
-      targetEntity: Entity,
-      targetRelation: ManyHasManyInversedRelation | null
-    ): T
+		visitManyHasManyOwner(
+			entity: Entity,
+			relation: ManyHasManyOwnerRelation,
+			targetEntity: Entity,
+			targetRelation: ManyHasManyInversedRelation | null
+		): T
 
-    visitManyHasManyInversed(
-      entity: Entity,
-      relation: ManyHasManyInversedRelation,
-      targetEntity: Entity,
-      targetRelation: ManyHasManyOwnerRelation
-    ): T
-  }
+		visitManyHasManyInversed(
+			entity: Entity,
+			relation: ManyHasManyInversedRelation,
+			targetEntity: Entity,
+			targetRelation: ManyHasManyOwnerRelation
+		): T
+	}
 
-  export interface RelationByGenericTypeVisitor<T> {
-    visitHasMany(entity: Entity, relation: Relation, targetEntity: Entity, targetRelation: Relation | null): T
+	export interface RelationByGenericTypeVisitor<T> {
+		visitHasMany(entity: Entity, relation: Relation, targetEntity: Entity, targetRelation: Relation | null): T
 
-    visitHasOne(
-      entity: Entity,
-      relation: Relation & NullableRelation,
-      targetEntity: Entity,
-      targetRelation: Relation | null
-    ): T
-  }
+		visitHasOne(
+			entity: Entity,
+			relation: Relation & NullableRelation,
+			targetEntity: Entity,
+			targetRelation: Relation | null
+		): T
+	}
 
-  export enum RelationType {
-    OneHasOne = 'OneHasOne',
-    OneHasMany = 'OneHasMany',
-    ManyHasOne = 'ManyHasOne',
-    ManyHasMany = 'ManyHasMany'
-  }
+	export enum RelationType {
+		OneHasOne = 'OneHasOne',
+		OneHasMany = 'OneHasMany',
+		ManyHasOne = 'ManyHasOne',
+		ManyHasMany = 'ManyHasMany'
+	}
 
-  export type AnyRelation =
-    | OneHasManyRelation
-    | ManyHasOneRelation
-    | OneHasOneInversedRelation
-    | OneHasOneOwnerRelation
-    | ManyHasManyInversedRelation
-    | ManyHasManyOwnerRelation
+	export type AnyRelation =
+		| OneHasManyRelation
+		| ManyHasOneRelation
+		| OneHasOneInversedRelation
+		| OneHasOneOwnerRelation
+		| ManyHasManyInversedRelation
+		| ManyHasManyOwnerRelation
 
-  export interface Relation<T extends RelationType = RelationType> {
-    name: string
-    relation: T
-    target: string
-  }
+	export interface Relation<T extends RelationType = RelationType> {
+		name: string
+		relation: T
+		target: string
+	}
 
-  export interface InversedRelation extends Relation {
-    ownedBy: string
-  }
+	export interface InversedRelation extends Relation {
+		ownedBy: string
+	}
 
-  export interface OwnerRelation extends Relation {
-    inversedBy?: string
-  }
+	export interface OwnerRelation extends Relation {
+		inversedBy?: string
+	}
 
-  export enum OnDelete {
-    cascade = 'cascade',
-    restrict = 'restrict',
-    setNull = 'set null'
-  }
+	export enum OnDelete {
+		cascade = 'cascade',
+		restrict = 'restrict',
+		setNull = 'set null'
+	}
 
-  export type JoiningColumn = {
-    columnName: string
-    onDelete: OnDelete
-  }
+	export type JoiningColumn = {
+		columnName: string
+		onDelete: OnDelete
+	}
 
-  export interface JoiningColumnRelation {
-    joiningColumn: JoiningColumn
-  }
+	export interface JoiningColumnRelation {
+		joiningColumn: JoiningColumn
+	}
 
-  export interface NullableRelation {
-    nullable: boolean
-  }
+	export interface NullableRelation {
+		nullable: boolean
+	}
 
-  export interface JoiningTable {
-    tableName: string
-    joiningColumn: JoiningColumn
-    inverseJoiningColumn: JoiningColumn
-  }
+	export interface JoiningTable {
+		tableName: string
+		joiningColumn: JoiningColumn
+		inverseJoiningColumn: JoiningColumn
+	}
 
-  export interface JoiningTableRelation {
-    joiningTable: JoiningTable
-  }
+	export interface JoiningTableRelation {
+		joiningTable: JoiningTable
+	}
 
-  export type OneHasManyRelation = Relation<RelationType.OneHasMany> & InversedRelation
-  export type ManyHasOneRelation = Relation<RelationType.ManyHasOne> &
-    OwnerRelation &
-    JoiningColumnRelation &
-    NullableRelation
-  export type OneHasOneInversedRelation = Relation<RelationType.OneHasOne> & InversedRelation & NullableRelation
-  export type OneHasOneOwnerRelation = Relation<RelationType.OneHasOne> &
-    OwnerRelation &
-    JoiningColumnRelation &
-    NullableRelation
-  export type ManyHasManyInversedRelation = Relation<RelationType.ManyHasMany> & InversedRelation
-  export type ManyHasManyOwnerRelation = Relation<RelationType.ManyHasMany> & OwnerRelation & JoiningTableRelation
+	export type OneHasManyRelation = Relation<RelationType.OneHasMany> & InversedRelation
+	export type ManyHasOneRelation = Relation<RelationType.ManyHasOne> &
+		OwnerRelation &
+		JoiningColumnRelation &
+		NullableRelation
+	export type OneHasOneInversedRelation = Relation<RelationType.OneHasOne> & InversedRelation & NullableRelation
+	export type OneHasOneOwnerRelation = Relation<RelationType.OneHasOne> &
+		OwnerRelation &
+		JoiningColumnRelation &
+		NullableRelation
+	export type ManyHasManyInversedRelation = Relation<RelationType.ManyHasMany> & InversedRelation
+	export type ManyHasManyOwnerRelation = Relation<RelationType.ManyHasMany> & OwnerRelation & JoiningTableRelation
 
-  export interface Schema {
-    enums: { [name: string]: string[] }
-    entities: { [name: string]: Entity }
-  }
+	export interface Schema {
+		enums: { [name: string]: string[] }
+		entities: { [name: string]: Entity }
+	}
 }
 
 export default Model

3f1048a420b865b32f3e29cf8e6130b3c2ac8aba
Jirka Vebr <jirkavebr@gmail.com>
2018-08-10 11:11:58 +0200

    Better typings

--- packages/cms-admin/src/binding/dao/EntityAccessor.ts
+++ packages/cms-admin/src/binding/dao/EntityAccessor.ts
@@ -3,8 +3,8 @@ import { DataContextValue } from '../coreComponents/DataContext'
 
 export default class EntityAccessor {
 	constructor(
-		public readonly primaryKey: string,
-		public readonly data: { [name in FieldName]?: DataContextValue | DataContextValue[] },
+		public readonly primaryKey: string | undefined,
+		public readonly data: { [name in FieldName]: DataContextValue | DataContextValue[] },
 		public readonly unlink: () => void
 	) {}
 }

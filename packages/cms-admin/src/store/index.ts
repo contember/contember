61b6aec6a41f652033cc60c207021d4e27147af6
Vojta Staněk <stanekv01@gmail.com>
2018-08-10 15:51:16 +0200

    Persist api token

--- packages/cms-admin/src/store/index.ts
+++ packages/cms-admin/src/store/index.ts
@@ -1,10 +1,12 @@
-import { applyMiddleware, createStore, Middleware, Store } from 'redux'
+import { applyMiddleware, createStore, Middleware, Store, compose, Reducer } from 'redux'
 
 import thunk from 'redux-thunk'
 import rootReducer from '../reducer'
 import State from '../state'
 import LocalStorageManager from '../model/LocalStorageManager'
 import GraphqlClient from '../model/GraphqlClient'
+import { createAction } from 'redux-actions'
+import { SET_TOKEN } from '../reducer/auth'
 
 export interface Services {
 	localStorageManager: LocalStorageManager
@@ -20,11 +22,35 @@ export function createServices(): Services {
 	}
 }
 
+export function persistState(services: Services) {
+	return (next: Function) => (reducer: Reducer, initialState: State): Store<State> => {
+		const store: Store<State> = next(reducer, initialState)
+
+		const persistedApiToken = services.localStorageManager.get(LocalStorageManager.Keys.API_TOKEN)
+		if (persistedApiToken) {
+			store.dispatch(createAction(SET_TOKEN, () => persistedApiToken)())
+		}
+
+		store.subscribe(() => {
+			const state = store.getState()
+			const token = state.auth.token
+			if (token) {
+				services.localStorageManager.set(LocalStorageManager.Keys.API_TOKEN, token)
+			} else {
+				services.localStorageManager.unset(LocalStorageManager.Keys.API_TOKEN)
+			}
+		})
+
+		return store
+	}
+}
+
 export function configureStore(initialState: State): Store<State, any> {
-	const create = window.devToolsExtension ? window.devToolsExtension()(createStore) : createStore
-	const middlewares: Middleware[] = [thunk.withExtraArgument(createServices())]
+	const composeEnhancers: typeof compose = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+	const services = createServices()
+	const middlewares: Middleware[] = [thunk.withExtraArgument(services)]
 
-	const createStoreWithMiddleware = applyMiddleware(...middlewares)(create)
+	const middlewareEnhancer = applyMiddleware(...middlewares)
 
-	return createStoreWithMiddleware(rootReducer, initialState) as Store<State, any>
+	return createStore(rootReducer, initialState, composeEnhancers(middlewareEnhancer, persistState(services)))
 }

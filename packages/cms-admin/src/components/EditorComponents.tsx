0af97582ec3bca61027d131334d30019755380cf
Matej Simek <matej@simek.pro>
2018-08-13 11:27:55 +0200

    EditorComponents: are simple components, react-emotion removed

--- packages/cms-admin/src/components/EditorComponents.tsx
+++ packages/cms-admin/src/components/EditorComponents.tsx
@@ -1,41 +1,29 @@
 import * as React from 'react'
-import styled from 'react-emotion'
+import classNames from 'classnames'
 
-export const Button = styled('span')`
-	cursor: pointer;
-	color: ${(props: any) =>
-	props.reversed
-		? props.active ? 'white' : '#aaa'
-		: props.active ? 'black' : '#ccc'};
-`
+export function Menu({ ...rest }: any) {
+	return <div className="editor-menu" {...rest} />
+}
 
-export const Icon = styled(({ className, ...rest } : any) => {
+export function Toolbar({ ...rest }: any) {
+	return <div className="editor-toolbar" {...rest} />
+}
+
+export function Button(props:any) {
+	return <span className={classNames({
+		'editor-button': true,
+		'is-active': props.active,
+		'is-reversed': props.reversed
+	})} />
+}
+
+export function Icon({ className, ...rest }: any) {
 	return <span className={`material-icons ${className}`} {...rest} />
-})`
-	font-size: 18px;
-	vertical-align: text-bottom;
-`
+}
 
-export const Menu = styled('div')`
-	& > * {
-	display: inline-block;
-	}
-	& > * + * {
-	margin-left: 15px;
-	}
-`
-
-export const Toolbar = styled(Menu)`
-	position: relative;
-	padding: 1px 18px 17px;
-	margin: 0 -20px;
-	border-bottom: 2px solid #eee;
-	margin-bottom: 20px;
-`
-
-export const Image = styled('img')`
-	display: block;
-	max-width: 100%;
-	max-height: 20em;
-	box-shadow: ${(props: any) => (props.selected ? '0 0 0 2px blue;' : 'none')};
-`
+export function Image(props: any) {
+	return <img className={classNames({
+		'editor-image': true,
+		'is-selected': props.selected
+	})} />
+}

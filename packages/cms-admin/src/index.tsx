e1fc71a68cb01c8545c2022e9387fe97673dc601
Jirka Vebr <jirkavebr@gmail.com>
2018-08-13 10:38:13 +0200

    WIP: this should be moved elsewhere

--- packages/cms-admin/src/index.tsx
+++ packages/cms-admin/src/index.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
+import { TextField } from './binding/facade'
 import { configureStore } from './store'
 import { Provider } from 'react-redux'
 import { populateRequest } from './actions/request'
 import { emptyState } from './state'
+import { GraphQlBuilder } from 'cms-client'
+import { Entity, Field, DataProvider, OneToMany, OneToOne } from './binding'
 import Router from './containers/router'
 
 const store = configureStore(emptyState)
 store.dispatch(populateRequest(document.location))
-window.onpopstate = (e) => {
+window.onpopstate = e => {
 	e.preventDefault()
 	store.dispatch(populateRequest(document.location))
 }
@@ -15,7 +18,56 @@ window.onpopstate = (e) => {
 export const root = (
 	<div>
 		<Provider store={store}>
-			<Router />
+			{/*<Router />*/}
+			<DataProvider>
+				<Entity name="Post" where={{ id: '1' }}>
+					<TextField name="publishedAt" />
+					<OneToOne field="author">
+						<Entity name="Author">
+							<TextField name="name" />
+						</Entity>
+					</OneToOne>
+					<OneToOne field="author">
+						{unlink => {
+							return (
+								<Entity name="Author">
+									<TextField name="name" />
+									<button type="button" onClick={unlink}>×</button>
+								</Entity>
+							)
+						}}
+					</OneToOne>
+					<OneToMany field="categories">
+						{(unlink) => {
+							return (
+								<Entity name="Category">
+									<OneToMany field="locales">
+										{(unlink) => {
+											return (
+												<Entity name="CategoryLocale" where={{ locale: { eq: new GraphQlBuilder.Literal('cs') } }}>
+													<TextField name="name" />
+													<button type="button" onClick={unlink}>×</button>
+												</Entity>
+											)
+										}}
+									</OneToMany>
+									<button type="button" onClick={unlink}>×</button>
+								</Entity>
+							)
+						}}
+					</OneToMany>
+					<OneToMany field="locales">
+						{(unlink) => {
+							return (
+								<Entity name="PostLocale">
+									<TextField name="title" />
+									<button type="button" onClick={unlink}>×</button>
+								</Entity>
+							)
+						}}
+					</OneToMany>
+				</Entity>
+			</DataProvider>
 		</Provider>
 	</div>
 )

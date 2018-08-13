cc6f6f91b7e1e2a81a87baa0723f7c09b89a8d79
Jirka Vebr <jirkavebr@gmail.com>
2018-08-13 10:38:13 +0200

    WIP: this should be moved elsewhere

--- packages/cms-admin/src/binding/index.tsx
+++ packages/cms-admin/src/binding/index.tsx
@@ -1,9 +1,10 @@
 import Entity from './coreComponents/Entity'
 import Field from './coreComponents/Field'
 import FieldAccessor from './dao/FieldAccessor'
+import DataProvider from './coreComponents/DataProvider'
 import OneToMany from './coreComponents/OneToMany'
 import OneToOne from './coreComponents/OneToOne'
 
-export { Entity, Field, FieldAccessor, OneToMany, OneToOne }
+export { Entity, Field, FieldAccessor, DataProvider, OneToMany, OneToOne }
 
 export * from './facade'

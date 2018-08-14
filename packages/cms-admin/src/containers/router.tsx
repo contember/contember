966294c92e5968c650713ffc7ac4350ec93f7221
Vojta Staněk <stanekv01@gmail.com>
2018-08-14 15:09:16 +0200

    Get router ready for pages

--- packages/cms-admin/src/containers/router.tsx
+++ packages/cms-admin/src/containers/router.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
 import State from '../state'
-import RequestState, { loginRequest, dashboardRequest } from '../state/request'
+import RequestState, { loginRequest, pageRequest } from '../state/request'
 import Link from '../components/Link'
 import Login from '../components/Login'
 
@@ -12,9 +12,7 @@ type RouteMap<N extends RouteName = RouteName> = { [K in N]: RouteHandler<K> }
 
 const routeMap: RouteMap = {
 	login: route => <Login />,
-	entity_edit: route => `This is an ${route.name}.`,
-	dashboard: route => `This is a ${route.name}.`,
-	entity_list: route => `This is an ${route.name} ${route}.`
+	project_page: route => `This is ${route.pageName} with parameters ${JSON.stringify(route.parameters)}`
 }
 
 function getHandler<N extends RouteName>(name: N): RouteHandler<N> {
@@ -37,7 +35,7 @@ export default connect<{ loading: boolean; route: RequestState | null }, {}, {},
 			<div>
 				<Link requestChange={loginRequest()}>Login</Link>
 				{` | `}
-				<Link requestChange={dashboardRequest('foo')}>Dashboard</Link>
+				<Link requestChange={pageRequest('blog', 'prod', 'foo', {name: 'foornt', test: {test: 'test'}})}>Project page</Link>
 			</div>
 			{loading && <div>Loading overlay</div>}
 			<div>{route && renderRoute(route)}</div>
--- packages/cms-admin/src/utils/url.ts
+++ packages/cms-admin/src/utils/url.ts
@@ -1,49 +1,52 @@
-import RequestState from "../state/request";
+import RequestState from '../state/request'
 
-function decode(str: string)
-{
-  return decodeURIComponent(str.replace(/\+/g, ' '))
+export type RecursiveStringObject = { [key: string]: string | RecursiveStringObject | undefined }
+
+function decode(str: string) {
+	const component = decodeURIComponent(str.replace(/\+/g, ' '))
+	try {
+		return JSON.parse(component)
+	} catch {
+		return component
+	}
 }
 
-export function parseParams(query: string): { [key: string]: string }
-{
-  if (!query) return {}
-  if (query.charAt(0) === '?') {
-    query = query.slice(1)
-  }
-  const pairs = query.split('&')
-  const result: any = {}
-  for (let i = 0; i < pairs.length; i++) {
-    const value = pairs[i]
-    const index = value.indexOf('=')
-    if (index > -1) {
-      result[decode(value.slice(0, index))] = decode(value.slice(index + 1))
-    } else if (value.length) {
-      result[decode(value)] = ''
-    }
-  }
+export function parseParams(query: string): RecursiveStringObject {
+	if (!query) return {}
+	if (query.charAt(0) === '?') {
+		query = query.slice(1)
+	}
+	const pairs = query.split('&')
+	const result: any = {}
+	for (let i = 0; i < pairs.length; i++) {
+		const value = pairs[i]
+		const index = value.indexOf('=')
+		if (index > -1) {
+			result[decode(value.slice(0, index))] = decode(value.slice(index + 1))
+		} else if (value.length) {
+			result[decode(value)] = ''
+		}
+	}
 
-  return result
-
+	return result
 }
 
-export function buildParams(params: { [key: string]: string | undefined }): string
-{
-  let result = ''
-  for (let key in params) {
-    const value = params[key]
-    if (value === undefined) {
-      continue
-    }
-    result += key + '=' + encodeURIComponent(value) + "&"
-  }
-  if (result === '') {
-    return ''
-  }
+export function buildParams(params: RecursiveStringObject): string {
+	let result = ''
+	for (let key in params) {
+		const value = params[key]
+		if (value === undefined) {
+			continue
+		}
+		const component = typeof value === 'string' ? value : JSON.stringify(value)
+		result += key + '=' + encodeURIComponent(component) + '&'
+	}
+	if (result === '') {
+		return ''
+	}
 
-  return '?' + result.substring(0, result.length - 1)
+	return '?' + result.substring(0, result.length - 1)
 }
-
 
 export function buildUrlFromRequest(request: RequestState): string {
 	return '/' + buildParams(request)

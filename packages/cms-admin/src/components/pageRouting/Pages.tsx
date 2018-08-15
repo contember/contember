0d3d070695c0d8f510ac77c1e16a72d3887aa93a
Vojta Staněk <stanekv01@gmail.com>
2018-08-15 16:42:03 +0200

    EditPage and PageProvider

--- packages/cms-admin/src/components/pageRouting/Pages.tsx
+++ packages/cms-admin/src/components/pageRouting/Pages.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react'
 import Page, { PageProps } from './Page'
+import PageProvider from './PageProvider'
 import { connect } from 'react-redux'
 import State from '../../state'
 
+type PageProviderElement = React.ReactElement<any> & { type: PageProvider }
 type PageElement = React.ReactElement<PageProps>
+type PageChild = PageElement | PageProviderElement
 
 export interface PagesProps {
 	project: string
 	stage: string
-	children: PageElement[] | PageElement
+	children: PageChild[] | PageChild
 }
 
 export interface PagesStateProps {
 	name: string
 	parameters: any
+}
+
+function isPageProvider(el: any): el is PageProviderElement {
+	return React.isValidElement(el) && typeof el.type === 'function' && typeof (el.type as any).getPageName === 'function'
 }
 
 function isPageElement(el: any): el is PageElement {
@@ -24,18 +31,26 @@ function isPageElement(el: any): el is PageElement {
 	}
 }
 
+export const ParametersContext = React.createContext<any>({})
+
 class Pages extends React.Component<PagesProps & PagesStateProps> {
 	render() {
 		if (!this.props.children) return null
 		let children: unknown[] = Array.isArray(this.props.children) ? this.props.children : [this.props.children]
-		if (children.some(child => !isPageElement(child))) {
+		if (children.some(child => !isPageElement(child) && !isPageProvider(child))) {
 			throw new Error('Pages has a child which is not a Page')
 		} else {
-			const child = (children as PageElement[]).find(child => child.props.name === this.props.name)
+			const child = (children as PageChild[]).find(
+				child => (isPageProvider(child) ? child.type.getPageName(child.props) : child.props.name) === this.props.name
+			)
 			if (child === undefined) {
 				throw new Error(`No such page as ${this.props.name}.`)
 			}
-			return child.props.children(this.props.parameters)
+			if (isPageProvider(child)) {
+				return <ParametersContext.Provider value={this.props.parameters}>{child}</ParametersContext.Provider>
+			} else {
+				return child.props.children(this.props.parameters)
+			}
 		}
 	}
 }
--- packages/cms-admin/src/components/pageRouting/index.ts
+++ packages/cms-admin/src/components/pageRouting/index.ts
@@ -1,3 +1,4 @@
 export { default as Pages } from './Pages'
 export { default as Page } from './Page'
 export { default as PageLink } from './PageLink'
+export { default as EditPage } from './EditPage'

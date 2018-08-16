65bd79c364132498af0fdfb757dcd2ebb594833b
Vojta Staněk <stanekv01@gmail.com>
2018-08-16 15:18:43 +0200

    Add JsDoc

--- packages/cms-admin/src/components/pageRouting/Page.tsx
+++ packages/cms-admin/src/components/pageRouting/Page.tsx
@@ -7,11 +7,14 @@ export interface PageProps<P extends Params = Params, N extends keyof P = keyof 
 	children: (params: P[N]) => React.ReactNode
 }
 
+/**
+ * Page specifies one page. It must have a `name` prop and it's child must be a function which takes page's params and returns React node to render.
+ */
 export default class Page<P extends Params, N extends keyof P = keyof P, K = P[N]> extends React.Component<{
 	name: N
 	children: (params: K) => React.ReactNode
 }> {
 	render(): React.ReactNode {
-		throw new Error(`Page component doesn't work if it is not places as a direct child of Admin component`)
+		throw new Error(`Page component doesn't work if it is not places as a direct child of Pages component`)
 	}
 }

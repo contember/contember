3d3cff109abd1b99e48d455f1f4ab3e296c83629
David Matejka <matej21@matej21.cz>
2018-08-22 13:57:09 +0200

    graphql schema builder test

---
+++ packages/cms-api/tests/cases/integration/graphQlSchema/graphQlSchemaBuilder.ts
@@ -0,0 +1,191 @@
+import 'mocha'
+import { printSchema } from 'graphql'
+import { expect } from 'chai'
+import { Acl, Model } from 'cms-common'
+import * as fs from 'fs'
+import * as path from 'path'
+import { promisify } from 'util'
+
+import SchemaBuilder from '../../../../src/content-schema/builder/SchemaBuilder'
+import GraphQlSchemaBuilderFactory from '../../../../src/content-api/graphQLSchema/GraphQlSchemaBuilderFactory'
+import AllowAllPermissionFactory from '../../../../src/acl/AllowAllPermissionFactory'
+
+const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
+
+interface Test {
+	schema: (builder: SchemaBuilder) => SchemaBuilder
+	permissions: (schema: Model.Schema) => Acl.Permissions
+	graphQlSchemaFile: string
+}
+
+const testSchema = async (test: Test) => {
+	const builder = test.schema(new SchemaBuilder())
+
+	const schemaFactory = new GraphQlSchemaBuilderFactory()
+	const schema = builder.buildSchema()
+	const schemaWithAcl = { ...schema, acl: { roles: {}, variables: {} } }
+	const permissions = test.permissions(schemaWithAcl)
+	const graphQlSchemaBuilder = schemaFactory.create(schemaWithAcl, permissions)
+	const graphQlSchema = graphQlSchemaBuilder.build()
+
+	const textSchema = printSchema(graphQlSchema)
+
+	const filename = path.join(__dirname, test.graphQlSchemaFile)
+	let expectedSchema: Promise<string>
+	try {
+		expectedSchema = readFile(filename, { encoding: 'utf8' })
+	} catch (e) {
+		await writeFile(filename, textSchema, { encoding: 'utf8' })
+		throw new Error(`Schema file ${filename} not found, creating with current schema`)
+	}
+	if (expectedSchema) {
+		expect(textSchema).equals(await expectedSchema)
+	}
+}
+
+describe('build gql schema from model schema', () => {
+	it('builds basic schema', async () => {
+		await testSchema({
+			schema: builder =>
+				builder
+					.entity('Author', e =>
+						e.column('name', c => c.type(Model.ColumnType.String)).oneHasMany('posts', r => r.target('Post'))
+					)
+					.entity('Category', e => e.column('name', c => c.type(Model.ColumnType.String)))
+					.entity('Post', e =>
+						e
+							.column('publishedAt', c => c.type(Model.ColumnType.DateTime))
+							.oneHasMany('locales', r =>
+								r.target('PostLocale', e => e.column('title', c => c.type(Model.ColumnType.String)))
+							)
+							.manyHasMany('categories', r => r.target('Category').inversedBy('posts'))
+					),
+			permissions: schema => new AllowAllPermissionFactory().create(schema),
+			graphQlSchemaFile: 'schema1.gql'
+		})
+	})
+
+	it('restricts access to fields by permissions', async () => {
+		await testSchema({
+			schema: builder =>
+				builder.entity('Test', e =>
+					e
+						.column('a', c => c.type(Model.ColumnType.String))
+						.column('b', c => c.type(Model.ColumnType.String))
+						.column('c', c => c.type(Model.ColumnType.String))
+				),
+			permissions: () => ({
+				Test: {
+					predicates: {},
+					operations: {
+						create: {
+							a: true
+						},
+						update: {
+							b: true
+						},
+						read: {
+							id: true,
+							c: true
+						}
+					}
+				}
+			}),
+			graphQlSchemaFile: 'schema2.gql'
+		})
+	})
+
+	describe('one has many', () => {
+		const oneHasManySchema = (builder: SchemaBuilder) =>
+			builder.entity('Root', e =>
+				e.oneHasMany('r', r => r.target('OneHasManyEntity', e => e.column('a', c => c.type(Model.ColumnType.String))))
+			)
+
+		it('all allowed', async () => {
+			await testSchema({
+				schema: oneHasManySchema,
+				permissions: schema => new AllowAllPermissionFactory().create(schema),
+				graphQlSchemaFile: 'schema3.gql'
+			})
+		})
+
+		it('restricted delete', async () => {
+			await testSchema({
+				schema: oneHasManySchema,
+				permissions: () => ({
+					Root: {
+						predicates: {},
+						operations: {
+							create: { r: true },
+							update: { r: true },
+							read: { id: true, r: true },
+							delete: true
+						}
+					},
+					OneHasManyEntity: {
+						predicates: {},
+						operations: {
+							create: { a: true },
+							update: { a: true },
+							read: { id: true, a: true }
+						}
+					}
+				}),
+				graphQlSchemaFile: 'schema4.gql'
+			})
+		})
+
+		it('restricted update', async () => {
+			await testSchema({
+				schema: oneHasManySchema,
+				permissions: () => ({
+					Root: {
+						predicates: {},
+						operations: {
+							create: { r: true },
+							update: { r: true },
+							read: { id: true, r: true },
+							delete: true
+						}
+					},
+					OneHasManyEntity: {
+						predicates: {},
+						operations: {
+							create: { a: true },
+							read: { id: true, a: true },
+							delete: true
+						}
+					}
+				}),
+				graphQlSchemaFile: 'schema5.gql'
+			})
+		})
+
+		it('restricted create', async () => {
+			await testSchema({
+				schema: oneHasManySchema,
+				permissions: () => ({
+					Root: {
+						predicates: {},
+						operations: {
+							create: { r: true },
+							update: { r: true },
+							read: { id: true, r: true },
+							delete: true
+						}
+					},
+					OneHasManyEntity: {
+						predicates: {},
+						operations: {
+							update: { a: true },
+							read: { id: true, a: true },
+							delete: true
+						}
+					}
+				}),
+				graphQlSchemaFile: 'schema6.gql'
+			})
+		})
+	})
+})

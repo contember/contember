e65ec6c861cb7e6d3b6aa902cf85290c985c2514
David Matejka <matej21@matej21.cz>
2018-08-22 13:32:00 +0200

    fix ConditionTypeProvider

--- packages/cms-api/src/content-api/graphQLSchema/customTypes.ts
+++ packages/cms-api/src/content-api/graphQLSchema/customTypes.ts
@@ -1,7 +1,27 @@
 import { GraphQLScalarType, Kind } from 'graphql'
 
+//todo: implement serialize, parseValue and parseLiteral properly
+
 export const GraphQLUUID: GraphQLScalarType = new GraphQLScalarType({
 	name: 'UUID',
+	serialize: str => String(str),
+	parseValue: str => String(str),
+	parseLiteral: function parseLiteral(ast) {
+		return ast.kind === Kind.STRING ? ast.value : undefined
+	}
+})
+
+export const GraphQLDate: GraphQLScalarType = new GraphQLScalarType({
+	name: 'Date',
+	serialize: str => String(str),
+	parseValue: str => String(str),
+	parseLiteral: function parseLiteral(ast) {
+		return ast.kind === Kind.STRING ? ast.value : undefined
+	}
+})
+
+export const GraphQLDateTime: GraphQLScalarType = new GraphQLScalarType({
+	name: 'DateTime',
 	serialize: str => String(str),
 	parseValue: str => String(str),
 	parseLiteral: function parseLiteral(ast) {

d0d16161caa788899f28ebb85934d68632d67baa
Martin Kucera <m.spidy@gmail.com>
2018-09-03 09:49:25 +0200

    cms-api: improve isUniqueWhere function readability

--- packages/cms-api/src/content-schema/inputUtils.ts
+++ packages/cms-api/src/content-schema/inputUtils.ts
@@ -4,19 +4,13 @@ export const isUniqueWhere = (entity: Model.Entity, where: Input.UniqueWhere): b
 	if (where[entity.primary] !== undefined) {
 		return true
 	}
-	for (const unique of entity.unique.map(it => it.fields)) {
-		if (
-			(() => {
-				for (const field of unique) {
-					if (where[field] == undefined) {
-						return false
-					}
-				}
-				return true
-			})()
-		) {
-			return true
+	uniqueKeys: for (const unique of entity.unique.map(it => it.fields)) {
+		for (const field of unique) {
+			if (where[field] === undefined) {
+				continue uniqueKeys
+			}
 		}
+		return true
 	}
 	return false
 }

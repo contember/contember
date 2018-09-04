cd484fb41cd908e77f0ffebc225f333336cbd805
Martin Kucera <m.spidy@gmail.com>
2018-09-04 10:38:17 +0200

    cms-common: assertNever function

--- packages/cms-common/src/index.ts
+++ packages/cms-common/src/index.ts
@@ -1,5 +1,6 @@
 import Input from './schema/input'
 import Model from './schema/model'
 import deepCopy from './utils/deepCopy'
+import assertNever from './utils/assertNever'
 
-export { Input, Model, deepCopy }
+export { Input, Model, deepCopy, assertNever }

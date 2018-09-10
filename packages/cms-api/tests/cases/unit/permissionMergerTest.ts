389d8c1c0d431db21d0023e3d1590dcf8b5ded69
David Matejka <matej21@matej21.cz>
2018-09-10 11:02:31 +0200

    PermissionMerger: rename to PermissionFactory + add primary field predicate postprocessing

--- packages/cms-api/tests/cases/unit/permissionMergerTest.ts
+++ packages/cms-api/tests/cases/unit/permissionMergerTest.ts
@@ -1,103 +1,120 @@
 import 'mocha'
-import { Acl } from 'cms-common'
+import { Acl, Model } from 'cms-common'
 import { expect } from 'chai'
-import PermissionMerger from '../../../src/acl/PermissionMerger'
+import PermissionFactory from '../../../src/acl/PermissionFactory'
+import SchemaBuilder from "../../../src/content-schema/builder/SchemaBuilder";
+
+interface Test {
+	acl: Acl.Schema,
+	roles: string[],
+	result: Acl.Permissions,
+}
+
+const execute = (test: Test) => {
+	const schema: Model.Schema = new SchemaBuilder()
+		.entity('Entity1', e => e)
+		.entity('Entity2', e => e)
+		.buildSchema()
+	const merger = new PermissionFactory(schema)
+	const result = merger.create(test.acl, test.roles)
+	expect(result).deep.equals(test.result)
+}
 
 describe('permission merger', () => {
 	it('merges inheritance', () => {
-		const acl: Acl.Schema = {
-			variables: {},
-			roles: {
-				role1: {
-					entities: {
-						Entity1: {
-							predicates: {},
-							operations: {
-								read: {
-									id: true
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {},
+								operations: {
+									read: {
+										id: true
+									}
 								}
 							}
 						}
-					}
-				},
-				role2: {
-					inherits: ['role1'],
-					entities: {
-						Entity2: {
-							predicates: {},
-							operations: {
-								read: {
-									id: true
+					},
+					role2: {
+						inherits: ['role1'],
+						entities: {
+							Entity2: {
+								predicates: {},
+								operations: {
+									read: {
+										id: true
+									}
 								}
 							}
 						}
-					}
-				}
-			}
-		}
-
-		const merger = new PermissionMerger()
-		const result = merger.merge(acl, ['role2'])
-		expect(result).deep.equals({
-			Entity1: {
-				predicates: {},
-				operations: {
-					read: {
-						id: true
 					}
 				}
 			},
-			Entity2: {
-				predicates: {},
-				operations: {
-					read: {
-						id: true
+			roles: ['role2'],
+			result: {
+				Entity1: {
+					predicates: {},
+					operations: {
+						read: {
+							id: true
+						}
+					}
+				},
+				Entity2: {
+					predicates: {},
+					operations: {
+						read: {
+							id: true
+						}
 					}
 				}
-			}
+			},
 		})
 	})
 
 	it('merges entity operations', () => {
-		const acl: Acl.Schema = {
-			variables: {},
-			roles: {
-				role1: {
-					entities: {
-						Entity1: {
-							predicates: {},
-							operations: {
-								read: {
-									id: true
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {},
+								operations: {
+									read: {
+										id: true
+									}
 								}
 							}
 						}
-					}
-				},
-				role2: {
-					entities: {
-						Entity1: {
-							predicates: {},
-							operations: {
-								read: {
-									title: true
+					},
+					role2: {
+						entities: {
+							Entity1: {
+								predicates: {},
+								operations: {
+									read: {
+										title: true
+									}
 								}
 							}
 						}
 					}
 				}
-			}
-		}
-
-		const merger = new PermissionMerger()
-		const result = merger.merge(acl, ['role1', 'role2'])
-		expect(result).deep.equals({
-			Entity1: {
-				predicates: {},
-				operations: {
-					read: {
-						id: true,
-						title: true
+			},
+			roles: ['role1', 'role2'],
+			result: {
+				Entity1: {
+					predicates: {},
+					operations: {
+						read: {
+							id: true,
+							title: true
+						}
 					}
 				}
 			}
@@ -105,49 +122,49 @@ describe('permission merger', () => {
 	})
 
 	it('merges entity operations with predicates', () => {
-		const acl: Acl.Schema = {
-			variables: {},
-			roles: {
-				role1: {
-					entities: {
-						Entity1: {
-							predicates: {},
-							operations: {
-								read: {
-									id: true
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {},
+								operations: {
+									read: {
+										id: true
+									}
 								}
 							}
 						}
-					}
-				},
-				role2: {
-					entities: {
-						Entity1: {
-							predicates: {
-								foo: { bar: { eq: 'abc' } }
-							},
-							operations: {
-								read: {
-									title: 'foo'
+					},
+					role2: {
+						entities: {
+							Entity1: {
+								predicates: {
+									foo: {bar: {eq: 'abc'}}
+								},
+								operations: {
+									read: {
+										title: 'foo'
+									}
 								}
 							}
 						}
 					}
 				}
-			}
-		}
-
-		const merger = new PermissionMerger()
-		const result = merger.merge(acl, ['role1', 'role2'])
-		expect(result).deep.equals({
-			Entity1: {
-				predicates: {
-					foo: { bar: { eq: 'abc' } }
-				},
-				operations: {
-					read: {
-						id: true,
-						title: 'foo'
+			},
+			roles: ['role1', 'role2'],
+			result: {
+				Entity1: {
+					predicates: {
+						foo: {bar: {eq: 'abc'}}
+					},
+					operations: {
+						read: {
+							id: true,
+							title: 'foo'
+						}
 					}
 				}
 			}
@@ -155,103 +172,103 @@ describe('permission merger', () => {
 	})
 
 	it('merges entity operations and drops predicate', () => {
-		const acl: Acl.Schema = {
-			variables: {},
-			roles: {
-				role1: {
-					entities: {
-						Entity1: {
-							predicates: {},
-							operations: {
-								read: {
-									id: true,
-									title: true
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {},
+								operations: {
+									read: {
+										id: true,
+										title: true
+									}
 								}
 							}
 						}
-					}
-				},
-				role2: {
-					entities: {
-						Entity1: {
-							predicates: {
-								foo: { bar: { eq: 'abc' } }
-							},
-							operations: {
-								read: {
-									title: 'foo'
+					},
+					role2: {
+						entities: {
+							Entity1: {
+								predicates: {
+									foo: {bar: {eq: 'abc'}}
+								},
+								operations: {
+									read: {
+										title: 'foo'
+									}
 								}
 							}
 						}
 					}
 				}
-			}
-		}
-
-		const merger = new PermissionMerger()
-		const result = merger.merge(acl, ['role1', 'role2'])
-		expect(result).deep.equals({
-			Entity1: {
-				predicates: {},
-				operations: {
-					read: {
-						id: true,
-						title: true
+			},
+			roles: ['role1', 'role2'],
+			result: {
+				Entity1: {
+					predicates: {},
+					operations: {
+						read: {
+							id: true,
+							title: true
+						}
 					}
 				}
-			}
+			},
 		})
 	})
 
 	it('merges entity operations and merges predicates', () => {
-		const acl: Acl.Schema = {
-			variables: {},
-			roles: {
-				role1: {
-					entities: {
-						Entity1: {
-							predicates: {
-								bar: { lorem: { eq: 'ipsum' } }
-							},
-							operations: {
-								read: {
-									id: true,
-									title: 'bar'
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {
+									bar: {lorem: {eq: 'ipsum'}}
+								},
+								operations: {
+									read: {
+										id: true,
+										title: 'bar'
+									}
 								}
 							}
 						}
-					}
-				},
-				role2: {
-					entities: {
-						Entity1: {
-							predicates: {
-								foo: { bar: { eq: 'abc' } }
-							},
-							operations: {
-								read: {
-									title: 'foo'
+					},
+					role2: {
+						entities: {
+							Entity1: {
+								predicates: {
+									foo: {bar: {eq: 'abc'}}
+								},
+								operations: {
+									read: {
+										title: 'foo'
+									}
 								}
 							}
 						}
 					}
 				}
-			}
-		}
-
-		const merger = new PermissionMerger()
-		const result = merger.merge(acl, ['role1', 'role2'])
-		expect(result).deep.equals({
-			Entity1: {
-				predicates: {
-					__merge__bar__foo: {
-						or: [{ lorem: { eq: 'ipsum' } }, { bar: { eq: 'abc' } }]
-					}
-				},
-				operations: {
-					read: {
-						id: true,
-						title: '__merge__bar__foo'
+			},
+			roles: ['role1', 'role2'],
+			result: {
+				Entity1: {
+					predicates: {
+						__merge__bar__foo: {
+							or: [{lorem: {eq: 'ipsum'}}, {bar: {eq: 'abc'}}]
+						}
+					},
+					operations: {
+						read: {
+							id: true,
+							title: '__merge__bar__foo'
+						}
 					}
 				}
 			}
@@ -259,104 +276,151 @@ describe('permission merger', () => {
 	})
 
 	it('merges delete operation', () => {
-		const acl: Acl.Schema = {
-			variables: {},
-			roles: {
-				role1: {
-					entities: {
-						Entity1: {
-							predicates: {
-								bar: { lorem: { eq: 'ipsum' } }
-							},
-							operations: {
-								delete: 'bar'
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {
+									bar: {lorem: {eq: 'ipsum'}}
+								},
+								operations: {
+									delete: 'bar'
+								}
 							}
 						}
-					}
-				},
-				role2: {
-					entities: {
-						Entity1: {
-							predicates: {
-								foo: { bar: { eq: 'abc' } }
-							},
-							operations: {
-								delete: 'foo'
+					},
+					role2: {
+						entities: {
+							Entity1: {
+								predicates: {
+									foo: {bar: {eq: 'abc'}}
+								},
+								operations: {
+									delete: 'foo'
+								}
 							}
 						}
 					}
 				}
-			}
-		}
-
-		const merger = new PermissionMerger()
-		const result = merger.merge(acl, ['role1', 'role2'])
-		expect(result).deep.equals({
-			Entity1: {
-				predicates: {
-					__merge__bar__foo: {
-						or: [{ lorem: { eq: 'ipsum' } }, { bar: { eq: 'abc' } }]
+			},
+			roles: ['role1', 'role2'],
+			result: {
+				Entity1: {
+					predicates: {
+						__merge__bar__foo: {
+							or: [{lorem: {eq: 'ipsum'}}, {bar: {eq: 'abc'}}]
+						}
+					},
+					operations: {
+						delete: '__merge__bar__foo'
 					}
-				},
-				operations: {
-					delete: '__merge__bar__foo'
 				}
 			}
 		})
 	})
 
 	it('merges predicates and resolves conflicts', () => {
-		const acl: Acl.Schema = {
-			variables: {},
-			roles: {
-				role1: {
-					entities: {
-						Entity1: {
-							predicates: {
-								foo: { lorem: { eq: 'ipsum' } }
-							},
-							operations: {
-								read: {
-									id: true,
-									title: 'foo'
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {
+									foo: {lorem: {eq: 'ipsum'}}
+								},
+								operations: {
+									read: {
+										id: true,
+										title: 'foo'
+									}
 								}
 							}
 						}
-					}
-				},
-				role2: {
-					entities: {
-						Entity1: {
-							predicates: {
-								foo: { bar: { eq: 'abc' } }
-							},
-							operations: {
-								read: {
-									content: 'foo'
+					},
+					role2: {
+						entities: {
+							Entity1: {
+								predicates: {
+									foo: {bar: {eq: 'abc'}}
+								},
+								operations: {
+									read: {
+										content: 'foo'
+									}
 								}
 							}
 						}
 					}
 				}
-			}
-		}
+			},
+			roles: ['role1', 'role2'],
+			result: {
+				Entity1: {
+					predicates: {
+						foo: {lorem: {eq: 'ipsum'}},
+						foo_: {bar: {eq: 'abc'}}
+					},
+					operations: {
+						read: {
+							id: true,
+							title: 'foo',
+							content: 'foo_'
+						}
+					}
+				}
+			},
+		})
+	})
 
-		const merger = new PermissionMerger()
-		const result = merger.merge(acl, ['role1', 'role2'])
-		expect(result).deep.equals({
-			Entity1: {
-				predicates: {
-					foo: { lorem: { eq: 'ipsum' } },
-					foo_: { bar: { eq: 'abc' } }
-				},
-				operations: {
-					read: {
-						id: true,
-						title: 'foo',
-						content: 'foo_'
+	it('makes primary predicate union of all other fields', () => {
+		execute({
+			acl: {
+				variables: {},
+				roles: {
+					role1: {
+						entities: {
+							Entity1: {
+								predicates: {
+									foo: {lorem: {eq: 'ipsum'}},
+									bar: {lorem: {eq: 'ipsum'}},
+								},
+								operations: {
+									read: {
+										title: 'foo',
+										description: 'bar',
+										content: 'bar',
+									}
+								}
+							}
+						}
+					},
+				}
+			},
+			roles: ['role1'],
+			result: {
+				Entity1: {
+					predicates: {
+						foo: {lorem: {eq: 'ipsum'}},
+						bar: {lorem: {eq: 'ipsum'}},
+						__merge__foo__bar: {
+							or: [{lorem: {eq: 'ipsum'}}, {lorem: {eq: 'ipsum'}}]
+						},
+					},
+					operations: {
+						read: {
+							id: '__merge__foo__bar',
+							title: 'foo',
+							description: 'bar',
+							content: 'bar',
+						}
 					}
 				}
-			}
+			},
 		})
 	})
 })

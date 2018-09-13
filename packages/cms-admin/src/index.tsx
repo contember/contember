916a2e0d1047dde39357672df6e60d1e98017132
Vojta Staněk <stanekv01@gmail.com>
2018-09-13 18:09:44 +0200

    Solve blueprint issues

--- packages/cms-admin/src/index.tsx
+++ packages/cms-admin/src/index.tsx
@@ -1,3 +1,5 @@
+import '@blueprintjs/core/lib/css/blueprint.css'
+import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 export { default as LayoutDefault } from './components/LayoutDefault'
 export { GraphQlBuilder } from 'cms-client'
 export * from './binding'

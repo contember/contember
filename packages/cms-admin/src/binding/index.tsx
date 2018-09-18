764c392d91f2d7816f11b8066e0103ee2c9811b7
Jirka Vebr <jirkavebr@gmail.com>
2018-09-18 15:31:00 +0200

    WIP: Add post list example page

--- packages/cms-admin/src/binding/index.tsx
+++ packages/cms-admin/src/binding/index.tsx
@@ -1,11 +1,21 @@
 import Entity from './coreComponents/Entity'
+import EntityListDataProvider from './coreComponents/EntityListDataProvider'
 import OneToMany from './coreComponents/OneToMany'
 import OneToOne from './coreComponents/OneToOne'
 import SingleEntityDataProvider from './coreComponents/SingleEntityDataProvider'
-import EntityListDataProvider from './coreComponents/EntityListDataProvider'
 import FieldAccessor from './dao/FieldAccessor'
+import SelectField from './facade/SelectField'
 import UnlinkButton from './facade/UnlinkButton'
 
-export { Entity, FieldAccessor, SingleEntityDataProvider, EntityListDataProvider, OneToMany, OneToOne, UnlinkButton }
+export {
+	Entity,
+	EntityListDataProvider,
+	FieldAccessor,
+	OneToOne,
+	OneToMany,
+	SelectField,
+	SingleEntityDataProvider,
+	UnlinkButton
+}
 
 export * from './facade'

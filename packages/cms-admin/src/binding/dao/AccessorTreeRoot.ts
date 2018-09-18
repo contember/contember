2ed246f4e4c4c62c232063964faf82421bb3a072
Jirka Vebr <jirkavebr@gmail.com>
2018-09-18 14:13:38 +0200

    WIP: Add AccessorTreeRoot

---
+++ packages/cms-admin/src/binding/dao/AccessorTreeRoot.ts
@@ -0,0 +1,17 @@
+import EntityAccessor from './EntityAccessor'
+import MarkerTreeRoot from './MarkerTreeRoot'
+import { TreeId } from './TreeId'
+
+export type RootAccessor = EntityAccessor | EntityAccessor[]
+
+export default class AccessorTreeRoot {
+	public readonly id: TreeId
+
+	private constructor(markerTreeRoot: MarkerTreeRoot, public readonly root: RootAccessor) {
+		this.id = markerTreeRoot.id
+	}
+
+	public static createInstance(markerTreeRoot: MarkerTreeRoot, accessorRoot: RootAccessor): AccessorTreeRoot {
+		return new AccessorTreeRoot(markerTreeRoot, accessorRoot)
+	}
+}

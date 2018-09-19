b2c528a24b6c78af1bc6538d1725f4051728f883
Jirka Vebr <jirkavebr@gmail.com>
2018-09-19 17:18:22 +0200

    SelectField: add an immutable wip version

--- packages/cms-admin/src/binding/facade/SelectField.tsx
+++ packages/cms-admin/src/binding/facade/SelectField.tsx
@@ -1,11 +1,25 @@
+import { HTMLSelect } from '@blueprintjs/core'
 import { GraphQlBuilder } from 'cms-client'
 import { Input } from 'cms-common'
 import * as React from 'react'
-import { MarkerTreeRootProvider } from '../coreComponents/DataMarkerProvider'
+import { EntityName, FieldName } from '../bindingTypes'
+import DataContext, { DataContextValue } from '../coreComponents/DataContext'
+import { SyntheticChildrenProvider } from '../coreComponents/DataMarkerProvider'
 import EnforceSubtypeRelation from '../coreComponents/EnforceSubtypeRelation'
+import Entity from '../coreComponents/Entity'
+import EntityListDataProvider from '../coreComponents/EntityListDataProvider'
+import Field from '../coreComponents/Field'
+import OneToOne from '../coreComponents/OneToOne'
+import AccessorTreeRoot from '../dao/AccessorTreeRoot'
+import DataBindingError from '../dao/DataBindingError'
+import EntityAccessor from '../dao/EntityAccessor'
+import FieldAccessor from '../dao/FieldAccessor'
 import MarkerTreeRoot from '../dao/MarkerTreeRoot'
 
 export interface SelectFieldProps {
+	name: FieldName
+	entityName: EntityName
+	optionFieldName: FieldName
 	where?: Input.Where<GraphQlBuilder.Literal>
 }
 
@@ -13,15 +27,68 @@ export default class SelectField extends React.Component<SelectFieldProps> {
 	public static displayName = 'SelectField'
 
 	public render() {
-		return null
+		return (
+			<DataContext.Consumer>
+				{(data: DataContextValue) => {
+					if (data instanceof EntityAccessor) {
+						const fieldAccessor = data.data[MarkerTreeRoot.getPlaceholderName(this.props.name)]
+						const currentValueEntity = data.data[this.props.name]
+
+						if (!(fieldAccessor instanceof AccessorTreeRoot) || !(currentValueEntity instanceof EntityAccessor)) {
+							throw new DataBindingError('Corrupted data')
+						}
+
+						const currentValueField = currentValueEntity.data[this.props.optionFieldName]
+						const subTreeData = fieldAccessor.root
+
+						if (!(currentValueField instanceof FieldAccessor)) {
+							throw new DataBindingError('Corrupted data')
+						}
+
+						if (Array.isArray(subTreeData)) {
+							return (
+								<HTMLSelect
+									value={currentValueEntity.primaryKey}
+									onChange={e => {
+										// TODO
+									}}
+									options={subTreeData.map(datum => {
+										const optionField = datum.data[this.props.optionFieldName]
+
+										if (!(optionField instanceof FieldAccessor)) {
+											throw new DataBindingError('Corrupted data')
+										}
+										return {
+											value: datum.primaryKey!,
+											label: optionField.currentValue,
+										}
+									})}
+								/>
+							)
+						}
+					}
+					throw new DataBindingError('Corrupted data')
+				}}
+			</DataContext.Consumer>
+		)
 	}
 
-	public static generateMarkerTreeRoot(props: SelectFieldProps, treeRoot: MarkerTreeRoot['root']): MarkerTreeRoot {
-		return MarkerTreeRoot.createInstance(treeRoot, {
-			where: props.where,
-			whereType: 'nonUnique'
-		})
+	public static generateSyntheticChildren(props: SelectFieldProps): React.ReactNode {
+		return (
+			<>
+				<EntityListDataProvider where={props.where} associatedField={props.name}>
+					<Entity name={props.entityName}>
+						<Field name={props.optionFieldName} />
+					</Entity>
+				</EntityListDataProvider>
+				<OneToOne field={props.name}>
+					<Entity name={props.entityName}>
+						<Field name={props.optionFieldName} />
+					</Entity>
+				</OneToOne>
+			</>
+		)
 	}
 }
 
-type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof SelectField, MarkerTreeRootProvider>
+type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof SelectField, SyntheticChildrenProvider>

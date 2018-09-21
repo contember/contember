e6a129b6678481d14029e0fa917d9769928f77c7
David Matejka <matej21@matej21.cz>
2018-09-21 16:30:44 +0200

    cs fix

--- packages/cms-api/tests/cases/unit/variableInjectorTest.ts
+++ packages/cms-api/tests/cases/unit/variableInjectorTest.ts
@@ -1,24 +1,19 @@
 import 'mocha'
 import { expect } from 'chai'
 import VariableInjector from '../../../src/acl/VariableInjector'
-import SchemaBuilder from "../../../src/content-schema/builder/SchemaBuilder";
+import SchemaBuilder from '../../../src/content-schema/builder/SchemaBuilder'
 import { Model } from 'cms-common'
 
 describe('variable injector', () => {
 	it('injects variable', () => {
 		const schema = new SchemaBuilder()
 			.enum('locale', ['cs', 'en'])
-			.entity('PostLocale', e => e
-				.column('locale', c => c.type(Model.ColumnType.Enum, {enumName: 'locale'}))
-				.column('foo', c => c.type(Model.ColumnType.String))
-				.column('public', c => c.type(Model.ColumnType.Bool))
-				.manyHasOne('post', r => r
-					.target('Post', e => e
-						.manyHasOne('site', r => r
-							.target('Site', e => e)
-						)
-					)
-				)
+			.entity('PostLocale', e =>
+				e
+					.column('locale', c => c.type(Model.ColumnType.Enum, { enumName: 'locale' }))
+					.column('foo', c => c.type(Model.ColumnType.String))
+					.column('public', c => c.type(Model.ColumnType.Bool))
+					.manyHasOne('post', r => r.target('Post', e => e.manyHasOne('site', r => r.target('Site', e => e))))
 			)
 			.buildSchema()
 
@@ -29,7 +24,7 @@ describe('variable injector', () => {
 		const result = injector.inject(schema.entities['PostLocale'], {
 			or: [
 				{
-					public: {eq: true},
+					public: { eq: true },
 				},
 				{
 					and: [
@@ -44,32 +39,32 @@ describe('variable injector', () => {
 						{
 							foo: 'bar',
 						},
-					]
-				}
-			]
+					],
+				},
+			],
 		})
 
 		expect(result).deep.eq({
 			or: [
 				{
-					public: {eq: true},
+					public: { eq: true },
 				},
 				{
 					and: [
 						{
 							post: {
-								site: {id: {in: [1, 2]}},
+								site: { id: { in: [1, 2] } },
 							},
 						},
 						{
-							locale: {eq: 'cs'},
+							locale: { eq: 'cs' },
 						},
 						{
-							foo: {never: true},
+							foo: { never: true },
 						},
 					],
-				}
-			]
+				},
+			],
 		})
 	})
 })

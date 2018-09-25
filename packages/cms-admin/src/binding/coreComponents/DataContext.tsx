ad15351208a57b8bd9e3130c8ef49db21e32c9fd
Jirka Vebr <jirkavebr@gmail.com>
2018-09-25 16:08:32 +0200

    Add a special accessor for top-level entities intended for deletion

--- packages/cms-admin/src/binding/coreComponents/DataContext.tsx
+++ packages/cms-admin/src/binding/coreComponents/DataContext.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import AccessorTreeRoot from '../dao/AccessorTreeRoot'
 import EntityAccessor from '../dao/EntityAccessor'
+import EntityForRemovalAccessor from '../dao/EntityForRemovalAccessor'
 import FieldAccessor from '../dao/FieldAccessor'
 
-export type DataContextValue = undefined | FieldAccessor | EntityAccessor | AccessorTreeRoot
+export type DataContextValue = undefined | FieldAccessor | EntityAccessor | AccessorTreeRoot | EntityForRemovalAccessor
 
 const dataContext = React.createContext<DataContextValue>(undefined)
 
--- packages/cms-admin/src/binding/dao/AccessorTreeRoot.ts
+++ packages/cms-admin/src/binding/dao/AccessorTreeRoot.ts
@@ -1,8 +1,11 @@
 import EntityAccessor from './EntityAccessor'
+import EntityForRemovalAccessor from './EntityForRemovalAccessor'
 import MarkerTreeRoot from './MarkerTreeRoot'
 import { TreeId } from './TreeId'
 
-export type RootAccessor = EntityAccessor | EntityAccessor[]
+export type RootAccessor =
+	| (EntityAccessor | EntityForRemovalAccessor)
+	| Array<EntityAccessor | EntityForRemovalAccessor | undefined> // Undefined is a "hole" after an non-persisted entity
 
 export default class AccessorTreeRoot {
 	public readonly id: TreeId

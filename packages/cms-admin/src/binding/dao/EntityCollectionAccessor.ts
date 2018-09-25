959676d1ae68b8e0b7dff4cd9bf57d43b29512d3
Jirka Vebr <jirkavebr@gmail.com>
2018-09-25 19:02:51 +0200

    Use top-level EntityCollectionAccessor as opposed to a naked array

--- packages/cms-admin/src/binding/dao/EntityCollectionAccessor.ts
+++ packages/cms-admin/src/binding/dao/EntityCollectionAccessor.ts
@@ -2,10 +2,9 @@ import EntityAccessor from './EntityAccessor'
 import EntityForRemovalAccessor from './EntityForRemovalAccessor'
 import EntityMarker from './EntityMarker'
 
-
 export default class EntityCollectionAccessor {
 	public constructor(
-		public readonly entities: Array<EntityAccessor | EntityForRemovalAccessor | undefined>,
+		public readonly entities: Array<EntityAccessor | EntityForRemovalAccessor | undefined>, // Undefined is a "hole" after an non-persisted entity
 		public readonly appendNew?: () => void,
 		public readonly prototype?: EntityMarker,
 	) {}

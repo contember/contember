629cfc6a0a37ba6329dfd430faf5a8adac27fa0a
Jirka Vebr <jirkavebr@gmail.com>
2018-10-16 13:50:48 +0200

    Uniform QL use across all fields

--- packages/cms-admin/src/binding/facade/TextareaField.tsx
+++ packages/cms-admin/src/binding/facade/TextareaField.tsx
@@ -4,9 +4,10 @@ import { ChangeEvent } from 'react'
 import { FieldName } from '../bindingTypes'
 import EnforceSubtypeRelation from '../coreComponents/EnforceSubtypeRelation'
 import Field from '../coreComponents/Field'
-import { FieldMarkerProvider } from '../coreComponents/MarkerProvider'
+import { SyntheticChildrenProvider } from '../coreComponents/MarkerProvider'
 import FieldAccessor from '../dao/FieldAccessor'
-import FieldMarker from '../dao/FieldMarker'
+import Parser from '../queryLanguage/Parser'
+import { TextFieldProps } from './TextField'
 
 export interface TextAreaFieldProps {
 	name: FieldName
@@ -19,8 +20,8 @@ export default class TextAreaField extends React.Component<TextAreaFieldProps> {
 	static displayName = 'TextAreaField'
 
 	public render() {
-		return (
-			<Field name={this.props.name}>
+		return Parser.generateWrappedField(this.props.name, fieldName => (
+			<Field name={fieldName}>
 				{(data: FieldAccessor<string>): React.ReactNode => {
 					return (
 						<FormGroup label={this.props.label}>
@@ -34,7 +35,7 @@ export default class TextAreaField extends React.Component<TextAreaFieldProps> {
 					)
 				}}
 			</Field>
-		)
+		))
 	}
 
 	private generateOnChange = (data: FieldAccessor<string>) => (e: ChangeEvent<HTMLTextAreaElement>) => {
@@ -42,9 +43,9 @@ export default class TextAreaField extends React.Component<TextAreaFieldProps> {
 		data.onChange && data.onChange(str)
 	}
 
-	public static generateFieldMarker(props: TextAreaFieldProps): FieldMarker {
-		return new FieldMarker(props.name)
+	public static generateSyntheticChildren(props: TextFieldProps): React.ReactNode {
+		return Parser.generateWrappedField(props.name, fieldName => <Field name={fieldName} />)
 	}
 }
 
-type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof TextAreaField, FieldMarkerProvider>
+type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof TextAreaField, SyntheticChildrenProvider>

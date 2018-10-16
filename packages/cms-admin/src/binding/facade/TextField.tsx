c4911e1d81586ba3c793b24e052e6c5fb4218fc1
Jirka Vebr <jirkavebr@gmail.com>
2018-10-16 13:45:51 +0200

    TextField: Utilize query language

--- packages/cms-admin/src/binding/facade/TextField.tsx
+++ packages/cms-admin/src/binding/facade/TextField.tsx
@@ -4,9 +4,10 @@ import { ChangeEvent } from 'react'
 import { FieldName } from '../bindingTypes'
 import EnforceSubtypeRelation from '../coreComponents/EnforceSubtypeRelation'
 import Field from '../coreComponents/Field'
-import { FieldMarkerProvider } from '../coreComponents/MarkerProvider'
+import { FieldMarkerProvider, SyntheticChildrenProvider } from '../coreComponents/MarkerProvider'
 import FieldAccessor from '../dao/FieldAccessor'
 import FieldMarker from '../dao/FieldMarker'
+import Parser from '../queryLanguage/Parser'
 
 export interface TextFieldProps {
 	name: FieldName
@@ -19,8 +20,8 @@ export default class TextField extends React.Component<TextFieldProps> {
 	static displayName = 'TextField'
 
 	public render() {
-		return (
-			<Field name={this.props.name}>
+		return Parser.generateWrappedField(this.props.name, fieldName => (
+			<Field name={fieldName}>
 				{(data: FieldAccessor<string | null, string>): React.ReactNode => {
 					return (
 						<FormGroup label={this.props.label} inline={this.props.inlineLabel}>
@@ -33,16 +34,16 @@ export default class TextField extends React.Component<TextFieldProps> {
 					)
 				}}
 			</Field>
-		)
+		))
 	}
 
 	private generateOnChange = (data: FieldAccessor<string | null, string>) => (e: ChangeEvent<HTMLInputElement>) => {
 		data.onChange && data.onChange(e.target.value)
 	}
 
-	public static generateFieldMarker(props: TextFieldProps): FieldMarker {
-		return new FieldMarker(props.name)
+	public static generateSyntheticChildren(props: TextFieldProps): React.ReactNode {
+		return Parser.generateWrappedField(props.name, fieldName => <Field name={fieldName} />)
 	}
 }
 
-type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof TextField, FieldMarkerProvider>
+type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof TextField, SyntheticChildrenProvider>

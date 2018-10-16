4321d6fc28ad3f18d1c88526bfc2fc5ddcb51097
Jirka Vebr <jirkavebr@gmail.com>
2018-10-17 00:42:59 +0200

    WIP: Smarter environment

--- packages/cms-admin/src/binding/dao/Environment.ts
+++ packages/cms-admin/src/binding/dao/Environment.ts
@@ -1,7 +1,13 @@
-class Environment {
-	public constructor(private readonly names: Environment.NameStore = {}) {}
+import { SelectedDimension } from '../../state/request'
 
-	public getName(name: keyof Environment.NameStore): Environment.Name | undefined {
+class Environment {
+	public constructor(
+		private readonly names: Environment.NameStore = {
+			dimensions: {}
+		}
+	) {}
+
+	public getValue(name: keyof Environment.NameStore): Environment.Value | undefined {
 		if (!(name in this.names)) {
 			return undefined
 		}
@@ -9,12 +15,17 @@ class Environment {
 	}
 
 	public getAllNames(): Environment.NameStore {
-		return {...this.names}
+		return { ...this.names }
+	}
+
+	public getDimensions(): SelectedDimension {
+		return { ...this.names.dimensions }
 	}
 
 	public putName(name: Environment.Name, value: Environment.Value): Environment {
 		return new Environment({
 			...this.names,
+			dimensions: { ...this.names.dimensions },
 			[name]: value
 		})
 	}
@@ -33,15 +44,35 @@ class Environment {
 
 		return new Environment(currentNames)
 	}
+
+	public static generateDelta(
+		currentEnvironment: Environment,
+		delta: Environment.DeltaFactory
+	): Partial<Environment.NameStore> {
+		const newDelta: Partial<Environment.NameStore> = {}
+
+		for (const name in delta) {
+			const value = delta[name]
+
+			newDelta[name] = typeof value === 'function' ? value(currentEnvironment) : delta[name]
+		}
+
+		return newDelta
+	}
 }
 
 namespace Environment {
 	export type Name = string
 
-	export type Value = string
+	export type Value = string | number | object | undefined | null
 
 	export interface NameStore {
-		[name: string]: string
+		dimensions: SelectedDimension
+		[name: string]: Value
+	}
+
+	export type DeltaFactory = {
+		[name: string]: ((environment: Environment) => Environment.Value) | Environment.Value
 	}
 }
 

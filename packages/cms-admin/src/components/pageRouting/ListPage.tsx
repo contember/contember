e28928c274df10d23a749377b33fb7d826ad50ed
Jirka Vebr <jirkavebr@gmail.com>
2018-10-17 17:04:55 +0200

    Add more utility Pages

---
+++ packages/cms-admin/src/components/pageRouting/ListPage.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import { EntityName } from '../../binding/bindingTypes'
+import EntityListDataProvider from '../../binding/coreComponents/EntityListDataProvider'
+import PageWithLayout from './PageWithLayout'
+
+interface ListPageProps {
+	entity: EntityName
+	layout?: React.ComponentType<{ children?: React.ReactNode }>
+}
+
+export default class ListPage extends React.Component<ListPageProps> {
+	static getPageName(props: ListPageProps) {
+		return `list_${props.entity.toLowerCase()}`
+	}
+
+	render(): React.ReactNode {
+		return <PageWithLayout layout={this.props.layout}>
+			<EntityListDataProvider name={this.props.entity}>{this.props.children}</EntityListDataProvider>
+		</PageWithLayout>
+	}
+}

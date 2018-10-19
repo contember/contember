6cc610e9b1d42f4cf81f7ca654dbab6f1674c1d9
Jirka Vebr <jirkavebr@gmail.com>
2018-10-19 11:26:04 +0200

    Add a FieldText component

---
+++ packages/cms-admin/src/binding/facade/FieldText.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react'
+import EnforceSubtypeRelation from '../coreComponents/EnforceSubtypeRelation'
+import Field from '../coreComponents/Field'
+import { SyntheticChildrenProvider } from '../coreComponents/MarkerProvider'
+import Environment from '../dao/Environment'
+import FieldAccessor from '../dao/FieldAccessor'
+import Parser from '../queryLanguage/Parser'
+import { TextFieldProps } from './TextField'
+
+interface FieldTextProps {
+	name: string
+}
+
+export default class FieldText extends React.Component<FieldTextProps> {
+	public static displayName = 'FieldText'
+
+	public render() {
+		return <Field name={this.props.name}>{(data: FieldAccessor) => data.currentValue}</Field>
+	}
+
+	public static generateSyntheticChildren(props: TextFieldProps, environment: Environment): React.ReactNode {
+		return Parser.generateWrappedField(props.name, fieldName => <Field name={fieldName} />, environment)
+	}
+}
+
+type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof FieldText, SyntheticChildrenProvider>

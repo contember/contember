4125bd96696e592c49562896f087cc914673b885
Jirka Vebr <jirkavebr@gmail.com>
2018-11-07 15:22:45 +0100

    Fix type errors

--- packages/cms-api/src/content-api/sql/utils.ts
+++ packages/cms-api/src/content-api/sql/utils.ts
@@ -1,8 +1,8 @@
 import { Input } from 'cms-common'
 
 export const resolveValue = <T>(value: Input.GenericValueLike<T>): PromiseLike<T> => {
-	if (typeof value === 'function') {
-		value = (value as () => T | PromiseLike<T>)()
+	if (value instanceof Function) {
+		value = value()
 	}
 	return Promise.resolve(value)
 }

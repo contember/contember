bc4eb9fe477a5becedc0ed5dfc4f2133dbd8c2b7
David Matejka <matej21@matej21.cz>
2018-11-09 17:21:30 +0100

    cms admin: add file upload

---
+++ packages/cms-admin/src/actions/upload.ts
@@ -0,0 +1,108 @@
+import GraphqlClient from '../model/GraphqlClient'
+import { loginRequest } from '../state/request'
+import { pushRequest } from './request'
+import { ActionCreator } from './types'
+import httpFetch from '../utils/httpFetch'
+import { createAction } from 'redux-actions'
+import {
+	UPLOAD_FINISH,
+	UPLOAD_SET_FAILURE,
+	UPLOAD_START,
+	UPLOAD_UPDATE_PROGRESS,
+	UploadFinishPayload,
+	UploadSetFailurePayload,
+	UploadStartPayload,
+	UploadUpdateProgressPayload
+} from '../reducer/upload'
+import { UploadStatus } from '../state/upload'
+import { readAsArrayBuffer, readAsDataUrl } from '../utils/fileReader'
+
+const mutation = `mutation ($contentType: String!) {
+	generateUploadUrl(contentType: $contentType) {
+		url
+		publicUrl
+	}
+}`
+
+const startUpload = createAction<UploadStartPayload, string, File, string | undefined>(
+	UPLOAD_START,
+	(id, file, thumbnailUrl) => ({
+		id,
+		data: {
+			status: UploadStatus.PREPARING,
+			name: file.name,
+			mime: file.type,
+			size: file.size,
+			thumbnailUrl
+		}
+	})
+)
+const setUploadFailed = createAction<UploadSetFailurePayload, string, string>(UPLOAD_SET_FAILURE, (id, reason) => ({
+	id,
+	reason
+}))
+const updateUploadProgress = createAction<UploadUpdateProgressPayload, string, number | null>(
+	UPLOAD_UPDATE_PROGRESS,
+	(id, progress) => ({ id, progress })
+)
+const finishUpload = createAction<UploadFinishPayload, string, string>(UPLOAD_FINISH, (id, resultUrl) => ({
+	id,
+	resultUrl
+}))
+
+export const uploadFile = (id: string, file: File): ActionCreator => async (dispatch, getState, services) => {
+	const state = getState()
+	if (!('stage' in state.request) || !('project' in state.request)) {
+		return
+	}
+	const thumbnailUrl = file.type.startsWith('image/') ? await readAsDataUrl(file) : undefined
+
+	dispatch(startUpload(id, file, thumbnailUrl))
+
+	const apiToken = state.auth.token
+	const graphqlClient = services.contentClientFactory.create(state.request.project, state.request.stage)
+
+	let uploadUrl: string
+	let publicUrl: string
+	try {
+		const variables = {
+			contentType: file.type
+		}
+		const result = await graphqlClient.request(mutation, variables, apiToken || undefined)
+
+		uploadUrl = result.generateUploadUrl.url
+		publicUrl = result.generateUploadUrl.publicUrl
+	} catch (error) {
+		if (error instanceof GraphqlClient.GraphqlAuthenticationError) {
+			dispatch(pushRequest(loginRequest()))
+			return
+		}
+		dispatch(setUploadFailed(id, 'An error has occurred while obtaining upload URL'))
+		throw error
+	}
+
+	const content = await readAsArrayBuffer(file)
+	dispatch(updateUploadProgress(id, 0))
+	try {
+		await httpFetch(
+			uploadUrl,
+			{
+				method: 'PUT',
+				headers: {
+					'Content-Type': file.type,
+					'Cache-Control': 'immutable'
+				},
+				body: content
+			},
+			{
+				onUploadProgress: e => {
+					dispatch(updateUploadProgress(id, e.lengthComputable ? (e.loaded / e.total) * 100 : null))
+				}
+			}
+		)
+	} catch (error) {
+		dispatch(setUploadFailed(id, `An error has occurred while uploading a file`))
+		throw error
+	}
+	dispatch(finishUpload(id, publicUrl))
+}
---
+++ packages/cms-admin/src/binding/facade/fields/UploadField.tsx
@@ -0,0 +1,121 @@
+import { FormGroup, IFormGroupProps } from '@blueprintjs/core'
+import * as React from 'react'
+import { FieldName } from '../../bindingTypes'
+import { EnforceSubtypeRelation, Field, SyntheticChildrenProvider } from '../../coreComponents'
+import { Environment, FieldAccessor } from '../../dao'
+import { Parser } from '../../queryLanguage'
+import Dropzone from 'react-dropzone'
+import { connect } from 'react-redux'
+import { uploadFile } from '../../../actions/upload'
+import { Dispatch } from '../../../actions/types'
+import { AnyUpload, UploadStatus } from '../../../state/upload'
+import State from '../../../state'
+import { assertNever } from 'cms-common'
+import { contentType } from 'mime-types'
+
+export interface UploadFieldOwnProps {
+	name: FieldName
+	label?: IFormGroupProps['label']
+	inlineLabel?: boolean
+}
+
+export interface UploadFieldDispatchProps {
+	onUpload: (id: string, file: File) => void
+}
+
+export interface UploadFieldStateProps {
+	uploads: { [id: string]: AnyUpload }
+}
+
+interface UploadFieldState {
+	uploadId?: string
+}
+
+class UploadFieldComponent extends React.Component<
+	UploadFieldOwnProps & UploadFieldDispatchProps & UploadFieldStateProps,
+	UploadFieldState
+> {
+	static displayName = 'UploadField'
+
+	state: UploadFieldState = {}
+
+	handleStartUpload(files: File[]) {
+		const uploadId = Math.random()
+			.toString(36)
+			.substring(2, 15)
+		this.setState({ uploadId })
+		this.props.onUpload(uploadId, files[0])
+	}
+
+	public render() {
+		const upload: AnyUpload | undefined = this.state.uploadId ? this.props.uploads[this.state.uploadId] : undefined
+
+		return (
+			<Field name={this.props.name}>
+				{(data: FieldAccessor<string | null, string>, env): React.ReactNode => {
+					if (upload && upload.status === UploadStatus.FINISHED && upload.resultUrl !== data.currentValue) {
+						data.onChange && data.onChange(upload.resultUrl)
+					}
+					return (
+						<FormGroup
+							label={env.applySystemMiddleware('labelMiddleware', this.props.label)}
+							inline={this.props.inlineLabel}
+						>
+							<Dropzone
+								onDrop={async (accepted: File[]) => {
+									this.handleStartUpload(accepted)
+								}}
+								accept="image/*"
+								multiple={false}
+							>
+								{data.currentValue && this.isImage(data.currentValue) ? <img src={data.currentValue} /> : null}
+								{upload && upload.thumbnailUrl ? <img src={upload.thumbnailUrl} /> : null}
+								{this.renderUploadStatusMessage(upload)}
+							</Dropzone>
+						</FormGroup>
+					)
+				}}
+			</Field>
+		)
+	}
+
+	private isImage(filename: string): boolean {
+		const contentTypeValue = contentType(filename.substring(filename.lastIndexOf('/') + 1))
+		return contentTypeValue !== false && contentTypeValue.startsWith('image/')
+	}
+
+	private renderUploadStatusMessage(upload?: AnyUpload) {
+		if (!upload) {
+			return 'Select a file to upload'
+		}
+		switch (upload.status) {
+			case UploadStatus.PREPARING:
+				return `Starting upload of ${upload.name}`
+			case UploadStatus.UPLOADING:
+				return `Upload of ${upload.name}: ${upload.progress && upload.progress.toFixed()}%`
+			case UploadStatus.FAILED:
+				return `Upload failed: ${upload.reason}`
+			case UploadStatus.FINISHED:
+				return `Upload has finished successfully`
+			default:
+				assertNever(upload)
+		}
+	}
+
+	public static generateSyntheticChildren(props: UploadFieldOwnProps, environment: Environment): React.ReactNode {
+		return Parser.generateWrappedField(props.name, fieldName => <Field name={fieldName} />, environment)
+	}
+}
+
+type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof UploadFieldComponent, SyntheticChildrenProvider>
+
+const UploadField = connect<UploadFieldStateProps, UploadFieldDispatchProps, UploadFieldOwnProps, State>(
+	state => ({
+		uploads: state.upload.uploads
+	}),
+	(dispatch: Dispatch) => ({
+		onUpload: (id, file) => dispatch(uploadFile(id, file))
+	})
+)(UploadFieldComponent)
+
+export { UploadField }

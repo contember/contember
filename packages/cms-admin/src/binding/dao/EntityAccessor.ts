f8d17444db40ceb41285833fadf18e99b77685c7
Jirka Vebr <jirkavebr@gmail.com>
2018-11-09 11:20:35 +0100

    EntityAccessor: custom id for unpersisted entities

--- packages/cms-admin/src/binding/dao/EntityAccessor.ts
+++ packages/cms-admin/src/binding/dao/EntityAccessor.ts
@@ -1,10 +1,31 @@
 import { EntityData } from './EntityData'
 
-export class EntityAccessor {
+class EntityAccessor {
+	public readonly primaryKey: string | EntityAccessor.UnpersistedEntityID
+
 	public constructor(
-		public readonly primaryKey: string | undefined,
+		primaryKey: string | EntityAccessor.UnpersistedEntityID | undefined,
 		public readonly data: EntityData,
 		public readonly replaceWith: (replacement: EntityAccessor) => void,
 		public readonly unlink?: () => void
-	) {}
+	) {
+		this.primaryKey = primaryKey || new EntityAccessor.UnpersistedEntityID()
+	}
 }
+
+namespace EntityAccessor {
+	export class UnpersistedEntityID {
+		public readonly value: string
+
+		private generateId = (() => {
+			let id = 0
+			return () => id++
+		})()
+
+		public constructor() {
+			this.value = `unpersistedEntity-${this.generateId()}`
+		}
+	}
+}
+
+export { EntityAccessor }

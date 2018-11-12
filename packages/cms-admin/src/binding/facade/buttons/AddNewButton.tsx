24a401be00ffe083b609824e9d2d67282677348d
Jirka Vebr <jirkavebr@gmail.com>
2018-11-12 23:55:44 +0100

    Allow rudimentary button styling

--- packages/cms-admin/src/binding/facade/buttons/AddNewButton.tsx
+++ packages/cms-admin/src/binding/facade/buttons/AddNewButton.tsx
@@ -1,8 +1,9 @@
-import { Button, Intent } from '@blueprintjs/core'
+import { Button, IButtonProps, Intent, IProps } from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
 import * as React from 'react'
 import { EntityCollectionAccessor } from '../../dao'
 
-interface AddNewButtonProps {
+interface AddNewButtonProps extends Pick<IButtonProps, 'icon' | 'intent' | 'large' | 'small'>, IProps {
 	addNew: EntityCollectionAccessor['addNew']
 }
 
@@ -10,8 +11,15 @@ export class AddNewButton extends React.PureComponent<AddNewButtonProps> {
 	public render() {
 		return (
 			this.props.addNew && (
-				<Button icon="plus" onClick={this.props.addNew} intent={Intent.PRIMARY}>
-					Add new
+				<Button
+					icon={this.props.icon || IconNames.ADD}
+					onClick={this.props.addNew}
+					intent={this.props.intent || Intent.NONE}
+					large={this.props.large !== undefined ? this.props.large : false}
+					small={this.props.small !== undefined ? this.props.small : false}
+					className={this.props.className}
+				>
+					{this.props.children}
 				</Button>
 			)
 		)

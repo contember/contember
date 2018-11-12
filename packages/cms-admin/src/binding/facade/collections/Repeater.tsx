c7377ec5a901c3564caf938c4692d8aa78a50ebe
Jirka Vebr <jirkavebr@gmail.com>
2018-11-12 18:57:44 +0100

    Consolidate repeater apis

--- packages/cms-admin/src/binding/facade/collections/Repeater.tsx
+++ packages/cms-admin/src/binding/facade/collections/Repeater.tsx
@@ -12,27 +12,14 @@ import { AddNewButton, UnlinkButton } from '../buttons'
 
 export interface RepeaterProps extends ToManyProps {}
 
-export class Repeater extends React.Component<RepeaterProps> {
+class Repeater extends React.Component<RepeaterProps> {
 	static displayName = 'Repeater'
 
 	public render() {
 		return (
 			<ToMany.CollectionRetriever {...this.props}>
 				{(field: EntityCollectionAccessor) => {
-					return (
-						<>
-							{field.entities.map(
-								(datum, i) =>
-									datum instanceof EntityAccessor && (
-										<DataContext.Provider value={datum} key={i}>
-											{this.props.children}
-											<UnlinkButton />
-										</DataContext.Provider>
-									)
-							)}
-							<AddNewButton addNew={field.addNew} />
-						</>
-					)
+					return <Repeater.EntityCollection entities={field}>{this.props.children}</Repeater.EntityCollection>
 				}}
 			</ToMany.CollectionRetriever>
 		)
@@ -42,5 +29,49 @@ export class Repeater extends React.Component<RepeaterProps> {
 		return <ToMany {...props}>{props.children}</ToMany>
 	}
 }
+
+namespace Repeater {
+	export interface ItemProps {
+		entity: EntityAccessor
+		displayUnlinkButton: boolean
+	}
+
+	export class Item extends React.PureComponent<ItemProps> {
+		public render() {
+			return (
+				<DataContext.Provider value={this.props.entity}>
+					{this.props.children}
+					{this.props.displayUnlinkButton && <UnlinkButton />}
+				</DataContext.Provider>
+			)
+		}
+	}
+
+	export interface EntityCollectionProps {
+		entities: EntityCollectionAccessor
+	}
+
+	export class EntityCollection extends React.PureComponent<EntityCollectionProps> {
+		public render() {
+			const entities = filterEntities(this.props.entities)
+			return (
+				<>
+					{entities.map(entity => (
+						<Item displayUnlinkButton={entities.length > 1} entity={entity} key={entity.getKey()}>
+							{this.props.children}
+						</Item>
+					))}
+					<AddNewButton addNew={this.props.entities.addNew} />
+				</>
+			)
+		}
+	}
+
+	export const filterEntities = (entities: EntityCollectionAccessor): EntityAccessor[] => {
+		return entities.entities.filter((item): item is EntityAccessor => item instanceof EntityAccessor)
+	}
+}
+
+export { Repeater }
 
 type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof Repeater, SyntheticChildrenProvider<RepeaterProps>>

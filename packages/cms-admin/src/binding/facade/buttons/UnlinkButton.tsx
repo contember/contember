0297e7bad28e231ce6790233bdfee123e89b1e1c
Jirka Vebr <jirkavebr@gmail.com>
2018-11-12 23:55:44 +0100

    Allow rudimentary button styling

--- packages/cms-admin/src/binding/facade/buttons/UnlinkButton.tsx
+++ packages/cms-admin/src/binding/facade/buttons/UnlinkButton.tsx
@@ -1,15 +1,27 @@
-import { Button, Intent } from '@blueprintjs/core'
+import { Button, IButtonProps, Intent, IProps } from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
 import * as React from 'react'
 import { DataContext, DataContextValue } from '../../coreComponents'
 import { EntityAccessor } from '../../dao'
 
-export class UnlinkButton extends React.Component {
+export interface UnlinkButtonProps extends Pick<IButtonProps, 'icon' | 'intent' | 'large' | 'small'>, IProps {}
+
+export class UnlinkButton extends React.Component<UnlinkButtonProps> {
 	public render() {
 		return (
 			<DataContext.Consumer>
 				{(value: DataContextValue) => {
 					if (value instanceof EntityAccessor) {
-						return <Button icon="cross" onClick={value.unlink} intent={Intent.DANGER} small={true} />
+						return (
+							<Button
+								icon={this.props.icon || IconNames.REMOVE}
+								onClick={value.unlink}
+								intent={this.props.intent || Intent.DANGER}
+								small={this.props.small !== undefined ? this.props.small : true}
+								large={this.props.large !== undefined ? this.props.large : false}
+								className={this.props.className}
+							/>
+						)
 					}
 				}}
 			</DataContext.Consumer>

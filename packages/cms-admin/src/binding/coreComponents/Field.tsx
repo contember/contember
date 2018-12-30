39327a1ce26cb18fa0ccee812a6cba63daf15998
Jirka Vebr <jirkavebr@gmail.com>
2018-12-30 20:08:59 +0100

    Field: internal refactoring

--- packages/cms-admin/src/binding/coreComponents/Field.tsx
+++ packages/cms-admin/src/binding/coreComponents/Field.tsx
@@ -29,35 +29,25 @@ class Field extends React.PureComponent<FieldProps> {
 
 	public render() {
 		return (
-			<EnvironmentContext.Consumer>
-				{environment =>
-					Parser.generateWrappedNode(
-						this.props.name,
-						fieldName => (
-							<DataContext.Consumer>
-								{(data: DataContextValue) => {
-									if (data instanceof EntityAccessor) {
-										const fieldData = data.data.getField(fieldName)
+			<Field.DataRetriever name={this.props.name}>
+				{(fieldName, data, environment) => {
+					if (data instanceof EntityAccessor) {
+						const fieldData = data.data.getField(fieldName)
 
-										if (this.props.children && fieldData instanceof FieldAccessor) {
-											return (
-												<Field.FieldInner accessor={fieldData} environment={environment}>
-													{this.props.children}
-												</Field.FieldInner>
-											)
-										}
-									} else if (data instanceof EntityForRemovalAccessor) {
-										// Do nothing
-									} else {
-										throw new DataBindingError('Corrupted data')
-									}
-								}}
-							</DataContext.Consumer>
-						),
-						environment
-					)
-				}
-			</EnvironmentContext.Consumer>
+						if (this.props.children && fieldData instanceof FieldAccessor) {
+							return (
+								<Field.FieldInner accessor={fieldData} environment={environment}>
+									{this.props.children}
+								</Field.FieldInner>
+							)
+						}
+					} else if (data instanceof EntityForRemovalAccessor) {
+						// Do nothing
+					} else {
+						throw new DataBindingError('Corrupted data')
+					}
+				}}
+			</Field.DataRetriever>
 		)
 	}
 
@@ -79,6 +69,31 @@ namespace Field {
 	export class FieldInner extends React.PureComponent<FieldInnerProps> {
 		public render() {
 			return this.props.children(this.props.accessor, this.props.environment)
+		}
+	}
+
+	export interface DataRetrieverProps {
+		name: FieldName
+		children: (fieldName: FieldName, data: DataContextValue, environment: Environment) => React.ReactNode
+	}
+
+	export class DataRetriever extends React.Component<DataRetrieverProps> {
+		public render() {
+			return (
+				<EnvironmentContext.Consumer>
+					{environment =>
+						Parser.generateWrappedNode(
+							this.props.name,
+							fieldName => (
+								<DataContext.Consumer>
+									{(data: DataContextValue) => this.props.children(fieldName, data, environment)}
+								</DataContext.Consumer>
+							),
+							environment
+						)
+					}
+				</EnvironmentContext.Consumer>
+			)
 		}
 	}
 }

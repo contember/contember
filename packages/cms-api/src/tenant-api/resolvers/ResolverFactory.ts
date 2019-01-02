3ea16b71722f0b4685d151ea584fb97f3c0c1655
Jirka Vebr <jirkavebr@gmail.com>
2019-01-02 22:14:04 +0100

    Update TypeScript from 3.1.6 to 3.2.2

--- packages/cms-api/src/tenant-api/resolvers/ResolverFactory.ts
+++ packages/cms-api/src/tenant-api/resolvers/ResolverFactory.ts
@@ -1,3 +1,5 @@
+import { GraphQLResolveInfo } from 'graphql'
+import { MutationResolvers } from '../schema/types'
 import MeQueryResolver from './query/MeQueryResolver'
 import SignUpMutationResolver from './mutation/SignUpMutationResolver'
 import SignInMutationResolver from './mutation/SignInMutationResolver'
@@ -5,6 +7,7 @@ import AddProjectMemberMutationResolver from './mutation/AddProjectMemberMutatio
 import { Config } from 'apollo-server-core'
 import SetupMutationResolver from './mutation/SetupMutationResolver'
 import UpdateProjectMemberVariablesMutationResolver from './mutation/UpdateProjectMemberVariablesMutationResolver'
+import ResolverContext from './ResolverContext'
 
 class ResolverFactory {
 	public constructor(
@@ -22,17 +25,79 @@ class ResolverFactory {
 				me: this.meQueryResolver.me.bind(this.meQueryResolver),
 			},
 			Mutation: {
-				signUp: this.signUpMutationResolver.signUp.bind(this.signUpMutationResolver),
-				signIn: this.signInMutationResolver.signIn.bind(this.signInMutationResolver),
-				addProjectMember: this.addProjectMemberMutationResolver.addProjectMember.bind(
-					this.addProjectMemberMutationResolver
-				),
-				setup: this.setupMutationResolver.setup.bind(this.setupMutationResolver),
-				updateProjectMemberVariables: this.updateProjectMemberVariablesMutationResolver.updateProjectMemberVariables.bind(
-					this.updateProjectMemberVariablesMutationResolver
-				),
+				signUp: (
+					parent: any,
+					args: ResolverFactory.FieldResolverArgs,
+					context: ResolverContext,
+					info: GraphQLResolveInfo
+				) =>
+					this.signUpMutationResolver.signUp.call(
+						this.signUpMutationResolver,
+						parent,
+						args as MutationResolvers.SignUpArgs,
+						context,
+						info
+					),
+				signIn: (
+					parent: any,
+					args: ResolverFactory.FieldResolverArgs,
+					context: ResolverContext,
+					info: GraphQLResolveInfo
+				) =>
+					this.signInMutationResolver.signIn.call(
+						this.signInMutationResolver,
+						parent,
+						args as MutationResolvers.SignInArgs,
+						context,
+						info
+					),
+				addProjectMember: (
+					parent: any,
+					args: ResolverFactory.FieldResolverArgs,
+					context: ResolverContext,
+					info: GraphQLResolveInfo
+				) =>
+					this.addProjectMemberMutationResolver.addProjectMember.call(
+						this.addProjectMemberMutationResolver,
+						parent,
+						args as MutationResolvers.AddProjectMemberArgs,
+						context,
+						info
+					),
+				setup: (
+					parent: any,
+					args: ResolverFactory.FieldResolverArgs,
+					context: ResolverContext,
+					info: GraphQLResolveInfo
+				) =>
+					this.setupMutationResolver.setup.call(
+						this.setupMutationResolver,
+						parent,
+						args as MutationResolvers.SetupArgs,
+						context,
+						info
+					),
+				updateProjectMemberVariables: (
+					parent: any,
+					args: ResolverFactory.FieldResolverArgs,
+					context: ResolverContext,
+					info: GraphQLResolveInfo
+				) =>
+					this.updateProjectMemberVariablesMutationResolver.updateProjectMemberVariables.call(
+						this.updateProjectMemberVariablesMutationResolver,
+						parent,
+						args as MutationResolvers.UpdateProjectMemberVariablesArgs,
+						context,
+						info
+					),
 			},
 		}
+	}
+}
+
+namespace ResolverFactory {
+	export type FieldResolverArgs = {
+		[argument: string]: any
 	}
 }
 

fd9e82cfbbabc60b503c2ca27a2e5d1da4bdc05f
Jirka Vebr <jirkavebr@gmail.com>
2019-01-03 15:46:10 +0100

    Simpler custom data binding components

--- packages/cms-admin/src/binding/facade/Component.tsx
+++ packages/cms-admin/src/binding/facade/Component.tsx
@@ -2,32 +2,35 @@ import * as React from 'react'
 import { EnvironmentContext, Props, SyntheticChildrenProvider } from '../coreComponents'
 import { Environment } from '../dao'
 
-type Component<P> = {
+interface Component<P extends {}, S> extends React.ComponentClass<P, S> {
 	render(props: Props<P>, environment: Environment): React.ReactNode
-	displayName?: string
 }
 
-// TODO: allow just a function as an argument after we upgrade to TS 3.2
-const Component = <P extends {}, S>(
-	Parent: React.ComponentClass<P, S> & Component<P>
+const Component = <P extends {}, S = {}>(
+	Parent: Component<P, S> | React.FunctionComponent<P>,
+	displayName?: string
 ): React.ComponentClass<P, S> & SyntheticChildrenProvider<P> => {
-	class Child extends Parent {
-		public static propTypes = Parent.propTypes
-		public static contextTypes = Parent.contextTypes
-		public static childContextTypes = Parent.childContextTypes
-		public static defaultProps = Parent.defaultProps
-		public static displayName = Parent.displayName || 'UserComponent'
+	const NormalizedParent: React.ComponentClass<P, S> = 'render' in Parent ? Parent : React.Component
+	const render: (props: Props<P>, environment: Environment) => React.ReactNode =
+		'render' in Parent ? Parent.render : Parent
+
+	class Child extends NormalizedParent {
+		public static propTypes = NormalizedParent.propTypes
+		public static contextTypes = NormalizedParent.contextTypes
+		public static childContextTypes = NormalizedParent.childContextTypes
+		public static defaultProps = NormalizedParent.defaultProps
+		public static displayName = NormalizedParent.displayName || displayName || 'UserComponent'
 
 		public render() {
 			return (
 				<EnvironmentContext.Consumer>
-					{(environment: Environment) => Parent.render(this.props, environment)}
+					{(environment: Environment) => render(this.props, environment)}
 				</EnvironmentContext.Consumer>
 			)
 		}
 
 		public static generateSyntheticChildren(props: P, environment: Environment): React.ReactNode {
-			return Parent.render(props, environment)
+			return render(props, environment)
 		}
 	}
 

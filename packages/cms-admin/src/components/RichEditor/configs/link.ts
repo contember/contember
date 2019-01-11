63b35315c794a753d9d9868bef22c6fc1d278a40
Vojta Staněk <stanekv01@gmail.com>
2019-01-11 17:40:09 +0100

    Json serializer

--- packages/cms-admin/src/components/RichEditor/configs/link.ts
+++ packages/cms-admin/src/components/RichEditor/configs/link.ts
@@ -1,49 +1,42 @@
-import { Change, Value } from 'slate'
+import { Editor, Value } from 'slate'
 import { RichEditorPluginConfig, simpleMarkPlugin, simpleMarkRule } from './utils'
-
-/**
- * A change helper to standardize wrapping links.
- */
-const wrapLink = (href: string) => (change: Change) =>
-	change.addMark({
-		type: 'link',
-		data: { href }
-	})
-
-/**
- * A change helper to standardize unwrapping links.
- */
-const unwrapLink = () => (change: Change) => change.removeMark('link')
 
 const LINK: RichEditorPluginConfig = {
 	node: 'mark',
 	type: 'link',
 	plugin: simpleMarkPlugin('link', 'a', ['href']),
 	htmlSerializer: simpleMarkRule('link', 'a', ['href']),
-	onToggle: (value: Value) => {
-		const hasLinks = value.activeMarks.some(mark => mark !== undefined && mark.type == 'link')
-		const change = value.change()
+	onToggle: (editor: Editor) => {
+		const hasLinks = editor.value.activeMarks.some(mark => mark !== undefined && mark.type == 'link')
+		// const change = value.
 
 		if (hasLinks) {
-			change.call(unwrapLink())
-		} else if (value.selection.isExpanded) {
+			// value.
+			editor.removeMark('link')
+		} else if (editor.value.selection.isExpanded) {
 			const href = window.prompt('Enter the URL of the link:')
 			if (href) {
-				change.call(wrapLink(href))
+				editor.addMark({
+					type: 'link',
+					data: { href }
+				})
 			}
 		} else {
 			const href = window.prompt('Enter the URL of the link:')
 			const text = window.prompt('Enter the text for the link:')
 
 			if (href && text) {
-				change
+				editor
 					.insertText(text)
 					.moveFocusBackward(text.length)
-					.call(wrapLink(href))
+					.addMark({
+						type: 'link',
+						data: { href }
+					})
 			}
 		}
 
-		return change
+		return editor
 	}
 }
 

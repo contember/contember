219ab1075c1c400e26c546c0d2b74457483291ae
Jirka Vebr <jirkavebr@gmail.com>
2019-01-25 00:30:52 +0100

    AlternativeFields: make sure the labels are actually rendered

--- packages/cms-admin/src/binding/facade/ui/AlternativeFields.tsx
+++ packages/cms-admin/src/binding/facade/ui/AlternativeFields.tsx
@@ -1,6 +1,7 @@
 import cn from 'classnames'
 import * as React from 'react'
-import { Component } from '../Component'
+import { Props } from '../../coreComponents'
+import { Environment } from '../../dao'
 import { RadioField, RadioFieldPublicProps } from '../fields'
 import { ChoiceField } from '../fields/ChoiceField'
 
@@ -8,39 +9,66 @@ export interface AlternativeFieldsProps extends RadioFieldPublicProps {
 	alternatives: AlternativeFields.ControllerFieldMetadata
 }
 
-class AlternativeFields extends Component<AlternativeFieldsProps>(
-	props => (
-		<div className="alternativeFields">
-			<ChoiceField name={props.name} options={Object.values(props.alternatives)}>
-				{(data, currentValue, onChange, environment) => {
-					const alternatives: React.ReactNodeArray = []
-					for (let i = 0, length = props.alternatives.length; i < length; i++) {
-						alternatives.push(
-							<div className={cn('alternativeFields-item', i === currentValue && 'is-active')} key={i}>
-								{props.alternatives[i][2]}
-							</div>
+class AlternativeFields extends React.PureComponent<AlternativeFieldsProps> {
+	public static displayName = 'AlternativeFields'
+
+	public render(): React.ReactNode {
+		return (
+			<div className="alternativeFields">
+				<ChoiceField name={this.props.name} options={Object.values(this.props.alternatives)}>
+					{(data, currentValue, onChange, environment) => {
+						const alternatives: React.ReactNodeArray = []
+						for (let i = 0, length = this.props.alternatives.length; i < length; i++) {
+							alternatives.push(
+								<div className={cn('alternativeFields-item', i === currentValue && 'is-active')} key={i}>
+									{this.props.alternatives[i][2]}
+								</div>
+							)
+						}
+						return (
+							<>
+								<RadioField.RadioFieldInner
+									name={this.props.name}
+									label={this.props.label}
+									inline={this.props.inline !== false}
+									data={data}
+									currentValue={currentValue}
+									onChange={onChange}
+									environment={environment}
+								/>
+								<div className="alternativeFields-items">{alternatives}</div>
+							</>
 						)
-					}
-					return (
-						<>
-							<RadioField.RadioFieldInner
-								name={props.name}
-								label={props.label}
-								inline={props.inline !== false}
-								data={data}
-								currentValue={currentValue}
-								onChange={onChange}
-								environment={environment}
-							/>
-							<div className="alternativeFields-items">{alternatives}</div>
-						</>
-					)
-				}}
-			</ChoiceField>
-		</div>
-	),
-	'AlternativeFields'
-) {}
+					}}
+				</ChoiceField>
+			</div>
+		)
+	}
+
+	public static generateSyntheticChildren(
+		props: Props<AlternativeFieldsProps>,
+		environment: Environment
+	): React.ReactNode {
+		const alternatives: React.ReactNodeArray = []
+
+		for (let i = 0, length = props.alternatives.length; i < length; i++) {
+			alternatives.push(<React.Fragment key={i}>{props.alternatives[i][2]}</React.Fragment>)
+		}
+
+		return (
+			<>
+				{ChoiceField.generateSyntheticChildren(
+					{
+						name: props.name,
+						options: Object.values(props.alternatives)
+					},
+					environment
+				)}
+				{alternatives}
+			</>
+		)
+	}
+}
 
 namespace AlternativeFields {
 	// This isn't React.ReactNode so as to exclude arrays and other likely irrelevant values

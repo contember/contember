ec3e93bca07fb588e4857d88ff8f3b80c86f830c
Jirka Vebr <jirkavebr@gmail.com>
2019-02-02 23:28:16 +0100

    Add ImmutableDataProvider

---
+++ packages/cms-admin/src/binding/coreComponents/ImmutableDataProvider.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { DataRendererProps } from './DataProvider'
+
+interface ImmutableDataProviderProps {
+	immutable: boolean
+	children: (
+		data: DataRendererProps['data'],
+		onDataAvailable: (data: DataRendererProps['data']) => void
+	) => React.ReactNode
+}
+
+interface ImmutableDataProviderState {
+	data: DataRendererProps['data']
+}
+
+export class ImmutableDataProvider extends React.PureComponent<ImmutableDataProviderProps, ImmutableDataProviderState> {
+	state = {
+		data: undefined
+	}
+
+	public render() {
+		return this.props.children(this.state.data, data => {
+			if (!this.state.data || !this.props.immutable) {
+				this.setState({
+					data: data
+				})
+			}
+		})
+	}
+}

384f11d77b5235dd72ae0de09be968b54e87a43f
Vojta Staněk <stanekv01@gmail.com>
2019-02-08 11:06:42 +0100

    Layout story: Use navbar title class

--- packages/cms-admin/storybook/components/LayoutDefault.tsx
+++ packages/cms-admin/storybook/components/LayoutDefault.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react'
 import { storiesOf } from '@storybook/react'
-// import { Icon } from '@blueprintjs/core'
-// import { Menu, MenuList, MenuPageLinkPrimary, MenuPageLink } from '../../src/components/ui'
 import LayoutDefault from '../../src/components/LayoutDefault'
 import { DummyAdmin } from '../DummyAdmin'
 import { menu } from './Menu'
 
 storiesOf('Layout', module).add('simple', () => (
 	<DummyAdmin>
-		{/* {menu} */}
 		<LayoutDefault
-			header={{ left: 'Blog', right: '' }}
+			header={{
+				left: (
+					<a className="navbar-title" href="#">
+						Blog
+					</a>
+				),
+				right: ''
+			}}
 			side={menu}
 			content={<div style={{ background: 'red', width: '100%', height: '100vh' }} />}
 		/>

b787cc025f2d91fdcc6fc874ffab9b8f3280a0cf
Vojta Staněk <stanekv01@gmail.com>
2019-02-11 09:18:28 +0100

    WIP

--- packages/cms-admin/src/binding/facade/fields/NumberField.tsx
+++ packages/cms-admin/src/binding/facade/fields/NumberField.tsx
@@ -1,16 +1,15 @@
-import { FormGroup, IFormGroupProps, IInputGroupProps, InputGroup } from '@blueprintjs/core'
 import * as React from 'react'
 import { ChangeEvent } from 'react'
 import { FieldName } from '../../bindingTypes'
+import { FormGroup, FormGroupProps, InputGroupProps, InputGroup } from '../../../components'
 import { EnforceSubtypeRelation, Field, SyntheticChildrenProvider } from '../../coreComponents'
 import { Environment, FieldAccessor } from '../../dao'
 import { QueryLanguage } from '../../queryLanguage'
 
 export interface NumberFieldProps {
 	name: FieldName
-	label?: IFormGroupProps['label']
-	large?: IInputGroupProps['large']
-	inlineLabel?: boolean
+	label?: FormGroupProps['label']
+	large?: InputGroupProps['large']
 }
 
 export class NumberField extends React.PureComponent<NumberFieldProps> {
@@ -22,7 +21,6 @@ export class NumberField extends React.PureComponent<NumberFieldProps> {
 				{(data: FieldAccessor<number>, env): React.ReactNode => (
 					<FormGroup
 						label={env.applySystemMiddleware('labelMiddleware', this.props.label)}
-						inline={this.props.inlineLabel}
 					>
 						<InputGroup
 							value={typeof data.currentValue === 'number' ? data.currentValue.toFixed(0) : '0'}

4cddcb83eb035d106fafb08073f2a157040de982
Jirka Vebr <jirkavebr@gmail.com>
2019-02-12 17:00:30 +0100

    Add qualified field list parsing

---
+++ packages/cms-admin/tests/cases/unit/queryLanguage/qualifiedFieldList.tsx
@@ -0,0 +1,28 @@
+import { expect } from 'chai'
+import 'mocha'
+import * as React from 'react'
+import { Parser } from '../../../../src/binding/queryLanguage'
+
+const parse = (input: string) => {
+	return Parser.parseQueryLanguageExpression(input, Parser.EntryPoint.QualifiedFieldList)
+}
+
+describe('qualified field list QueryLanguage parser', () => {
+	it('should parse a complete qualified field list', () => {
+		expect(parse("Author[age > 20].children[name != 'james'].name")).eql({
+			entityName: 'Author',
+			filter: {
+				age: { gt: 20 }
+			},
+			toManyProps: [
+				{
+					field: 'children',
+					filter: {
+						name: { notEq: 'james' }
+					}
+				}
+			],
+			fieldName: 'name'
+		})
+	})
+})

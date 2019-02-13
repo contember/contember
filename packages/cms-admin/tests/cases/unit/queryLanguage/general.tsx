8aa580621ecaeb5079dc25faaf38abc6bfc8b848
Jirka Vebr <jirkavebr@gmail.com>
2019-02-13 13:34:02 +0100

    cs fix

--- packages/cms-admin/tests/cases/unit/queryLanguage/general.tsx
+++ packages/cms-admin/tests/cases/unit/queryLanguage/general.tsx
@@ -8,14 +8,18 @@ import { QueryLanguage } from '../../../../src/binding/queryLanguage'
 
 describe('query language parser', () => {
 	it('should resolve variables adhering to the principle maximal munch', () => {
-		expect(QueryLanguage.wrapRelativeSingleField('a(a=$a).ab(ab = $ab).x(x = $x).foo', name => (
-			<TextField name={name} />
-		), new Environment({
-			ab: 456,
-			a: 123,
-			x: "'x'",
-			dimensions: {}
-		}))).eql((
+		expect(
+			QueryLanguage.wrapRelativeSingleField(
+				'a(a=$a).ab(ab = $ab).x(x = $x).foo',
+				name => <TextField name={name} />,
+				new Environment({
+					ab: 456,
+					a: 123,
+					x: "'x'",
+					dimensions: {}
+				})
+			)
+		).eql(
 			<ToOne field="a" reducedBy={{ a: 123 }}>
 				<ToOne field="ab" reducedBy={{ ab: 456 }}>
 					<ToOne field="x" reducedBy={{ x: 'x' }}>
@@ -23,20 +27,24 @@ describe('query language parser', () => {
 					</ToOne>
 				</ToOne>
 			</ToOne>
-		))
+		)
 	})
 
 	it('should resolve variables with multiple levels of replacement', () => {
-		expect(QueryLanguage.wrapRelativeSingleField('a($a).foo', name => (
-			<TextField name={name} />
-		), new Environment({
-			ab: 456,
-			a: 'ab = $ab',
-			dimensions: {}
-		}))).eql((
+		expect(
+			QueryLanguage.wrapRelativeSingleField(
+				'a($a).foo',
+				name => <TextField name={name} />,
+				new Environment({
+					ab: 456,
+					a: 'ab = $ab',
+					dimensions: {}
+				})
+			)
+		).eql(
 			<ToOne field="a" reducedBy={{ ab: 456 }}>
 				<TextField name="foo" />
 			</ToOne>
-		))
+		)
 	})
 })

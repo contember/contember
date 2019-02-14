e03409ea7c3f63d41a1afd62d999cb8e51d637f4
Jirka Vebr <jirkavebr@gmail.com>
2019-02-14 15:37:07 +0100

    Introduce a ToOne atomic primitive

--- packages/cms-admin/src/binding/coreComponents/ToOne.tsx
+++ packages/cms-admin/src/binding/coreComponents/ToOne.tsx
@@ -1,64 +1,85 @@
 import { GraphQlBuilder } from 'cms-client'
 import { Input } from 'cms-common'
 import * as React from 'react'
-import { FieldName, Filter } from '../bindingTypes'
+import { FieldName, Filter, RelativeEntityList } from '../bindingTypes'
 import { EntityAccessor, EntityFields, Environment, ReferenceMarker } from '../dao'
 import { VariableInputTransformer } from '../model/VariableInputTransformer'
+import { QueryLanguage } from '../queryLanguage'
 import { DataContext, DataContextValue } from './DataContext'
 import { EnforceSubtypeRelation } from './EnforceSubtypeRelation'
 import { EnvironmentContext } from './EnvironmentContext'
-import { ReferenceMarkerProvider } from './MarkerProvider'
+import { Props, ReferenceMarkerProvider, SyntheticChildrenProvider } from './MarkerProvider'
 
 export interface ToOneProps {
-	field: FieldName
-	reducedBy?: Input.UniqueWhere<GraphQlBuilder.Literal>
-	filter?: Filter
+	field: RelativeEntityList
 }
 
 class ToOne extends React.PureComponent<ToOneProps> {
 	static displayName = 'ToOne'
 
 	public render() {
-		return (
-			<EnvironmentContext.Consumer>
-				{(environment: Environment) => (
-					<DataContext.Consumer>
-						{(data: DataContextValue) => {
-							if (data instanceof EntityAccessor) {
-								const field = data.data.getField(
-									this.props.field,
-									ReferenceMarker.ExpectedCount.UpToOne,
-									VariableInputTransformer.transformFilter(this.props.filter, environment),
-									this.props.reducedBy
-								)
-
-								if (field instanceof EntityAccessor) {
-									return <ToOne.ToOneInner accessor={field}>{this.props.children}</ToOne.ToOneInner>
-								}
-							}
-						}}
-					</DataContext.Consumer>
-				)}
-			</EnvironmentContext.Consumer>
-		)
+		return <EnvironmentContext.Consumer>
+			{(environment: Environment) => (
+				ToOne.generateSyntheticChildren(this.props, environment)
+			)}
+		</EnvironmentContext.Consumer>
 	}
 
-	public static generateReferenceMarker(
-		props: ToOneProps,
-		fields: EntityFields,
-		environment: Environment
-	): ReferenceMarker {
-		return new ReferenceMarker(
-			props.field,
-			ReferenceMarker.ExpectedCount.UpToOne,
-			fields,
-			VariableInputTransformer.transformFilter(props.filter, environment),
-			props.reducedBy
-		)
+	public static generateSyntheticChildren(props: Props<ToOneProps>, environment: Environment): React.ReactNode {
+		return QueryLanguage.wrapRelativeSingleEntity(props.field, props.children, environment)
 	}
 }
 
 namespace ToOne {
+	export interface AtomicPrimitivePublicProps {
+		field: FieldName
+		reducedBy?: Input.UniqueWhere<GraphQlBuilder.Literal>
+		filter?: Filter
+	}
+
+	interface AtomicPrimitiveProps extends AtomicPrimitivePublicProps {
+		environment: Environment
+	}
+
+	export class AtomicPrimitive extends React.PureComponent<AtomicPrimitiveProps> {
+		static displayName = 'ToOne.AtomicPrimitive'
+
+		public render() {
+			return <DataContext.Consumer>
+				{(data: DataContextValue) => {
+				if (data instanceof EntityAccessor) {
+					const field = data.data.getField(
+						this.props.field,
+						ReferenceMarker.ExpectedCount.UpToOne,
+						VariableInputTransformer.transformFilter(this.props.filter, this.props.environment),
+						this.props.reducedBy
+					)
+
+					if (field instanceof EntityAccessor) {
+						return <ToOne.ToOneInner accessor={field}>{this.props.children}</ToOne.ToOneInner>
+					}
+				}
+			}}
+		</DataContext.Consumer>
+		}
+
+		public static generateReferenceMarker(
+			props: AtomicPrimitiveProps,
+			fields: EntityFields,
+			environment: Environment
+		): ReferenceMarker {
+			return new ReferenceMarker(
+				props.field,
+				ReferenceMarker.ExpectedCount.UpToOne,
+				fields,
+				VariableInputTransformer.transformFilter(props.filter, environment),
+				props.reducedBy
+			)
+		}
+	}
+
+	type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof AtomicPrimitive, ReferenceMarkerProvider>
+
 	export interface ToOneInnerProps {
 		accessor: EntityAccessor
 	}
@@ -72,4 +93,4 @@ namespace ToOne {
 
 export { ToOne }
 
-type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof ToOne, ReferenceMarkerProvider>
+type EnforceDataBindingCompatibility = EnforceSubtypeRelation<typeof ToOne, SyntheticChildrenProvider>
--- packages/cms-admin/tests/cases/unit/queryLanguage/general.tsx
+++ packages/cms-admin/tests/cases/unit/queryLanguage/general.tsx
@@ -8,43 +8,39 @@ import { QueryLanguage } from '../../../../src/binding/queryLanguage'
 
 describe('query language parser', () => {
 	it('should resolve variables adhering to the principle maximal munch', () => {
+		const environment = new Environment({
+			ab: 456,
+			a: 123,
+			x: "'x'",
+			dimensions: {}
+		})
 		expect(
 			QueryLanguage.wrapRelativeSingleField(
 				'a(a=$a).ab(ab = $ab).x(x = $x).foo',
 				name => <TextField name={name} />,
-				new Environment({
-					ab: 456,
-					a: 123,
-					x: "'x'",
-					dimensions: {}
-				})
+				environment
 			)
 		).eql(
-			<ToOne field="a" reducedBy={{ a: 123 }}>
-				<ToOne field="ab" reducedBy={{ ab: 456 }}>
-					<ToOne field="x" reducedBy={{ x: 'x' }}>
+			<ToOne.AtomicPrimitive field="a" reducedBy={{ a: 123 }} environment={environment}>
+				<ToOne.AtomicPrimitive field="ab" reducedBy={{ ab: 456 }} environment={environment}>
+					<ToOne.AtomicPrimitive field="x" reducedBy={{ x: 'x' }} environment={environment}>
 						<TextField name="foo" />
-					</ToOne>
-				</ToOne>
-			</ToOne>
+					</ToOne.AtomicPrimitive>
+				</ToOne.AtomicPrimitive>
+			</ToOne.AtomicPrimitive>
 		)
 	})
 
 	it('should resolve variables with multiple levels of replacement', () => {
-		expect(
-			QueryLanguage.wrapRelativeSingleField(
-				'a($a).foo',
-				name => <TextField name={name} />,
-				new Environment({
-					ab: 456,
-					a: 'ab = $ab',
-					dimensions: {}
-				})
-			)
-		).eql(
-			<ToOne field="a" reducedBy={{ ab: 456 }}>
+		const environment = new Environment({
+			ab: 456,
+			a: 'ab = $ab',
+			dimensions: {}
+		})
+		expect(QueryLanguage.wrapRelativeSingleField('a($a).foo', name => <TextField name={name} />, environment)).eql(
+			<ToOne.AtomicPrimitive field="a" reducedBy={{ ab: 456 }} environment={environment}>
 				<TextField name="foo" />
-			</ToOne>
+			</ToOne.AtomicPrimitive>
 		)
 	})
 })

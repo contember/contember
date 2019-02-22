c44ea49e411ce3832b3f405104ab678365f063e5
David Matejka <matej21@matej21.cz>
2019-02-22 15:04:23 +0100

    quanti: add contact messages

--- packages/cms-admin/src/binding/facade/ui/FieldText.tsx
+++ packages/cms-admin/src/binding/facade/ui/FieldText.tsx
@@ -6,13 +6,20 @@ import { TextFieldProps } from '../fields'
 
 interface FieldTextProps {
 	name: string
+	formatter?: (value: string | null) => React.ReactNode
 }
 
 export class FieldText extends React.PureComponent<FieldTextProps> {
 	public static displayName = 'FieldText'
 
 	public render() {
-		return <Field name={this.props.name}>{(data: FieldAccessor) => data.currentValue}</Field>
+		return (
+			<Field name={this.props.name}>
+				{(data: FieldAccessor<string>) =>
+					this.props.formatter ? this.props.formatter(data.currentValue) : data.currentValue
+				}
+			</Field>
+		)
 	}
 
 	public static generateSyntheticChildren(props: TextFieldProps, environment: Environment): React.ReactNode {

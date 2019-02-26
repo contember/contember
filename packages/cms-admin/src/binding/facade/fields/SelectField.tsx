5c265106ef303681abbc0abbe662954f5f6c645e
Jirka Vebr <jirkavebr@gmail.com>
2019-02-26 16:48:26 +0100

    Move Component to aux

--- packages/cms-admin/src/binding/facade/fields/SelectField.tsx
+++ packages/cms-admin/src/binding/facade/fields/SelectField.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { FormGroup, FormGroupProps, Select } from '../../../components'
 import { FieldName } from '../../bindingTypes'
 import { Environment } from '../../dao'
-import { Component } from '../Component'
+import { Component } from '../aux'
 import { ChoiceField, ChoiceFieldProps } from './ChoiceField'
 
 export interface SelectFieldPublicProps {

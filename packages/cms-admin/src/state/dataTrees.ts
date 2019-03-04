ab58e5a6f0c201d32aab30b5074122dfa52acb9d
Jirka Vebr <jirkavebr@gmail.com>
2019-03-04 09:57:30 +0100

    Move DataTree delta types

--- packages/cms-admin/src/state/dataTrees.ts
+++ packages/cms-admin/src/state/dataTrees.ts
@@ -4,16 +4,6 @@ export type DataTreeDirtinessState = boolean
 
 export type DataTreeMutationState = boolean
 
-export interface DataTreeDirtinessDelta {
-	dataTreeId: DataTreeId
-	isDirty: DataTreeDirtinessState
-}
-
-export interface DataTreeMutationStateDelta {
-	dataTreeId: DataTreeId
-	isMutating: DataTreeMutationState
-}
-
 export interface DataTreeState {
 	isDirty: DataTreeDirtinessState
 	isMutating: DataTreeMutationState

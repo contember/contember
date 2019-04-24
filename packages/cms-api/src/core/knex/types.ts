6e21235f98b7c98e6f0eaf2d796ce9ed667ef857
David Matejka <matej21@matej21.cz>
2019-04-24 10:35:16 +0200

    drop dead code

--- packages/cms-api/src/core/knex/types.ts
+++ packages/cms-api/src/core/knex/types.ts
@@ -10,24 +10,6 @@ export type Value =
 	| Array<boolean>
 	| Buffer
 
-export interface Formatter {
-	bindings: Array<any>
-
-	wrap(value: any): string
-
-	wrapString(value: string): string
-
-	columnize(target: any): string
-
-	parametrize(values: any): string
-
-	values(values: Array<any>): string
-
-	parameter(values: any): string
-
-	operator(values: string): string
-}
-
 export interface Raw {
 	sql: string
 	bindings: Value[]

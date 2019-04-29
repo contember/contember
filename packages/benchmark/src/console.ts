2423ed7354633e3d6eb9c79ebe39a792e8d28827
David Matejka <matej21@matej21.cz>
2019-04-29 18:00:36 +0200

    update benchmark init

---
+++ packages/benchmark/src/console.ts
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { CompositionRoot, readConfig } from 'cms-api'
+import * as path from 'path'
+import App from './projects/app/src/model'
+
+;(async () => {
+	const configFile = path.join(__dirname, '../../src/config/config.yaml')
+	const config = await readConfig(configFile)
+	const projectsDirectory = path.join(__dirname, '../../src/projects')
+
+	const container = new CompositionRoot().createMasterContainer(config, projectsDirectory, {
+		app: App
+	})
+
+	container.cli.run(process.argv)
+})().catch(e => {
+	console.log(e)
+	process.exit(1)
+})

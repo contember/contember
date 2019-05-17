31fa00eead8d5c05ca55b402d6b8affaf5a46006
David Matejka <matej21@matej21.cz>
2019-05-17 18:10:56 +0200

    add sql migrations dry runner

---
+++ packages/cms-api/src/cli/DryRunCommand.ts
@@ -0,0 +1,37 @@
+import CommandConfiguration from '../core/cli/CommandConfiguration'
+import { ProjectContainer } from '../CompositionRoot'
+import Command from '../core/cli/Command'
+
+type Args = {
+	project: string
+	migration?: string
+}
+
+class DryRunCommand extends Command<Args, {}> {
+	constructor(private readonly projectContainers: ProjectContainer[]) {
+		super()
+	}
+
+	protected configure(configuration: CommandConfiguration): void {
+		configuration.description('Show SQL executed by a migration')
+		configuration.argument('project')
+		configuration.argument('migration').optional()
+	}
+
+	protected async execute(input: Command.Input<Args, {}>): Promise<void> {
+		const projectSlug = input.getArgument('project')
+		const projectContainer = this.projectContainers.find(it => it.project.slug === projectSlug)
+		if (!projectContainer) {
+			throw new Error(`Undefined project ${projectSlug}`)
+		}
+		const container = projectContainer.systemExecutionContainerFactory.create(projectContainer.systemDbClient)
+		const sql = await container.migrationSqlDryRunner.getSql(input.getArgument('migration'))
+		if (!sql.trim()) {
+			console.log('No SQL to execute')
+		} else {
+			console.log(sql)
+		}
+	}
+}
+
+export default DryRunCommand

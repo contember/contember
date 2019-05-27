9886c244406243dafdab82fe4058d91c5f2e4653
David Matejka <matej21@matej21.cz>
2019-05-27 16:04:45 +0200

    api: export schema with validation rules

---
+++ packages/cms-api/src/content-api/graphQLSchema/ContentSchemaFactory.ts
@@ -0,0 +1,84 @@
+import { GraphQLSchema } from 'graphql'
+import { makeExecutableSchema } from 'graphql-tools'
+import schema from './content-schema.graphql'
+import * as ContentSchema from './content-schema.types'
+import { assertNever, Schema, Validation } from 'cms-common'
+
+export class ContentSchemaFactory {
+	constructor(private readonly schema: Schema) {}
+
+	createFieldSchema(entityName: string, fieldName: string): ContentSchema._Field {
+		const rules = (this.schema.validation[entityName] || {})[fieldName] || []
+		const validators: ContentSchema._Validator[] = []
+
+		const createValue = (value: number | string | boolean): ContentSchema._AnyValue & { __typename: string } => {
+			if (value === undefined) {
+				return { __typename: '_UndefinedValue', undefinedValue: true }
+			}
+			if (typeof value === 'string') {
+				return { __typename: '_StringValue', stringValue: value }
+			}
+			if (typeof value === 'number') {
+				return value % 1 === 0
+					? { __typename: '_IntValue', intValue: value }
+					: { __typename: '_FloatValue', floatValue: value }
+			}
+			if (typeof value === 'boolean') {
+				return { __typename: '_BooleanValue', booleanValue: value }
+			}
+			throw new Error(`Argument of type ${typeof value} is not supported yet`)
+		}
+
+		const processValidator = (validator: Validation.Validator) => {
+			const i = validators.length
+			const args: (ContentSchema._Argument & { __typename: string })[] = []
+			const apiValidator: ContentSchema._Validator = { operation: validator.operation, arguments: args }
+			validators.push(apiValidator)
+			for (const arg of validator.args) {
+				switch (arg.type) {
+					case 'path':
+						args.push({ __typename: '_PathArgument', path: arg.path })
+						break
+					case 'literal':
+						args.push({ __typename: '_LiteralArgument', value: createValue(arg.value) })
+						break
+					case 'validator':
+						args.push({ __typename: '_ValidatorArgument', validator: processValidator(arg.validator) })
+						break
+					default:
+						assertNever(arg)
+				}
+			}
+
+			return i
+		}
+
+		const processedRules = rules.map(
+			(it): ContentSchema._Rule => ({ message: it.message, validator: processValidator(it.validator) })
+		)
+		return {
+			name: fieldName,
+			rules: processedRules,
+			validators: validators,
+		}
+	}
+
+	public create(): GraphQLSchema {
+		return makeExecutableSchema({
+			typeDefs: schema,
+			resolvers: {
+				Query: {
+					schema: (): ContentSchema._Schema => ({
+						enums: Object.entries(this.schema.model.enums).map(([name, values]) => ({ name, values })),
+						entities: Object.values(this.schema.model.entities).map(entity => ({
+							name: entity.name,
+							fields: Object.values(entity.fields).map(
+								(field): ContentSchema._Field => this.createFieldSchema(entity.name, field.name)
+							),
+						})),
+					}),
+				},
+			},
+		})
+	}
+}

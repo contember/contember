d0629dc4507d4469209442ac1be3c2b08ed5edb7
Jirka Vebr <jirkavebr@gmail.com>
2019-06-04 12:31:04 +0200

    Add SimpleRelativeSingleField

---
+++ packages/cms-admin/src/binding/facade/aux/SimpleRelativeSingleField.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react'
+import { Field, FieldPublicProps, Props } from '../../coreComponents'
+import { Environment } from '../../dao'
+import { QueryLanguage } from '../../queryLanguage'
+import { Component } from './Component'
+
+export const SimpleRelativeSingleField = function<P extends FieldPublicProps>(
+	render: React.FunctionComponent<P>,
+	displayName: string
+) {
+	return Component<P>(render, displayName, (props: Props<P>, environment: Environment) =>
+		QueryLanguage.wrapRelativeSingleField(
+			props.name,
+			fieldName => (
+				<Field
+					defaultValue={
+						props.defaultValue === undefined
+							? render.defaultProps
+								? render.defaultProps.defaultValue
+								: undefined
+							: props.defaultValue
+					}
+					name={fieldName}
+				/>
+			),
+			environment
+		)
+	)
+}

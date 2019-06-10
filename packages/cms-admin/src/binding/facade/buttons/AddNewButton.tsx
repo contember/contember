0910a87ce92ae5923169b4b1d4e69956dadb00da
Jirka Vebr <jirkavebr@gmail.com>
2019-06-10 14:30:04 +0200

    AddNewButton: freeze during mutations

--- packages/cms-admin/src/binding/facade/buttons/AddNewButton.tsx
+++ packages/cms-admin/src/binding/facade/buttons/AddNewButton.tsx
@@ -2,6 +2,7 @@ import { Icon } from '@blueprintjs/core'
 import { IconName, IconNames } from '@blueprintjs/icons'
 import * as React from 'react'
 import { Button, ButtonProps } from '../../../components'
+import { MutationStateContext } from '../../coreComponents/PersistState'
 import { EntityCollectionAccessor } from '../../dao'
 
 export interface AddNewButtonProps extends React.PropsWithChildren<ButtonProps> {
@@ -10,10 +11,11 @@ export interface AddNewButtonProps extends React.PropsWithChildren<ButtonProps> 
 }
 
 export const AddNewButton = React.memo((props: AddNewButtonProps) => {
+	const isMutating = React.useContext(MutationStateContext)
 	const { addNew, icon, ...rest } = props
 	if (addNew) {
 		return (
-			<Button onClick={addNew} small {...rest}>
+			<Button onClick={addNew} disabled={isMutating} small {...rest}>
 				<Icon icon={icon || IconNames.ADD} />
 				{props.children || 'Add'}
 			</Button>

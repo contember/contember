c750c7ef90a97e7d617adb41dc05ea0a32c632a1
David Matejka <matej21@matej21.cz>
2019-06-12 10:41:13 +0200

    fix tests

--- packages/cms-api/tests/src/testUuid.ts
+++ packages/cms-api/tests/src/testUuid.ts
@@ -1,4 +1,17 @@
+import sinon from 'sinon'
+import * as uuid from '../../src/utils/uuid'
+
 export const testUuidPrefix = '123e4567-e89b-12d3-a456-'
 export const testUuid = (number: number) => {
 	return testUuidPrefix + number.toString().padStart(12, '0')
+}
+
+export const withMockedUuid = async <R>(cb: () => R | Promise<R>): Promise<R> => {
+	let id = 1
+	const uuidStub = sinon.stub(uuid, 'uuid').callsFake(() => testUuid(id++))
+	try {
+		return await cb()
+	} finally {
+		uuidStub.restore()
+	}
 }

63abc9c53044c61920ebe11e37cdbde7a4b28a08
David Matejka <matej21@matej21.cz>
2019-06-12 14:43:32 +0200

    validation: add some tests

--- packages/cms-api/tests/example-project/src/index.ts
+++ packages/cms-api/tests/example-project/src/index.ts
@@ -2,10 +2,12 @@ import { Schema } from 'cms-common'
 import { emptySchema } from '../../../src/content-schema/schemaUtils'
 import * as definitions from './model'
 import { createModel } from '../../../src/content-schema/definition'
+import { parseDefinition } from '../../../src/content-api/input-validation'
 
 const model = createModel(definitions)
 const schema: Schema = {
 	...emptySchema,
+	validation: parseDefinition(definitions),
 	model,
 }
 

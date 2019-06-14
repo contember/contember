264cce314ede6f0a15020bf4ef00b2f7367fbf4a
Jirka Vebr <jirkavebr@gmail.com>
2019-06-14 13:49:37 +0200

    Add TokenExposer

---
+++ packages/cms-admin/src/components/Dev/TokenExposer.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react'
+import { connect } from 'react-redux'
+import State from '../../state'
+import { Button } from '../ui'
+import { isDevMode } from './isDevMode'
+
+interface TokenExposerStateProps {
+	token: string | undefined
+}
+
+interface TokenExposerOwnProps {}
+
+interface TokenExposerProps extends TokenExposerStateProps, TokenExposerOwnProps {}
+
+const TokenExposerComponent = (props: TokenExposerProps) => {
+	const [isCopying, setIsCopying] = React.useState(false)
+
+	if (!navigator.clipboard || !isDevMode()) {
+		return null
+	}
+
+	return (
+		<Button
+			onClick={_ => {
+				if (isCopying) {
+					return
+				}
+				if (!props.token) {
+					alert('Error: token unavailable!')
+				}
+				setIsCopying(true)
+				navigator.clipboard
+					.writeText(`{"Authorization": "Bearer ${props.token}"}`)
+					.then(() => {
+						alert('Copied!')
+					})
+					.catch(() => {
+						alert('Error!')
+					})
+					.finally(() => {
+						setIsCopying(false)
+					})
+			}}
+		>
+			Copy authenticated header
+		</Button>
+	)
+}
+
+export const TokenExposer = connect<TokenExposerStateProps, {}, TokenExposerOwnProps, State>(state => {
+	return {
+		token: state.auth.identity ? state.auth.identity.token : undefined
+	}
+})(TokenExposerComponent)

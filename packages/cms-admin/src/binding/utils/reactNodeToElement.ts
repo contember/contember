c58130ec0aebfbbaa938feab4ed06447129271d6
Jirka Vebr <jirkavebr@gmail.com>
2019-06-20 12:42:00 +0200

    reactNodeToElement

---
+++ packages/cms-admin/src/binding/utils/reactNodeToElement.ts
@@ -0,0 +1,16 @@
+import { isEmptyObject } from 'cms-common'
+import * as React from 'react'
+
+export const reactNodeToElement = function(node: React.ReactNode): React.ReactElement | null {
+	if (
+		node === undefined ||
+		typeof node === 'string' ||
+		typeof node === 'number' ||
+		typeof node === 'boolean' ||
+		(typeof node === 'object' && node !== null && isEmptyObject(node))
+	) {
+		return null
+	}
+
+	return node as React.ReactElement | null
+}

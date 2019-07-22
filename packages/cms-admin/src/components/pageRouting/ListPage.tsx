e8e2e32a9d06030c1fa21cbcda83a77fafd28117
David Matejka <matej21@matej21.cz>
2019-07-22 14:22:23 +0200

    list page: support filter, orderBy, limit and offset

--- packages/cms-admin/src/components/pageRouting/ListPage.tsx
+++ packages/cms-admin/src/components/pageRouting/ListPage.tsx
@@ -1,11 +1,18 @@
-import { lcfirst } from 'cms-common'
+import { Input, lcfirst } from 'cms-common'
 import * as React from 'react'
 import { DataRendererProps, EntityListDataProvider } from '../../binding/coreComponents'
 import { CommonRendererProps, ListRenderer } from '../../binding/facade/renderers'
 import PageWithLayout from './PageWithLayout'
 import SpecificPageProps from './SpecificPageProps'
+import { Filter } from '../../binding'
+import { CrudQueryBuilder } from 'cms-client'
 
-interface ListPageProps<DRP> extends SpecificPageProps<DRP> {}
+interface ListPageProps<DRP> extends SpecificPageProps<DRP> {
+	filter?: string | Filter
+	orderBy?: Input.OrderBy<CrudQueryBuilder.OrderDirection>[]
+	offset?: number
+	limit?: number
+}
 
 export default class ListPage<DRP extends CommonRendererProps> extends React.Component<ListPageProps<DRP>> {
 	static getPageName(props: ListPageProps<DataRendererProps>) {
@@ -19,6 +26,10 @@ export default class ListPage<DRP extends CommonRendererProps> extends React.Com
 					entityName={this.props.entity}
 					renderer={this.props.renderer || (ListRenderer as React.ComponentClass<DRP & DataRendererProps>)}
 					rendererProps={this.props.rendererProps}
+					filter={this.props.filter}
+					orderBy={this.props.orderBy}
+					offset={this.props.offset}
+					limit={this.props.limit}
 				>
 					{this.props.children}
 				</EntityListDataProvider>

80accd833ea256c9f03b57b12a997a808a33d79a
Filip Chalupa <chalupa.filip@gmail.com>
2019-07-26 18:33:52 +0200

    Avatar: gravatar

--- packages/cms-admin/src/components/ui/MenuPageLinkPrimary.tsx
+++ packages/cms-admin/src/components/ui/MenuPageLinkPrimary.tsx
@@ -3,7 +3,6 @@ import PageLink, { PageLinkProps } from '../pageRouting/PageLink'
 import { Avatar, AvatarShape, AvatarSize } from './Avatar'
 
 export interface MenuPageLinkPrimaryProps {
-	avatar: React.ReactNode
 	name: React.ReactNode
 	note?: React.ReactNode
 	change: PageLinkProps<any>['change']
@@ -11,15 +10,13 @@ export interface MenuPageLinkPrimaryProps {
 
 export class MenuPageLinkPrimary extends React.PureComponent<MenuPageLinkPrimaryProps> {
 	render() {
-		const { avatar, name, note } = this.props
+		const { name, note } = this.props
 		return (
 			<PageLink
 				Component={props => (
 					<li className="menuPrimaryItem">
 						<a {...props} className="menuPrimaryItem-link">
-							<Avatar shape={AvatarShape.Square} size={AvatarSize.Size2}>
-								{avatar}
-							</Avatar>
+							<Avatar shape={AvatarShape.Square} size={AvatarSize.Size2} />
 							<div className="menuPrimaryItem-texts">
 								<div className="menuPrimaryItem-name">{name}</div>
 								{note && <div className="menuPrimaryItem-note">{note}</div>}

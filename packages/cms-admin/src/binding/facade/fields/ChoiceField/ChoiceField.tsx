cb753528e922a792045506735baa7ed7e32c46d0
Jirka Vebr <jirkavebr@gmail.com>
2019-07-30 18:22:16 +0200

    Split the behemoth of a file ChoiceField.tsx into multiple smaller ones

---
+++ packages/cms-admin/src/binding/facade/fields/ChoiceField/ChoiceField.tsx
@@ -0,0 +1,155 @@
+import { GraphQlBuilder } from 'cms-client'
+import * as React from 'react'
+import { FieldName, PRIMARY_KEY_NAME, Scalar } from '../../../bindingTypes'
+import {
+	EnforceSubtypeRelation,
+	EntityListDataProvider,
+	Field,
+	FieldMetadata,
+	SyntheticChildrenProvider,
+	ToMany,
+	ToOne
+} from '../../../coreComponents'
+import { EntityAccessor, Environment, Literal, VariableLiteral, VariableScalar } from '../../../dao'
+import { QueryLanguage } from '../../../queryLanguage'
+import { DynamicChoiceField, DynamicChoiceFieldProps } from './DynamicChoiceField'
+import { StaticChoiceField, StaticChoiceFieldProps } from './StaticChoiceField'
+
+export enum ChoiceArity {
+	Single = 'single',
+	Multiple = 'multiple'
+}
+
+export interface ChoiceFieldPublicProps {
+	name: FieldName
+}
+
+export interface BaseChoiceMetadata extends Omit<FieldMetadata, 'data'> {
+	data: ChoiceField.Data<ChoiceField.DynamicValue | ChoiceField.StaticValue>
+}
+
+export interface SingleChoiceFieldMetadata extends BaseChoiceMetadata {
+	currentValue: ChoiceField.ValueRepresentation
+	onChange: (newValue: ChoiceField.ValueRepresentation) => void
+}
+
+export interface MultipleChoiceFieldMetadata extends BaseChoiceMetadata {
+	currentValues: Array<ChoiceField.ValueRepresentation>
+	onChange: (optionKey: ChoiceField.ValueRepresentation, isChosen: boolean) => void
+}
+
+export type ChoiceFieldBaseProps = ChoiceFieldPublicProps &
+	(
+		| {
+				arity: ChoiceArity.Single
+				children: (metadata: SingleChoiceFieldMetadata) => React.ReactNode
+		  }
+		| {
+				arity: ChoiceArity.Multiple
+				children: (metadata: MultipleChoiceFieldMetadata) => React.ReactNode
+		  })
+
+export type ChoiceFieldProps = ChoiceFieldBaseProps & {
+	options: StaticChoiceFieldProps['options'] | DynamicChoiceFieldProps['options']
+}
+
+class ChoiceField extends React.PureComponent<ChoiceFieldProps> {
+	public static displayName = 'ChoiceField'
+
+	public render() {
+		return (
+			<Field.DataRetriever name={this.props.name}>
+				{rawMetadata => {
+					// Unfortunately, the "any" type is necessary because the TS inference otherwise fails here for some reason.
+					const commonProps: any = {
+						rawMetadata,
+						name: this.props.name,
+						options: this.props.options,
+						arity: this.props.arity,
+						children: this.props.children
+					}
+
+					if (Array.isArray(this.props.options)) {
+						return <StaticChoiceField {...commonProps} />
+					}
+					return <DynamicChoiceField {...commonProps} />
+				}}
+			</Field.DataRetriever>
+		)
+	}
+
+	public static generateSyntheticChildren(
+		props: Omit<ChoiceFieldProps, 'children'>,
+		environment: Environment
+	): React.ReactNode {
+		if (Array.isArray(props.options)) {
+			return QueryLanguage.wrapRelativeSingleField(props.name, fieldName => <Field name={fieldName} />, environment)
+		}
+
+		const metadata = QueryLanguage.wrapQualifiedFieldList(
+			props.options,
+			fieldName => <Field name={fieldName} />,
+			environment
+		)
+
+		return QueryLanguage.wrapRelativeSingleField(
+			props.name,
+			fieldName => (
+				<>
+					<EntityListDataProvider
+						entityName={metadata.entityName}
+						filter={metadata.filter}
+						associatedField={props.name}
+					>
+						{metadata.children}
+					</EntityListDataProvider>
+					{props.arity === ChoiceArity.Single && (
+						<ToOne field={fieldName}>
+							<Field name={PRIMARY_KEY_NAME} />
+						</ToOne>
+					)}
+					{props.arity === ChoiceArity.Multiple && (
+						<ToMany field={fieldName}>
+							<Field name={PRIMARY_KEY_NAME} />
+						</ToMany>
+					)}
+				</>
+			),
+			environment
+		)
+	}
+}
+
+namespace ChoiceField {
+	export type Label = React.ReactNode
+
+	export type ScalarValue = Scalar | VariableScalar
+
+	export type LiteralValue = VariableLiteral | Literal
+
+	export type StaticValue = GraphQlBuilder.Literal | Scalar
+
+	export type DynamicValue = EntityAccessor['primaryKey']
+
+	// This is just the JS array index as specified in options or as returned from the server.
+	export type ValueRepresentation = number
+
+	export interface SingleDatum<ActualValue extends Environment.Value = string> {
+		key: ValueRepresentation
+		label: Label
+		actualValue: ActualValue
+	}
+
+	export type Data<ActualValue extends Environment.Value = string> = SingleDatum<ActualValue>[]
+
+	export type InnerBaseProps = ChoiceFieldBaseProps & {
+		rawMetadata: Field.RawMetadata
+	}
+}
+
+type EnforceDataBindingCompatibility = EnforceSubtypeRelation<
+	typeof ChoiceField,
+	SyntheticChildrenProvider<ChoiceFieldProps>
+>
+
+export { ChoiceField }
---
+++ packages/cms-admin/src/binding/facade/fields/ChoiceField/StaticChoiceField.tsx
@@ -0,0 +1,98 @@
+import { GraphQlBuilder } from 'cms-client'
+import * as React from 'react'
+import { Scalar } from '../../../bindingTypes'
+import { Field } from '../../../coreComponents'
+import { DataBindingError, Environment, Literal, VariableLiteral, VariableScalar } from '../../../dao'
+import { VariableInputTransformer } from '../../../model/VariableInputTransformer'
+import { ChoiceArity, ChoiceField, ChoiceFieldProps } from './ChoiceField'
+
+export type StaticChoiceFieldProps = ChoiceField.InnerBaseProps & {
+	options: Array<[ChoiceField.LiteralValue, ChoiceField.Label]> | Array<[ChoiceField.ScalarValue, ChoiceField.Label]>
+}
+
+export class StaticChoiceField extends React.PureComponent<StaticChoiceFieldProps> {
+	public render() {
+		if (this.props.arity === ChoiceArity.Multiple) {
+			throw new DataBindingError('Static multiple-choice choice fields are not supported yet.')
+		}
+
+		const rawOptions = this.props.options
+		const children = this.props.children
+
+		if (rawOptions.length === 0) {
+			return null
+		}
+
+		const environment = this.props.rawMetadata.environment
+		const options: Array<[GraphQlBuilder.Literal | Scalar, ChoiceField.Label]> = this.isLiteralStaticMode(rawOptions)
+			? this.normalizeLiteralStaticOptions(rawOptions, environment)
+			: this.normalizeScalarStaticOptions(rawOptions, environment)
+
+		return (
+			<Field name={this.props.rawMetadata.fieldName}>
+				{({ data, ...otherMetadata }): React.ReactNode => {
+					if (this.props.arity === ChoiceArity.Multiple) {
+						throw new DataBindingError('Static multiple-choice choice fields are not supported yet.')
+					}
+
+					const currentValue: ChoiceField.ValueRepresentation = options.findIndex(([value]) => {
+						return (
+							data.hasValue(value) ||
+							(value instanceof GraphQlBuilder.Literal &&
+								typeof data.currentValue === 'string' &&
+								value.value === data.currentValue)
+						)
+					}, null)
+
+					return children({
+						data: options.map((item, i) => ({
+							key: i,
+							label: item[1],
+							actualValue: item[0]
+						})),
+						currentValue,
+						onChange: (newValue: ChoiceField.ValueRepresentation) => {
+							data.onChange && data.onChange(options[newValue][0])
+						},
+						...otherMetadata
+					})
+				}}
+			</Field>
+		)
+	}
+
+	private normalizeLiteralStaticOptions(
+		options: Array<[ChoiceField.LiteralValue, ChoiceField.Label]>,
+		environment: Environment
+	): Array<[GraphQlBuilder.Literal, ChoiceField.Label]> {
+		return options.map(
+			([key, value]): [GraphQlBuilder.Literal, ChoiceField.Label] => [
+				key instanceof VariableLiteral ? VariableInputTransformer.transformVariableLiteral(key, environment) : key,
+				value
+			]
+		)
+	}
+
+	private normalizeScalarStaticOptions(
+		options: Array<[ChoiceField.ScalarValue, ChoiceField.Label]>,
+		environment: Environment
+	): Array<[Scalar, ChoiceField.Label]> {
+		return options.map(
+			([key, value]): [Scalar, ChoiceField.Label] => [
+				key instanceof VariableScalar ? VariableInputTransformer.transformVariableScalar(key, environment) : key,
+				value
+			]
+		)
+	}
+
+	private isLiteralStaticMode(
+		options: ChoiceFieldProps['options']
+	): options is Array<[ChoiceField.LiteralValue, ChoiceField.Label]> {
+		if (options.length === 0) {
+			return false
+		}
+
+		const optionIndicator = options[0][0]
+		return optionIndicator instanceof VariableLiteral || optionIndicator instanceof Literal
+	}
+}

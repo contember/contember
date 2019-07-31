77150301fb47836ba2df808dc2cdfca8010eec7a
Jirka Vebr <jirkavebr@gmail.com>
2019-07-31 15:00:56 +0200

    Add a rudimentary checkbox ui component

---
+++ packages/cms-admin/src/components/ui/Checkbox.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+
+export interface CheckboxProps {
+	checked: boolean
+	readOnly: boolean
+	label: React.ReactNode
+	onChange: (isChecked: boolean) => void
+}
+
+export const Checkbox = React.memo((props: CheckboxProps) => (
+	<div>
+		<label>
+			<input
+				type="checkbox"
+				checked={props.checked}
+				readOnly={props.readOnly}
+				onChange={e => props.onChange(e.currentTarget.checked)}
+			/>
+			{props.label}
+		</label>
+	</div>
+))

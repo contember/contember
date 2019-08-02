7d8477bb11d83f3138c242051c252ad459fdb965
Filip Chalupa <chalupa.filip@gmail.com>
2019-08-02 14:53:01 +0200

    Revert "Form group: basic clickable label"
    This reverts commit d4c2a66d138b1038f473e4269de2f8ee46203951.
    
    # Conflicts:
    #	packages/cms-admin/src/components/ui/FormGroup.tsx

--- packages/cms-admin/src/components/ui/FormGroup.tsx
+++ packages/cms-admin/src/components/ui/FormGroup.tsx
@@ -14,11 +14,11 @@ export class FormGroup extends React.PureComponent<FormGroupProps> {
 		const { label, children, horizontal } = this.props
 
 		return (
-			<label className={cn('formGroup', horizontal && 'formGroup-horizontal')}>
+			<div className={cn('formGroup', horizontal && 'formGroup-horizontal')}>
 				{label && <div className="formGroup-label">{label}</div>}
 				<FormErrors errors={this.props.errors} />
-				<div className="formGroup-field"> {children}</div>
-			</label>
+				<div className="formGroup-field">{children}</div>
+			</div>
 		)
 	}
 }

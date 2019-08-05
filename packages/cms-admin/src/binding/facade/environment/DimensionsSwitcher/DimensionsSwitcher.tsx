7fa2744397cfe0e1f6dee5c934157ffee8645ee2
Jirka Vebr <jirkavebr@gmail.com>
2019-08-05 14:36:38 +0200

    DimensionsSwitcher: new API

--- packages/cms-admin/src/binding/facade/environment/DimensionsSwitcher/DimensionsSwitcher.tsx
+++ packages/cms-admin/src/binding/facade/environment/DimensionsSwitcher/DimensionsSwitcher.tsx
@@ -3,14 +3,19 @@ import {
 	DataBindingError,
 	EntityListDataProvider,
 	EntityListDataProviderProps,
-	EnvironmentContext,
+	Environment,
+	Field,
 	FieldText
 } from '../../../index'
 import { QueryLanguage } from '../../../queryLanguage'
 import { DimensionsRenderer, DimensionsRendererProps } from './DimensionsRenderer'
 
-export interface DimensionsSwitcherBaseProps extends Omit<DimensionsRendererProps, 'labelFactory'> {
-	options: string
+export interface DimensionsSwitcherBaseProps
+	extends Omit<DimensionsRendererProps, 'labelFactory' | 'minItems' | 'maxItems'> {
+	optionEntities: string
+	minItems?: number
+	maxItems?: number
+	labelField: string
 	orderBy?: EntityListDataProviderProps<unknown>['orderBy']
 }
 
@@ -19,26 +24,33 @@ export interface DimensionsSwitcherProps extends DimensionsSwitcherBaseProps {
 }
 
 export const DimensionsSwitcher: React.FunctionComponent<DimensionsSwitcherProps> = props => {
-	if (props.minItems > props.maxItems) {
+	const minItems = props.minItems === undefined ? 1 : props.minItems
+	const maxItems = props.maxItems === undefined ? 2 : props.maxItems
+
+	if (minItems > maxItems) {
 		throw new DataBindingError(
 			`DimensionSwitcher: 'minItems' for dimension ${props.dimension} must be no greater than 'maxItems'.`
 		)
 	}
-	if (props.minItems < 1) {
+	if (minItems < 1) {
 		throw new DataBindingError(`DimensionSwitcher: 'minItems' for dimension ${props.dimension} must be at least 1.`)
 	}
-	if (props.defaultValue.length < props.minItems || props.defaultValue.length > props.maxItems) {
+	if (props.defaultValue.length < minItems || props.defaultValue.length > maxItems) {
 		throw new DataBindingError(
 			`DimensionSwitcher: the number of default values for dimension ${props.dimension} must not be between` +
 				`'minItems' and 'maxItems'.`
 		)
 	}
 
-	const environment = React.useContext(EnvironmentContext)
-	const children = props.children
-	const metadata: QueryLanguage.WrappedQualifiedEntityList | QueryLanguage.WrappedQualifiedFieldList = children
-		? QueryLanguage.wrapQualifiedEntityList(props.options, children, environment)
-		: QueryLanguage.wrapQualifiedFieldList(props.options, fieldName => <FieldText name={fieldName} />, environment)
+	const environment = new Environment()
+	const metadata = QueryLanguage.wrapQualifiedEntityList(
+		props.optionEntities,
+		<>
+			{props.children || <FieldText name={props.labelField} />}
+			<Field name={props.slugField} />
+		</>,
+		environment
+	)
 
 	return (
 		<EntityListDataProvider<DimensionsRendererProps>
@@ -51,21 +63,15 @@ export const DimensionsSwitcher: React.FunctionComponent<DimensionsSwitcherProps
 				defaultValue: props.defaultValue,
 				dimension: props.dimension,
 				labelFactory: metadata.children,
-				minItems: props.minItems,
-				maxItems: props.maxItems,
+				minItems: minItems,
+				maxItems: maxItems,
 				renderSelected: props.renderSelected,
 				slugField: props.slugField
 			}}
 		>
 			{metadata.children}
-			<FieldText name={props.slugField} />
 		</EntityListDataProvider>
 	)
 }
 
 DimensionsSwitcher.displayName = 'DimensionsSwitcher'
-
-DimensionsSwitcher.defaultProps = {
-	minItems: 1,
-	maxItems: 2
-}

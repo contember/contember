1d15d60ca0ed5e9f992aee72971ded430f5dede6
Jirka Vebr <jirkavebr@gmail.com>
2019-08-13 15:51:39 +0200

    Fix storybook

--- packages/ui/storybook/components/Spinner.tsx
+++ packages/ui/storybook/components/Spinner.tsx
@@ -1,5 +1,26 @@
+import { number, withKnobs } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import { Spinner } from '../../src'
 
-storiesOf('Spinner', module).add('simple', () => <Spinner />)
+const stories = storiesOf('Spinner', module)
+stories.addDecorator(withKnobs)
+
+stories.add('simple', () => {
+	const fontSize = number('Font size', 16, {
+		range: true,
+		min: 16,
+		max: 160,
+		step: 1,
+	})
+
+	return (
+		<div
+			style={{
+				fontSize: `${fontSize / 16}rem`,
+			}}
+		>
+			<Spinner />
+		</div>
+	)
+})
--- packages/ui/storybook/config/webpack.config.ts
+++ packages/ui/storybook/config/webpack.config.ts
@@ -1,39 +1,22 @@
-module.exports = (storybookBaseConfig: any, configType: string, config: any) => {
-	const defaultConfig = config
-	return {
-		...defaultConfig,
-		module: {
-			...defaultConfig.module,
-			rules: [
-				// Temp fix for issue: https://github.com/storybooks/storybook/issues/3346
-				...defaultConfig.module.rules.filter(
-					(rule: any) =>
-						!(rule.use && rule.use.length && rule.use.find(({ loader }: any) => loader === 'babel-loader')),
-				),
-				// .filter(
-				// 	(rule: any) =>
-				// 		!(rule.use && rule.use.length && rule.use.find(({ loader }: any) => loader === 'css-loader'))
-				// ),
-				{
-					test: /\.jsx?$/,
-					include: require('path').resolve('./'),
-					exclude: /(node_modules|dist)/,
-					loader: 'babel-loader',
-				},
-				{
-					test: /\.jsx?$/,
-					exclude: /node_modules/,
-					loader: 'source-map-loader',
-					enforce: 'pre',
-				},
-				{
-					test: /\.((s*)css|sass)$/,
-					use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'],
-					exclude: /node_modules/,
-					include: require('path').resolve('./'),
-				},
-			],
+module.exports = ({ config }: any) => {
+	// https://github.com/storybookjs/storybook/issues/3346
+	config.module.rules = config.module.rules.filter(
+		(rule: any) => !(rule.use && rule.use.length && rule.use.find(({ loader }: any) => loader === 'babel-loader')),
+	)
+	config.module.rules.push({
+		test: /\.(ts|tsx)$/,
+		loader: require.resolve('babel-loader'),
+		options: {
+			presets: [['react-app', { flow: false, typescript: true }]],
 		},
-	}
+	})
+	config.resolve.extensions.push('.ts', '.tsx')
+	config.module.rules.push({
+		test: /\.((s*)css|sass)$/,
+		use: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'],
+		exclude: /node_modules/,
+		include: require('path').resolve('./'),
+	})
+
+	return config
 }
-// npm i -D style-loader css-loader resolve-url-loader sass-loader node-sass source-map-loader

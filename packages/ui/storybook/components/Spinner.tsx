d3cd92a941b775236b306884b89b1325290cedd6
Jirka Vebr <jirkavebr@gmail.com>
2019-08-13 16:06:20 +0200

    Use knobs globally

--- packages/ui/storybook/components/Spinner.tsx
+++ packages/ui/storybook/components/Spinner.tsx
@@ -1,26 +1,24 @@
-import { number, withKnobs } from '@storybook/addon-knobs'
+import { number } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import { Spinner } from '../../src'
 
-const stories = storiesOf('Spinner', module)
-stories.addDecorator(withKnobs)
+storiesOf('Spinner', module)
+	.add('simple', () => {
+		const fontSize = number('Font size', 16, {
+			range: true,
+			min: 16,
+			max: 160,
+			step: 1,
+		})
 
-stories.add('simple', () => {
-	const fontSize = number('Font size', 16, {
-		range: true,
-		min: 16,
-		max: 160,
-		step: 1,
+		return (
+			<div
+				style={{
+					fontSize: `${fontSize / 16}rem`,
+				}}
+			>
+				<Spinner />
+			</div>
+		)
 	})
-
-	return (
-		<div
-			style={{
-				fontSize: `${fontSize / 16}rem`,
-			}}
-		>
-			<Spinner />
-		</div>
-	)
-})

cf36ac8fe494ed410093feb9e23e5325a780febd
Jirka Vebr <jirkavebr@gmail.com>
2019-08-15 17:40:43 +0200

    Basic button styles & vars

---
+++ packages/ui/storybook/components/Button.tsx
@@ -0,0 +1,29 @@
+import { number, text } from '@storybook/addon-knobs'
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import { Button, ButtonProps } from '../../src'
+import { allIntents } from '../utils/intents'
+
+storiesOf('Button', module).add('simple', () => {
+	const fontSize = number('Font size', 16, {
+		range: true,
+		min: 16,
+		max: 64,
+		step: 1,
+	})
+	const props: Partial<ButtonProps> = {
+		children: text('Text', 'Pretty button'),
+	}
+
+	return (
+		<div
+			style={{
+				fontSize: `${fontSize / 16}rem`,
+			}}
+		>
+			{allIntents.map(intent => (
+				<Button intent={intent} {...props} />
+			))}
+		</div>
+	)
+})

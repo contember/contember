232b2eba29869967171b62af01d0d9ad81b25b2a
Jirka Vebr <jirkavebr@gmail.com>
2019-08-15 17:40:26 +0200

    Add intent types & utils

---
+++ packages/ui/storybook/utils/intents.ts
@@ -0,0 +1,5 @@
+import { Intent } from '../../src/types'
+
+export const intents: Intent[] = ['primary', 'secondary', 'tertiary', 'success', 'warn', 'danger', 'dark']
+
+export const allIntents = Array<undefined | Intent>(undefined).concat(intents)

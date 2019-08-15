e429537d4acec1fab35aef5b6927394a18c522e4
Filip Chalupa <chalupa.filip@gmail.com>
2019-08-16 01:10:24 +0200

    Navbar: avatar as Button

--- packages/cms-admin/src/components/LayoutDefault.tsx
+++ packages/cms-admin/src/components/LayoutDefault.tsx
@@ -7,6 +7,7 @@ import { Icon } from '@blueprintjs/core'
 import { default as PageLink } from './pageRouting/PageLink'
 import { connect } from 'react-redux'
 import State from '../state'
+import { Button } from '@contember/ui'
 
 export interface LayoutOwnProps {
 	header: {
@@ -85,9 +86,9 @@ class LayoutDefault extends React.PureComponent<LayoutOwnProps & LayoutStateProp
 						{this.props.header.right}
 						<Dropdown.Revealer
 							opener={
-								<button type="button">
+								<Button distinction="seamless">
 									<Avatar size={AvatarSize.Size2} email={this.props.identity} />
-								</button>
+								</Button>
 							}
 						>
 							<Dropdown>

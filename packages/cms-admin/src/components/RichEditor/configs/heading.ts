92ad6aea563c31f7c6503ff3bffb2f9272392cc3
Vojta Staněk <stanekv01@gmail.com>
2019-08-16 13:45:47 +0200

    Add second level heading

--- packages/cms-admin/src/components/RichEditor/configs/heading.ts
+++ packages/cms-admin/src/components/RichEditor/configs/heading.ts
@@ -1,3 +1,5 @@
 import { RichEditorPluginConfig, createBlockPluginConfig } from './utils'
 
-export const HEADING: RichEditorPluginConfig = createBlockPluginConfig('heading', 'h1')
+export const HEADING_H2: RichEditorPluginConfig = createBlockPluginConfig('heading', 'h2')
+export const HEADING = HEADING_H2
+export const HEADING_H3: RichEditorPluginConfig = createBlockPluginConfig('heading_h3', 'h3')

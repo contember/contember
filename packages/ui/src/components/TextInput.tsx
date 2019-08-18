282515885d1ac4721b681ec4271b871567c1308b
Jirka Vebr <jirkavebr@gmail.com>
2019-08-18 15:54:51 +0200

    Further improve controls

--- packages/ui/src/components/TextInput.tsx
+++ packages/ui/src/components/TextInput.tsx
@@ -2,7 +2,7 @@ import { ChangeEventHandler } from 'react'
 import * as React from 'react'
 import cn from 'classnames'
 import TextareaAutosize from 'react-textarea-autosize'
-import { Size } from '../types'
+import { ControlDistinction, Size } from '../types'
 import { toViewClass } from '../utils'
 
 interface TextAreaProps extends Omit<React.TextareaHTMLAttributes<HTMLTextAreaElement>, 'onChange'> {
@@ -17,28 +17,29 @@ interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, '
 export type TextInputProps = {
 	value: string
 	size?: Size
+	distinction?: ControlDistinction
 	onChange: (newValue: string) => void
 } & (TextAreaProps | InputProps)
 
 export const TextInput = React.memo(
-	React.forwardRef((props: TextInputProps, ref) => {
-		const finalClassName = cn('input', toViewClass(props.size), props.className)
+	React.forwardRef(({ className, size, distinction, onChange, ...otherProps }: TextInputProps, ref) => {
+		const finalClassName = cn('input', toViewClass(size), toViewClass(distinction), className)
 		const innerOnChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = event =>
-			props.onChange(event.target.value)
+			onChange(event.target.value)
 
-		if (props.allowNewlines) {
-			const { className, size, onChange, ...otherProps } = props
+		if (otherProps.allowNewlines) {
+			const { allowNewlines, ...textareaProps } = otherProps
 			return (
 				<TextareaAutosize
 					ref={ref as any}
 					className={finalClassName}
 					onChange={innerOnChange}
 					useCacheForDOMMeasurements
-					{...otherProps}
+					{...textareaProps}
 				/>
 			)
 		}
-		const { className, size, onChange, ...otherProps } = props
-		return <input ref={ref as any} type="text" className={finalClassName} onChange={innerOnChange} {...otherProps} />
+		const { allowNewlines, ...inputProps } = otherProps
+		return <input ref={ref as any} type="text" className={finalClassName} onChange={innerOnChange} {...inputProps} />
 	}),
 )
--- packages/ui/storybook/components/TextInput.tsx
+++ packages/ui/storybook/components/TextInput.tsx
@@ -1,4 +1,4 @@
-import { boolean, text } from '@storybook/addon-knobs'
+import { boolean, radios, text } from '@storybook/addon-knobs'
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import { TextInput } from '../../src/components'
@@ -6,9 +6,6 @@ import { sizeKnob } from '../utils/knobs'
 
 const SimpleTextInputStory = () => {
 	const [value, setValue] = React.useState('')
-	const allowNewlines = boolean('Allow newlines', false)
-	const size = sizeKnob()
-	const placeholder = text('Placeholder', 'Placeholder text')
 
 	// The cast is frankly just TS inference being insufficient but in practice that won't matter as the value of
 	// the 'allowNewlines' props will typically be a constant which TS handles just fine.
@@ -16,9 +13,18 @@ const SimpleTextInputStory = () => {
 		<TextInput
 			value={value}
 			onChange={newValue => setValue(newValue)}
-			size={size}
-			allowNewlines={allowNewlines as true}
-			placeholder={placeholder}
+			allowNewlines={boolean('Allow newlines', false) as true}
+			size={sizeKnob()}
+			placeholder={text('Placeholder', 'Placeholder text')}
+			readOnly={boolean('Read only', false)}
+			distinction={radios(
+				'Distinction',
+				{
+					Default: 'default',
+					Seamless: 'seamless',
+				},
+				'default',
+			)}
 		/>
 	)
 }

0e83a6f08403f2ea6cf0b0a48b0ef94564bd36a4
Jirka Vebr <jirkavebr@gmail.com>
2019-08-19 01:35:22 +0200

    RichEditor: avoid label hell

--- packages/cms-admin/src/binding/facade/fields/RichTextField.tsx
+++ packages/cms-admin/src/binding/facade/fields/RichTextField.tsx
@@ -1,8 +1,10 @@
 import { TextInputProps } from '@contember/ui'
 import * as React from 'react'
 import RichEditor, { LineBreakBehavior, RichEditorProps } from '../../../components/RichEditor'
+import { Field } from '../../coreComponents'
 import { FieldAccessor } from '../../dao'
-import { SimpleRelativeSingleField, SimpleRelativeSingleFieldProps } from '../auxiliary'
+import { QueryLanguage } from '../../queryLanguage'
+import { Component, SimpleRelativeSingleFieldProps } from '../auxiliary'
 
 export { LineBreakBehavior, Block, Mark } from '../../../components/RichEditor'
 
@@ -12,19 +14,40 @@ export type RichTextFieldProps = SimpleRelativeSingleFieldProps &
 		blocks?: RichEditorProps['blocks']
 	}
 
-export const RichTextField = SimpleRelativeSingleField<RichTextFieldProps, string>((fieldMetadata, props) => {
-	const generateOnChange = (data: FieldAccessor<string>) => (val: string) => {
-		data.updateValue && data.updateValue(val)
-	}
-	return (
-		<RichEditor
-			onChange={generateOnChange(fieldMetadata.data)}
-			value={fieldMetadata.data.currentValue || ''}
-			lineBreakBehavior={props.lineBreakBehavior}
-			label={fieldMetadata.environment.applySystemMiddleware('labelMiddleware', props.label)}
-			blocks={props.blocks}
-			readOnly={fieldMetadata.isMutating}
-			{...props}
-		/>
-	)
-}, 'RichTextField')
+export const RichTextField = Component<RichTextFieldProps>(
+	props => (
+		<Field<string> name={props.name} defaultValue={props.defaultValue}>
+			{fieldMetadata => {
+				const generateOnChange = (data: FieldAccessor<string>) => (val: string) => {
+					data.updateValue && data.updateValue(val)
+				}
+				return (
+					<RichEditor
+						onChange={generateOnChange(fieldMetadata.data)}
+						value={fieldMetadata.data.currentValue || ''}
+						lineBreakBehavior={props.lineBreakBehavior}
+						label={fieldMetadata.environment.applySystemMiddleware('labelMiddleware', props.label)}
+						blocks={props.blocks}
+						readOnly={fieldMetadata.isMutating}
+						{...props}
+					/>
+				)
+			}}
+		</Field>
+	),
+	(props, environment) => (
+		<>
+			{QueryLanguage.wrapRelativeSingleField(
+				props.name,
+				fieldName => (
+					<Field defaultValue={props.defaultValue} name={fieldName} />
+				),
+				environment,
+			)}
+			{props.label}
+			{props.labelDescription}
+			{props.description}
+		</>
+	),
+	'RichTextField',
+)

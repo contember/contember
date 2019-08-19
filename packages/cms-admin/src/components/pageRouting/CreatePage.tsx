277e1c54d1ffdadebf27d6ef76bb36d653c8f283
Jirka Vebr <jirkavebr@gmail.com>
2019-08-19 18:39:12 +0200

    Redirect after create

--- packages/cms-admin/src/components/pageRouting/CreatePage.tsx
+++ packages/cms-admin/src/components/pageRouting/CreatePage.tsx
@@ -2,9 +2,13 @@ import { lcfirst } from 'cms-common'
 import * as React from 'react'
 import { DataRendererProps, EntityCreator } from '../../binding/coreComponents'
 import { CommonRendererProps } from '../../binding/facade/renderers'
+import { RequestChange } from '../../state/request'
+import { DynamicLink } from '../DynamicLink'
 import { SpecificPageProps } from './SpecificPageProps'
 
-interface CreatePageProps<DRP> extends SpecificPageProps<DRP> {}
+interface CreatePageProps<DRP> extends SpecificPageProps<DRP> {
+	redirectOnSuccess?: RequestChange // TODO we cannot really redirect to an edit page of the newly-created entity.
+}
 
 export class CreatePage<DRP extends CommonRendererProps = CommonRendererProps> extends React.Component<
 	CreatePageProps<DRP>
@@ -14,10 +18,28 @@ export class CreatePage<DRP extends CommonRendererProps = CommonRendererProps> e
 	}
 
 	render(): React.ReactNode {
+		if (!this.props.redirectOnSuccess) {
+			return (
+				<EntityCreator name={this.props.entity} renderer={this.props.renderer} rendererProps={this.props.rendererProps}>
+					{this.props.children}
+				</EntityCreator>
+			)
+		}
+
 		return (
-			<EntityCreator name={this.props.entity} renderer={this.props.renderer} rendererProps={this.props.rendererProps}>
-				{this.props.children}
-			</EntityCreator>
+			<DynamicLink
+				requestChange={this.props.redirectOnSuccess}
+				Component={({ onClick }) => (
+					<EntityCreator
+						name={this.props.entity}
+						renderer={this.props.renderer}
+						rendererProps={this.props.rendererProps}
+						onSuccessfulPersist={onClick}
+					>
+						{this.props.children}
+					</EntityCreator>
+				)}
+			/>
 		)
 	}
 }

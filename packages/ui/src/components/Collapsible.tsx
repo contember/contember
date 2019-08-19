612a6463a25da01ccb598a14193c942bf533e26e
Filip Chalupa <chalupa.filip@gmail.com>
2019-08-19 15:37:21 +0200

    Collapsible: multiple transition types

--- packages/ui/src/components/Collapsible.tsx
+++ packages/ui/src/components/Collapsible.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react'
 import cn from 'classnames'
+import { CollapsibleTransition } from '../types'
+import { toStateClass, toViewClass } from '../utils'
 
 export interface CollapsibleProps {
 	expanded: boolean
+	transition?: CollapsibleTransition
 	children?: React.ReactNode
 }
 
-export const Collapsible = React.memo((props: CollapsibleProps) => {
+export const Collapsible = React.memo(({ transition = 'topInsert', ...otherProps }: CollapsibleProps) => {
 	const contentRef = React.useRef<HTMLDivElement>(null)
 	const [isTransitioning, setIsTransitioning] = React.useState(false)
 	const [contentHeight, setContentHeight] = React.useState('auto')
-	const [delayedExpanded, setDelayedExpanded] = React.useState(props.expanded)
+	const [delayedExpanded, setDelayedExpanded] = React.useState(otherProps.expanded)
 
 	const onTransitionEnd = () => {
 		setContentHeight('auto')
@@ -23,29 +26,34 @@ export const Collapsible = React.memo((props: CollapsibleProps) => {
 	}
 
 	React.useEffect(() => {
-		if (props.expanded !== delayedExpanded) {
+		if (otherProps.expanded !== delayedExpanded) {
 			setIsTransitioning(true)
 			updateContentHeight()
 			requestAnimationFrame(() => {
 				contentRef.current!.clientHeight // Force reflow
-				setDelayedExpanded(props.expanded)
+				setDelayedExpanded(otherProps.expanded)
 			})
 		}
-	}, [delayedExpanded, props.expanded])
+	}, [delayedExpanded, otherProps.expanded])
 
 	return (
 		<div
-			className={cn('collapsible', delayedExpanded && 'is-expanded', isTransitioning && 'is-transitioning')}
+			className={cn(
+				'collapsible',
+				toViewClass(`transition-${transition}`),
+				toStateClass('expanded', delayedExpanded),
+				toStateClass('transitioning', isTransitioning),
+			)}
 			style={
 				{
 					'--content-height': contentHeight,
 				} as React.CSSProperties // Custom properties not supported workaround
 			}
-			aria-hidden={!props.expanded}
+			aria-hidden={!otherProps.expanded}
 			onTransitionEnd={onTransitionEnd}
 		>
 			<div className="collapsible-content" ref={contentRef}>
-				{props.children}
+				{otherProps.children}
 			</div>
 		</div>
 	)

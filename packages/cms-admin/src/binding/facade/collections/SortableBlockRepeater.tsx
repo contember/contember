518cea78080d59c724fc37ce114e3ec9df36f1bf
Jirka Vebr <jirkavebr@gmail.com>
2019-08-19 11:32:23 +0200

    SortableBlockRepeater: avoid displaying uneditable null-blocks

--- packages/cms-admin/src/binding/facade/collections/SortableBlockRepeater.tsx
+++ packages/cms-admin/src/binding/facade/collections/SortableBlockRepeater.tsx
@@ -17,6 +17,7 @@ export interface SortableBlockRepeaterProps extends SortableRepeaterProps {
 	discriminationField: RelativeSingleField
 	addNewLabel?: React.ReactNode
 	children?: never
+	emptyMessage?: React.ReactNode
 }
 
 export const SortableBlockRepeater = Component<SortableBlockRepeaterProps>(
@@ -43,63 +44,89 @@ export const SortableBlockRepeater = Component<SortableBlockRepeaterProps>(
 			props.field,
 			atomicPrimitiveProps => (
 				<ToMany.AccessorRetriever {...atomicPrimitiveProps}>
-					{(field: EntityCollectionAccessor) => (
-						// Intentionally not applying label system middleware
-						<FieldSet legend={props.label} errors={field.errors}>
-							<div className="cloneable">
-								<div className="cloneable-content">
-									<Sortable
-										entities={field}
-										sortBy={props.sortBy}
-										label={props.label}
-										enableAddingNew={false}
-										enableUnlink={props.enableUnlink}
-										enableUnlinkAll={props.enableUnlinkAll}
-										removeType={props.removeType}
-									>
-										<AlternativeFields
-											label={props.label}
-											alternatives={props.alternatives}
-											name={props.discriminationField}
-											allowBlockTypeChange={true}
-										/>
-									</Sortable>
+					{(field: EntityCollectionAccessor) => {
+						const filteredEntities: EntityAccessor[] = field.entities.filter(
+							(item): item is EntityAccessor => item instanceof EntityAccessor,
+						)
+						const firstEntity = filteredEntities[0]
+						const firstDiscrimination = firstEntity.data.getField(props.discriminationField)
+
+						if (!(firstDiscrimination instanceof FieldAccessor)) {
+							return
+						}
+						const firstDiscriminationNull = firstDiscrimination.currentValue === null
+
+						const shouldViewContent = filteredEntities.length > 1 || !firstDiscriminationNull
+
+						return (
+							// Intentionally not applying label system middleware
+							<FieldSet legend={props.label} errors={field.errors}>
+								<div className="cloneable">
+									{shouldViewContent && (
+										<div className="cloneable-content">
+											<Sortable
+												entities={field}
+												sortBy={props.sortBy}
+												label={props.label}
+												enableAddingNew={false}
+												enableUnlink={props.enableUnlink}
+												enableUnlinkAll={props.enableUnlinkAll}
+												removeType={props.removeType}
+											>
+												<AlternativeFields
+													label={props.label}
+													alternatives={props.alternatives}
+													name={props.discriminationField}
+													allowBlockTypeChange={true}
+												/>
+											</Sortable>
+										</div>
+									)}
+									{shouldViewContent || (
+										<div className="cloneable-emptyMessage">
+											{props.emptyMessage || 'There is no content yet. Try adding a new block.'}
+										</div>
+									)}
+									{field.addNew && (
+										<div className="cloneable-button">
+											<SelectFieldInner
+												label={props.addNewLabel}
+												data={addNewOptions}
+												currentValue={-1}
+												onChange={newValue => {
+													if (!(newValue in addNewOptions)) {
+														return
+													}
+													const targetValue = addNewOptions[newValue].actualValue
+													if (filteredEntities.length === 1 && firstDiscriminationNull) {
+														return firstDiscrimination.updateValue && firstDiscrimination.updateValue(targetValue)
+													}
+													field.addNew &&
+														field.addNew(getAccessor => {
+															const accessor = getAccessor()
+															const newlyAdded = accessor.entities[accessor.entities.length - 1]
+															if (!(newlyAdded instanceof EntityAccessor)) {
+																return
+															}
+															// TODO this will fail horribly if QL is present here
+															const discriminationField = newlyAdded.data.getField(props.discriminationField)
+															if (!(discriminationField instanceof FieldAccessor) || !discriminationField.updateValue) {
+																return
+															}
+															discriminationField.updateValue(targetValue)
+														})
+												}}
+												environment={environment}
+												errors={[]}
+												firstOptionCaption="+ Add new"
+												isMutating={isMutating}
+											/>
+										</div>
+									)}
 								</div>
-								{field.addNew && (
-									<div className="cloneable-button">
-										<SelectFieldInner
-											label={props.addNewLabel}
-											data={addNewOptions}
-											currentValue={-1}
-											onChange={newValue => {
-												if (!(newValue in addNewOptions)) {
-													return
-												}
-												field.addNew &&
-													field.addNew(getAccessor => {
-														const accessor = getAccessor()
-														const newlyAdded = accessor.entities[accessor.entities.length - 1]
-														if (!(newlyAdded instanceof EntityAccessor)) {
-															return
-														}
-														// TODO this will fail horribly if QL is present here
-														const discriminationField = newlyAdded.data.getField(props.discriminationField)
-														if (!(discriminationField instanceof FieldAccessor) || !discriminationField.updateValue) {
-															return
-														}
-														discriminationField.updateValue(addNewOptions[newValue].actualValue)
-													})
-											}}
-											environment={environment}
-											errors={[]}
-											firstOptionCaption="+ Add new"
-											isMutating={isMutating}
-										/>
-									</div>
-								)}
-							</div>
-						</FieldSet>
-					)}
+							</FieldSet>
+						)
+					}}
 				</ToMany.AccessorRetriever>
 			),
 			environment,

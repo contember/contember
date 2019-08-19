cab653002cd8254c43831e151135f018e463dd70
Vojta Staněk <stanekv01@gmail.com>
2019-08-19 13:56:15 +0200

    cs fix

--- packages/cms-admin/src/components/RichEditor/utils.tsx
+++ packages/cms-admin/src/components/RichEditor/utils.tsx
@@ -19,7 +19,13 @@ interface ActionButtonProps {
 }
 
 export const ActionButton: React.SFC<ActionButtonProps> = ({ icon, onClick, isActive, disabled }) => (
-	<Button onClick={() => onClick()} distinction={isActive ? 'default' : 'seamless'} intent={isActive ? "primary" : "default"} disabled={disabled} flow="squarish">
+	<Button
+		onClick={() => onClick()}
+		distinction={isActive ? 'default' : 'seamless'}
+		intent={isActive ? 'primary' : 'default'}
+		disabled={disabled}
+		flow="squarish"
+	>
 		<Icon icon={icon} color="currentColor" />
 	</Button>
 )

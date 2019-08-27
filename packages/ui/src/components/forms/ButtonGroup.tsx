5d6ff50c5ee6fc52f09c3557e1bfe756801a5a84
Jirka Vebr <jirkavebr@gmail.com>
2019-08-27 12:31:44 +0200

    ButtonGroup: topToolbar

--- packages/ui/src/components/forms/ButtonGroup.tsx
+++ packages/ui/src/components/forms/ButtonGroup.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react'
 import cn from 'classnames'
 import { Size } from '../../types'
-import { toEnumViewClass } from '../../utils'
+import { toEnumViewClass, toViewClass } from '../../utils'
 
 export interface ButtonGroupProps {
 	children?: React.ReactNode
 	size?: Size
+	isTopToolbar?: boolean
 }
 
-export const ButtonGroup = React.memo(({ size, children }: ButtonGroupProps) => (
-	<div className={cn('button-group', toEnumViewClass(size))} role="group">
+export const ButtonGroup = React.memo(({ size, isTopToolbar, children }: ButtonGroupProps) => (
+	<div className={cn('button-group', toEnumViewClass(size), toViewClass('isTopToolbar', isTopToolbar))} role="group">
 		{children}
 	</div>
 ))

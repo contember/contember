71a9d7faafbe924c3a726670e00976af83c0715c
Filip Chalupa <chalupa.filip@gmail.com>
2019-08-27 15:22:06 +0200

    Storybook: dropdown2 - interactive

--- packages/ui/storybook/components/Dropdown2.tsx
+++ packages/ui/storybook/components/Dropdown2.tsx
@@ -1,15 +1,40 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
 import { Dropdown2 } from '../../src'
+import { useCallback, useState } from 'react'
 
-storiesOf('Dropdown2', module).add('simple', () => {
+const Component = () => {
+	const [isOpen, setIsOpen] = useState(false)
+	const toggleIsOpen = useCallback(() => {
+		setIsOpen(!isOpen)
+	}, [isOpen])
+	const close = useCallback(() => {
+		setIsOpen(false)
+	}, [])
+
 	return (
-		<Dropdown2 isOpen={true} handle={<button type="button">Handle</button>}>
+		<Dropdown2
+			isOpen={isOpen}
+			handle={
+				<button type="button" onClick={toggleIsOpen}>
+					Handle
+				</button>
+			}
+			onCloseRequest={close}
+		>
 			<div style={{ width: '100%' }}>
 				Content
 				<br />
 				goes here
 			</div>
 		</Dropdown2>
+	)
+}
+
+storiesOf('Dropdown2', module).add('simple', () => {
+	return (
+		<div style={{ display: 'flex' }}>
+			<Component />
+		</div>
 	)
 })

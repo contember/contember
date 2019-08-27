8e26364aed1f400b69b172193387faa60b42a03a
Jirka Vebr <jirkavebr@gmail.com>
2019-08-27 18:56:21 +0200

    wip

--- packages/cms-admin/src/binding/facade/fields/DateField.tsx
+++ packages/cms-admin/src/binding/facade/fields/DateField.tsx
@@ -1,31 +1,55 @@
 import { SingleLineTextInputProps, TextInput } from '@contember/ui'
 import * as React from 'react'
 import DatePicker from 'react-datepicker'
+import { FieldMetadata } from '../../coreComponents'
 import { FieldAccessor } from '../../dao'
 import { SimpleRelativeSingleField, SimpleRelativeSingleFieldProps } from '../auxiliary'
 
 export type DateFieldProps = SimpleRelativeSingleFieldProps &
-	Omit<SingleLineTextInputProps, 'value' | 'onChange' | 'validationState'> & {
-		ref?: React.Ref<HTMLInputElement>
-	}
+	Omit<SingleLineTextInputProps, 'value' | 'onChange' | 'validationState'>
 
-export const DateField = SimpleRelativeSingleField<DateFieldProps, string>((fieldMetadata, props) => {
+export const DateField = SimpleRelativeSingleField<DateFieldProps, string>(
+	(fieldMetadata, props) => <DateFieldInner fieldMetadata={fieldMetadata} {...props} />,
+	'DateField',
+)
+
+export interface DateFieldInnerProps extends Omit<DateFieldProps, 'name' | 'label'> {
+	fieldMetadata: FieldMetadata<string>
+}
+
+export const DateFieldInner = React.memo<DateFieldInnerProps>(props => {
 	const generateOnChange = (data: FieldAccessor<string>) => (date: Date | null) => {
 		data.updateValue && data.updateValue(date ? date.toISOString() : null)
 	}
+	const { onFocus: outerOnFocus, onBlur: outerOnBlur } = props
+	const UnderlyingTextInput = React.useMemo(
+		() =>
+			React.forwardRef<any, any>((innerProps, ref) => {
+				const { className, onFocus, onBlur, ...legalProps } = innerProps
+				return (
+					<TextInput
+						{...legalProps}
+						ref={ref}
+						onFocus={(e: React.FocusEvent<HTMLInputElement>) => {
+							outerOnFocus && outerOnFocus(e)
+							innerProps.onFocus && innerProps.onFocus(e)
+						}}
+						onBlur={(e: React.FocusEvent<HTMLInputElement>) => {
+							outerOnBlur && outerOnBlur(e)
+							innerProps.onBlur && innerProps.onBlur(e)
+						}}
+					/>
+				)
+			}),
+		[outerOnBlur, outerOnFocus],
+	)
 	return (
 		<DatePicker
-			selected={fieldMetadata.data.currentValue !== null ? new Date(fieldMetadata.data.currentValue) : null}
-			onChange={generateOnChange(fieldMetadata.data)}
-			readOnly={fieldMetadata.isMutating}
+			selected={props.fieldMetadata.data.currentValue !== null ? new Date(props.fieldMetadata.data.currentValue) : null}
+			onChange={generateOnChange(props.fieldMetadata.data)}
+			readOnly={props.fieldMetadata.isMutating}
 			isClearable={true}
 			customInput={<UnderlyingTextInput />}
-			customInputRef={props.ref as any}
 		/>
 	)
-}, 'DateField')
-
-const UnderlyingTextInput = React.forwardRef<any, any>((props, ref) => {
-	const { className, ...legalProps } = props
-	return <TextInput {...legalProps} ref={ref} />
 })

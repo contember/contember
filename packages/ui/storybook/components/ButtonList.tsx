a8b6c3f04ba6a3db03952e2f8bfbada00359c86b
Jirka Vebr <jirkavebr@gmail.com>
2019-08-29 11:40:13 +0200

    ButtonList: not just FF :(

--- packages/ui/storybook/components/ButtonList.tsx
+++ packages/ui/storybook/components/ButtonList.tsx
@@ -1,6 +1,6 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
-import { ButtonList } from '../../src/components/forms/ButtonList'
+import { ButtonList } from '../../src/components/forms'
 import { simpleButtonStory } from './Button'
 
 storiesOf('ButtonList', module).add('simple', () => <ButtonList>{simpleButtonStory()}</ButtonList>)

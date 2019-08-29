7addc9596464c3d3ad9c229b42ce21851ade7081
Jirka Vebr <jirkavebr@gmail.com>
2019-08-29 12:21:17 +0200

    Add the titleBar component

---
+++ packages/ui/src/components/TitleBar.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { ButtonList } from './forms'
+import { Heading, HeadingProps } from './Heading'
+
+export interface TitleBarProps {
+	navigation?: React.ReactNode // This can contain any number of buttons but only buttons
+	children: React.ReactNode
+	headingProps?: HeadingProps
+	actions?: React.ReactNode // This can contain any number of buttons but only buttons
+}
+
+export const TitleBar = React.memo(({ navigation, children, headingProps, actions }: TitleBarProps) => {
+	return (
+		<div className="titleBar">
+			{navigation && (
+				<nav className="titleBar-navigation">
+					<ButtonList>{navigation}</ButtonList>
+				</nav>
+			)}
+			<div className="titleBar-heading">
+				<Heading {...headingProps}>{children}</Heading>
+			</div>
+			{actions && (
+				<div className="titleBar-actions">
+					<ButtonList>{actions}</ButtonList>
+				</div>
+			)}
+		</div>
+	)
+})

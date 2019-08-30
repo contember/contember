8bdf70783efe37edf041ecceeaa8806ea48a7a97
Filip Chalupa <chalupa.filip@gmail.com>
2019-08-31 00:11:37 +0200

    Dropdown: storybook alignment option

--- packages/ui/storybook/components/Dropdown.tsx
+++ packages/ui/storybook/components/Dropdown.tsx
@@ -1,13 +1,22 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
-import { Dropdown } from '../../src'
+import { Dropdown, DropdownProps } from '../../src'
+import { radios } from '@storybook/addon-knobs'
 
 const Component = () => {
+	const alignment: DropdownProps['alignment'] = radios('Alignment', {
+		Default: 'default',
+		Center: 'center',
+		Start: 'start',
+		End: 'end',
+	})
+
 	return (
 		<Dropdown
 			buttonProps={{
 				children: 'Toggle',
 			}}
+			alignment={alignment}
 		>
 			<>Dropdown content goes here</>
 		</Dropdown>
@@ -16,7 +25,7 @@ const Component = () => {
 
 storiesOf('Dropdown', module).add('simple', () => {
 	return (
-		<div style={{ display: 'flex' }}>
+		<div style={{ display: 'flex', justifyContent: 'center' }}>
 			<Component />
 		</div>
 	)

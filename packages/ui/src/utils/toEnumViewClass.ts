584dd856260e230a634508f0ee54485a4b39378c
Filip Chalupa <chalupa.filip@gmail.com>
2019-08-30 16:27:25 +0200

    Collapsible: default transition option

--- packages/ui/src/utils/toEnumViewClass.ts
+++ packages/ui/src/utils/toEnumViewClass.ts
@@ -1,3 +1,4 @@
 import { toEnumClass } from './toEnumClass'
 
-export const toEnumViewClass = (name: string | undefined) => toEnumClass('view-', name)
+export const toEnumViewClass = (name: string | undefined, namedDefault?: string) =>
+	toEnumClass('view-', name, namedDefault)

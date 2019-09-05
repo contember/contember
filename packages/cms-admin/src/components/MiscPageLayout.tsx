34b1ef19a999968e168aff2d47a06c466cd65663
Jirka Vebr <jirkavebr@gmail.com>
2019-09-05 15:17:14 +0200

    Temporary: add a MiscPageLayout

---
+++ packages/cms-admin/src/components/MiscPageLayout.tsx
@@ -0,0 +1,14 @@
+import { Aether, Box, BoxProps } from '@contember/ui'
+import * as React from 'react'
+
+export interface MiscPageLayoutProps extends BoxProps {}
+
+export const MiscPageLayout = React.memo<MiscPageLayoutProps>(props => (
+	<Aether>
+		<div className="centerCard">
+			<div className="centerCard-in">
+				<Box {...props} />
+			</div>
+		</div>
+	</Aether>
+))

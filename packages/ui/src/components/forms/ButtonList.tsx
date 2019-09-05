35c7ce2f4ba561deddb6346807cb3cb3aaaa583a
Jirka Vebr <jirkavebr@gmail.com>
2019-09-05 15:16:22 +0200

    ButtonList: add block flow

--- packages/ui/src/components/forms/ButtonList.tsx
+++ packages/ui/src/components/forms/ButtonList.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react'
+import cn from 'classnames'
+import { ButtonListFlow } from '../../types'
+import { toEnumViewClass } from '../../utils'
 
 export interface ButtonListProps {
 	children?: React.ReactNode
+	flow?: ButtonListFlow
 }
 
-export const ButtonList = React.memo(({ children }: ButtonListProps) => (
-	<div className="button-list" role="group">
+export const ButtonList = React.memo(({ children, flow }: ButtonListProps) => (
+	<div className={cn('button-list', toEnumViewClass(flow, 'inline'))} role="group">
 		{children}
 	</div>
 ))

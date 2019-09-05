a356e04c7368bf91b88d906988f2e1344fa6efb5
Jirka Vebr <jirkavebr@gmail.com>
2019-09-05 22:07:54 +0200

    ImageField: use Boxes

--- packages/cms-admin/src/binding/facade/fields/ImageUploadField.tsx
+++ packages/cms-admin/src/binding/facade/fields/ImageUploadField.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { Field } from '../../coreComponents'
 import { QueryLanguage } from '../../queryLanguage'
 import { Component, SimpleRelativeSingleFieldProps } from '../auxiliary'
 import { UploadField } from './UploadField'

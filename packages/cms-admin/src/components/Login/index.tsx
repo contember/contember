478927798f7b4c607bcc22ab99f030c01322fc81
Jirka Vebr <jirkavebr@gmail.com>
2019-09-05 15:17:26 +0200

    Improve the login page

--- packages/cms-admin/src/components/Login/index.tsx
+++ packages/cms-admin/src/components/Login/index.tsx
@@ -1,11 +1,11 @@
-import { Card, Elevation } from '@blueprintjs/core'
-import { Button, FormGroup, TextInput, ErrorList } from '@contember/ui'
+import { Button, ErrorList, FormGroup, TextInput } from '@contember/ui'
 import * as React from 'react'
 import { connect } from 'react-redux'
 import { login, tryAutoLogin } from '../../actions/auth'
 import { Dispatch } from '../../actions/types'
 import State from '../../state'
 import { AuthStatus } from '../../state/auth'
+import { MiscPageLayout } from '../MiscPageLayout'
 
 class Login extends React.PureComponent<Login.Props, Login.State> {
 	state: Login.State = {
@@ -22,67 +22,62 @@ class Login extends React.PureComponent<Login.Props, Login.State> {
 		const loading = this.props.status === AuthStatus.LOADING
 
 		return (
-			<div className="centerCard-wrap">
-				<Card elevation={Elevation.ONE} className="centerCard">
-					<div className="login-site">
-						<h1 className="login-site-name">Contember CMS</h1>
-					</div>
-					<form
-						onSubmit={async e => {
-							e.preventDefault()
-							await this.props.login(this.state.email, this.state.password, this.state.rememberMe)
-						}}
-					>
-						{this.props.errorMessage && (
-							<>
-								<ErrorList
-									size="large"
-									errors={[
-										{
-											key: '1',
-											message: this.props.errorMessage,
-										},
-									]}
-								/>
-								<br />
-							</>
-						)}
-						<FormGroup label="Email">
-							<TextInput
-								value={this.state.email}
-								autoComplete="username"
-								type="email"
-								disabled={loading}
-								onChange={e => this.setState({ email: e.target.value })}
+			<MiscPageLayout heading="Contember CMS">
+				<form
+					onSubmit={async e => {
+						e.preventDefault()
+						await this.props.login(this.state.email, this.state.password, this.state.rememberMe)
+					}}
+				>
+					{this.props.errorMessage && (
+						<>
+							<ErrorList
+								size="large"
+								errors={[
+									{
+										key: '1',
+										message: this.props.errorMessage,
+									},
+								]}
 							/>
-						</FormGroup>
-						<FormGroup label="Password">
-							<TextInput
-								type="password"
-								autoComplete="current-password"
-								value={this.state.password}
-								disabled={loading}
-								onChange={e => this.setState({ password: e.target.value })}
-							/>
-						</FormGroup>
+							<br />
+						</>
+					)}
+					<FormGroup label="Email">
+						<TextInput
+							value={this.state.email}
+							autoComplete="username"
+							type="email"
+							disabled={loading}
+							onChange={e => this.setState({ email: e.target.value })}
+						/>
+					</FormGroup>
+					<FormGroup label="Password">
+						<TextInput
+							type="password"
+							autoComplete="current-password"
+							value={this.state.password}
+							disabled={loading}
+							onChange={e => this.setState({ password: e.target.value })}
+						/>
+					</FormGroup>
 
-						<FormGroup label="Remember me">
-							<input
-								type="checkbox"
-								checked={this.state.rememberMe}
-								value={this.state.password}
-								disabled={loading}
-								onChange={(e: React.FormEvent<HTMLInputElement>) =>
-									this.setState({ rememberMe: e.currentTarget.checked })
-								}
-							/>
-						</FormGroup>
-						<Button type="submit" disabled={loading}>
-							Submit
-						</Button>
-					</form>
-				</Card>
-			</div>
+					<FormGroup label="Remember me">
+						<input
+							type="checkbox"
+							checked={this.state.rememberMe}
+							value={this.state.password}
+							disabled={loading}
+							onChange={(e: React.FormEvent<HTMLInputElement>) =>
+								this.setState({ rememberMe: e.currentTarget.checked })
+							}
+						/>
+					</FormGroup>
+					<Button type="submit" disabled={loading}>
+						Submit
+					</Button>
+				</form>
+			</MiscPageLayout>
 		)
 	}
 }

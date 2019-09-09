ce7c68ebb1b71907f11da23ffe97de9a56417e99
David Matejka <matej21@matej21.cz>
2019-09-09 13:36:45 +0200

    db client: update generics

--- packages/engine-tenant-api/src/model/commands/CommandBus.ts
+++ packages/engine-tenant-api/src/model/commands/CommandBus.ts
@@ -1,15 +1,19 @@
 import { Command } from './Command'
-import { Client } from '@contember/database'
+import { Client, Connection } from '@contember/database'
 import { Providers } from '../providers'
 
-export class CommandBus {
-	constructor(private readonly client: Client, private readonly providers: Providers) {}
+export class CommandBus<ConnectionType extends Connection.ConnectionLike = Connection.ConnectionLike> {
+	constructor(public readonly client: Client<ConnectionType>, private readonly providers: Providers) {}
 
 	public async execute<T>(command: Command<T>): Promise<T> {
 		return await command.execute({ db: this.client, providers: this.providers, bus: this })
 	}
 
-	async transaction<T>(transactionScope: (wrapper: CommandBus) => Promise<T> | T): Promise<T> {
-		return await this.client.transaction(client => transactionScope(new CommandBus(client, this.providers)))
+	async transaction<T>(
+		transactionScope: (wrapper: CommandBus<Connection.TransactionLike>) => Promise<T> | T,
+	): Promise<T> {
+		return await this.client.transaction(client =>
+			transactionScope(new CommandBus<Connection.TransactionLike>(client, this.providers)),
+		)
 	}
 }

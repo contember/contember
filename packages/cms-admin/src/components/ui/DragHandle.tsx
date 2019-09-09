cfeb43068cb55f2786fa44499e04b5b86b4ddd10
Jirka Vebr <jirkavebr@gmail.com>
2019-09-09 13:28:08 +0200

    Smaller drag handle

--- packages/cms-admin/src/components/ui/DragHandle.tsx
+++ packages/cms-admin/src/components/ui/DragHandle.tsx
@@ -6,13 +6,11 @@ export interface DragHandleProps {
 
 export const DragHandle = (props: DragHandleProps) => {
 	return (
-		<svg xmlns="http://www.w3.org/2000/svg" width="10" height="24" viewBox="0 0 10 24" className={props.className}>
-			<g transform="translate(-310 -236)">
-				<path
-					d="M310 237.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm-7 7a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm-7 7a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm-7 7a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z"
-					fill="currentColor"
-				/>
-			</g>
+		<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 10 17" className={props.className}>
+			<path
+				d="M0 1.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm-7 7a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm-7 7a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z"
+				fill="currentColor"
+			/>
 		</svg>
 	)
 }

b2e69ed7cf0b3a1e1b10874144e61070bdcf9ea2
David Matejka <matej21@matej21.cz>
2019-09-11 18:01:39 +0200

    tenant: update permissions setup

--- packages/engine-tenant-api/src/model/authorization/PermissionsFactory.ts
+++ packages/engine-tenant-api/src/model/authorization/PermissionsFactory.ts
@@ -11,7 +11,10 @@ class PermissionsFactory {
 		permissions.allow(Identity.SystemRole.SELF, ...PermissionActions.PERSON_CHANGE_PASSWORD)
 		permissions.allow(Identity.SystemRole.PERSON, ...PermissionActions.PERSON_SIGN_OUT)
 		permissions.allow(Identity.SystemRole.PROJECT_MEMBER, ...PermissionActions.PROJECT_VIEW)
-		// permissions.allow(Identity.SystemRole.PROJECT_MEMBER, ...PermissionActions.PROJECT_VIEW_MEMBERS)
+		permissions.allow(Identity.SystemRole.PROJECT_ADMIN, ...PermissionActions.PROJECT_VIEW_MEMBERS)
+		permissions.allow(Identity.SystemRole.PROJECT_ADMIN, ...PermissionActions.PROJECT_ADD_MEMBER)
+		permissions.allow(Identity.SystemRole.PROJECT_ADMIN, ...PermissionActions.PROJECT_UPDATE_MEMBER)
+		permissions.allow(Identity.SystemRole.PROJECT_ADMIN, ...PermissionActions.PROJECT_REMOVE_MEMBER)
 
 		return permissions
 	}

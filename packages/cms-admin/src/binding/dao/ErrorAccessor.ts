5c5b77be76f6c6baa1ed0863720007a3ff3efa35
Jirka Vebr <jirkavebr@gmail.com>
2019-09-11 11:25:22 +0200

    ErrorAccessor: remove hashing

--- packages/cms-admin/src/binding/dao/ErrorAccessor.ts
+++ packages/cms-admin/src/binding/dao/ErrorAccessor.ts
@@ -1,16 +1,3 @@
-import { Hashing } from '../utils'
-
-class ErrorAccessor {
-	private _key?: string
-
+export class ErrorAccessor {
 	public constructor(public readonly message: string) {}
-
-	public get key() {
-		if (this._key === undefined) {
-			this._key = Hashing.hash(this.message).toString()
-		}
-		return this._key
-	}
 }
-
-export { ErrorAccessor }

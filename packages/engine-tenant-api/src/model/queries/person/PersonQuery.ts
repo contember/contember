8d76116c718bf51e7161251dcf5525a3ebdd7942
David Matejka <matej21@matej21.cz>
2019-09-18 10:55:29 +0200

    tenant api: fix person query

--- packages/engine-tenant-api/src/model/queries/person/PersonQuery.ts
+++ packages/engine-tenant-api/src/model/queries/person/PersonQuery.ts
@@ -1,5 +1,4 @@
-import { DatabaseQuery } from '@contember/database'
-import { DatabaseQueryable } from '@contember/database'
+import { ConditionBuilder, DatabaseQuery, DatabaseQueryable } from '@contember/database'
 import { MaybePersonRow, PersonRow } from './types'
 
 class PersonQuery extends DatabaseQuery<MaybePersonRow> {
@@ -29,10 +28,24 @@ class PersonQuery extends DatabaseQuery<MaybePersonRow> {
 			.select(['identity', 'roles'])
 			.from('person')
 			.join('identity', 'identity', expr => expr.columnsEq(['identity', 'id'], ['person', 'identity_id']))
-			.where(this.condition)
+			.where(expr => this.applyCondition(expr))
 			.getResult()
 
 		return this.fetchOneOrNull(rows)
+	}
+
+	private applyCondition(conditionBuilder: ConditionBuilder): void {
+		if ('email' in this.condition) {
+			conditionBuilder.compare(['person', 'email'], ConditionBuilder.Operator.eq, this.condition.email)
+		} else if ('id' in this.condition) {
+			conditionBuilder.compare(['person', 'id'], ConditionBuilder.Operator.eq, this.condition.id)
+		} else if ('identity_id' in this.condition) {
+			conditionBuilder.compare(['person', 'identity_id'], ConditionBuilder.Operator.eq, this.condition.identity_id)
+		} else {
+			;((_: never): never => {
+				throw new Error()
+			})(this.condition)
+		}
 	}
 }
 

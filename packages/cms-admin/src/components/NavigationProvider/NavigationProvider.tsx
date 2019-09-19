2b6acfa09e3f088167c5235a1b96e2549d54f8bd
Jirka Vebr <jirkavebr@gmail.com>
2019-09-19 12:57:31 +0200

    Avoid constant navigation rerendering

--- packages/cms-admin/src/components/NavigationProvider/NavigationProvider.tsx
+++ packages/cms-admin/src/components/NavigationProvider/NavigationProvider.tsx
@@ -1,46 +1,55 @@
 import * as React from 'react'
-import { useSelector } from 'react-redux'
+import { shallowEqual, useSelector } from 'react-redux'
 import { PageLink } from '../pageRouting'
 import { DynamicLink } from '../DynamicLink'
 import { Navigation } from '@contember/ui'
 import State from '../../state'
 import { requestStateToPath } from '../../utils/url'
 import routes from '../../routes'
-import { pageRequest } from '../../state/request'
+import { PageRequest, pageRequest } from '../../state/request'
 import { isUrlActive } from '../../utils/isUrlActive'
 
 export interface NavigationIsActiveProviderProps {
 	children?: React.ReactNode
 }
 
-export function NavigationIsActiveProvider(props: NavigationIsActiveProviderProps) {
-	const isActive = useSelector<State, (to: string | Navigation.CustomTo) => boolean>(state => {
-		return (to: string | Navigation.CustomTo) => {
-			if (state.view.route && state.view.route.name === 'project_page') {
-				const url = requestStateToPath(
-					routes(state.projectsConfigs.configs),
-					pageRequest(
-						state.view.route.project,
-						state.view.route.stage,
-						typeof to === 'string' ? to : to.pageName,
-						typeof to === 'string' ? {} : to.parameters,
-					)(state.request),
-				)
-				return isUrlActive(url)
-			} else {
+export const NavigationIsActiveProvider = React.memo((props: NavigationIsActiveProviderProps) => {
+	const viewRoute = useSelector((state: State): PageRequest<any> | undefined => {
+		if (!state.view.route || state.view.route.name !== 'project_page') {
+			return undefined
+		}
+		return state.view.route
+	}, shallowEqual)
+	const configs = useSelector((state: State) => state.projectsConfigs.configs, shallowEqual)
+	const request = useSelector((state: State) => state.request, shallowEqual)
+	const isActive = React.useCallback(
+		(to: string | Navigation.CustomTo) => {
+			if (viewRoute === undefined) {
 				return false
 			}
-		}
-	})
+			const url = requestStateToPath(
+				routes(configs),
+				pageRequest(
+					viewRoute.project,
+					viewRoute.stage,
+					typeof to === 'string' ? to : to.pageName,
+					typeof to === 'string' ? {} : to.parameters,
+				)(request),
+			)
+			return isUrlActive(url)
+		},
+		[configs, request, viewRoute],
+	)
 
 	return <Navigation.IsActiveContext.Provider value={isActive}>{props.children}</Navigation.IsActiveContext.Provider>
-}
+})
+NavigationIsActiveProvider.displayName = 'NavigationIsActiveProvider'
 
 export interface NavigationProviderProps {
 	children?: React.ReactNode
 }
 
-export function NavigationProvider(props: NavigationProviderProps) {
+export const NavigationProvider = React.memo((props: NavigationProviderProps) => {
 	return (
 		<NavigationIsActiveProvider>
 			<Navigation.MiddlewareContext.Provider
@@ -86,4 +95,5 @@ export function NavigationProvider(props: NavigationProviderProps) {
 			</Navigation.MiddlewareContext.Provider>
 		</NavigationIsActiveProvider>
 	)
-}
+})
+NavigationProvider.displayName = 'NavigationProvider'

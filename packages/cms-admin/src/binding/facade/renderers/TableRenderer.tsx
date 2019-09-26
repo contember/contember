ee8eda089b4e9bd619a7ed491797c272138c1cdf
Filip Chalupa <chalupa.filip@gmail.com>
2019-09-26 15:28:53 +0200

    Table2: name changed to Table

--- packages/cms-admin/src/binding/facade/renderers/TableRenderer.tsx
+++ packages/cms-admin/src/binding/facade/renderers/TableRenderer.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Table2, Table2Cell, Table2Row } from '@contember/ui'
+import { Table, TableRow } from '@contember/ui'
 import { AccessorContext, DataRendererProps } from '../../coreComponents'
 import { CollectionRenderer } from './CollectionRenderer'
 import { CommonRendererProps } from './CommonRendererProps'
@@ -18,13 +18,13 @@ export class TableRenderer extends React.PureComponent<DataRendererProps & Table
 						{DefaultRenderer.renderTitleBar(this.props)}
 						{this.props.beforeContent}
 						{(entities.length > 0 || this.props.tableHeader) && (
-							<Table2 heading={this.props.tableHeader}>
+							<Table heading={this.props.tableHeader}>
 								{entities.map(value => (
 									<AccessorContext.Provider value={value} key={value.getKey()}>
-										<Table2Row>{this.props.children}</Table2Row>
+										<TableRow>{this.props.children}</TableRow>
 									</AccessorContext.Provider>
 								))}
-							</Table2>
+							</Table>
 						)}
 						{!!entities.length || <div>There are no items to display.</div>}
 					</>

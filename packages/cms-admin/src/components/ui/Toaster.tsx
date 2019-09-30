da2c048a8717378fa9570e92a1cb28437c9b3f1b
Filip Chalupa <chalupa.filip@gmail.com>
2019-09-30 16:41:31 +0200

    Validation message: toaster update, action support

--- packages/cms-admin/src/components/ui/Toaster.tsx
+++ packages/cms-admin/src/components/ui/Toaster.tsx
@@ -1,35 +1,45 @@
 import * as React from 'react'
 import { connect } from 'react-redux'
-import cn from 'classnames'
-import ToastsState, { Toast, ToastId, ToastType } from '../../state/toasts'
+import ToastsState, { ToastId, ToastType } from '../../state/toasts'
 import State from '../../state'
 import { Icon } from '@blueprintjs/core'
 import { Dispatch } from '../../actions/types'
 import { dismissToast } from '../../actions/toasts'
+import { Button, ValidationMessage, ValidationMessageProps } from '@contember/ui'
 
-const typeClassName: { [K in ToastType]: string } = {
-	[ToastType.Success]: 'view-success',
-	[ToastType.Warning]: 'view-warning',
-	[ToastType.Error]: 'view-error',
-	[ToastType.Info]: 'view-info',
+const toastTypeToMessageType: { [K in ToastType]: ValidationMessageProps['type'] } = {
+	[ToastType.Success]: 'valid',
+	[ToastType.Warning]: 'warning',
+	[ToastType.Error]: 'invalid',
+	[ToastType.Info]: 'info',
 }
 
 class ToasterConnected extends React.PureComponent<Toaster.ToasterStateProps & Toaster.ToasterDispatcherProps> {
 	render() {
 		return (
-			<div className="toast-wrap">
+			<div className="toaster">
 				{this.props.toasts.map(toast => (
-					<div key={toast.id} className={cn('toast', typeClassName[toast.type])}>
-						<p className="toast-message">{toast.message}</p>
-						<button
-							className="toast-button"
-							onClick={e => {
-								e.preventDefault()
-								this.props.dismissToast(toast.id)
-							}}
+					<div key={toast.id} className="toaster-item">
+						<ValidationMessage
+							type={toastTypeToMessageType[toast.type]}
+							flow="block"
+							framed
+							action={
+								<Button
+									intent="default"
+									distinction="seamless"
+									flow="squarish"
+									onClick={() => {
+										this.props.dismissToast(toast.id)
+									}}
+									bland
+								>
+									<Icon icon="cross" color="currentColor" />
+								</Button>
+							}
 						>
-							<Icon icon="cross" color="currentColor" />
-						</button>
+							{toast.message}
+						</ValidationMessage>
 					</div>
 				))}
 			</div>

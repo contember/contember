6dca75c83fcce5c739809908abc7cd78d0a7fc09
David Matejka <matej21@matej21.cz>
2019-09-30 17:17:59 +0200

    api: create column modification: add possibility to fill a column from another column

--- packages/engine-system-api/src/model/migrations/modifications/columns/CreateColumnModification.ts
+++ packages/engine-system-api/src/model/migrations/modifications/columns/CreateColumnModification.ts
@@ -5,8 +5,9 @@ import { addField, SchemaUpdater, updateEntity, updateModel } from '../schemaUpd
 import { Modification } from '../Modification'
 import { EventType } from '../../../EventType'
 import { wrapIdentifier } from '@contember/database'
-import { resolveDefaultValue } from '@contember/schema-utils'
+import { getColumnName, resolveDefaultValue } from '@contember/schema-utils'
 import { escapeValue } from '../../../../utils/pgMigrateHelpers'
+import { ImplementationException } from '../../../../utils/exceptions'
 
 class CreateColumnModification implements Modification<CreateColumnModification.Data> {
 	constructor(private readonly data: CreateColumnModification.Data, private readonly schema: Schema) {}
@@ -14,15 +15,24 @@ class CreateColumnModification implements Modification<CreateColumnModification.
 	public createSql(builder: MigrationBuilder): void {
 		const entity = this.schema.model.entities[this.data.entityName]
 		const column = this.data.field
+		const hasSeed = this.data.fillValue !== undefined || this.data.copyValue !== undefined
 		builder.addColumn(entity.tableName, {
 			[column.columnName]: {
 				type: column.type === Model.ColumnType.Enum ? `"${column.columnType}"` : column.columnType,
-				notNull: !column.nullable && this.data.fillValue === undefined,
+				notNull: !column.nullable && !hasSeed,
 			},
 		})
-		if (this.data.fillValue !== undefined) {
-			builder.sql(`UPDATE ${wrapIdentifier(entity.tableName)} 
-  SET ${wrapIdentifier(column.columnName)} = ${escapeValue(this.data.fillValue)}`)
+		if (hasSeed) {
+			if (this.data.fillValue !== undefined) {
+				builder.sql(`UPDATE ${wrapIdentifier(entity.tableName)} 
+	  SET ${wrapIdentifier(column.columnName)} = ${escapeValue(this.data.fillValue)}`)
+			} else if (this.data.copyValue !== undefined) {
+				const copyFrom = getColumnName(this.schema.model, entity, this.data.copyValue)
+				builder.sql(`UPDATE ${wrapIdentifier(entity.tableName)} 
+	  SET ${wrapIdentifier(column.columnName)} = ${wrapIdentifier(copyFrom)}`)
+			} else {
+				throw new ImplementationException()
+			}
 
 			// event applier defers constraint check, we need to fire them before ALTER
 			builder.sql(`SET CONSTRAINTS ALL IMMEDIATE`)
@@ -49,9 +59,15 @@ class CreateColumnModification implements Modification<CreateColumnModification.
 			}
 
 			try {
-				const fillValue = this.data.fillValue
-				const value =
-					fillValue !== undefined ? fillValue : resolveDefaultValue(this.data.field, { now: () => it.createdAt })
+				let value: any = null
+				if (this.data.fillValue !== undefined) {
+					value = this.data.fillValue
+				} else if (this.data.copyValue !== undefined) {
+					const columnName = getColumnName(this.schema.model, entity, this.data.copyValue)
+					value = it.values[columnName] !== undefined ? it.values[columnName] : null
+				} else {
+					value = resolveDefaultValue(this.data.field, { now: () => it.createdAt })
+				}
 				return {
 					...it,
 					[this.data.field.columnName]: value,
@@ -73,6 +89,7 @@ namespace CreateColumnModification {
 		entityName: string
 		field: Model.AnyColumn
 		fillValue?: any
+		copyValue?: string
 	}
 }
 

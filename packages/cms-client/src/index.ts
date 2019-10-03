7b1195dffd76f13198ba7ede9feca3c1de415529
Jirka Vebr <jirkavebr@gmail.com>
2019-10-03 13:48:15 +0200

    Move GraphQlClient to cms-client

--- packages/cms-client/src/index.ts
+++ packages/cms-client/src/index.ts
@@ -2,3 +2,4 @@ import * as GraphQlBuilder from './graphQlBuilder'
 import * as CrudQueryBuilder from './crudQueryBuilder'
 
 export { GraphQlBuilder, CrudQueryBuilder }
+export * from './graphQlClient'

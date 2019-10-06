a4ab3e2fcf139ecafcf9f7157c656d83a654d553
Filip Chalupa <chalupa.filip@gmail.com>
2019-10-06 16:10:59 +0200

    Storybook: icon - subfolder

--- packages/ui/storybook/components/Icon/blueprintIconNames.ts
+++ packages/ui/storybook/components/Icon/blueprintIconNames.ts
@@ -1,9 +1,6 @@
-import { color, text, number } from '@storybook/addon-knobs'
-import { storiesOf } from '@storybook/react'
-import * as React from 'react'
-import { Icon, IconProps } from '../../src'
+import { IconProps } from '../../../src/components'
 
-const ICON_NAMES: IconProps['blueprintIcon'][] = [
+export const blueprintIconNames: IconProps['blueprintIcon'][] = [
 	'add',
 	'add-column-left',
 	'add-column-right',
@@ -481,50 +478,3 @@ const ICON_NAMES: IconProps['blueprintIcon'][] = [
 	'zoom-out',
 	'zoom-to-fit',
 ]
-
-storiesOf('Icon', module).add('simple', () => {
-	const blueprintIcon = text('Blueprint icon name', 'chart') as IconProps['blueprintIcon'] // Cast is sort of ok since Blueprint icons can handle corrupted icon name
-	const fontSize = number('Font size', 96, {
-		range: true,
-		min: 12,
-		max: 160,
-		step: 1,
-	})
-	const fontColor = color('Font color', '#000000')
-
-	return (
-		<div style={{ color: fontColor }}>
-			<h1>Dynamic size</h1>
-			<div
-				style={{
-					fontSize: `${fontSize / 16}rem`,
-				}}
-			>
-				<Icon blueprintIcon={blueprintIcon} />
-			</div>
-			<hr />
-			<h1>Predefined sizes:</h1>
-			{[16, 24, 32, 48, 96].map(fontSize => (
-				<div
-					style={{
-						fontSize: `${fontSize / 16}rem`,
-					}}
-					key={fontSize}
-				>
-					{fontSize}px: <Icon blueprintIcon={blueprintIcon} /> example
-				</div>
-			))}
-			<hr />
-			<h1>
-				<a href="https://blueprintjs.com/docs/#icons" target="_blank" rel="noopener noreferrer">
-					Blueprint icon names <Icon blueprintIcon="arrow-top-right" />
-				</a>
-			</h1>
-			{ICON_NAMES.map(blueprintIcon => (
-				<div>
-					<Icon blueprintIcon={blueprintIcon} /> {blueprintIcon}
-				</div>
-			))}
-		</div>
-	)
-})

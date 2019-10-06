6e7b5fc3d15f9ebb34980a287813ede5d1e8eb34
Filip Chalupa <chalupa.filip@gmail.com>
2019-10-06 15:37:21 +0200

    Icon: blueprint integration

--- packages/ui/src/components/Icon.tsx
+++ packages/ui/src/components/Icon.tsx
@@ -1,4 +1,28 @@
 import * as React from 'react'
+import { IconName, IconSvgPaths16 } from '@blueprintjs/icons'
 
-export const Icon = React.memo(() => <div className="icon" />)
+export interface IconProps {
+	children?: never
+	title?: string
+	blueprintIcon: IconName
+}
+
+const renderSvgPaths = (iconName: IconName): JSX.Element[] | null => {
+	const pathStrings = IconSvgPaths16[iconName]
+	if (!pathStrings) {
+		return null
+	}
+	return pathStrings.map((d, i) => <path key={i} d={d} fillRule="evenodd" />)
+}
+
+export const Icon = React.memo((props: IconProps) => {
+	return (
+		<div className="icon">
+			<svg viewBox="0 0 16 16" className="icon-blueprintSvg">
+				{props.title && <desc>{props.title}</desc>}
+				{renderSvgPaths(props.blueprintIcon)}
+			</svg>
+		</div>
+	)
+})
 Icon.displayName = 'Icon'

e903921d72fbb52429cf00894889541f034c8664
Jirka Vebr <jirkavebr@gmail.com>
2019-10-08 17:34:23 +0200

    Cms-api: nicer auth errors

--- packages/cms-api/src/http/responseUtils.ts
+++ packages/cms-api/src/http/responseUtils.ts
@@ -2,6 +2,6 @@ import { KoaContext } from '../core/koa/types'
 
 export const createGraphqlInvalidAuthResponse = (ctx: KoaContext<any>, message: string): void => {
 	ctx.set('Content-type', 'application/json')
-	ctx.status = 500
+	ctx.status = 401
 	ctx.body = JSON.stringify({ errors: [{ message, code: 401 }] })
 }

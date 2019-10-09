30eabbeabe4080780c5dd927bb6a174e66b2d426
Jirka Vebr <jirkavebr@gmail.com>
2019-10-09 10:47:35 +0200

    Improve tenant api messages

--- packages/cms-admin/src/apiClient/tenantErrorMessages.ts
+++ packages/cms-admin/src/apiClient/tenantErrorMessages.ts
@@ -2,8 +2,8 @@ export const tenantErrorMessages: { [key: string]: string } = {
 	TOO_WEAK: 'Password is too weak.',
 	EMAIL_ALREADY_EXISTS: 'Email already exists.',
 	UNKNOWN_EMAIL: 'Unknown email.',
-	INVALID_PASSWORD: 'Password is invalid',
-	ALREADY_MEMBER: 'User is already member of this project.',
+	INVALID_PASSWORD: 'Password is invalid.',
+	ALREADY_MEMBER: 'User is already a member of this project.',
 }
 
 export const getTenantErrorMessage = (errorCode: string): string => {

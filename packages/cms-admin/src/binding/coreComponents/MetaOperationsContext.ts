48186176ee73c5462e3274c4bc49f83575691ac4
Jirka Vebr <jirkavebr@gmail.com>
2019-10-09 17:25:06 +0200

    Remove obsolete files

--- packages/cms-admin/src/binding/coreComponents/MetaOperationsContext.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react'
-import { MetaOperationsAccessor } from '../dao'
-
-export type MetaOperationsContextValue = MetaOperationsAccessor | undefined
-
-const MetaOperationsContext = React.createContext<MetaOperationsContextValue>(undefined)
-
-export { MetaOperationsContext }

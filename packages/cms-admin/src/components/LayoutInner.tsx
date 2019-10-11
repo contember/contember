51e073773024f4ae4b7ccad12f3442c0b0642f4f
Jirka Vebr <jirkavebr@gmail.com>
2019-10-11 14:39:11 +0200

    Refine inner layout

--- packages/cms-admin/src/components/LayoutInner.tsx
+++ packages/cms-admin/src/components/LayoutInner.tsx
@@ -1,19 +1,10 @@
 import { Box } from '@contember/ui'
 import * as React from 'react'
 
-export const LayoutInner: React.FC<{}> = ({ children }) => {
-	return <div className="layout-content-in">{children}</div>
-}
+export const LayoutInner = React.memo(({ children }) => <div className="layout-content-in">{children}</div>)
 
-export interface LayoutSideProps {
-	showBox?: boolean
-}
-
-export const LayoutSide: React.FC<LayoutSideProps> = ({ children, showBox }) => {
-	return (
-		<div className="layout-content-side">
-			{showBox && <Box>{children}</Box>}
-			{showBox || children}
-		</div>
-	)
-}
+export const LayoutSide = React.memo(({ children }) => (
+	<div className="layout-content-side">
+		<Box>{children}</Box>
+	</div>
+))

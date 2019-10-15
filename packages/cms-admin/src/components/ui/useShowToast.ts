f7c640d41f2beb449a2411727ac2ce7d195a2925
Jirka Vebr <jirkavebr@gmail.com>
2019-10-15 17:57:42 +0200

    Add useDismissToast

--- packages/cms-admin/src/components/ui/useShowToast.ts
+++ packages/cms-admin/src/components/ui/useShowToast.ts
@@ -1,11 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { addToast } from '../../actions/toasts'
-import { ToastDefinition } from '../../state/toasts'
+import { ToastDefinition, ToastId } from '../../state/toasts'
 import * as React from 'react'
 
 // TODO this is a very bad place for this file
 export const useShowToast = () => {
 	const dispatch = useDispatch()
 
-	return React.useCallback((definition: ToastDefinition) => dispatch(addToast(definition)), [dispatch])
+	return React.useCallback(
+		(definition: ToastDefinition): ToastId | undefined => {
+			const toastAction = addToast(definition)
+			dispatch(toastAction)
+			return toastAction.payload ? toastAction.payload.id : undefined
+		},
+		[dispatch],
+	)
 }

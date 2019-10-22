41461e75aaec44e7b3101ca8603a0bccb4e6ce22
Filip Chalupa <chalupa.filip@gmail.com>
2019-10-22 13:47:02 +0200

    Icon: drag handle

--- packages/cms-admin/src/components/ui/DragHandle.tsx
+++ packages/cms-admin/src/components/ui/DragHandle.tsx
@@ -1,16 +1,6 @@
 import * as React from 'react'
+import { Icon } from '@contember/ui'
 
-export interface DragHandleProps {
-	className?: string
-}
-
-export const DragHandle = (props: DragHandleProps) => {
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 10 17" className={props.className}>
-			<path
-				d="M0 1.5a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm-7 7a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm-7 7a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0zm7 0a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z"
-				fill="currentColor"
-			/>
-		</svg>
-	)
+export const DragHandle = () => {
+	return <Icon blueprintIcon="drag-handle-vertical" />
 }

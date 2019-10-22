cafc0c75f149ec3e6dd0d332701618e6fa39f9cb
Filip Chalupa <chalupa.filip@gmail.com>
2019-10-22 13:53:43 +0200

    Icon: navigate back chevron

--- packages/cms-admin/src/components/pageRouting/NavigateBackButton.tsx
+++ packages/cms-admin/src/components/pageRouting/NavigateBackButton.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react'
 import { PageLinkButton, PageLinkButtonProps } from './PageLinkButton'
+import { Icon } from '@contember/ui'
 
 export const NavigateBackButton = React.memo<PageLinkButtonProps>(props => (
 	<PageLinkButton distinction="seamless" size="small" style={{ marginLeft: '-.3em' }} {...props}>
-		<span
-			style={{ fontSize: '.7em', display: 'inline-block', top: '-.09em', position: 'relative', marginRight: '.25em' }}
-		>
-			❰
-		</span>
+		<Icon blueprintIcon="chevron-left" />
 		{props.children}
 	</PageLinkButton>
 ))

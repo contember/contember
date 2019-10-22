739b8232bd5d495dcde52e0ce11d22c835fdeff4
Jirka Vebr <jirkavebr@gmail.com>
2019-10-22 23:22:21 +0200

    EnvContext: better default value creation

--- packages/cms-admin/src/binding/accessorRetrievers/EnvironmentContext.ts
+++ packages/cms-admin/src/binding/accessorRetrievers/EnvironmentContext.ts
@@ -1,4 +1,4 @@
 import * as React from 'react'
 import { Environment } from '../dao'
 
-export const EnvironmentContext = React.createContext<Environment>(new Environment())
+export const EnvironmentContext = React.createContext<Environment>(Environment.create())

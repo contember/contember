6dd0a0f522f573b2f762d6a9a3ee4197f762447f
Jirka Vebr <jirkavebr@gmail.com>
2019-10-23 11:45:18 +0200

    MutationGenerator: improve create mutations

--- packages/cms-admin/src/binding/model/MutationGenerator.ts
+++ packages/cms-admin/src/binding/model/MutationGenerator.ts
@@ -181,16 +181,24 @@ export class MutationGenerator {
 		return queryBuilder.create(
 			entityName,
 			builder => {
+				let writeBuilder = this.registerCreateMutationPart(
+					entity,
+					entityFields,
+					new CrudQueryBuilder.WriteDataBuilder(),
+				)
+				if (
+					constraints &&
+					constraints.whereType === 'unique' &&
+					writeBuilder.data !== undefined &&
+					!isEmptyObject(writeBuilder.data)
+				) {
+					// Shallow cloning the constraints like this IS too naïve but it will likely last surprisingly long before we
+					// run into issues.
+					writeBuilder = new CrudQueryBuilder.WriteDataBuilder({ ...writeBuilder.data, ...constraints.where })
+				}
+
 				return builder
-					.data(
-						this.registerCreateMutationPart(
-							entity,
-							entityFields,
-							new CrudQueryBuilder.WriteDataBuilder(
-								constraints && constraints.whereType === 'unique' ? constraints.where : undefined,
-							),
-						),
-					)
+					.data(writeBuilder)
 					.node(builder => builder.column(PRIMARY_KEY_NAME))
 					.ok()
 					.validation()

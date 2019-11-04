c8e50dbdfe836c983b61d2d69d078839014bd742
David Matejka <matej21@matej21.cz>
2019-11-04 10:00:22 +0100

    content api: format date in ISO format

--- packages/engine-content-api/tests/cases/integration/graphQlRequests/query.ts
+++ packages/engine-content-api/tests/cases/integration/graphQlRequests/query.ts
@@ -1822,4 +1822,36 @@ describe('Queries', () => {
 			},
 		})
 	})
+
+	it('Format date query', async () => {
+		await execute({
+			schema: new SchemaBuilder()
+				.entity('Post', entity => entity.column('publishedAt', column => column.type(Model.ColumnType.DateTime)))
+				.buildSchema(),
+			query: GQL`
+        query {
+          getPost(by: {id: "${testUuid(1)}"}) {
+            publishedAt
+          }
+        }`,
+			executes: [
+				...sqlTransaction([
+					{
+						sql: SQL`select "root_"."published_at" as "root_publishedAt", "root_"."id" as "root_id"
+                     from "public"."post" as "root_"
+                     where "root_"."id" = ?`,
+						response: { rows: [{ root_id: testUuid(1), root_publishedAt: new Date('2019-11-01T05:00:00.000Z') }] },
+						parameters: [testUuid(1)],
+					},
+				]),
+			],
+			return: {
+				data: {
+					getPost: {
+						publishedAt: '2019-11-01T05:00:00.000Z',
+					},
+				},
+			},
+		})
+	})
 })

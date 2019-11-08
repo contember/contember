aec136b51bb1a41a0b5224503f0ad04eb632e552
David Matejka <matej21@matej21.cz>
2019-11-08 16:28:18 +0100

    schema-definition: inline Interface type

--- packages/schema-definition/src/model/definition/types.ts
+++ packages/schema-definition/src/model/definition/types.ts
@@ -1,7 +1,6 @@
-import { Interface } from '@contember/utils'
 import FieldDefinition from './FieldDefinition'
 
+export type Interface<T> = { [P in keyof T]: T[P] }
 export type RelationTarget = EntityConstructor<EntityType<any>>
 export type EntityType<T> = { [K in keyof T]: Interface<FieldDefinition<any>> }
 export type EntityConstructor<T> = { new (): T }
-export { Interface }

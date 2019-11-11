fb07576e0f7e56948e9701299690f57273531118
David Matejka <matej21@matej21.cz>
2019-11-11 16:10:35 +0100

    content api: conditions - add isNull, depreacate "null"

--- packages/engine-content-api/tests/cases/integration/graphQlRequests/query.ts
+++ packages/engine-content-api/tests/cases/integration/graphQlRequests/query.ts
@@ -1854,4 +1854,45 @@ describe('Queries', () => {
 			},
 		})
 	})
+
+	it('Complex condition', async () => {
+		await execute({
+			schema: new SchemaBuilder()
+				.entity('Post', entity => entity.column('title', column => column.type(Model.ColumnType.String)))
+				.buildSchema(),
+			query: GQL`
+        query {
+          listPost(filter: {title: {or: [
+	          {or: [{eq: "A"}, {eq: "B"}]},
+	          {not: {lt: "X"}},
+	          {and: [{isNull: true}, {isNull: false}]}
+          ]}}) {
+            id
+          }
+        }
+			`,
+			executes: [
+				...sqlTransaction([
+					{
+						sql: SQL`select "root_"."id" as "root_id" 
+from "public"."post" as "root_" 
+where (("root_"."title" = ? or "root_"."title" = ?) 
+      or not("root_"."title" < ?) 
+      or "root_"."title" is null and not("root_"."title" is null))`,
+						parameters: ['A', 'B', 'X'],
+						response: { rows: [{ root_id: testUuid(1) }] },
+					},
+				]),
+			],
+			return: {
+				data: {
+					listPost: [
+						{
+							id: testUuid(1),
+						},
+					],
+				},
+			},
+		})
+	})
 })

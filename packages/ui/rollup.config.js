a5fd1fe7930087822fe885721c6f0f7a944d1095
Jirka Vebr <jirkavebr@gmail.com>
2019-11-11 16:28:06 +0100

    Improve UI build

--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -1,10 +1,31 @@
+//import analyzer from 'rollup-plugin-analyzer'
 import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript'
+import globalTsconfig from './../../tsconfig.settings.json'
 
 export default {
-	input: 'dist/src/index.js',
+	input: 'src/index.ts',
 	output: {
 		file: 'dist/bundle.js',
-		format: 'cjs',
+		format: 'es',
 	},
-	plugins: [commonjs({})],
+	external: ['react', 'react-dom'],
+	plugins: [
+		resolve({
+			preferBuiltins: true,
+			dedupe: ['react', 'react-dom', 'react-is'],
+			// modulesOnly: true
+		}),
+		commonjs({
+			namedExports: {
+				['prop-types']: ['oneOfType', 'func', 'shape', 'any', 'number', 'object', 'bool', 'string'],
+			},
+		}),
+		typescript({
+			...globalTsconfig.compilerOptions,
+			jsx: 'react',
+		}),
+		// analyzer(),
+	],
 }

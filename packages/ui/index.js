82d9de2a9053a95f74953b181784857bbac5e0e4
Jirka Vebr <jirkavebr@gmail.com>
2019-11-12 16:35:15 +0100

    UI: only ES module index.js

--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,7 +1,3 @@
 'use strict'
 
-if (process.env.NODE_ENV === 'production') {
-	module.exports = require('./dist/bundle.js')
-} else {
-	module.exports = require('./dist/src/index.js')
-}
+export * from './dist/src/index.js'

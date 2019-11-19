1d33d89e1e0e1e8ba19e70a69e9219df1ded6697
Jirka Vebr <jirkavebr@gmail.com>
2019-11-19 17:21:18 +0100

    Add box sections

---
+++ packages/ui/src/components/Box/BoxSection.tsx
@@ -0,0 +1,34 @@
+import cn from 'classnames'
+import * as React from 'react'
+import { IncreaseBoxDepth, IncreaseHeadingDepth } from '../../auxiliary'
+import { BoxDepthContext, HeadingDepthContext } from '../../contexts'
+import { BoxDistinction } from '../../types'
+import { toEnumViewClass, toViewClass } from '../../utils'
+import { Heading } from '../Heading'
+
+export interface BoxSectionProps {
+	heading?: React.ReactNode
+	actions?: React.ReactNode
+	children: React.ReactNode
+}
+
+export const BoxSection = React.memo(({ actions, children, heading }: BoxSectionProps) => {
+	const headingDepth = React.useContext(HeadingDepthContext)
+
+	return (
+		<div className={cn('box-section')}>
+			{heading && (
+				<div className="box-section-heading">
+					<Heading depth={headingDepth} size="small">
+						{heading}
+					</Heading>
+				</div>
+			)}
+			{actions && <div className="box-section-actions">{actions}</div>}
+			<div className="box-section-content">
+				<IncreaseHeadingDepth currentDepth={headingDepth}>{children}</IncreaseHeadingDepth>
+			</div>
+		</div>
+	)
+})
+BoxSection.displayName = 'BoxSection'

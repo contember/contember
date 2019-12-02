6bf66fed79425b3c44ed60b087b74750e0561837
David Matejka <matej21@matej21.cz>
2019-12-02 13:31:48 +0100

    docker build script fix

--- scripts/docker/build.sh
+++ scripts/docker/build.sh
@@ -9,7 +9,8 @@ if [[ $VERSION =~ ^v((([0-9]+)\.([0-9]+))\.[0-9]+)$ ]]; then
   ALL_VERSIONS=( "${BASH_REMATCH[1]}" "${BASH_REMATCH[2]}" )
   NEWER_MINOR=$((${BASH_REMATCH[4]} + 1))
   REGEX="^v${BASH_REMATCH[3]}\.${NEWER_MINOR}\.[0-9]+$"
-  NEWER_VERSIONS=$(git tag -l | grep -E "$REGEX" || true )
+  GIT_TAGS=$(git tag -l)
+  NEWER_VERSIONS=$(echo "$GIT_TAGS" | grep -E "$REGEX" || true )
   if [ "${#NEWER_VERSIONS}" -eq 0 ]; then
     ALL_VERSIONS+=( "${BASH_REMATCH[3]}" )
   fi

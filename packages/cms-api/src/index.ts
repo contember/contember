1528a6473f0c621e344489761dbb958f1173249b
David Matejka <matej21@matej21.cz>
2019-12-11 16:26:13 +0100

    api config: usability improvements, add some defaults

--- packages/cms-api/src/index.ts
+++ packages/cms-api/src/index.ts
@@ -1,6 +1,6 @@
 import CompositionRoot from './CompositionRoot'
 import Project from './config/Project'
-import { readConfig } from './config/config'
+import { Config, readConfig } from './config/config'
 import { Schema } from '@contember/schema'
 import { Server } from 'http'
 
@@ -8,11 +8,10 @@ export { CompositionRoot, Project, readConfig }
 
 export async function run(
 	debug: boolean,
-	configFile: string,
+	config: Config,
 	projectsDirectory: string,
 	projectSchemas?: { [name: string]: Schema },
 ): Promise<Server> {
-	let config = await readConfig(configFile)
 	const container = new CompositionRoot().createMasterContainer(debug, config, projectsDirectory, projectSchemas)
 	await container.initializer.initialize()
 	return await container.serverRunner.run()
---
+++ packages/config-loader/src/config/parametersResolving.ts
@@ -0,0 +1,80 @@
+export type Path = (string | number)[]
+export type DataResolver = (path: Path) => any
+export type ParametersResolver = (parameterParts: string[], path: Path, dataResolver: DataResolver) => string
+
+export class UndefinedParameterError extends Error {}
+
+export const createObjectParametersResolver = (parameters: any): ParametersResolver => parts =>
+	parts.reduce((current, part) => {
+		if (current === null || typeof current !== 'object' || typeof current[part] === 'undefined') {
+			throw new UndefinedParameterError(`Parameter "${parts.join('.')}" not found.`)
+		}
+		return current[part]
+	}, parameters)
+
+export const resolveParameters = (data: any, parametersResolver: ParametersResolver): any => {
+	let recursionLevel = 0
+	const dataResolver: DataResolver = path => {
+		const value = path.reduce((value, part) => {
+			if (typeof part === 'string') {
+				if (typeof value === 'object' && value !== null && typeof value[part] !== 'undefined') {
+					return value[part]
+				}
+			} else {
+				if (Array.isArray(value) && typeof value[part] !== 'undefined') {
+					return value[part]
+				}
+			}
+			return undefined
+		}, data)
+		if (recursionLevel++ > 20) {
+			throw new Error('Possible recursion in configuration')
+		}
+		const result = resolveParametersInternal(value, path, (parts, path) =>
+			parametersResolver(parts, path, dataResolver),
+		)
+		recursionLevel--
+		return result
+	}
+	return resolveParametersInternal(data, [], (parts, path) => parametersResolver(parts, path, dataResolver))
+}
+
+const resolveParametersInternal = (
+	data: any,
+	path: Path,
+	parametersResolver: (parts: string[], path: Path) => string,
+): any => {
+	if (Array.isArray(data)) {
+		return data.map((it, index) => resolveParametersInternal(it, [...path, index], parametersResolver))
+	}
+	if (typeof data === 'string') {
+		const match = /^%(\w+(?:\.\w+)*)(?:::(\w+))?%$/.exec(data)
+		if (match) {
+			const [, parameter, cast] = match
+			const parts = parameter.split('.')
+			const value = parametersResolver(parts, path)
+			if (cast) {
+				switch (cast) {
+					case 'number':
+						return Number(value)
+					case 'string':
+						return String(value)
+					default:
+						throw new Error(`Unsupported cast to ${cast}`)
+				}
+			}
+			return value
+		} else {
+			return data
+		}
+	}
+	if (data === null) {
+		return data
+	}
+	if (typeof data === 'object') {
+		return Object.entries(data)
+			.map(([key, value]: [string, any]) => [key, resolveParametersInternal(value, [...path, key], parametersResolver)])
+			.reduce((result, [key, value]) => ({ ...result, [key]: value }), {})
+	}
+	return data
+}

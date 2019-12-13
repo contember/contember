1ae98a032ada0292bdb8a4c7ff48ced93a11a3f0
Jirka Vebr <jirkavebr@gmail.com>
2019-12-13 23:49:04 +0100

    Empty repeaters: include an add button

--- packages/cms-admin/src/components/bindingFacade/collections/helpers/EmptyMessage.tsx
+++ packages/cms-admin/src/components/bindingFacade/collections/helpers/EmptyMessage.tsx
@@ -1,15 +1,11 @@
-import { Box, Message } from '@contember/ui'
+import { Message } from '@contember/ui'
 import * as React from 'react'
 
 export interface EmptyMessageProps {
 	children: React.ReactNode
 }
 
-export const EmptyMessage = React.memo((props: EmptyMessageProps) => {
-	return (
-		<Box>
-			<Message flow="generousBlock">{props.children}</Message>
-		</Box>
-	)
-})
+export const EmptyMessage = React.memo((props: EmptyMessageProps) => (
+	<Message flow="generousBlock">{props.children}</Message>
+))
 EmptyMessage.displayName = 'EmptyMessage'

e6359660369ce40e73b74a35191783521f6b7566
David Matejka <matej21@matej21.cz>
2019-12-13 13:42:34 +0100

    admin: add startup helper

--- packages/cms-admin/src/index.ts
+++ packages/cms-admin/src/index.ts
@@ -1,6 +1,7 @@
 export * from './components/LayoutDefault'
 export * from './acl'
 export * from './binding'
+export * from './bootstrap'
 export * from './components'
 export * from './tenant'
 export { ProjectConfig } from './state/projectsConfigs'

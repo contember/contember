d5c5f2669f2cd9e0608244da1c2fef7f483826f3
Jirka Vebr <jirkavebr@gmail.com>
2019-12-20 17:37:15 +0100

    Export more low-level stuff

--- packages/admin/src/index.ts
+++ packages/admin/src/index.ts
@@ -9,3 +9,9 @@ export * from './tenant'
 export { ProjectConfig } from './state/projectsConfigs'
 export * from './utils/eventSourceReader'
 export * from './reexport'
+
+// Redux
+export * from './state'
+export * from './store'
+export * from './actions'
+export * from './reducer'

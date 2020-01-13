7dba4f1250cfe7bbf1908c7119030ea4efff1b37
David Matejka <matej21@matej21.cz>
2020-01-13 17:27:32 +0100

    add script for contember api update

---
+++ scripts/dev/update-contember-api.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+lernaupdate --non-interactive --dependency "@contember/cli@latest"
+lernaupdate --non-interactive --dependency "@contember/schema-definition@latest"
+lernaupdate --non-interactive --dependency "@contember/schema@latest"

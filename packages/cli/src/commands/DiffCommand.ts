2ff3fb5c94152ba09eed30c66854091ae0f5a363
David Matejka <matej21@matej21.cz>
2020-01-17 13:17:14 +0100

    cli diff command: support generating diff for all projects

--- packages/cli/src/commands/DiffCommand.ts
+++ packages/cli/src/commands/DiffCommand.ts
@@ -6,6 +6,7 @@ import { isDeepStrictEqual } from 'util'
 import { Input } from '../cli/Input'
 import { MigrationsContainerFactory } from '../MigrationsContainer'
 import { getProjectDirectories } from '../NamingHelper'
+import { listProjects } from '../utils/project'
 
 type Args = {
 	projectName: string
@@ -27,35 +28,46 @@ export class DiffCommand extends Command<Args, Options> {
 	}
 
 	protected async execute(input: Input<Args, Options>): Promise<void> {
-		const [projectName, migrationName] = [input.getArgument('projectName'), input.getArgument('migrationName')]
-		const { migrationsDir, projectDir } = getProjectDirectories(projectName, {
-			projectDir: input.getOption('project-dir'),
-			migrationsDir: input.getOption('migrations-dir'),
-		})
-		const schema: Schema = require(projectDir).default
-
-		const container = new MigrationsContainerFactory(migrationsDir).create()
-		const validator = new SchemaValidator()
-		const [validSchema, errors] = validator.validate(schema)
-
-		if (errors.length === 0 && !isDeepStrictEqual(validSchema, schema)) {
-			throw new Error('There is something wrong with a schema validator')
+		const [projectArg, migrationName] = [input.getArgument('projectName'), input.getArgument('migrationName')]
+		const allProjects = projectArg === '.'
+		const projects = allProjects ? await listProjects({ workspaceDirectory: process.cwd() }) : [projectArg]
+		const projectDirArg = input.getOption('project-dir')
+		const migrationsDirArg = input.getOption('migrations-dir')
+		if (allProjects && (projectDirArg || migrationsDirArg)) {
+			throw new Error('Migrations dir and project dir options are not allowed when using "*" as a project name')
 		}
-		if (errors.length > 0) {
-			console.error('Schema is invalid:')
-			for (const err of errors) {
-				console.error('\t' + err.path.join('.') + ': ' + err.message)
+		for (const projectName of projects) {
+			console.group(`Project ${projectName}:`)
+			const { migrationsDir, projectDir } = getProjectDirectories(projectName, {
+				projectDir: projectDirArg,
+				migrationsDir: migrationsDirArg,
+			})
+			const schema: Schema = require(projectDir).default
+
+			const container = new MigrationsContainerFactory(migrationsDir).create()
+			const validator = new SchemaValidator()
+			const [validSchema, errors] = validator.validate(schema)
+
+			if (errors.length === 0 && !isDeepStrictEqual(validSchema, schema)) {
+				throw new Error('There is something wrong with a schema validator')
 			}
-			return
+			if (errors.length > 0) {
+				console.group('Schema is invalid:')
+				for (const err of errors) {
+					console.error(err.path.join('.') + ': ' + err.message)
+				}
+				console.groupEnd()
+				continue
+			}
+
+			const result = await container.migrationDiffCreator.createDiff(schema, migrationName)
+
+			if (result === null) {
+				console.log('Nothing to do')
+			} else {
+				console.log(`${result} created`)
+			}
+			console.groupEnd()
 		}
-
-		const result = await container.migrationDiffCreator.createDiff(schema, migrationName)
-
-		if (result === null) {
-			console.log('Nothing to do')
-			return
-		}
-
-		console.log(`${result} created`)
 	}
 }

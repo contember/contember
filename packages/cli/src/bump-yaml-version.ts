5ebb9dad34e16f11c6ddde5c8f14dad385f60a3c
David Matejka <matej21@matej21.cz>
2020-01-17 13:15:59 +0100

    change structure of contember.workspace.yaml

--- packages/cli/src/bump-yaml-version.ts
+++ packages/cli/src/bump-yaml-version.ts
@@ -3,7 +3,13 @@ import { readJson } from 'fs-extra'
 import { join } from 'path'
 ;(async () => {
 	const { version } = await readJson(join(process.cwd(), 'package.json'))
-	await updateYaml(join(process.cwd(), process.argv[2]), ({ version: _null, ...data }) => ({ version, ...data }))
+	await updateYaml(join(process.cwd(), process.argv[2]), json => ({
+		...json,
+		api: {
+			...(json.api as object),
+			version,
+		},
+	}))
 })().catch(e => {
 	console.log(e)
 	process.exit(1)

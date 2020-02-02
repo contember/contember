ee01d9ba17fc04a31718c972bdaf75252e06555d
Jirka Vebr <jirkavebr@gmail.com>
2020-02-02 22:02:09 +0100

    UI: tweak the way the HoveringToolbar component is to be used

--- packages/ui/src/components/HoveringToolbar.tsx
+++ packages/ui/src/components/HoveringToolbar.tsx
@@ -1,22 +1,24 @@
-import * as React from 'react'
 import cn from 'classnames'
+import * as React from 'react'
 import { useComponentClassName } from '../auxiliary'
-import { toStateClass } from '../utils'
+import { HoveringToolbarScope } from '../types'
+import { toEnumViewClass, toStateClass } from '../utils'
 import { ButtonGroup } from './forms'
-import { Portal } from './Portal'
 
 export interface HoveringToolbarProps {
 	isActive?: boolean
+	scope?: HoveringToolbarScope
 	children: React.ReactNode
 }
 
 export const HoveringToolbar = React.memo(
-	React.forwardRef<HTMLDivElement, HoveringToolbarProps>(({ isActive, children }: HoveringToolbarProps, ref) => (
-		<Portal>
-			<div className={cn(useComponentClassName('hoveringToolbar'), toStateClass('active', isActive))} ref={ref}>
-				<ButtonGroup>{children}</ButtonGroup>
-			</div>
-		</Portal>
+	React.forwardRef<HTMLDivElement, HoveringToolbarProps>(({ isActive, scope, children }: HoveringToolbarProps, ref) => (
+		<div
+			className={cn(useComponentClassName('hoveringToolbar'), toStateClass('active', isActive), toEnumViewClass(scope))}
+			ref={ref}
+		>
+			<ButtonGroup>{children}</ButtonGroup>
+		</div>
 	)),
 )
 HoveringToolbar.displayName = 'HoveringToolbar'

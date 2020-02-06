85a2119f69742c1d531c0c09cc11ed1c5cc995d7
Jirka Vebr <jirkavebr@gmail.com>
2020-02-06 13:38:45 +0100

    Improve anchor rendering

--- packages/admin/src/components/bindingFacade/richText/plugins/anchors/AnchorRenderer.tsx
+++ packages/admin/src/components/bindingFacade/richText/plugins/anchors/AnchorRenderer.tsx
@@ -6,8 +6,24 @@ export interface AnchorRendererProps extends Omit<RenderElementProps, 'element'>
 	element: AnchorElement
 }
 
+const onClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+	// I cannot believe this works.
+	window.open(e.currentTarget.href, '_blank')
+}
+
+const style = {
+	cursor: 'pointer',
+} as const
+
 export const AnchorRenderer: React.FunctionComponent<AnchorRendererProps> = (props: AnchorRendererProps) => (
-	<a {...props.attributes} href={props.element.href}>
+	<a
+		{...props.attributes}
+		href={props.element.href}
+		title={props.element.href}
+		target="_blank"
+		onClickCapture={onClick}
+		style={style}
+	>
 		{props.children}
 	</a>
 )

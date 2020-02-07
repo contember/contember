cea2339fcfa1c267678d47977bcdb41e117f1724
Viliam Kopecky <vk@viliamkopecky.cz>
2020-02-07 18:23:19 +0100

    Layout enhanced example

--- packages/ui/storybook/components/Layout.tsx
+++ packages/ui/storybook/components/Layout.tsx
@@ -1,6 +1,16 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
-import { Button, Layout, LayoutHeading } from '../../src/components'
+import {
+	Button,
+	Layout,
+	LayoutHeading,
+	Trio,
+	Breadcrumbs,
+	ContentStatus,
+	SaveControl,
+	DimensionSwitcher,
+	UserMiniControl,
+} from '../../src/components'
 
 storiesOf('Layout', module)
 	.add('large placeholders', () => {
@@ -30,15 +40,34 @@ storiesOf('Layout', module)
 		return (
 			<Layout
 				topStart={<LayoutHeading label="My Admin" />}
-				topCenter="topCenter"
-				topEnd="topEnd"
+				topCenter={
+					<DimensionSwitcher
+						dimensions={[
+							{ key: 'site', label: 'Site', options: [{ value: 'cz', label: 'CZ', active: true }] },
+							{
+								key: 'lang',
+								label: 'Language',
+								options: [
+									{ value: 'cz', label: 'CZ', active: true },
+									{ value: 'en', label: 'EN', active: true },
+									{ value: 'de', label: 'DE', active: false },
+								],
+							},
+						]}
+					/>
+				}
+				topEnd={<UserMiniControl />}
 				sideBarStart="sideBarStart"
 				sideBarCenter="sideBarCenter"
 				sideBarEnd="sideBarEnd"
-				mainStart="mainStart"
+				mainStart={
+					<div style={{ fontSize: 12, margin: '10px 0' }}>
+						<Trio start={<Breadcrumbs items={[<a href="#">Posts</a>, 'Edit post']} />} end={<ContentStatus />} />
+					</div>
+				}
 				mainCenter="mainCenter"
 				mainEnd="mainEnd"
-				actionsEnd={<Button>Save</Button>}
+				actionsEnd={<SaveControl />}
 			></Layout>
 		)
 	})

b58654f3e481aabd845def5cfeb8a1702d01bc60
David Matejka <matej21@matej21.cz>
2020-02-07 13:53:20 +0100

    cli: better choosing between local, docker and local-in-docker cli

--- packages/cli/contember-docker.sh
+++ packages/cli/contember-docker.sh
@@ -4,11 +4,10 @@ IFS=$'\n\t'
 
 IMAGE="contember/cli:latest"
 
-if [[ -f "`pwd`/node_modules/.bin/contember" ]]; then
+if [[ -f "`pwd`/node_modules/.bin/contember" && -x "$(command -v npx)" ]]; then
 	echo "Using local Contember CLI"
 	npx contember "$@"
 else
-	echo "Using docker Contember CLI"
 	docker run -ti --network host --rm \
 	  -v "$(pwd)":/src -v /var/run/docker.sock:/var/run/docker.sock \
 	  "$IMAGE" "$@"

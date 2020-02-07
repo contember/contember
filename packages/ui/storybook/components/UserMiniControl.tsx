3a577fbd7da36ae78a44dfffefa9c1465d6601ff
Viliam Kopecky <vk@viliamkopecky.cz>
2020-02-07 18:21:58 +0100

    UserMiniControl

---
+++ packages/ui/storybook/components/UserMiniControl.tsx
@@ -0,0 +1,5 @@
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+import { UserMiniControl } from '../../src'
+
+storiesOf('UserMiniControl', module).add('simple', () => <UserMiniControl />)

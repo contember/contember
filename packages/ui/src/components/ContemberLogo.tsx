20deac843701072bae438a6ca47c691e19e73887
Viliam Kopecky <vk@viliamkopecky.cz>
2020-02-07 16:13:29 +0100

    ContemberLogo

---
+++ packages/ui/src/components/ContemberLogo.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import cn from 'classnames'
+import { useClassNamePrefix } from '../auxiliary'
+
+export interface ContemberLogoProps {
+	logotyp?: boolean
+	baseline?: boolean
+	size?: number
+}
+
+export const ContemberLogo = ({ logotyp, baseline, size }: ContemberLogoProps) => {
+	const prefix = useClassNamePrefix()
+	return (
+		<span
+			className={cn(`${prefix}contemberLogo`, logotyp && 'view-logotyp', baseline && 'view-baseline')}
+			style={{ fontSize: `${size ?? 1}em` }}
+		>
+			<span className={`${prefix}contemberLogo-image`}></span>
+		</span>
+	)
+}
+ContemberLogo.displayName = 'ContemberLogo'

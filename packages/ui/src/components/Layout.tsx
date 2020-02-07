33509e4d90f657ac61367cd315436a9b5d6d7fcd
Viliam Kopecky <vk@viliamkopecky.cz>
2020-02-07 16:14:07 +0100

    Layout

--- packages/ui/src/components/Layout.tsx
+++ packages/ui/src/components/Layout.tsx
@@ -1,12 +1,114 @@
+import cn from 'classnames'
 import * as React from 'react'
 import { useClassNamePrefix } from '../auxiliary'
+import { Trio } from './Trio'
 
-export interface LayoutProps {
+interface LayoutProps {
 	children?: React.ReactNode
+
+	top?: React.ReactNode
+	topStart?: React.ReactNode
+	topCenter?: React.ReactNode
+	topEnd?: React.ReactNode
+
+	sideBar?: React.ReactNode
+	sideBarStart?: React.ReactNode
+	sideBarCenter?: React.ReactNode
+	sideBarEnd?: React.ReactNode
+
+	actions?: React.ReactNode
+	actionsStart?: React.ReactNode
+	actionsCenter?: React.ReactNode
+	actionsEnd?: React.ReactNode
+
+	main?: React.ReactNode
+	mainStart?: React.ReactNode
+	mainCenter?: React.ReactNode
+	mainEnd?: React.ReactNode
 }
 
-export const Layout = ({ children }: LayoutProps) => {
+export const Layout = ({
+	children,
+	top,
+	topStart,
+	topCenter,
+	topEnd,
+	sideBar,
+	sideBarStart,
+	sideBarCenter,
+	sideBarEnd,
+	actions,
+	actionsStart,
+	actionsCenter,
+	actionsEnd,
+	main,
+	mainStart,
+	mainCenter,
+	mainEnd,
+}: LayoutProps) => {
 	const prefix = useClassNamePrefix()
-	return <div className={`${prefix}layout`}>{children}</div>
+
+	const [collapsed, setCollapsed] = React.useState(false)
+
+	const toggleCollapsed = React.useCallback(() => {
+		setCollapsed(!collapsed)
+	}, [collapsed, setCollapsed])
+
+	const spaced = React.useCallback(
+		(content?: React.ReactNode) => content && <div className={`${prefix}layout-space`}>{content}</div>,
+		[prefix],
+	)
+
+	return (
+		<div className={`${prefix}layout`}>
+			<div className={`${prefix}layout-topBar`}>
+				{top}
+				{(topStart || topCenter || topEnd) && (
+					<>
+						<div className={`${prefix}layout-topBar-start`}>{spaced(topStart)}</div>
+						<div className={`${prefix}layout-topBar-center`}>{spaced(topCenter)}</div>
+						<div className={`${prefix}layout-topBar-end`}>{spaced(topEnd)}</div>
+					</>
+				)}
+			</div>
+			<div className={`${prefix}layout-bottom`}>
+				<div className={cn(`${prefix}layout-sideBar`, collapsed && 'view-collapsed')}>
+					<button type="button" className={`${prefix}layout-sideBar-collapseButton`} onClick={toggleCollapsed}>
+						<span className={`${prefix}layout-sideBar-collapseButton-label`}>Close</span>
+					</button>
+					<div className={`${prefix}layout-sideBar-in`}>
+						<div className={`${prefix}layout-sideBar-in-in`}>
+							{sideBar}
+							<Trio start={spaced(sideBarStart)} center={spaced(sideBarCenter)} end={spaced(sideBarEnd)} column />
+						</div>
+					</div>
+				</div>
+				<div className={`${prefix}layout-main`}>
+					{(actions || actionsStart || actionsCenter || actionsEnd) && (
+						<div className={`${prefix}layout-mainActions-wrap`}>
+							<div className={`${prefix}layout-mainActions`}>
+								{actions}
+								<Trio start={spaced(actionsStart)} center={spaced(actionsCenter)} end={spaced(actionsEnd)} />
+							</div>
+						</div>
+					)}
+					{main}
+					<Trio
+						start={spaced(
+							(mainStart || children) && (
+								<>
+									{mainStart}
+									{children}
+								</>
+							),
+						)}
+						center={spaced(mainCenter)}
+						end={spaced(mainEnd)}
+						column
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
 Layout.displayName = 'Layout'
--- packages/ui/storybook/components/Layout.tsx
+++ packages/ui/storybook/components/Layout.tsx
@@ -1,7 +1,44 @@
 import { storiesOf } from '@storybook/react'
 import * as React from 'react'
-import { Layout } from '../../src/components'
+import { Button, Layout, LayoutHeading } from '../../src/components'
 
-storiesOf('Layout', module).add('simple', () => {
-	return <Layout />
-})
+storiesOf('Layout', module)
+	.add('large placeholders', () => {
+		return <Layout top="top" main="main" sideBar="sideBar" actions="actions"></Layout>
+	})
+	.add('placeholders', () => {
+		return (
+			<Layout
+				topStart="topStart"
+				topCenter="topCenter"
+				topEnd="topEnd"
+				sideBarStart="sideBarStart"
+				sideBarCenter="sideBarCenter"
+				sideBarEnd="sideBarEnd"
+				mainStart="mainStart"
+				mainCenter="mainCenter"
+				mainEnd="mainEnd"
+				actionsStart="actionsStart"
+				actionsCenter="actionsCenter"
+				actionsEnd="actionsEnd"
+			>
+				children
+			</Layout>
+		)
+	})
+	.add('some components', () => {
+		return (
+			<Layout
+				topStart={<LayoutHeading label="My Admin" />}
+				topCenter="topCenter"
+				topEnd="topEnd"
+				sideBarStart="sideBarStart"
+				sideBarCenter="sideBarCenter"
+				sideBarEnd="sideBarEnd"
+				mainStart="mainStart"
+				mainCenter="mainCenter"
+				mainEnd="mainEnd"
+				actionsEnd={<Button>Save</Button>}
+			></Layout>
+		)
+	})

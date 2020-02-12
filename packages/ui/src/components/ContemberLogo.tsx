6a13aff04ab9f3b98ae15dc03ecc805c3a48dc54
Viliam Kopecky <vk@viliamkopecky.cz>
2020-02-12 18:09:12 +0100

    Rename logotyp => logotype

--- packages/ui/src/components/ContemberLogo.tsx
+++ packages/ui/src/components/ContemberLogo.tsx
@@ -3,16 +3,16 @@ import cn from 'classnames'
 import { useClassNamePrefix } from '../auxiliary'
 
 export interface ContemberLogoProps {
-	logotyp?: boolean
+	logotype?: boolean
 	baseline?: boolean
 	size?: number
 }
 
-export const ContemberLogo = ({ logotyp, baseline, size }: ContemberLogoProps) => {
+export const ContemberLogo = ({ logotype, baseline, size }: ContemberLogoProps) => {
 	const prefix = useClassNamePrefix()
 	return (
 		<span
-			className={cn(`${prefix}contemberLogo`, logotyp && 'view-logotyp', baseline && 'view-baseline')}
+			className={cn(`${prefix}contemberLogo`, logotype && 'view-logotype', baseline && 'view-baseline')}
 			style={{ fontSize: `${size ?? 1}em` }}
 		>
 			<span className={`${prefix}contemberLogo-image`} />

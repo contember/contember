1393f154079773de0ce984a4aee80c28654486d4
Viliam Kopecky <vk@viliamkopecky.cz>
2020-02-12 18:54:46 +0100

    SaveControl dynamic content

--- packages/ui/src/components/SaveControl.tsx
+++ packages/ui/src/components/SaveControl.tsx
@@ -2,9 +2,13 @@ import * as React from 'react'
 import cn from 'classnames'
 import { useClassNamePrefix, useCloseOnEscapeOrClickOutside } from '../auxiliary'
 
-export interface SaveControlProps {}
+export interface SaveControlProps {
+	primaryAction?: React.ReactNode
+	onPrimary?: () => void
+	children?: React.ReactNode
+}
 
-export function SaveControl({}: SaveControlProps) {
+export function SaveControl({ primaryAction, children, onPrimary }: SaveControlProps) {
 	const prefix = useClassNamePrefix()
 	const [open, setOpen] = React.useState(false)
 
@@ -21,22 +25,17 @@ export function SaveControl({}: SaveControlProps) {
 	return (
 		<div className={cn(`${prefix}saveControl`, open && 'view-open')}>
 			<div className={cn(`${prefix}saveControl-button`)} ref={refs.buttonRef}>
-				<button type="button" className={cn(`${prefix}saveControl-button-primary`)}>
-					<div className={cn(`${prefix}saveControl-button-label`)}>Save</div>
+				<button type="button" className={cn(`${prefix}saveControl-button-primary`)} onClick={onPrimary}>
+					<div className={cn(`${prefix}saveControl-button-label`)}>{primaryAction || 'Save'}</div>
 				</button>
-				<button type="button" className={cn(`${prefix}saveControl-button-toggle`)} onClick={toggleOpen} />
+				{children && <button type="button" className={cn(`${prefix}saveControl-button-toggle`)} onClick={toggleOpen} />}
 			</div>
 
-			<div className={cn(`${prefix}saveControl-window`)} ref={refs.contentRef}>
-				<div className={cn(`${prefix}saveControl-window-in`)}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer egestas tempor ipsum, sit amet pulvinar mi
-					hendrerit vel. Morbi suscipit turpis eu tincidunt interdum. Suspendisse potenti. Suspendisse sagittis aliquam
-					egestas. Suspendisse id condimentum nisi. Phasellus vel nibh non turpis venenatis egestas quis ac orci. Mauris
-					magna magna, cursus quis mi eget, congue luctus lorem. Pellentesque nunc diam, pellentesque ac nunc aliquet,
-					mattis sagittis orci. Donec vehicula feugiat viverra. Vivamus vestibulum ligula vitae placerat fermentum.
-					Morbi vel purus quis nisl auctor sagittis.
+			{children && (
+				<div className={cn(`${prefix}saveControl-window`)} ref={refs.contentRef}>
+					<div className={cn(`${prefix}saveControl-window-in`)}>{children}</div>
 				</div>
-			</div>
+			)}
 		</div>
 	)
 }

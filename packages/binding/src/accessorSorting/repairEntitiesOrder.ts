7eb619d773f35b4e0308fac52d7ffce9ec0cc7e9
Jirka Vebr <jirkavebr@gmail.com>
2020-02-19 10:31:52 +0100

    Add repairEntitiesOrder

---
+++ packages/binding/src/accessorSorting/repairEntitiesOrder.ts
@@ -0,0 +1,23 @@
+import { EntityAccessor, EntityListAccessor } from '../accessors'
+import { RelativeSingleField } from '../treeParameters'
+
+export const repairEntitiesOrder = (
+	sortableByField: RelativeSingleField,
+	entityList: EntityListAccessor,
+	sortedEntities: EntityAccessor[],
+) => {
+	entityList.batchUpdates(getAccessor => {
+		let listAccessor: EntityListAccessor = getAccessor()
+		for (let i = 0, len = sortedEntities.length; i < len; i++) {
+			const entity = sortedEntities[i]
+			const orderField = entity.getRelativeSingleField(sortableByField)
+
+			if (orderField.currentValue !== i) {
+				// TODO ideally, this condition should just be `orderField.currentValue === null`
+				// We should generally try to touch the indexes less
+				orderField.updateValue?.(i)
+				listAccessor = getAccessor()
+			}
+		}
+	})
+}

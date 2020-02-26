100bff274eb63a25b547dba571598cfb7a689c90
Filip Chalupa <chalupa.filip@gmail.com>
2020-02-26 14:29:30 +0100

    DateTimeField component

---
+++ packages/admin/src/components/bindingFacade/fields/DateTimeField.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { DateFieldProps, DateFieldInner } from './DateField'
+import { SimpleRelativeSingleField } from '../auxiliary'
+
+export type DateTimeFieldProps = Omit<DateFieldProps, 'showTimeSelect'>
+
+export const DateTimeField = SimpleRelativeSingleField<DateTimeFieldProps, string>(
+	(fieldMetadata, { dateFormat, ...props }) => {
+		return <DateFieldInner fieldMetadata={fieldMetadata} {...props} dateFormat={dateFormat || 'Pp'} showTimeSelect />
+	},
+	'DateTimeField',
+)

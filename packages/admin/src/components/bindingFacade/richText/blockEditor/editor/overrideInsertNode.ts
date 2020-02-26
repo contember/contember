a70771a69e6b9a19236bdd6c1f03fa5d73a1522f
Jirka Vebr <jirkavebr@gmail.com>
2020-02-26 11:36:21 +0100

    Improve node insertion places

--- packages/admin/src/components/bindingFacade/richText/blockEditor/editor/overrideInsertNode.ts
+++ packages/admin/src/components/bindingFacade/richText/blockEditor/editor/overrideInsertNode.ts
@@ -1,5 +1,5 @@
 import { EntityAccessor } from '@contember/binding'
-import { Editor, Node as SlateNode, Range as SlateRange, Transforms } from 'slate'
+import { Editor, Node as SlateNode, Point, Range as SlateRange, Transforms } from 'slate'
 import { isContemberBlockElement } from '../elements'
 import { BlockSlateEditor } from './BlockSlateEditor'
 
@@ -21,10 +21,11 @@ export const overrideInsertNode = <E extends BlockSlateEditor>(editor: E, option
 			if (!selection || SlateRange.isExpanded(selection)) {
 				return
 			}
-			Editor.withoutNormalizing(editor, () => {
-				let [topLevelIndex] = selection.focus.path
 
+			const [topLevelIndex] = selection.focus.path
+			Editor.withoutNormalizing(editor, () => {
 				if (SlateNode.string(editor.children[topLevelIndex]) === '') {
+					// The current element is empty so we replace remove it and insert the new one in its place.
 					Transforms.removeNodes(editor, {
 						at: [topLevelIndex],
 					})
@@ -32,9 +33,27 @@ export const overrideInsertNode = <E extends BlockSlateEditor>(editor: E, option
 						at: [topLevelIndex],
 					})
 				} else {
-					Transforms.insertNodes(editor, node, {
-						at: [topLevelIndex + 1],
-					})
+					const [start, end] = Editor.edges(editor, [topLevelIndex])
+
+					if (Point.equals(start, selection.focus)) {
+						// We're at the beginning of a block so we insert above it
+						Transforms.insertNodes(editor, node, {
+							at: [topLevelIndex],
+						})
+					} else if (Point.equals(end, selection.focus)) {
+						// We're at the end of a block so we insert underneath it.
+						Transforms.insertNodes(editor, node, {
+							at: [topLevelIndex + 1],
+						})
+					} else {
+						// We're in the middle so we split it and then insert between the two resulting chunks.
+						Transforms.splitNodes(editor, {
+							at: selection.focus,
+						})
+						Transforms.insertNodes(editor, node, {
+							at: [topLevelIndex + 1],
+						})
+					}
 				}
 			})
 		})

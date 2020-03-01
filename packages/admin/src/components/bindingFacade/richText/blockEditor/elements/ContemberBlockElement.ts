78740a7a743bd8d5c222d30fc37d013e4a9d34eb
Jirka Vebr <jirkavebr@gmail.com>
2020-03-01 20:11:58 +0100

    Add a todo comment

--- packages/admin/src/components/bindingFacade/richText/blockEditor/elements/ContemberBlockElement.ts
+++ packages/admin/src/components/bindingFacade/richText/blockEditor/elements/ContemberBlockElement.ts
@@ -7,7 +7,7 @@ export const contemberBlockElementType: ContemberBlockElementType = '__contember
 export interface ContemberBlockElement extends Element {
 	type: ContemberBlockElementType
 	blockType: FieldValue
-	entityKey: string
+	entityKey: string // TODO this could probably be feasibly replaced by an index of the entities array
 }
 
 export const isContemberBlockElement = (node: Node): node is ContemberBlockElement =>

cfb0659bcac5f4aba3f78da94a2004818f6f4013
Jirka Vebr <jirkavebr@gmail.com>
2020-03-01 22:32:18 +0100

    Add a new helper

---
+++ packages/admin/src/components/bindingFacade/richText/ContemberEditor/topLevelNodes.ts
@@ -0,0 +1,12 @@
+import { Editor as SlateEditor, Element as SlateElement } from 'slate'
+import { BaseEditor } from '../plugins/essentials'
+
+export const topLevelNodes = <E extends BaseEditor>(editor: E) => {
+	// We manually filter out void nodes because it appears that Slate doesn't respect the voids setting from here.
+	// The combination of isElement and mode: 'highest' is really just a roundabout way of excluding the Editor.
+	return SlateEditor.nodes(editor, {
+		match: node => SlateElement.isElement(node) && !editor.isVoid(node),
+		mode: 'highest',
+		voids: false,
+	})
+}

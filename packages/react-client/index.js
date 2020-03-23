7237bae55c2875bd001d64db38d1fa708f41aa6d
Jirka Vebr <jirkavebr@gmail.com>
2020-03-23 21:40:06 +0100

    Runtime support for __DEV_MODE_

--- packages/react-client/index.js
+++ packages/react-client/index.js
@@ -3,5 +3,6 @@
 if (process.env.NODE_ENV === 'production') {
 	module.exports = require('./dist/bundle.js')
 } else {
+	;(globalThis || window || {})['__DEV_MODE__'] = true
 	module.exports = require('./dist/src/index.js')
 }

84c438c1b9e3baed27d02c3564d9f797c5495642
Jirka Vebr <jirkavebr@gmail.com>
2020-03-25 13:25:51 +0100

    Unify element typings

--- packages/admin/src/components/bindingFacade/richText/plugins/headings/HeadingElement.ts
+++ packages/admin/src/components/bindingFacade/richText/plugins/headings/HeadingElement.ts
@@ -3,7 +3,7 @@ import { BaseEditor, ElementNode } from '../essentials'
 export const headingElementType = 'heading' as const
 
 export interface HeadingElement extends ElementNode {
-	type: 'heading'
+	type: typeof headingElementType
 	level: 1 | 2
 	children: BaseEditor['children']
 }

deef6dc5f8e49fa89efbd2bacfaf516eb3631e50
Jirka Vebr <jirkavebr@gmail.com>
2020-04-03 13:44:37 +0200

    Allow for heading levels all the way to h6

--- packages/admin/src/components/bindingFacade/richText/plugins/headings/HeadingElement.ts
+++ packages/admin/src/components/bindingFacade/richText/plugins/headings/HeadingElement.ts
@@ -4,7 +4,7 @@ export const headingElementType = 'heading' as const
 
 export interface HeadingElement extends ElementNode {
 	type: typeof headingElementType
-	level: 1 | 2
+	level: 1 | 2 | 3 | 4 | 5 | 6
 	isNumbered?: boolean
 	children: BaseEditor['children']
 }

931829955e481e52173d60d190ec63ffeb7d3e4b
Jirka Vebr <jirkavebr@gmail.com>
2020-04-04 14:07:00 +0200

    Add the hasMarks helper

---
+++ packages/admin/src/components/bindingFacade/richText/ContemberEditor/hasMarks.ts
@@ -0,0 +1,30 @@
+import { Node as SlateNode, Path } from 'slate'
+import { BaseEditor, TextNode, TextSpecifics } from '../plugins'
+
+export const hasMarks = <T extends TextNode, E extends BaseEditor>(
+	editor: E,
+	marks: TextSpecifics<T>,
+	options: {
+		from?: Path
+		to?: Path
+	} = {},
+) => {
+	const from = options.from ?? editor.selection?.anchor.path
+	const to = options.to ?? editor.selection?.focus.path
+
+	if (!from && !to) {
+		return false
+	}
+
+	for (const [text] of SlateNode.texts(editor, {
+		from,
+		to,
+	})) {
+		for (const markName in marks) {
+			if (!(markName in text) || text[markName] !== (marks as any)[markName]) {
+				return false
+			}
+		}
+	}
+	return true
+}
--- packages/admin/src/components/bindingFacade/richText/ContemberEditor/index.ts
+++ packages/admin/src/components/bindingFacade/richText/ContemberEditor/index.ts
@@ -1,9 +1,11 @@
 import { ejectElement } from './ejectElement'
+import { hasMarks } from './hasMarks'
 import { isElementType } from './isElementType'
 import { topLevelNodes } from './topLevelNodes'
 
 export const ContemberEditor = {
 	ejectElement,
+	hasMarks,
 	isElementType,
 	topLevelNodes,
 }

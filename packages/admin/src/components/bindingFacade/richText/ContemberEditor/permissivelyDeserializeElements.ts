e082b5d22f1fd7d0b2148641d3619c91d88f1942
Jirka Vebr <jirkavebr@gmail.com>
2020-04-06 22:46:23 +0200

    Add (de)serialization implementation to ContemberEditor

---
+++ packages/admin/src/components/bindingFacade/richText/ContemberEditor/permissivelyDeserializeElements.ts
@@ -0,0 +1,28 @@
+import { BaseEditor, ElementNode, TextNode } from '../baseEditor'
+import { Text as SlateText } from 'slate'
+
+export const permissivelyDeserializeElements = <E extends BaseEditor>(
+	editor: E,
+	serializedElement: string,
+): ElementNode[] => {
+	try {
+		const deserialized:
+			| {
+					formatVersion: number
+					children: ElementNode[] | TextNode[]
+			  }
+			| ElementNode = JSON.parse(serializedElement)
+
+		if ('formatVersion' in deserialized) {
+			const children = deserialized.children
+
+			if (SlateText.isText(children[0])) {
+				return [editor.createDefaultElement(children)]
+			}
+			return children as ElementNode[]
+		}
+		return [deserialized]
+	} catch (e) {
+		return [editor.createDefaultElement([{ text: serializedElement }])]
+	}
+}

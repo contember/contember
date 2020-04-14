db14656b8f1449355691b8acf4b9af0ddc5c3c1e
Jirka Vebr <jirkavebr@gmail.com>
2020-04-14 14:29:31 +0200

    Refine file renderer api

--- packages/admin/src/components/bindingFacade/upload/fileRenderers/FileRenderer.ts
+++ packages/admin/src/components/bindingFacade/upload/fileRenderers/FileRenderer.ts
@@ -1,10 +1,10 @@
-import { EntityAccessor } from '@contember/binding'
+import { EntityAccessor, Environment } from '@contember/binding'
 import * as React from 'react'
 
 export interface FileRenderer {
 	canPreviewFile?: (file: File, previewUrl: string) => boolean
 	renderFilePreview?: (file: File, previewUrl: string) => React.ReactNode
 
-	canRenderFile: (entityAccessor: EntityAccessor) => boolean
-	renderFile: (entityAccessor: EntityAccessor) => React.ReactNode
+	canRenderFile: (entityAccessor: EntityAccessor, environment: Environment) => boolean
+	renderFile: (entityAccessor: EntityAccessor, environment: Environment) => React.ReactNode
 }

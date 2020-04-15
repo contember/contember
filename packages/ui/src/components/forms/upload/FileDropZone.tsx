ba63fa42b0bf03fdf4f7c8ee10f3e75c8959499e
Jirka Vebr <jirkavebr@gmail.com>
2020-04-15 21:30:16 +0200

    FileDropZone: forwardRef

--- packages/ui/src/components/forms/upload/FileDropZone.tsx
+++ packages/ui/src/components/forms/upload/FileDropZone.tsx
@@ -3,13 +3,17 @@ import { useClassNamePrefix } from '../../../auxiliary'
 import cn from 'classnames'
 import { toStateClass } from '../../../utils'
 
-export interface FileDropZoneProps {
-	isActive: boolean
+export interface FileDropZoneProps extends React.HTMLAttributes<HTMLDivElement> {
+	isActive?: boolean
 	children?: React.ReactNode
 }
 
-export const FileDropZone = React.memo(({ isActive, children }: FileDropZoneProps) => {
-	const prefix = useClassNamePrefix()
-	return <div className={cn(`${prefix}fileDropZone`, toStateClass('active', isActive))}>{children}</div>
-})
+export const FileDropZone = React.memo(
+	React.forwardRef<HTMLDivElement, FileDropZoneProps>(({ isActive, className, ...props }, ref) => {
+		const prefix = useClassNamePrefix()
+		return (
+			<div {...props} className={cn(`${prefix}fileDropZone`, toStateClass('active', isActive), className)} ref={ref} />
+		)
+	}),
+)
 FileDropZone.displayName = 'FileDropZone'

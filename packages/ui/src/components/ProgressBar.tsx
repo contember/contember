94ef37412358dc9ff4800bd7b95f12aaaf9aa571
Jirka Vebr <jirkavebr@gmail.com>
2020-04-15 13:44:47 +0200

    Add the ProgressBar component

---
+++ packages/ui/src/components/ProgressBar.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import { useClassNamePrefix } from '../auxiliary'
+
+export interface ProgressBarProps {
+	progress: number
+}
+
+export const ProgressBar = React.memo(({ progress }: ProgressBarProps) => {
+	const clampedProgress = Math.max(0, Math.min(progress, 1))
+	const prefix = useClassNamePrefix()
+
+	return (
+		<div className={`${prefix}progressBar`}>
+			<div className={`${prefix}progressBar-bar`} style={{ transform: `scaleX(${clampedProgress})` }} />
+		</div>
+	)
+})
+ProgressBar.displayName = 'ProgressBar'

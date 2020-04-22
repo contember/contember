c7cac6e011e4183f6c9c135ffb7edc520579e6fc
David Matejka <matej21@matej21.cz>
2020-04-22 15:48:02 +0200

    npm publish tagging: tag prelease as a "next"

--- scripts/npm/publish.sh
+++ scripts/npm/publish.sh
@@ -4,5 +4,9 @@ echo "registry=https://registry.npmjs.org/
 //registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN
 access=public" > ~/.npmrc
 
+NPM_TAG="next"
+if [[ $VERSION =~ ^v((([0-9]+)\.([0-9]+))\.[0-9]+)$ ]]; then
+	NPM_TAG="latest"
+fi
 
-npx lerna publish from-git --yes
+npx lerna publish from-git --yes --dist-tag $NPM_TAG

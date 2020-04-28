a8578ce7f2af8392e49a366ea41eb33e604576da
Jirka Vebr <jirkavebr@gmail.com>
2020-04-28 15:19:16 +0200

    Further refine sortable start cancelling

--- packages/admin/src/components/bindingFacade/collections/Repeater/shouldCancelStart.ts
+++ packages/admin/src/components/bindingFacade/collections/Repeater/shouldCancelStart.ts
@@ -1,28 +1,28 @@
 import { SortableContainerProps } from 'react-sortable-hoc'
 
+const interactiveElements = new Set(['INPUT', 'TEXTAREA', 'SELECT', 'OPTION', 'BUTTON'])
+
 // This is almost completely copied from the defaultShouldCancelStart.js from react-sortable-hoc.
 // We just needed to add support for slate void elements as well as some additional checks.
 export const shouldCancelStart: SortableContainerProps['shouldCancelStart'] = event => {
 	// Cancel sorting if the event target is an `input`, `textarea`, `select` or `option`
-	const interactiveElements = ['INPUT', 'TEXTAREA', 'SELECT', 'OPTION', 'BUTTON']
-
-	if (!(event.target instanceof Node)) {
-		return true
-	}
-
-	if (event.target instanceof Element && interactiveElements.indexOf(event.target.tagName) !== -1) {
-		// Return true to cancel sorting
+	if (!(event.target instanceof Element)) {
 		return true
 	}
 
 	let el: Node | null = event.target
 
 	while (el) {
-		if (el instanceof Element && el.getAttribute('data-slate-void') === 'true') {
-			return false
-		}
-		if (el instanceof HTMLElement && el.contentEditable === 'true') {
-			return true
+		if (el instanceof Element) {
+			if (interactiveElements.has(el.tagName)) {
+				return true
+			}
+			if (el.getAttribute('data-slate-void') === 'true') {
+				return false
+			}
+			if (el instanceof HTMLElement && el.contentEditable === 'true') {
+				return true
+			}
 		}
 		el = el.parentNode
 	}

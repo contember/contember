cffe6b30c909ae867fed3e6e2305c842884dfa9c
Jirka Vebr <jirkavebr@gmail.com>
2020-04-29 19:52:11 +0200

    Add a vimeo embed handler

--- packages/admin/src/components/bindingFacade/richText/blockEditor/embed/EmbedHandlers.ts
+++ packages/admin/src/components/bindingFacade/richText/blockEditor/embed/EmbedHandlers.ts
@@ -1,5 +1,7 @@
+import { VimeoEmbedHandler } from './VimeoEmbedHandler'
 import { YouTubeEmbedHandler } from './YouTubeEmbedHandler'
 
 export namespace EmbedHandlers {
+	export const Vimeo = VimeoEmbedHandler
 	export const YouTube = YouTubeEmbedHandler
 }

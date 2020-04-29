c65ad6511616ed172dad36ec3c0df068b2a37949
Jirka Vebr <jirkavebr@gmail.com>
2020-04-29 18:27:18 +0200

    Use new discrimination routines

--- packages/admin/src/components/bindingFacade/blocks/Block.tsx
+++ packages/admin/src/components/bindingFacade/blocks/Block.tsx
@@ -1,6 +1,6 @@
-import { GraphQlBuilder } from '@contember/client'
+import { Component } from '@contember/binding'
 import * as React from 'react'
-import { Component, FieldValue, Scalar, VariableLiteral, VariableScalar } from '@contember/binding'
+import { SugaredDiscriminateBy, SugaredDiscriminateByScalar } from '../discrimination'
 
 export interface BlockCommonProps {
 	label?: React.ReactNode
@@ -8,9 +8,6 @@ export interface BlockCommonProps {
 	alternate?: React.ReactNode
 	children: React.ReactNode
 }
-
-export type SugaredDiscriminateBy = GraphQlBuilder.Literal | VariableLiteral | string
-export type SugaredDiscriminateByScalar = Scalar | VariableScalar
 
 export interface LiteralBasedBlockProps extends BlockCommonProps {
 	discriminateBy: SugaredDiscriminateBy
@@ -20,27 +17,6 @@ export interface ScalarBasedBlockProps extends BlockCommonProps {
 }
 
 export type BlockProps = LiteralBasedBlockProps | ScalarBasedBlockProps
-
-export interface NormalizedBlockCommonProps extends BlockCommonProps {
-	discriminateBy: FieldValue
-}
-
-export interface NormalizedLiteralBasedBlock extends NormalizedBlockCommonProps {
-	discriminateBy: GraphQlBuilder.Literal
-}
-export interface NormalizedScalarBasedBlock extends NormalizedBlockCommonProps {
-	discriminateBy: Scalar
-}
-
-export type NormalizedBlocks =
-	| {
-			discriminationKind: 'literal'
-			blocks: Map<string, NormalizedLiteralBasedBlock>
-	  }
-	| {
-			discriminationKind: 'scalar'
-			blocks: Map<Scalar, NormalizedScalarBasedBlock>
-	  }
 
 export const Block = Component<BlockProps>(
 	props => <>{props.children}</>,
--- packages/admin/src/components/bindingFacade/blocks/useNormalizedBlocks.ts
+++ packages/admin/src/components/bindingFacade/blocks/useNormalizedBlocks.ts
@@ -1,55 +1,19 @@
-import { BindingError, Scalar, useEnvironment, VariableInputTransformer } from '@contember/binding'
 import * as React from 'react'
-import { BlockCommonProps, NormalizedBlocks, NormalizedLiteralBasedBlock, NormalizedScalarBasedBlock } from './Block'
+import { NormalizedDiscriminatedData, useDiscriminatedData } from '../discrimination'
+import { BlockCommonProps, LiteralBasedBlockProps, ScalarBasedBlockProps } from './Block'
 import { useBlockProps } from './useBlockProps'
 
+export type NormalizedBlocks = NormalizedDiscriminatedData<BlockCommonProps>
+
 export const useNormalizedBlocks = (children: React.ReactNode): NormalizedBlocks => {
-	const environment = useEnvironment()
 	const propList = useBlockProps(children)
-	return React.useMemo<NormalizedBlocks>(() => {
-		const literalBased: Map<string, NormalizedLiteralBasedBlock> = new Map()
-		const scalarBased: Map<Scalar, NormalizedScalarBasedBlock> = new Map()
 
-		if (propList.length === 0) {
-			return {
-				discriminationKind: 'literal',
-				blocks: literalBased,
-			}
-		}
-
-		for (const props of propList) {
-			const commonProps: BlockCommonProps = props
-			if ('discriminateBy' in props) {
-				const literal = VariableInputTransformer.transformVariableLiteral(props.discriminateBy, environment)
-				literalBased.set(literal.value, {
-					...commonProps,
-					discriminateBy: literal,
-				})
-			} else if ('discriminateByScalar' in props) {
-				const scalar = VariableInputTransformer.transformVariableScalar(props.discriminateByScalar, environment)
-				scalarBased.set(scalar, {
-					...commonProps,
-					discriminateBy: scalar,
-				})
-			}
-		}
-
-		if (scalarBased.size && literalBased.size === 0) {
-			return {
-				discriminationKind: 'scalar',
-				blocks: scalarBased,
-			}
-		}
-		if (literalBased.size && scalarBased.size === 0) {
-			return {
-				discriminationKind: 'literal',
-				blocks: literalBased,
-			}
-		}
-
-		throw new BindingError(
+	return useDiscriminatedData<LiteralBasedBlockProps, ScalarBasedBlockProps>(propList, {
+		undiscriminatedItemMessage:
+			`Each block must be discriminated by either exactly one of the ` +
+			`'discriminateBy' or 'discriminateByScalar' props.`,
+		mixedDiscriminationMessage:
 			`Detected a set of Block components of non-uniform discrimination methods. ` +
-				`They all have to use either 'discriminateBy' or 'discriminateByScalar'.`,
-		)
-	}, [environment, propList])
+			`They all have to use either 'discriminateBy' or 'discriminateByScalar'.`,
+	})
 }

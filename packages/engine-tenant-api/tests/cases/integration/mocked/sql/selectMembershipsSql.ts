1d365934d730dec4db848460099253b70ef59256
David Matejka <matej21@matej21.cz>
2020-05-18 11:23:28 +0200

    tenant api: fix fetching project membeership in a batch query

--- packages/engine-tenant-api/tests/cases/integration/mocked/sql/selectMembershipsSql.ts
+++ packages/engine-tenant-api/tests/cases/integration/mocked/sql/selectMembershipsSql.ts
@@ -8,14 +8,14 @@ export const selectMembershipsSql = (args: {
 	membershipsResponse: Membership[]
 }): ExpectedQuery => ({
 	sql: SQL`
-		with "memberships" as (select "project_membership"."id", "project_membership"."role"
+		with "memberships" as (select "project_membership"."id", "project_membership"."role", "project_membership"."identity_id"
 		                       from "tenant"."project_membership"
 		                       where "identity_id" IN (?) and "project_id" = ?),
 			"variables" as (select "membership_id", json_agg(json_build_object('name', variable, 'values', value)) as "variables"
 			                from "tenant"."project_membership_variable"
 				                     inner join "memberships" on "project_membership_variable"."membership_id" = "memberships"."id"
 			                group by "membership_id")
-		select "role", coalesce(variables, '[]'::json) as "variables"
+		select "role", coalesce(variables, '[]'::json) as "variables", "identity_id" as "identityId"
 		from "memberships"
 			     left join "variables" on "memberships"."id" = "variables"."membership_id"`,
 	parameters: [args.identityId, args.projectId],

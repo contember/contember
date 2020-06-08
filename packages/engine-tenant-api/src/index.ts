bffcda31142589e82dabed0d34246249017f40c2
David Matejka <matej21@matej21.cz>
2020-06-08 14:57:32 +0200

    tenant: allow to set login token and superadmin credentials using env variables

--- packages/engine-tenant-api/src/index.ts
+++ packages/engine-tenant-api/src/index.ts
@@ -4,5 +4,6 @@ export * from './model'
 export * from './TenantContainer'
 export * from './resolvers'
 export * from './utils'
+export * from './migrations'
 export { Schema }
 export { default as typeDefs } from './schema/tenant.graphql'

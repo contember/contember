f169566b958edbbf09625af5f31f9939983b9cdb
Jirka Vebr <jirkavebr@gmail.com>
2020-06-15 11:20:44 +0200

    Add a comment

--- packages/binding/src/accessors/TreeRootAccessor.ts
+++ packages/binding/src/accessors/TreeRootAccessor.ts
@@ -4,5 +4,10 @@ import { GetSubTree } from './GetSubTree'
 // This allows us to have several parallel sub-trees without one having to be the main tree
 // and all the other ones subordinate.
 export class TreeRootAccessor {
+	/**
+	 * Whenever an update occurs, a new instance of this class is created.
+	 * @param getEntityByKey Guaranteed to be referentially stable between updates.
+	 * @param getSubTree Guaranteed to be referentially stable between updates.
+	 */
 	public constructor(public readonly getEntityByKey: GetEntityByKey, public readonly getSubTree: GetSubTree) {}
 }

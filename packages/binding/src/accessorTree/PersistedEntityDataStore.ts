97e54ccf3982da5d485abbbd0ebcdee768526785
Jirka Vebr <jirkavebr@gmail.com>
2020-06-17 08:58:47 +0200

    Better comment

--- packages/binding/src/accessorTree/PersistedEntityDataStore.ts
+++ packages/binding/src/accessorTree/PersistedEntityDataStore.ts
@@ -4,8 +4,8 @@ export class BoxedHasOneRelationId {
 	public constructor(public readonly id: string) {}
 }
 
-// hasMany relations are encoded as sets of entity ids
-// hasOne relations are encoded as BoxedHasOneRelationId just to make them different from actual strings.
+// HasMany relations are encoded as sets of entity ids.
+// HasOne relations are encoded as BoxedHasOneRelationId just to make them different from actual strings.
 //		Not sure whether that is a reasonable precaution or just pure overhead.
 export type SingleEntityPersistedData = Map<string, Scalar | BoxedHasOneRelationId | Set<string>>
 

e8ab625125a973fda7ea1afef8c0354b3d16d8fd
Jirka Vebr <jirkavebr@gmail.com>
2020-06-28 13:09:20 +0200

    Mutation component merging

--- packages/client/src/crudQueryBuilder/WriteDataBuilder.ts
+++ packages/client/src/crudQueryBuilder/WriteDataBuilder.ts
@@ -1,6 +1,7 @@
-import { Input } from '@contember/schema'
-import { isEmptyObject } from '../utils'
+import { Input, Value } from '@contember/schema'
 import { Literal } from '../graphQlBuilder'
+import { isEmptyObject } from '../utils'
+import { CrudQueryBuilderError } from './CrudQueryBuilderError'
 import { WriteOperation } from './types'
 import { WriteManyRelationBuilder } from './WriteManyRelationBuilder'
 import { WriteOneRelationBuilder } from './WriteOneRelationBuilder'
@@ -39,17 +40,92 @@ class WriteDataBuilder<Op extends WriteOperation.ContentfulOperation> {
 		const resolvedData = WriteManyRelationBuilder.instantiateFromFactory(data).data
 		return resolvedData === undefined || resolvedData.length === 0
 			? this
-			: new WriteDataBuilder<Op>({
-					...this.data,
-					[fieldName]: resolvedData,
-			  })
+			: new WriteDataBuilder<Op>(this.mergeInFreshData(this.data, fieldName, resolvedData))
 	}
 
 	public one(fieldName: string, data: WriteOneRelationBuilder.BuilderFactory<Op>): WriteDataBuilder<Op> {
 		const resolvedData = WriteOneRelationBuilder.instantiateFromFactory(data).data
 		return resolvedData === undefined || isEmptyObject(resolvedData)
 			? this
-			: new WriteDataBuilder<Op>({ ...this.data, [fieldName]: resolvedData })
+			: new WriteDataBuilder<Op>(this.mergeInFreshData(this.data, fieldName, resolvedData))
+	}
+
+	private mergeInFreshData(
+		original: WriteDataBuilder.DataFormat[Op['op']],
+		fieldName: string,
+		fresh: WriteManyRelationBuilder.DataFormat[Op['op']] | WriteOneRelationBuilder.DataFormat[Op['op']],
+	): WriteDataBuilder.DataFormat[Op['op']] {
+		if (fieldName in original) {
+			const existingValue = original[fieldName]
+			if (Array.isArray(existingValue)) {
+				if (Array.isArray(fresh)) {
+					return { ...original, [fieldName]: [...existingValue, ...fresh] }
+				}
+				throw new CrudQueryBuilderError(`Inconsistent data.`)
+			}
+			if (Array.isArray(fresh)) {
+				throw new CrudQueryBuilderError(`Inconsistent data.`)
+			}
+			return { ...original, [fieldName]: this.mergeUpdateOne(existingValue, fresh) }
+		}
+		return { ...original, [fieldName]: fresh }
+	}
+
+	private mergeUpdateOne(
+		original: Value.FieldValue<Literal> | Input.CreateOneRelationInput<Literal> | Input.UpdateOneRelationInput<Literal>,
+		fresh: Value.FieldValue<Literal> | Input.CreateOneRelationInput<Literal> | Input.UpdateOneRelationInput<Literal>,
+	): Value.FieldValue<Literal> | Input.CreateOneRelationInput<Literal> | Input.UpdateOneRelationInput<Literal> {
+		// TODO This implementation pretty bad but it will have to do for now.
+		if (original instanceof Literal) {
+			if (fresh instanceof Literal) {
+				if (original.value === fresh.value) {
+					return original
+				}
+			}
+			throw new CrudQueryBuilderError(`Inconsistent data.`)
+		}
+		if (fresh instanceof Literal) {
+			throw new CrudQueryBuilderError(`Inconsistent data.`)
+		}
+
+		if (Array.isArray(original)) {
+			if (Array.isArray(fresh)) {
+				return [...original, ...fresh]
+			}
+			throw new CrudQueryBuilderError(`Inconsistent data.`)
+		}
+		if (Array.isArray(fresh)) {
+			throw new CrudQueryBuilderError(`Inconsistent data.`)
+		}
+
+		if (original === null) {
+			if (fresh === null) {
+				return original
+			}
+		}
+		if (fresh === null) {
+			throw new CrudQueryBuilderError(`Inconsistent data.`)
+		}
+
+		if (typeof original === 'object') {
+			if (typeof fresh === 'object') {
+				const composite: any = { ...original }
+				for (const field in fresh) {
+					const fromFresh = (fresh as any)[field]
+					composite[field] = field in composite ? this.mergeUpdateOne(composite[field], fromFresh) : fromFresh
+				}
+				return composite
+			}
+			throw new CrudQueryBuilderError(`Inconsistent data.`)
+		}
+		if (typeof fresh === 'object') {
+			throw new CrudQueryBuilderError(`Inconsistent data.`)
+		}
+
+		if (original === fresh) {
+			return original
+		}
+		throw new CrudQueryBuilderError(`Inconsistent data.`)
 	}
 }
 

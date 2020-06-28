94b9519efbbeb2dc67785ad983492d7120af2657
Jirka Vebr <jirkavebr@gmail.com>
2020-06-28 18:24:08 +0200

    Describe the entity creation process

--- packages/binding/src/treeParameters/EntityCreationParameters.ts
+++ packages/binding/src/treeParameters/EntityCreationParameters.ts
@@ -7,6 +7,21 @@ export const EntityCreationParametersDefaults = {
 
 export interface DesugaredEntityCreationParameters {}
 
+/*
+It can be somewhat tricky to understand how these parameters actually work. These are all only relevant when *creating
+a new entity*, NOT during updates.
+
+First, it is determined if any *bearing* field/relation has anything to be persisted. This occurs:
+- For fields if the resolved value (taking defaultValue into account) is not null
+- For relations if its children have anything to persist or if forceCreation is true.
+
+If at least one of the bearing fields/relations have anything to persist or in the rare case when all fields/relations
+are nonbearing, the nonbearing fields/relations are also processed using the same method as described for bearing
+fields/relations.
+
+Lastly, setOnCreate comes to play. It doesn't influence whether the entity will be persisted or not but if after
+all of the above it is concluded that it will, the values from it are be used.
+ */
 export interface EntityCreationParameters {
 	forceCreation: boolean
 	isNonbearing: boolean

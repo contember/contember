60665a6592110f1ff89a1bc1f56782b80b4a93bc
Jirka Vebr <jirkavebr@gmail.com>
2020-07-02 17:09:53 +0200

    Better onBeforeUpdate subcription

--- packages/binding/src/entityEvents/useEntityBeforeUpdate.ts
+++ packages/binding/src/entityEvents/useEntityBeforeUpdate.ts
@@ -1,13 +1,17 @@
 import * as React from 'react'
-import { useParentEntityAccessor } from '../accessorPropagation'
+import { useEntityKey, useGetEntityByKey, useParentEntityAccessor } from '../accessorPropagation'
 import { EntityAccessor } from '../accessors'
 
 export const useEntityBeforeUpdate = (listener: EntityAccessor.EntityEventListenerMap['beforeUpdate']) => {
-	const entity = useParentEntityAccessor()
-	const addEventListener = entity.addEventListener // The identity of this function is guaranteed to be stable
+	const entityKey = useEntityKey()
+	const getEntityByKey = useGetEntityByKey()
+	const potentiallyStaleParent = getEntityByKey(entityKey)
+
+	// The identity of this function is guaranteed to be stable
+	const stableAddEventListenerReference = potentiallyStaleParent.addEventListener
 
 	React.useEffect(() => {
 		// addEventListener returns an unsubscribe function, which we're deliberately re-returning from here.
-		return addEventListener('beforeUpdate', listener)
-	}, [addEventListener, listener])
+		return stableAddEventListenerReference('beforeUpdate', listener)
+	}, [stableAddEventListenerReference, listener])
 }

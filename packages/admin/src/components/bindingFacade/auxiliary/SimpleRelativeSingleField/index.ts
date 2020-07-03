68b399cfb63de2b6ccb9ea88c1426aca570c45ce
Jirka Vebr <jirkavebr@gmail.com>
2020-07-03 14:51:32 +0200

    Checkbox: change default default value to false

--- packages/admin/src/components/bindingFacade/auxiliary/SimpleRelativeSingleField/index.ts
+++ packages/admin/src/components/bindingFacade/auxiliary/SimpleRelativeSingleField/index.ts
@@ -1,1 +1,2 @@
+export * from './SimpleRelativeFieldProxy'
 export * from './SimpleRelativeSingleField'
--- packages/admin/src/components/bindingFacade/fields/CheckboxField.tsx
+++ packages/admin/src/components/bindingFacade/fields/CheckboxField.tsx
@@ -1,22 +1,52 @@
+import { Component, Field, FieldAccessor } from '@contember/binding'
 import * as React from 'react'
-import { FieldAccessor } from '@contember/binding'
 import { Checkbox } from '../../ui'
-import { SimpleRelativeSingleField, SimpleRelativeSingleFieldProps } from '../auxiliary'
+import {
+	SimpleRelativeSingleFieldMetadata,
+	SimpleRelativeSingleFieldProps,
+	SimpleRelativeSingleFieldProxy,
+} from '../auxiliary'
 
 export type CheckboxFieldProps = SimpleRelativeSingleFieldProps
 
-export const CheckboxField = SimpleRelativeSingleField<CheckboxFieldProps, boolean>((fieldMetadata, props) => {
-	const generateOnChange = (data: FieldAccessor<boolean>) => (isChecked: boolean) => {
-		data.updateValue && data.updateValue(isChecked)
-	}
-	return (
-		<Checkbox
-			checked={!!fieldMetadata.field.currentValue}
-			onChange={generateOnChange(fieldMetadata.field)}
-			readOnly={fieldMetadata.isMutating}
-			errors={fieldMetadata.field.errors}
-		>
-			{fieldMetadata.environment.applySystemMiddleware('labelMiddleware', props.label)}
-		</Checkbox>
-	)
-}, 'CheckboxField')
+export const CheckboxField = Component<CheckboxFieldProps>(
+	props => (
+		<SimpleRelativeSingleFieldProxy
+			{...props}
+			render={(fieldMetadata: SimpleRelativeSingleFieldMetadata<boolean>, props) => {
+				const generateOnChange = (data: FieldAccessor<boolean>) => (isChecked: boolean) => {
+					data.updateValue && data.updateValue(isChecked)
+				}
+				return (
+					<Checkbox
+						checked={!!fieldMetadata.field.currentValue}
+						onChange={generateOnChange(fieldMetadata.field)}
+						readOnly={fieldMetadata.isMutating}
+						errors={fieldMetadata.field.errors}
+					>
+						{fieldMetadata.environment.applySystemMiddleware('labelMiddleware', props.label)}
+					</Checkbox>
+				)
+			}}
+		/>
+	),
+	props => {
+		let isNonbearing = props.isNonbearing
+		let defaultValue = props.defaultValue
+
+		if (props.defaultValue === undefined && props.isNonbearing !== false) {
+			defaultValue = false
+			isNonbearing = true
+		}
+
+		return (
+			<>
+				<Field defaultValue={defaultValue} field={props.field} isNonbearing={isNonbearing} />
+				{props.label}
+				{props.labelDescription}
+				{props.description}
+			</>
+		)
+	},
+	'CheckboxField',
+)

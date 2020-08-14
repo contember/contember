8b6f942775d6636657c4d3f739e23368b20edbc8
David Matejka <matej21@matej21.cz>
2020-08-14 12:47:52 +0200

    validation: node context refactoring, change evaluation of some operations on some node types [BC break]

--- packages/engine-content-api/src/input-validation/ValidationEvaluation.ts
+++ packages/engine-content-api/src/input-validation/ValidationEvaluation.ts
@@ -1,5 +1,6 @@
 import { Validation } from '@contember/schema'
 import ValidationContext from './ValidationContext'
+import acceptContextVisitor = ValidationContext.acceptContextVisitor
 
 const getValueOrLiteral = (
 	context: ValidationContext.AnyContext,
@@ -28,6 +29,49 @@ export const evaluateValidation = (
 	return validatorEvaluators[validator.operation](context, ...(validator.args as any[]))
 }
 
+const fixedResult = (value: null | boolean) => () => value
+
+const cannotApplyOnNode = (validationName: string, contextType: string) => () => {
+	throw new Error(`Cannot apply ${validationName} in ${contextType} context`)
+}
+
+const trinaryOr = (values: Iterable<boolean | null>) => {
+	let result: null | false = false
+	for (const val of values) {
+		if (val === true) {
+			return true
+		}
+		if (val === null) {
+			result = null
+		}
+	}
+	return result
+}
+const trinaryAnd = (values: Iterable<boolean | null>) => {
+	let result: null | true = true
+	for (const val of values) {
+		if (val === false) {
+			return false
+		}
+		if (val === null) {
+			result = null
+		}
+	}
+	return result
+}
+
+function* validatorListEval(context: ValidationContext.AnyContext, validators: Validation.ValidatorArgument[]) {
+	for (const val of validators) {
+		yield evaluateValidation(context, val.validator)
+	}
+}
+
+function* nodeListEval(context: ValidationContext.NodeListContext, { validator }: Validation.ValidatorArgument) {
+	for (const node of context.nodes) {
+		yield evaluateValidation(node, validator)
+	}
+}
+
 const validatorEvaluators: {
 	[K in keyof Validation.ValidatorArguments]: (
 		context: ValidationContext.AnyContext,
@@ -35,27 +79,96 @@ const validatorEvaluators: {
 	) => boolean | null
 } = {
 	empty: (context: ValidationContext.AnyContext) => {
-		if (ValidationContext.isNodeListContext(context)) {
-			return context.nodes.length === 0
-		}
-		if (ValidationContext.isNodeContext(context) && !ValidationContext.isValueContext(context)) {
-			return null
-		}
-		const value = getValueFromContext(context)
-		return !value
+		return acceptContextVisitor(context, {
+			visitNodeContext: fixedResult(false),
+			visitNodeListContext: (context: ValidationContext.NodeListContext) => context.nodes.length === 0,
+			visitValueContext: (context: ValidationContext.ValueContext) =>
+				context.value === undefined || context.value === null || context.value === '',
+			visitUndefinedContext: fixedResult(true),
+		})
 	},
 	defined: (context: ValidationContext.AnyContext) => {
-		if (ValidationContext.isNodeListContext(context)) {
-			throw new Error('Unexpected node list')
-		}
-		if (ValidationContext.isUndefinedNodeContext(context)) {
-			return false
-		}
-		if (ValidationContext.isValueContext(context)) {
-			const value = getValueFromContext(context)
-			return value !== null && value !== undefined
-		}
-		return context.node !== undefined
+		return acceptContextVisitor(context, {
+			visitNodeContext: fixedResult(true),
+			visitNodeListContext: fixedResult(true),
+			visitValueContext: (context: ValidationContext.ValueContext) =>
+				context.value !== undefined && context.value !== null,
+			visitUndefinedContext: fixedResult(false),
+		})
+	},
+	pattern: (context: ValidationContext.AnyContext, patternArgument: Validation.LiteralArgument<[string, string]>) => {
+		return acceptContextVisitor(context, {
+			visitNodeContext: cannotApplyOnNode('pattern', 'node'),
+			visitNodeListContext: cannotApplyOnNode('pattern', 'node list'),
+			visitValueContext: (context: ValidationContext.ValueContext) => {
+				if (context.value === undefined || context.value === null) {
+					return null
+				}
+				if (typeof context.value !== 'string') {
+					throw new Error(`Cannot apply pattern validation on ${typeof context.value}`)
+				}
+				return new RegExp(patternArgument.value[0], patternArgument.value[1]).test(context.value)
+			},
+			visitUndefinedContext: fixedResult(null),
+		})
+	},
+	equals: (context: ValidationContext.AnyContext, other: Validation.LiteralArgument | Validation.PathArgument) => {
+		return acceptContextVisitor(context, {
+			visitNodeContext: cannotApplyOnNode('equals', 'node'),
+			visitNodeListContext: cannotApplyOnNode('equals', 'node list'),
+			visitValueContext: (context: ValidationContext.ValueContext) => {
+				const otherValue = getValueOrLiteral(context, other)
+				if (otherValue === null) {
+					return context.value === otherValue
+				}
+				if (context.value === null || context.value === undefined) {
+					return null
+				}
+				return context.value === otherValue
+			},
+			visitUndefinedContext: fixedResult(null),
+		})
+	},
+	lengthRange: (
+		context: ValidationContext.AnyContext,
+		min: Validation.LiteralArgument<number | null>,
+		max: Validation.LiteralArgument<number | null>,
+	) => {
+		const doesMatchRange = (value: number) =>
+			(min.value === null || min.value <= value) && (max.value === null || max.value >= value)
+		return acceptContextVisitor(context, {
+			visitNodeContext: cannotApplyOnNode('length range', 'node'),
+			visitNodeListContext: (context: ValidationContext.NodeListContext) => {
+				return doesMatchRange(context.nodes.length)
+			},
+			visitValueContext: (context: ValidationContext.ValueContext) => {
+				if (context.value === null || context.value === undefined) {
+					return null
+				}
+				return doesMatchRange(String(context.value).length)
+			},
+			visitUndefinedContext: fixedResult(null),
+		})
+	},
+	range: (
+		context: ValidationContext.AnyContext,
+		min: Validation.LiteralArgument<number | null>,
+		max: Validation.LiteralArgument<number | null>,
+	) => {
+		return acceptContextVisitor(context, {
+			visitNodeContext: cannotApplyOnNode('range', 'node'),
+			visitNodeListContext: cannotApplyOnNode('range', 'node list'),
+			visitValueContext: (context: ValidationContext.ValueContext) => {
+				if (context.value === null || context.value === undefined) {
+					return null
+				}
+				if (typeof context.value !== 'number') {
+					throw new Error(`Cannot apply range validation on ${typeof context.value}`)
+				}
+				return (min.value === null || min.value <= context.value) && (max.value === null || max.value >= context.value)
+			},
+			visitUndefinedContext: fixedResult(null),
+		})
 	},
 	not: (context: ValidationContext.AnyContext, { validator }: Validation.ValidatorArgument) => {
 		const result = evaluateValidation(context, validator)
@@ -79,89 +192,11 @@ const validatorEvaluators: {
 		const ruleResult = evaluateValidation(context, rule)
 		return ruleResult
 	},
-	pattern: (context: ValidationContext.AnyContext, patternArgument: Validation.LiteralArgument<[string, string]>) => {
-		const value = getValueFromContext(context)
-		if (value === undefined || value === null) {
-			return null
-		}
-		if (typeof value !== 'string') {
-			throw new Error(`Cannot apply pattern validation on ${typeof value}`)
-		}
-		return new RegExp(patternArgument.value[0], patternArgument.value[1]).test(value)
-	},
-	equals: (context: ValidationContext.AnyContext, other: Validation.LiteralArgument | Validation.PathArgument) => {
-		const contextValue = getValueFromContext(context)
-		const otherValue = getValueOrLiteral(context, other)
-		if (otherValue === null) {
-			return contextValue === otherValue
-		}
-		if (contextValue === null || contextValue === undefined) {
-			return null
-		}
-		return contextValue === otherValue
-	},
-	lengthRange: (
-		context: ValidationContext.AnyContext,
-		min: Validation.LiteralArgument<number | null>,
-		max: Validation.LiteralArgument<number | null>,
-	) => {
-		let value: number
-		if (ValidationContext.isValueContext(context)) {
-			value = String(getValueFromContext(context)).length
-		} else if (ValidationContext.isNodeListContext(context)) {
-			value = context.nodes.length
-		} else if (ValidationContext.isUndefinedNodeContext(context)) {
-			return null
-		} else {
-			throw new Error('Value or List context is required for range operation')
-		}
-		return (min.value === null || min.value <= value) && (max.value === null || max.value >= value)
-	},
-	range: (
-		context: ValidationContext.AnyContext,
-		min: Validation.LiteralArgument<number | null>,
-		max: Validation.LiteralArgument<number | null>,
-	) => {
-		if (ValidationContext.isValueContext(context)) {
-			const value = getValueFromContext(context)
-			if (value === null) {
-				return null
-			}
-			if (typeof value !== 'number') {
-				throw new Error(`Cannot apply range validation on ${typeof value}`)
-			}
-			return (min.value === null || min.value <= value) && (max.value === null || max.value >= value)
-		} else if (ValidationContext.isUndefinedNodeContext(context)) {
-			return null
-		} else {
-			throw new Error('Value context is required for range operation')
-		}
-	},
 	and: (context: ValidationContext.AnyContext, ...values: Validation.ValidatorArgument[]) => {
-		let result: null | true = true
-		for (const val of values) {
-			const validationResult = evaluateValidation(context, val.validator)
-			if (validationResult === false) {
-				return false
-			}
-			if (validationResult === null) {
-				result = null
-			}
-		}
-		return result
+		return trinaryAnd(validatorListEval(context, values))
 	},
 	or: (context: ValidationContext.AnyContext, ...values: Validation.ValidatorArgument[]) => {
-		let result: null | false = false
-		for (const val of values) {
-			const validationResult = evaluateValidation(context, val.validator)
-			if (validationResult === true) {
-				return true
-			}
-			if (validationResult === null) {
-				result = null
-			}
-		}
-		return result
+		return trinaryOr(validatorListEval(context, values))
 	},
 	inContext: (
 		context: ValidationContext.AnyContext,
@@ -170,37 +205,22 @@ const validatorEvaluators: {
 	) => {
 		return evaluateValidation(ValidationContext.changeContext(context, contextArg.path), validator)
 	},
-	every: (context: ValidationContext.AnyContext, { validator }: Validation.ValidatorArgument) => {
-		if (!ValidationContext.isNodeListContext(context)) {
-			throw new Error('NodeListContext expected for "every" operation')
-		}
-		let result: true | null = true
-		for (const node of context.nodes) {
-			const validationResult = evaluateValidation(node, validator)
-			if (validationResult === false) {
-				return false
-			}
-			if (validationResult === null) {
-				result = null
-			}
-		}
-		return result
+
+	every: (context: ValidationContext.AnyContext, arg: Validation.ValidatorArgument) => {
+		return acceptContextVisitor(context, {
+			visitNodeContext: cannotApplyOnNode('every', 'node'),
+			visitNodeListContext: context => trinaryAnd(nodeListEval(context, arg)),
+			visitValueContext: cannotApplyOnNode('every', 'value'),
+			visitUndefinedContext: fixedResult(null),
+		})
 	},
-	any: (context: ValidationContext.AnyContext, { validator }: Validation.ValidatorArgument) => {
-		if (!ValidationContext.isNodeListContext(context)) {
-			throw new Error('NodeListContext expected for "any" operation')
-		}
-		let result: false | null = false
-		for (const node of context.nodes) {
-			const validationResult = evaluateValidation(node, validator)
-			if (validationResult === true) {
-				return true
-			}
-			if (validationResult === null) {
-				result = null
-			}
-		}
-		return result
+	any: (context: ValidationContext.AnyContext, arg: Validation.ValidatorArgument) => {
+		return acceptContextVisitor(context, {
+			visitNodeContext: cannotApplyOnNode('any', 'node'),
+			visitNodeListContext: context => trinaryOr(nodeListEval(context, arg)),
+			visitValueContext: cannotApplyOnNode('any', 'value'),
+			visitUndefinedContext: fixedResult(null),
+		})
 	},
 	filter: (
 		context: ValidationContext.AnyContext,

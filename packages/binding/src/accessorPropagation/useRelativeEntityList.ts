b133fe486fca11883d2c80b11d156fc693597575
Jirka Vebr <jirkavebr@gmail.com>
2020-08-20 15:10:57 +0200

    Accessor propagation: update core hooks to listen to connection changes

--- packages/binding/src/accessorPropagation/useRelativeEntityList.ts
+++ packages/binding/src/accessorPropagation/useRelativeEntityList.ts
@@ -1,7 +1,8 @@
-import { useConstantValueInvariant } from '@contember/react-utils'
+import { useConstantLengthInvariant, useConstantValueInvariant } from '@contember/react-utils'
 import * as React from 'react'
 import { useEntityKey, useGetEntityByKey } from '../accessorPropagation'
 import { EntityListAccessor } from '../accessors'
+import { useOnConnectionUpdate } from '../entityEvents'
 import { SugaredRelativeEntityList } from '../treeParameters'
 import { useAccessorUpdateSubscription } from './useAccessorUpdateSubscription'
 import { useDesugaredRelativeEntityList } from './useDesugaredRelativeEntityList'
@@ -28,7 +29,19 @@ function useRelativeEntityList(
 
 	if (relativeEntityList) {
 		// eslint-disable-next-line react-hooks/rules-of-hooks
-		return useAccessorUpdateSubscription(getEntityList)
+		useConstantLengthInvariant(
+			relativeEntityList.hasOneRelationPath,
+			'Cannot change the length of the hasOneRelation path!',
+		)
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const [entityList, forceUpdate] = useAccessorUpdateSubscription(getEntityList, true)
+
+		if (relativeEntityList.hasOneRelationPath.length) {
+			// eslint-disable-next-line react-hooks/rules-of-hooks
+			useOnConnectionUpdate(relativeEntityList.hasOneRelationPath[0].field, forceUpdate)
+		}
+
+		return entityList
 	}
 	return undefined
 }

b6cc9f3d5b934794f5a83ee7109d63e40aea46fb
Jirka Vebr <jirkavebr@gmail.com>
2020-08-26 13:33:15 +0200

    Delete templates for now

--- packages/template-project-with-admin/admin/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Pages } from '@contember/admin'
-import * as React from 'react'
-import { Layout } from './Layout'
-import * as pageList from './pages'
-
-export default () => <Pages layout={Layout} children={Object.values(pageList)} />
--- packages/template-project-with-admin/api/model/Post.ts
+++
@@ -1,8 +0,0 @@
-import { SchemaDefinition as def } from '@contember/schema-definition'
-import { Image } from './Image'
-
-export class Post {
-	title = def.stringColumn().notNull()
-	content = def.stringColumn().notNull()
-	image = def.manyHasOne(Image)
-}

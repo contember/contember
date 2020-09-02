287ca48469b7cf22a08aede2e306602e5805c763
Jirka Vebr <jirkavebr@gmail.com>
2020-09-02 17:35:40 +0200

    Slighty (though deliberately not completely) improve index.js files

--- packages/binding/index.js
+++ packages/binding/index.js
@@ -1,8 +1,3 @@
 'use strict'
 
-if (process.env.NODE_ENV === 'production') {
-	module.exports = require('./dist/bundle.js')
-} else {
-	;(globalThis || window || {})['__DEV_MODE__'] = true
-	module.exports = require('./dist/src/index.js')
-}
+export * from './dist/bundle.js'

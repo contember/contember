23b6b7ad8e5faf954bfd16c9649620fa4d7d2d99
Jirka Vebr <jirkavebr@gmail.com>
2020-09-02 17:06:53 +0200

    Abolish non-standard chrome-only credentials api

--- packages/react-client/src/auth/useLoginRequest.ts
+++ packages/react-client/src/auth/useLoginRequest.ts
@@ -3,17 +3,16 @@ import * as React from 'react'
 import { ApiRequestState } from '../apiRequest'
 import { useLoginToken } from '../config'
 import { useTenantApiRequest } from '../tenant'
-import { invokeIfSupportsCredentials } from './invokeIfSupportsCredentials'
 
 export const useLoginRequest = (): [
 	ApiRequestState<LoginMutationResponse>,
-	(email: string, password: string) => void,
+	(email: string, password: string) => Promise<LoginMutationResponse>,
 ] => {
 	const [requestState, sendRequest] = useTenantApiRequest<LoginMutationResponse>()
 	const loginToken = useLoginToken()
 
 	const login = React.useCallback(
-		(email: string, password: string) => {
+		(email: string, password: string) =>
 			sendRequest(
 				loginMutation,
 				{
@@ -22,34 +21,9 @@ export const useLoginRequest = (): [
 					expiration: 3600 * 24 * 14, // Two weeks
 				},
 				loginToken,
-			).then(async () => {
-				invokeIfSupportsCredentials(async () => {
-					const credentials = await navigator.credentials.create({
-						password: {
-							password,
-							id: email,
-						},
-					})
-					if (credentials) {
-						await navigator.credentials.store(credentials)
-					}
-				})
-			})
-		},
+			),
 		[sendRequest, loginToken],
 	)
-
-	React.useEffect(() => {
-		invokeIfSupportsCredentials(async () => {
-			const credentials = await navigator.credentials.get({
-				password: true,
-				mediation: 'silent',
-			})
-			if (credentials instanceof PasswordCredential && credentials.password) {
-				login(credentials.id, credentials.password)
-			}
-		})
-	}, [login])
 
 	return [requestState, login]
 }

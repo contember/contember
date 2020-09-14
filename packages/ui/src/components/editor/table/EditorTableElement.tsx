dfe3270a8ba8a1e99dc23bfb52607459b544e178
Jirka Vebr <jirkavebr@gmail.com>
2020-09-14 22:25:24 +0200

    Better editor table styles

--- packages/ui/src/components/editor/table/EditorTableElement.tsx
+++ packages/ui/src/components/editor/table/EditorTableElement.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { useClassNamePrefix } from '../../../auxiliary'
 import cn from 'classnames'
+import { Icon } from '../../Icon'
+import { Button } from '../../forms'
 
 export interface EditorTableElementProps {
 	rowCount: number
@@ -34,46 +36,62 @@ export const EditorTableElement = React.memo(function EditorTableElement({
 				} as React.CSSProperties
 			}
 		>
+			<div className={cn(`${prefix}editorTable-handle`)} contentEditable={false} />
 			<div className={cn(`${prefix}editorTable-columnControls`)} contentEditable={false}>
 				{Array.from({ length: columnCount + 1 }, (_, columnNumber) => (
-					<React.Fragment key={columnNumber}>
+					<div
+						className={cn(`${prefix}editorTable-columnControls-in`)}
+						style={{ [`--${prefix}editorTable-column`]: columnNumber } as React.CSSProperties}
+						key={columnNumber}
+					>
 						{columnNumber < columnCount && (
-							<div
-								className={cn(`${prefix}editorTable-columnControls-item`)}
-								style={{ [`--${prefix}editorTable-column`]: columnNumber } as React.CSSProperties}
-								key={columnNumber}
-							>
-								cc
-							</div>
+							// TODO select column
+							<div className={cn(`${prefix}editorTable-columnControls-item`)} key={columnNumber} />
 						)}
-						<div
-							className={cn(`${prefix}editorTable-columnControls-columnLine`)}
-							style={{ [`--${prefix}editorTable-column`]: columnNumber } as React.CSSProperties}
-						/>
-					</React.Fragment>
+						<Button
+							onClick={() => 0}
+							className={cn(`${prefix}editorTable-columnControls-add`)}
+							flow="circular"
+							size="small"
+							distinction="seamless"
+						>
+							<Icon blueprintIcon="plus" />
+						</Button>
+						<div className={cn(`${prefix}editorTable-columnControls-line`)} />
+					</div>
 				))}
 			</div>
 			<div className={cn(`${prefix}editorTable-rowControls`)} contentEditable={false}>
 				{Array.from({ length: rowCount + 1 }, (_, rowNumber) => (
-					<React.Fragment key={rowNumber}>
+					<div
+						className={cn(`${prefix}editorTable-rowControls-in`)}
+						style={{ [`--${prefix}editorTable-row`]: rowNumber } as React.CSSProperties}
+						key={rowNumber}
+					>
 						{rowNumber < rowCount && (
-							<div
-								className={cn(`${prefix}editorTable-rowControls-item`)}
-								style={{ [`--${prefix}editorTable-row`]: rowNumber } as React.CSSProperties}
-							>
-								rc
-							</div>
+							// TODO select row
+							<div className={cn(`${prefix}editorTable-rowControls-item`)} />
 						)}
-						<div
-							className={cn(`${prefix}editorTable-rowControls-rowLine`)}
-							style={{ [`--${prefix}editorTable-row`]: rowNumber } as React.CSSProperties}
-						/>
-					</React.Fragment>
+						<Button
+							onClick={() => 0}
+							className={cn(`${prefix}editorTable-rowControls-add`)}
+							flow="circular"
+							size="small"
+							distinction="seamless"
+						>
+							<Icon blueprintIcon="plus" />
+						</Button>
+						<div className={cn(`${prefix}editorTable-rowControls-line`)} />
+					</div>
 				))}
 			</div>
+			<button type="button" className={cn(`${prefix}editorTable-appendColumn`)} contentEditable={false}>
+				<Icon blueprintIcon="plus" />
+			</button>
+			<button type="button" className={cn(`${prefix}editorTable-appendRow`)} contentEditable={false}>
+				<Icon blueprintIcon="plus" />
+			</button>
 			{children}
-			{/*<div className={cn(`${prefix}editorTable-addColumn`)}>+</div>*/}
-			{/*<div className={cn(`${prefix}editorTable-addRow`)}>+</div>*/}
 		</div>
 	)
 })

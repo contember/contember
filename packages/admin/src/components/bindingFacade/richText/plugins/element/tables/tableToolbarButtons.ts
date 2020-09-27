2a13d4377815464d8699228502cd34ae0fec9989
Jirka Vebr <jirkavebr@gmail.com>
2020-09-27 18:58:44 +0200

    Table icon

--- packages/admin/src/components/bindingFacade/richText/plugins/element/tables/tableToolbarButtons.ts
+++ packages/admin/src/components/bindingFacade/richText/plugins/element/tables/tableToolbarButtons.ts
@@ -3,7 +3,7 @@ import { TableElement, tableElementType } from './TableElement'
 
 export const tableToolbarButton: ElementToolbarButton<TableElement> = {
 	elementType: tableElementType,
-	blueprintIcon: 'th',
+	contemberIcon: 'table',
 	label: 'Table',
 	title: 'Table',
 }

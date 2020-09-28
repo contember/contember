b05269237db581b43c3250cb0a53f7d71a92f0c4
David Matejka <matej21@matej21.cz>
2020-09-28 11:02:04 +0200

    content api: merge selections over the same field

--- packages/engine-content-api/src/graphQlResolver/GraphQlQueryAstFactory.ts
+++ packages/engine-content-api/src/graphQlResolver/GraphQlQueryAstFactory.ts
@@ -12,8 +12,11 @@ import { SelectionSetNode } from 'graphql/language/ast'
 import { getArgumentValues } from 'graphql/execution/values'
 import { ResolveInfoUtils } from '../utils/ResolveInfoUtils'
 import { FieldNode, ObjectNode } from '../inputProcessing'
+import { isDeepStrictEqual } from 'util'
 
 type NodeFilter = (node: GraphQlFieldNode, path: string[]) => boolean
+type AnyNode = ObjectNode | FieldNode
+
 export default class GraphQlQueryAstFactory {
 	constructor(private readonly argumentValuesResolver: typeof getArgumentValues) {}
 
@@ -46,13 +49,7 @@ export default class GraphQlQueryAstFactory {
 		const type = field.type
 		const resolvedType = GraphQlQueryAstFactory.resolveObjectType(type)
 
-		const fields: (FieldNode | ObjectNode)[] = this.processSelectionSet(
-			info,
-			resolvedType,
-			node.selectionSet,
-			[...path, alias],
-			filter,
-		)
+		const fields: AnyNode[] = this.processSelectionSet(info, resolvedType, node.selectionSet, [...path, alias], filter)
 
 		return new ObjectNode(
 			name,
@@ -70,14 +67,15 @@ export default class GraphQlQueryAstFactory {
 		selectionSet: SelectionSetNode,
 		path: string[],
 		filter: NodeFilter,
-	): (FieldNode | ObjectNode)[] {
-		const fields: (FieldNode | ObjectNode)[] = []
-		for (let subNode of selectionSet.selections) {
+	): AnyNode[] {
+		const fields: ObjectNode | FieldNode[] = []
+		for (const subNode of selectionSet.selections) {
 			if (isIt<GraphQlFieldNode>(subNode, 'kind', 'Field')) {
-				const result = this.createFromNode(info, parentType, subNode, path, filter)
-				if (result !== null) {
-					fields.push(result)
+				const field = this.createFromNode(info, parentType, subNode, path, filter)
+				if (field === null) {
+					continue
 				}
+				fields.push(field)
 			} else if (isIt<FragmentSpreadNode>(subNode, 'kind', 'FragmentSpread')) {
 				const fragmentDefinition = info.fragments[subNode.name.value]
 				if (!fragmentDefinition) {
@@ -88,20 +86,19 @@ export default class GraphQlQueryAstFactory {
 				if (!GraphQlQueryAstFactory.itIs<GraphQLObjectType>(subField, 'GraphQLObjectType')) {
 					throw new Error('GraphQlQueryAstFactory: subfield is expected to be GraphQLObjectType')
 				}
-				fields.push(
-					...this.processSelectionSet(
-						info,
-						subField as GraphQLObjectType,
-						fragmentDefinition.selectionSet,
-						path,
-						filter,
-					),
+				const fragmentSelection = this.processSelectionSet(
+					info,
+					subField as GraphQLObjectType,
+					fragmentDefinition.selectionSet,
+					path,
+					filter,
 				)
+				fields.push(...fragmentSelection)
 			} else {
-				throw new Error('FragmentSpread and InlineFragment are not supported yet')
+				throw new Error('InlineFragment are not supported yet')
 			}
 		}
-		return fields
+		return GraphQlQueryAstFactory.mergeFieldList(fields)
 	}
 
 	public static resolveObjectType(type: GraphQLOutputType): GraphQLObjectType {
@@ -126,5 +123,30 @@ export default class GraphQlQueryAstFactory {
 
 	private static itIs<T>(val: any, name: string): val is T {
 		return typeof val === 'object' && val.constructor && val.constructor.name === name
+	}
+
+	private static mergeFieldList(nodes: AnyNode[]): AnyNode[] {
+		const result: Record<string, AnyNode> = {}
+		for (const node of nodes) {
+			result[node.alias] = result[node.alias] ? GraphQlQueryAstFactory.mergeFields(result[node.alias], node) : node
+		}
+		return Object.values(result)
+	}
+
+	private static mergeFields(left: AnyNode, right: AnyNode): AnyNode {
+		if (left.constructor !== right.constructor) {
+			throw new Error(`GraphQlQueryAstFactory: cannot merge ${left.constructor.name} and ${right.constructor.name}`)
+		}
+		if (left.name !== right.name) {
+			throw new Error(`GraphQlQueryAstFactory: incompatible fields ${left.name} and ${right.name}`)
+		}
+		if (left instanceof ObjectNode && right instanceof ObjectNode) {
+			if (!isDeepStrictEqual(left.args, right.args)) {
+				throw new Error(`GraphQlQueryAstFactory: incompatible args`)
+			}
+			const fields = GraphQlQueryAstFactory.mergeFieldList([...left.fields, ...right.fields])
+			return new ObjectNode(left.name, left.alias, fields, left.args, left.meta, left.path)
+		}
+		return left
 	}
 }

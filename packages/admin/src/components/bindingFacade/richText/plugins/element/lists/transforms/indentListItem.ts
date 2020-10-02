4f7b3bcccb0adcb8020cbd8c1eb16e38e2b6332f
Jirka Vebr <jirkavebr@gmail.com>
2020-10-02 22:39:49 +0200

    List indentation: check if there is a list already in prev li

--- packages/admin/src/components/bindingFacade/richText/plugins/element/lists/transforms/indentListItem.ts
+++ packages/admin/src/components/bindingFacade/richText/plugins/element/lists/transforms/indentListItem.ts
@@ -1,8 +1,10 @@
-import { Editor, Node as SlateNode, Path as SlatePath, Text, Transforms } from 'slate'
+import { Editor, Element as SlateElement, Node as SlateNode, Path as SlatePath, Text, Transforms } from 'slate'
 import { BaseEditor } from '../../../../baseEditor'
 import { ContemberEditor } from '../../../../ContemberEditor'
 import { EditorWithLists } from '../EditorWithLists'
 import { ListItemElement } from '../ListItemElement'
+import { OrderedListElement } from '../OrderedListElement'
+import { UnorderedListElement } from '../UnorderedListElement'
 
 export const indentListItem = (
 	editor: EditorWithLists<BaseEditor>,
@@ -17,31 +19,50 @@ export const indentListItem = (
 	if (previousListEntry === undefined) {
 		return false
 	}
-	let [previousListItem, previousListItemPath] = previousListEntry
-	const previousContainsInlines = Editor.hasInlines(editor, previousListItem)
-	const [parentListElement] = Editor.parent(editor, listItemPath)
 
 	Editor.withoutNormalizing(editor, () => {
-		if (previousContainsInlines) {
-			const [previousStart, previousEnd] = Editor.edges(editor, previousListItemPath)
-			Transforms.wrapNodes(editor, editor.createDefaultElement([]), {
-				match: node => Text.isText(node) || editor.isInline(node),
-				at: {
-					anchor: previousStart,
-					focus: previousEnd,
-				},
-			})
-		}
-		// Getting a new previousListItem because the children may have changed due to the branch above.
-		previousListItem = SlateNode.get(editor, previousListItemPath) as ListItemElement
-		// We're appending at the end: children.length is just lastOne + 1
-		const newListItemPath = [...previousListItemPath, previousListItem.children.length]
+		const [parentListElement] = Editor.parent(editor, listItemPath)
+		let [previousListItem, previousListItemPath] = previousListEntry
 
-		Transforms.moveNodes(editor, {
-			at: listItemPath,
-			to: newListItemPath,
-		})
-		Transforms.wrapNodes(editor, { ...parentListElement, children: [] }, { at: newListItemPath })
+		const lastPreviousListItemChild = previousListItem.children[previousListItem.children.length - 1]
+		const previousEndsWithCompatibleList =
+			SlateElement.isElement(lastPreviousListItemChild) &&
+			editor.isList(lastPreviousListItemChild, ContemberEditor.elementToSpecifics(parentListElement))
+
+		if (previousEndsWithCompatibleList) {
+			const newListItemPath = [
+				...previousListItemPath,
+				previousListItem.children.length - 1,
+				(lastPreviousListItemChild as UnorderedListElement | OrderedListElement).children.length,
+			]
+
+			Transforms.moveNodes(editor, {
+				at: listItemPath,
+				to: newListItemPath,
+			})
+		} else {
+			const previousContainsInlines = Editor.hasInlines(editor, previousListItem)
+			if (previousContainsInlines) {
+				const [previousStart, previousEnd] = Editor.edges(editor, previousListItemPath)
+				Transforms.wrapNodes(editor, editor.createDefaultElement([]), {
+					match: node => Text.isText(node) || editor.isInline(node),
+					at: {
+						anchor: previousStart,
+						focus: previousEnd,
+					},
+				})
+			}
+			// Getting a new previousListItem because the children may have changed due to the branch above.
+			previousListItem = SlateNode.get(editor, previousListItemPath) as ListItemElement
+			// We're appending at the end: children.length is just lastOne + 1
+			const newListItemPath = [...previousListItemPath, previousListItem.children.length]
+
+			Transforms.moveNodes(editor, {
+				at: listItemPath,
+				to: newListItemPath,
+			})
+			Transforms.wrapNodes(editor, { ...parentListElement, children: [] }, { at: newListItemPath })
+		}
 	})
 
 	return true

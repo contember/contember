a3a128e9a45c247c1b1a10dd6c17e6a740d165c1
Jirka Vebr <jirkavebr@gmail.com>
2020-10-04 15:10:29 +0200

    Optimize isUrl a bit

--- packages/admin/src/components/bindingFacade/richText/plugins/element/anchors/isUrl.ts
+++ packages/admin/src/components/bindingFacade/richText/plugins/element/anchors/isUrl.ts
@@ -1,4 +1,10 @@
 export const isUrl = (url: string): boolean => {
+	if (url.length < 4) {
+		// This is almost definitely not an url anyway.
+		// By returning early, we avoid the relatively costly URL constructor and can speed up the most common case of
+		// just inserting a single character which better be as fast as possible.
+		return false
+	}
 	try {
 		const urlObject = new URL(url)
 		return true

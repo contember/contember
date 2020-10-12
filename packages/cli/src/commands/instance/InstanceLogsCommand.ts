dd7c9f5bc651584b0b0d917fd312b2a3a1b7364e
David Matejka <matej21@matej21.cz>
2020-10-12 16:14:53 +0200

    cli instance log command: support tail

--- packages/cli/src/commands/instance/InstanceLogsCommand.ts
+++ packages/cli/src/commands/instance/InstanceLogsCommand.ts
@@ -6,18 +6,22 @@ type Args = {
 	instanceName: string
 }
 
-type Options = {}
+type Options = {
+	tail: string | true
+}
 
 export class InstanceLogsCommand extends Command<Args, Options> {
 	protected configure(configuration: CommandConfiguration<Args, Options>): void {
 		configuration.description('Show Contember instance logs')
 		configuration.argument('instanceName').optional()
+		configuration.option('tail').valueOptional()
 	}
 
 	protected async execute(input: Input<Args, Options>): Promise<void> {
 		const workspaceDirectory = process.cwd()
 		const { instanceDirectory } = await resolveInstanceEnvironmentFromInput({ input, workspaceDirectory })
 		const dockerCompose = new DockerCompose(instanceDirectory)
-		await dockerCompose.run(['logs', '-f']).output
+		const tail = input.getOption('tail')
+		await dockerCompose.run(['logs', '-f', '--tail', tail === true ? '0' : tail ?? 'all']).output
 	}
 }

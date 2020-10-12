bc704eb923c3a97c58994daf8ea7cf6b6e9b49d5
David Matejka <matej21@matej21.cz>
2020-10-12 16:17:28 +0200

    cli: check if package-lock version matches installed version

--- packages/cli/src/utils/contember.ts
+++ packages/cli/src/utils/contember.ts
@@ -1,7 +1,18 @@
 import { join } from 'path'
 import { packageRoot } from '../pathUtils'
+import { pathExists } from 'fs-extra'
 
-export const getContemberVersion = () => {
+export const getCliVersion = () => {
 	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	return require(join(packageRoot, 'package.json')).version
+}
+
+export const getRequestedCliVersion = async (): Promise<string | undefined> => {
+	const lockPath = join(process.cwd(), 'package-lock.json')
+	if (!(await pathExists(lockPath))) {
+		return undefined
+	}
+	// eslint-disable-next-line @typescript-eslint/no-var-requires
+	const lockfile = require(lockPath)
+	return lockfile?.dependencies?.['@contember/cli']?.version
 }

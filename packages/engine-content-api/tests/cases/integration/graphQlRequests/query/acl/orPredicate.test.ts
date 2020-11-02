6671978e2233b7ae821c8c8c63871b639d695648
David Matejka <matej21@matej21.cz>
2020-11-02 20:39:53 +0100

    content api: add OR predicate test

---
+++ packages/engine-content-api/tests/cases/integration/graphQlRequests/query/acl/orPredicate.test.ts
@@ -0,0 +1,73 @@
+import { SchemaBuilder } from '@contember/schema-definition'
+import { Acl } from '@contember/schema'
+import { test } from 'uvu'
+import { execute } from '../../../../../src/test'
+import { GQL, SQL } from '../../../../../src/tags'
+import { testUuid } from '../../../../../src/testUuid'
+
+const schema = new SchemaBuilder()
+	.entity('Author', e => e.column('firstName').column('lastName').column('username'))
+	.buildSchema()
+
+const permissions: Acl.Permissions = {
+	Author: {
+		predicates: {
+			authorPredicate: {
+				or: [
+					{
+						firstName: { eq: 'John' },
+						lastName: { eq: 'Doe' },
+					},
+					{
+						username: { eq: 'johndoe' },
+					},
+				],
+			},
+		},
+		operations: {
+			read: {
+				id: 'authorPredicate',
+			},
+		},
+	},
+}
+
+test('predicate with OR', async () => {
+	await execute({
+		schema: schema,
+		permissions: permissions,
+		variables: {},
+		query: GQL`
+        query {
+          listAuthor {
+            id
+          }
+        }`,
+		executes: [
+			{
+				sql: SQL`
+					select "root_"."id" as "root_id" from "public"."author" as "root_"
+					where ("root_"."first_name" = ? and "root_"."last_name" = ? or "root_"."username" = ?)`,
+				parameters: ['John', 'Doe', 'johndoe'],
+				response: {
+					rows: [
+						{
+							root_id: testUuid(1),
+						},
+					],
+				},
+			},
+		],
+		return: {
+			data: {
+				listAuthor: [
+					{
+						id: testUuid(1),
+					},
+				],
+			},
+		},
+	})
+})
+
+test.run()

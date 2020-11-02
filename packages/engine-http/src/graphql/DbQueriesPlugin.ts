b5b7ec5105d91bc4ff428d721a0d2008a4b8643b
David Matejka <matej21@matej21.cz>
2020-11-02 18:57:12 +0100

    db queries plugin: fix context

--- packages/engine-http/src/graphql/DbQueriesPlugin.ts
+++ packages/engine-http/src/graphql/DbQueriesPlugin.ts
@@ -6,24 +6,23 @@ type Query = { sql: string; bindings: any; elapsed: number; error?: string; meta
 
 type Context = { db?: Client }
 export default class DbQueriesPlugin implements ApolloServerPlugin<Context> {
-	private readonly queries: Query[] = []
-
-	private listener: EventManager.QueryEndCallback = ({ sql, parameters, meta }, { timing }) =>
-		this.queries.push({ sql, bindings: parameters, elapsed: timing ? timing.selfDuration : 0, meta })
-
 	requestDidStart({ context: { db } }: GraphQLRequestContext<Context>): GraphQLRequestListener<Context> {
 		if (!db) {
 			return {}
 		}
-		db.eventManager.on(EventManager.Event.queryEnd, this.listener)
+		const queries: Query[] = []
+		const listener: EventManager.QueryEndCallback = ({ sql, parameters, meta }, { timing }) =>
+			queries.push({ sql, bindings: parameters, elapsed: timing ? timing.selfDuration : 0, meta })
+
+		db.eventManager.on(EventManager.Event.queryEnd, listener)
 		return {
 			willSendResponse: ({ response, context: { db } }) => {
 				if (!db) {
 					return
 				}
-				db.eventManager.removeListener(EventManager.Event.queryEnd, this.listener)
+				db.eventManager.removeListener(EventManager.Event.queryEnd, listener)
 				const extensions = response.extensions || (response.extensions = {})
-				extensions.dbQueries = this.queries.map(it => ({
+				extensions.dbQueries = queries.map(it => ({
 					...it,
 					path: (it.meta && it.meta.path) || [],
 				}))

46be06e3d56a2512a3f7ac0339a6fe32e5eea39b
David Matejka <matej21@matej21.cz>
2020-11-30 15:19:20 +0100

    schema migrations - differ: fix when updating a relation and also creating an inverse side [fixes #136]

--- packages/schema-migrations/tests/cases/integration/createRelationInverseSide.test.ts
+++ packages/schema-migrations/tests/cases/integration/createRelationInverseSide.test.ts
@@ -1,81 +1,50 @@
 import { testMigrations } from '../../src/tests'
 import { Model } from '@contember/schema'
 import { SQL } from '../../src/tags'
+import { SchemaBuilder } from '@contember/schema-definition'
 
 testMigrations('create inverse side relation (post with locales)', {
-	originalSchema: {
-		entities: {
-			Post: {
-				name: 'Post',
-				primary: 'id',
-				primaryColumn: 'id',
-				tableName: 'post',
-				fields: {},
-				unique: {},
-			},
-			PostLocale: {
-				name: 'PostLocale',
-				primary: 'id',
-				primaryColumn: 'id',
-				tableName: 'post_locale',
-				fields: {
-					post: {
-						name: 'post',
-						type: Model.RelationType.ManyHasOne,
-						target: 'Post',
-						joiningColumn: {
-							columnName: 'post_id',
-							onDelete: Model.OnDelete.restrict,
-						},
-						nullable: true,
-					},
-				},
-				unique: {},
-			},
-		},
-		enums: {},
-	},
-	updatedSchema: {
-		entities: {
-			Post: {
-				name: 'Post',
-				primary: 'id',
-				primaryColumn: 'id',
-				tableName: 'post',
-				fields: {
-					locales: {
-						name: 'locales',
-						type: Model.RelationType.OneHasMany,
-						target: 'PostLocale',
-						ownedBy: 'post',
-					},
-				},
-				unique: {},
-			},
-			PostLocale: {
-				name: 'PostLocale',
-				primary: 'id',
-				primaryColumn: 'id',
-				tableName: 'post_locale',
-				fields: {
-					post: {
-						inversedBy: 'locales',
-						name: 'post',
-						type: Model.RelationType.ManyHasOne,
-						target: 'Post',
-						joiningColumn: {
-							columnName: 'post_id',
-							onDelete: Model.OnDelete.restrict,
-						},
-						nullable: true,
-					},
-				},
-				unique: {},
-			},
-		},
-		enums: {},
-	},
+	originalSchema: new SchemaBuilder()
+		.entity('Post', e => e)
+		.entity('PostLocale', e => e.manyHasOne('post', r => r.target('Post')))
+		.buildSchema(),
+	updatedSchema: new SchemaBuilder()
+		.entity('Post', e => e)
+		.entity('PostLocale', e => e.manyHasOne('post', r => r.target('Post').inversedBy('locales')))
+		.buildSchema(),
 	diff: [
+		{
+			modification: 'createRelationInverseSide',
+			entityName: 'Post',
+			relation: {
+				name: 'locales',
+				type: Model.RelationType.OneHasMany,
+				target: 'PostLocale',
+				ownedBy: 'post',
+			},
+		},
+	],
+	sql: SQL``,
+})
+
+testMigrations('create inverse side relation together with changing onDelete behaviour', {
+	originalSchema: new SchemaBuilder()
+		.entity('Post', e => e)
+		.entity('PostLocale', e => e.manyHasOne('post', r => r.target('Post')))
+		.buildSchema(),
+	updatedSchema: new SchemaBuilder()
+		.entity('Post', e => e)
+		.entity('PostLocale', e =>
+			e.manyHasOne('post', r => r.target('Post').inversedBy('locales').onDelete(Model.OnDelete.cascade)),
+		)
+		.buildSchema(),
+	diff: [
+		{
+			modification: 'updateRelationOnDelete',
+			entityName: 'PostLocale',
+			fieldName: 'post',
+			onDelete: 'cascade',
+		},
 		{
 			modification: 'createRelationInverseSide',
 			entityName: 'Post',

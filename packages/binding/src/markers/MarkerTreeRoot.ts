8f06056628cb85cd127221196bdfd45c4bfe35ee
Jirka Vebr <jirkavebr@gmail.com>
2021-02-13 16:12:21 +0100

    MarkerTreeRoot: add generated ids

--- packages/binding/src/markers/MarkerTreeRoot.ts
+++ packages/binding/src/markers/MarkerTreeRoot.ts
@@ -2,9 +2,21 @@ import { Alias, PlaceholderName } from '../treeParameters'
 import { EntityListSubTreeMarker } from './EntityListSubTreeMarker'
 import { EntitySubTreeMarker } from './EntitySubTreeMarker'
 
+export type MarkerTreeRootId = string
+
 export class MarkerTreeRoot {
+	private static getNextSeed = (() => {
+		let seed = 0
+		return () => seed++
+	})()
+
+	public readonly treeId: MarkerTreeRootId
+
 	public constructor(
 		public readonly subTrees: Map<PlaceholderName, EntitySubTreeMarker | EntityListSubTreeMarker>,
 		public readonly placeholdersByAliases: Map<Alias, PlaceholderName>,
-	) {}
+	) {
+		const enumerabilityPreventingEntropy = (Math.random() * 1e5).toFixed(0)
+		this.treeId = `tree-${enumerabilityPreventingEntropy}-${MarkerTreeRoot.getNextSeed()}`
+	}
 }

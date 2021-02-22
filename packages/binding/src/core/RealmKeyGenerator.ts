12c7654c51c9c89e7bb784e1dfa65e3e37db9c8d
Jirka Vebr <jirkavebr@gmail.com>
2021-02-22 16:17:34 +0100

    EntityRealm: include parent id

--- packages/binding/src/core/RealmKeyGenerator.ts
+++ packages/binding/src/core/RealmKeyGenerator.ts
@@ -1,18 +1,32 @@
 import { RuntimeId } from '../accessorTree'
 import { EntityFieldMarkersContainer } from '../markers'
-import { EntityRealmKey } from '../treeParameters'
-import {
-	EntityListBlueprint,
-	EntityRealmBlueprint,
-	EntityRealmState,
-	EntityRealmStateStub,
-	getEntityMarker,
-} from './state'
+import { EntityId, EntityRealmKey, PlaceholderName } from '../treeParameters'
+import { assertNever } from '../utils'
+import { EntityListState, EntityRealmBlueprint, EntityRealmState } from './state'
 
 const GLUE = '--'
 const keyEndRegex = new RegExp(`${GLUE}\\d+$`)
 
 export class RealmKeyGenerator {
+	private static getParentStateId = (() => {
+		// We're using this id, and not, well, the entity id, because it *SIGNIFICANTLY* reduces the complexity of
+		// entity id changes.
+		let seed = 0
+		const rootSeed = seed++
+		const seedCache: WeakMap<EntityRealmState | EntityListState, string> = new WeakMap()
+
+		return (entity: EntityRealmState | EntityListState | undefined) => {
+			if (entity === undefined) {
+				return rootSeed
+			}
+			let existing = seedCache.get(entity)
+
+			if (existing === undefined) {
+				seedCache.set(entity, (existing = (seed++).toFixed(0)))
+			}
+			return existing
+		}
+	})()
 	private static getMarkerId = (() => {
 		let seed = 0
 		const seedCache: WeakMap<EntityFieldMarkersContainer, string> = new WeakMap()
@@ -32,25 +46,34 @@ export class RealmKeyGenerator {
 		return candidate.startsWith(`key${GLUE}`) && keyEndRegex.test(candidate)
 	}
 
-	private static generateKey(id: string, container: EntityFieldMarkersContainer) {
+	private static generateKey(
+		parent: EntityRealmState | EntityListState | undefined,
+		placeholderName: PlaceholderName,
+		id: EntityId,
+		container: EntityFieldMarkersContainer,
+	): EntityRealmKey {
 		// The static prefix serves to really emphasize that this is *NOT* just a uuid
-		return `key${GLUE}${id}${GLUE}${this.getMarkerId(container)}`
-	}
-
-	public static getRealmStateKey(realm: EntityRealmState): EntityRealmKey {
-		return this.generateKey(realm.entity.id.value, getEntityMarker(realm).fields)
-	}
-
-	public static getRealmStubKey(stub: EntityRealmStateStub): EntityRealmKey {
-		return this.generateKey(stub.entity.id.value, getEntityMarker(stub).fields)
-	}
-
-	public static getListEntityRealmKey(id: RuntimeId | string, blueprint: EntityListBlueprint): EntityRealmKey {
-		return this.generateKey(typeof id === 'string' ? id : id.value, blueprint.marker.fields)
+		return `key${GLUE}${this.getParentStateId(parent)}${GLUE}${placeholderName}${id}${GLUE}${this.getMarkerId(
+			container,
+		)}`
 	}
 
 	public static getRealmKey(id: RuntimeId, blueprint: EntityRealmBlueprint): EntityRealmKey {
+		const parent = blueprint.parent
+		let placeholderName: PlaceholderName
+
+		if (blueprint.type === 'subTree') {
+			placeholderName = blueprint.marker.placeholderName
+		} else if (blueprint.type === 'listEntity') {
+			placeholderName = blueprint.parent.blueprint.marker.placeholderName
+		} else if (blueprint.type === 'hasOne') {
+			placeholderName = blueprint.marker.placeholderName
+		} else {
+			return assertNever(blueprint)
+		}
 		return this.generateKey(
+			parent,
+			placeholderName,
 			id.value,
 			blueprint.type === 'listEntity' ? blueprint.parent.blueprint.marker.fields : blueprint.marker.fields,
 		)

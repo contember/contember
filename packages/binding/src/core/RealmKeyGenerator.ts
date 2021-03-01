3954040da63027bfcbf84bad80997a4dcfcc897b
Jirka Vebr <jirkavebr@gmail.com>
2021-03-01 13:18:16 +0100

    Realm keys: use a dedicated cache

--- packages/binding/src/core/RealmKeyGenerator.ts
+++ packages/binding/src/core/RealmKeyGenerator.ts
@@ -3,43 +3,16 @@ import { EntityFieldMarkersContainer } from '../markers'
 import { EntityId, EntityRealmKey, PlaceholderName } from '../treeParameters'
 import { assertNever } from '../utils'
 import { EntityListState, EntityRealmBlueprint, EntityRealmState } from './state'
+import { WeakIdCache } from './structures'
 
 const GLUE = '--'
 const keyEndRegex = new RegExp(`${GLUE}\\d+$`)
 
 export class RealmKeyGenerator {
-	private static getParentStateId = (() => {
-		// We're using this id, and not, well, the entity id, because it *SIGNIFICANTLY* reduces the complexity of
-		// entity id changes.
-		let seed = 0
-		const rootSeed = seed++
-		const seedCache: WeakMap<EntityRealmState | EntityListState, string> = new WeakMap()
-
-		return (entity: EntityRealmState | EntityListState | undefined) => {
-			if (entity === undefined) {
-				return rootSeed
-			}
-			let existing = seedCache.get(entity)
-
-			if (existing === undefined) {
-				seedCache.set(entity, (existing = (seed++).toFixed(0)))
-			}
-			return existing
-		}
-	})()
-	private static getMarkerId = (() => {
-		let seed = 0
-		const seedCache: WeakMap<EntityFieldMarkersContainer, string> = new WeakMap()
-
-		return (container: EntityFieldMarkersContainer) => {
-			let existing = seedCache.get(container)
-
-			if (existing === undefined) {
-				seedCache.set(container, (existing = (seed++).toFixed(0)))
-			}
-			return existing
-		}
-	})()
+	// We're using this id, and not, well, the entity id, because it *SIGNIFICANTLY* reduces the complexity of
+	// entity id changes.
+	private static parentStateIdCache = new WeakIdCache<EntityRealmState | EntityListState>()
+	private static markerIdCache = new WeakIdCache<EntityFieldMarkersContainer>()
 
 	public static vaguelyAppearsToBeAKey(candidate: string): boolean {
 		// This is just for error messages. No need to be super precise.
@@ -53,9 +26,9 @@ export class RealmKeyGenerator {
 		container: EntityFieldMarkersContainer,
 	): EntityRealmKey {
 		// The static prefix serves to really emphasize that this is *NOT* just a uuid
-		return `key${GLUE}${this.getParentStateId(parent)}${GLUE}${placeholderName}${id}${GLUE}${this.getMarkerId(
-			container,
-		)}`
+		return `key${GLUE}${this.parentStateIdCache.getOptionalKeyId(
+			parent,
+		)}${GLUE}${placeholderName}${id}${GLUE}${this.markerIdCache.getId(container)}`
 	}
 
 	public static getRealmKey(id: RuntimeId, blueprint: EntityRealmBlueprint): EntityRealmKey {

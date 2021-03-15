69d1009754bb5061a8b0f86eecb00866e004a987
Jirka Vebr <jirkavebr@gmail.com>
2021-03-15 19:50:52 +0100

    Admin: add DialogProvider

--- packages/admin/src/components/Admin.tsx
+++ packages/admin/src/components/Admin.tsx
@@ -1,6 +1,6 @@
 import { Environment, EnvironmentContext } from '@contember/binding'
 import { ContemberClient } from '@contember/react-client'
-import { ContainerSpinner } from '@contember/ui'
+import { ContainerSpinner, DialogProvider } from '@contember/ui'
 import { ComponentType, lazy, LazyExoticComponent, memo, Suspense, useEffect, useState } from 'react'
 import { createAction } from 'redux-actions'
 import { populateRequest } from '../actions/request'
@@ -53,74 +53,76 @@ export const Admin = memo((props: AdminProps) => {
 			<ReduxStoreProvider store={store}>
 				<NavigationProvider>
 					<NavigationIsActiveProvider>
-						<Router
-							routes={{
-								login: () => <Login />,
-								projects_list: () => <ProjectsList configs={props.configs} />,
-								project_page: (() => {
-									const normalizedConfigs: {
-										[project: string]: {
-											[stage: string]: ProjectConfig & {
-												lazyComponent: LazyExoticComponent<ComponentType<any>>
-												rootEnvironment: Environment
+						<DialogProvider>
+							<Router
+								routes={{
+									login: () => <Login />,
+									projects_list: () => <ProjectsList configs={props.configs} />,
+									project_page: (() => {
+										const normalizedConfigs: {
+											[project: string]: {
+												[stage: string]: ProjectConfig & {
+													lazyComponent: LazyExoticComponent<ComponentType<any>>
+													rootEnvironment: Environment
+												}
+											}
+										} = {}
+
+										for (const config of props.configs) {
+											if (!(config.project in normalizedConfigs)) {
+												normalizedConfigs[config.project] = {}
+											}
+											if (config.stage in normalizedConfigs[config.project]) {
+												throw new Error(
+													`Duplicate project-stage pair supplied for project '${config.project}' and stage '${config.stage}'`,
+												)
+											}
+											normalizedConfigs[config.project][config.stage] = {
+												...config,
+												lazyComponent: lazy(config.component),
+												rootEnvironment: adminWideEnvironment.putDelta({
+													dimensions: config.defaultDimensions || {},
+												}),
 											}
 										}
-									} = {}
 
-									for (const config of props.configs) {
-										if (!(config.project in normalizedConfigs)) {
-											normalizedConfigs[config.project] = {}
-										}
-										if (config.stage in normalizedConfigs[config.project]) {
-											throw new Error(
-												`Duplicate project-stage pair supplied for project '${config.project}' and stage '${config.stage}'`,
+										return ({ route }: { route: PageRequest<any> }) => {
+											const config = props.configs.find(
+												({ project, stage }) => project === route.project && stage === route.stage,
 											)
-										}
-										normalizedConfigs[config.project][config.stage] = {
-											...config,
-											lazyComponent: lazy(config.component),
-											rootEnvironment: adminWideEnvironment.putDelta({
-												dimensions: config.defaultDimensions || {},
-											}),
-										}
-									}
+											const relevantConfig = normalizedConfigs[route.project][route.stage]
+											const Component = relevantConfig.lazyComponent
 
-									return ({ route }: { route: PageRequest<any> }) => {
-										const config = props.configs.find(
-											({ project, stage }) => project === route.project && stage === route.stage,
-										)
-										const relevantConfig = normalizedConfigs[route.project][route.stage]
-										const Component = relevantConfig.lazyComponent
-
-										if (config) {
-											relevantConfig.rootEnvironment = relevantConfig.rootEnvironment
-												.updateDimensionsIfNecessary(route.dimensions, config.defaultDimensions || {})
-												.putDelta({
-													...route.parameters,
-												})
-											return (
-												<ContemberClient
-													apiBaseUrl={props.clientConfig.apiBaseUrl}
-													loginToken={props.clientConfig.loginToken}
-													project={route.project}
-													sessionToken={store.getState().auth.identity?.token}
-													stage={route.stage}
-												>
-													<EnvironmentContext.Provider value={relevantConfig.rootEnvironment}>
-														<Suspense fallback={<ContainerSpinner />}>
-															<Component />
-														</Suspense>
-													</EnvironmentContext.Provider>
-												</ContemberClient>
-											)
-										} else {
-											return <>{`No such project or stage as ${route.project}/${route.stage}`}</>
+											if (config) {
+												relevantConfig.rootEnvironment = relevantConfig.rootEnvironment
+													.updateDimensionsIfNecessary(route.dimensions, config.defaultDimensions || {})
+													.putDelta({
+														...route.parameters,
+													})
+												return (
+													<ContemberClient
+														apiBaseUrl={props.clientConfig.apiBaseUrl}
+														loginToken={props.clientConfig.loginToken}
+														project={route.project}
+														sessionToken={store.getState().auth.identity?.token}
+														stage={route.stage}
+													>
+														<EnvironmentContext.Provider value={relevantConfig.rootEnvironment}>
+															<Suspense fallback={<ContainerSpinner />}>
+																<Component />
+															</Suspense>
+														</EnvironmentContext.Provider>
+													</ContemberClient>
+												)
+											} else {
+												return <>{`No such project or stage as ${route.project}/${route.stage}`}</>
+											}
 										}
-									}
-								})(),
-							}}
-						/>
-						<Toaster />
+									})(),
+								}}
+							/>
+							<Toaster />
+						</DialogProvider>
 					</NavigationIsActiveProvider>
 				</NavigationProvider>
 			</ReduxStoreProvider>

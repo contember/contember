6d870e4e5e362bc67f8799614c3842a836273553
David Matejka <matej21@matej21.cz>
2021-03-15 16:28:20 +0100

    db queries plugin: add failed queries

--- packages/engine-http/src/graphql/DbQueriesPlugin.ts
+++ packages/engine-http/src/graphql/DbQueriesPlugin.ts
@@ -2,7 +2,7 @@ import { Client, EventManager } from '@contember/database'
 import { ApolloServerPlugin, GraphQLRequestListener } from 'apollo-server-plugin-base'
 import { GraphQLRequestContext } from 'apollo-server-core'
 
-type Query = { sql: string; bindings: any; elapsed: number; error?: string; meta?: any; rowCount?: number }
+type Query = { sql: string; bindings: any; elapsed?: number; error?: string; meta?: any; rowCount?: number }
 
 export default class DbQueriesPlugin<Ctx extends Record<string, any>> implements ApolloServerPlugin<Ctx> {
 	constructor(private readonly dbResolver: (context: Ctx) => Client) {}
@@ -15,7 +15,15 @@ export default class DbQueriesPlugin<Ctx extends Record<string, any>> implements
 		const queries: Query[] = []
 		const listener: EventManager.QueryEndCallback = ({ sql, parameters, meta }, { timing, rowCount }) =>
 			queries.push({ sql, bindings: parameters, elapsed: timing ? timing.selfDuration : 0, meta, rowCount })
+		const errorListener: EventManager.QueryErrorCallback = ({ sql, parameters, meta }, error) =>
+			queries.push({
+				sql,
+				bindings: parameters,
+				meta,
+				error: 'message' in error ? error.message : 'unknown error',
+			})
 		db.eventManager.on(EventManager.Event.queryEnd, listener)
+		db.eventManager.on(EventManager.Event.queryError, errorListener)
 		return {
 			willSendResponse: ({ response, context }) => {
 				const db = this.dbResolver(context)
@@ -23,6 +31,7 @@ export default class DbQueriesPlugin<Ctx extends Record<string, any>> implements
 					return
 				}
 				db.eventManager.removeListener(EventManager.Event.queryEnd, listener)
+				db.eventManager.removeListener(EventManager.Event.queryError, errorListener)
 				const extensions = response.extensions || (response.extensions = {})
 				extensions.dbQueries = queries.map(it => ({
 					...it,

717ad79b1a19ecccf72c2e6f1be304cfe9fb9122
Jirka Vebr <jirkavebr@gmail.com>
2021-05-17 13:39:31 +0200

    Add a new package: admin-i18n

---
+++ packages/admin-i18n/src/messages/csCZ.ts
@@ -0,0 +1,12 @@
+import type { AdminDictionary } from '@contember/admin'
+
+export const csCZ: AdminDictionary = {
+	repeater: {
+		addButton: {
+			text: 'Přidat další',
+		},
+		emptyMessage: {
+			text: 'Nic tu není. Zkuste přidat novou položku.',
+		},
+	},
+}

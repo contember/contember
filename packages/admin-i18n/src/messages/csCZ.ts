d718d8394a14bf6705e55cf76ffc4f746fdd4a18
Jirka Vebr <jirkavebr@gmail.com>
2021-05-17 16:18:01 +0200

    Translate Grid to Czech

--- packages/admin-i18n/src/messages/csCZ.ts
+++ packages/admin-i18n/src/messages/csCZ.ts
@@ -1,6 +1,47 @@
 import type { AdminDictionary } from '@contember/admin'
 
 export const csCZ: AdminDictionary = {
+	dataGrid: {
+		emptyMessage: {
+			text: 'Žádné položky.',
+		},
+		columnFiltering: {
+			showMenuButton: {
+				text: 'Všechny filtry',
+			},
+			heading: 'Všechny filtry',
+			emptyMessage: {
+				text: 'Žádné aktivní filtry.',
+			},
+			columnColumn: {
+				text: 'Sloupec',
+			},
+			filterColumn: {
+				text: 'Filtr',
+			},
+			addFilterButton: {
+				text: 'Přidat filtr',
+			},
+		},
+		columnHiding: {
+			showMenuButton: {
+				text: 'Sloupce',
+			},
+			heading: 'Sloupce',
+		},
+		paging: {
+			first: 'První',
+			previous: 'Předchozí',
+			next: 'Následující',
+			last: 'Poslední',
+
+			status: {
+				unknownPageTotal: 'Strana {pageNumber}',
+				knownPageTotal: 'Strana {pageNumber} z {totalPageCount}',
+				itemCount: '({itemCount, plural, =0 {Žádné položky} one {# položka} few {# položky} other {# položek}})',
+			},
+		},
+	},
 	repeater: {
 		addButton: {
 			text: 'Přidat další',
--- packages/admin/src/adminDictionary.ts
+++ packages/admin/src/adminDictionary.ts
@@ -1,6 +1,7 @@
+import type { DataGridDictionary, RepeaterDictionary } from './components'
+
 // This isn't in i18n in order to avoid unnecessary circular imports.
-import type { RepeaterDictionary } from './components'
 
 // This should ideally be a complete list of all individual dictionaries throughout the entire package.
 // That way, translation packages can implement this and have TS warn them about missing messages.
-export type AdminDictionary = RepeaterDictionary
+export type AdminDictionary = DataGridDictionary & RepeaterDictionary

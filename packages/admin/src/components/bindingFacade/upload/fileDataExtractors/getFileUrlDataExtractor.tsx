b1b6ca607333f4f316bc175e181385416d92f104
Jirka Vebr <jirkavebr@gmail.com>
2021-06-21 17:19:24 +0200

    FileUrlDataExtractor: mandatory url field

--- packages/admin/src/components/bindingFacade/upload/fileDataExtractors/getFileUrlDataExtractor.tsx
+++ packages/admin/src/components/bindingFacade/upload/fileDataExtractors/getFileUrlDataExtractor.tsx
@@ -4,20 +4,20 @@ import type { S3FileUploader } from '@contember/client'
 import type { FileDataExtractor } from '../interfaces'
 
 export interface FileUrlDataExtractorProps {
-	urlField?: SugaredFieldProps['field']
+	urlField: SugaredFieldProps['field']
 }
 
 export const getFileUrlDataExtractor: (
 	props: FileUrlDataExtractorProps,
 ) => FileDataExtractor<unknown, S3FileUploader.SuccessMetadata> = ({ urlField }) => ({
-	staticRender: () => !!urlField && <SugaredField field={urlField} />,
+	staticRender: () => <SugaredField field={urlField} />,
 	destroy: ({ entity }) => {
-		!!urlField && entity.getField(urlField).updateValue(null)
+		entity.getField(urlField).updateValue(null)
 	},
 	populateFields: ({ entity, uploadResult }) => {
 		if (!uploadResult.fileUrl || typeof uploadResult.fileUrl !== 'string') {
 			return
 		}
-		!!urlField && entity.getField(urlField).updateValue(uploadResult.fileUrl)
+		entity.getField(urlField).updateValue(uploadResult.fileUrl)
 	},
 })

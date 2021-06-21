7a427c4887b6b29053935e25bd887baf1232565a
Jirka Vebr <jirkavebr@gmail.com>
2021-06-21 18:42:29 +0200

    Add a forgotten default prop

--- packages/admin/src/components/bindingFacade/upload/internalComponents/FileInput.tsx
+++ packages/admin/src/components/bindingFacade/upload/internalComponents/FileInput.tsx
@@ -22,7 +22,7 @@ export function FileInput({
 	description,
 	labelDescription,
 	children,
-	enableAddingNew,
+	enableAddingNew = true,
 
 	addButtonComponent: AddButton = Button,
 	addButtonComponentExtraProps,

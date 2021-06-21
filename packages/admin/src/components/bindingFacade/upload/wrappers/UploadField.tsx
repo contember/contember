0d1656000871d51825b4d9e7906ed0f653ea59af
Jirka Vebr <jirkavebr@gmail.com>
2021-06-21 18:08:35 +0200

    Add UploadField

---
+++ packages/admin/src/components/bindingFacade/upload/wrappers/UploadField.tsx
@@ -0,0 +1,63 @@
+import { Component, useEnvironment } from '@contember/binding'
+import type { ReactNode } from 'react'
+import { useMemo } from 'react'
+import type { FileInputPublicProps } from '../internalComponents'
+import { BareUploadField } from '../internalComponents'
+import { getResolvedFileKinds, HybridFileKindProps } from '../templating'
+
+export interface UploadFieldProps extends FileInputPublicProps, HybridFileKindProps {
+	children?: ReactNode
+}
+
+export const UploadField = Component<UploadFieldProps>(
+	({
+		acceptFile,
+		acceptMimeTypes,
+		children,
+		discriminationField,
+		extractors,
+		hasUploadedFile,
+		renderFilePreview,
+		renderUploadedFile,
+		uploader,
+		...props
+	}) => {
+		const environment = useEnvironment()
+		const fileKinds = useMemo(
+			() =>
+				getResolvedFileKinds(
+					{
+						acceptFile,
+						acceptMimeTypes,
+						children,
+						discriminationField,
+						extractors,
+						hasUploadedFile,
+						renderFilePreview,
+						renderUploadedFile,
+						uploader,
+					},
+					environment,
+					'UploadField',
+				),
+			[
+				acceptFile,
+				acceptMimeTypes,
+				children,
+				discriminationField,
+				extractors,
+				hasUploadedFile,
+				renderFilePreview,
+				renderUploadedFile,
+				uploader,
+				environment,
+			],
+		)
+
+		return <BareUploadField {...props} fileKinds={fileKinds} />
+	},
+	(props, environment) => (
+		<BareUploadField {...props} fileKinds={getResolvedFileKinds(props, environment, 'UploadField')} />
+	),
+	'UploadField',
+)
--- packages/admin/src/components/bindingFacade/upload/wrappers/index.ts
+++ packages/admin/src/components/bindingFacade/upload/wrappers/index.ts
@@ -1,1 +1,2 @@
 export * from './FileRepeater'
+export * from './UploadField'

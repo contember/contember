6dd91ab8bb71feb10b55f0b16fee7e9c3914b75a
Jirka Vebr <jirkavebr@gmail.com>
2021-06-23 00:24:16 +0200

    AcceptFileKindError: more consistent naming

--- packages/admin/src/components/bindingFacade/upload/interfaces/AcceptFileKindError.ts
+++ packages/admin/src/components/bindingFacade/upload/interfaces/AcceptFileKindError.ts
@@ -1,9 +1,9 @@
 export interface AcceptFileKindErrorOptions {
-	userMessage: string
+	endUserMessage: string
 }
 
 export class AcceptFileKindError extends Error {
 	public constructor(public readonly options: AcceptFileKindErrorOptions) {
-		super(options.userMessage)
+		super(options.endUserMessage)
 	}
 }

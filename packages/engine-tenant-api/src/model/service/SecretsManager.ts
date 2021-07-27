c7c1e7093ff53accc77e8548d3d107b51a2c0002
David Matejka <matej21@matej21.cz>
2021-07-27 16:41:01 +0200

    tenant: mark project as updated when changing a secret

--- packages/engine-tenant-api/src/model/service/SecretsManager.ts
+++ packages/engine-tenant-api/src/model/service/SecretsManager.ts
@@ -1,4 +1,4 @@
-import { SetProjectSecretCommand } from '../commands'
+import { SetProjectSecretCommand, UpdateProjectCommand } from '../commands'
 import { Providers } from '../providers'
 import { ListProjectSecretsQuery } from '../queries'
 import { DatabaseContext } from '../utils'
@@ -7,7 +7,10 @@ export class SecretsManager {
 	constructor(private dbContext: DatabaseContext, private providers: Providers) {}
 
 	public async setSecret(projectId: string, key: string, value: string): Promise<void> {
-		await this.dbContext.commandBus.execute(new SetProjectSecretCommand(projectId, key, value))
+		await this.dbContext.transaction(async db => {
+			await db.commandBus.execute(new SetProjectSecretCommand(projectId, key, value))
+			await db.commandBus.execute(new UpdateProjectCommand(projectId, {}))
+		})
 	}
 
 	public async readSecrets(projectId: string): Promise<Record<string, string>> {

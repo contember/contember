4db45eb83d7e6b7bdcf988726303b26b2d020a96
Jirka Vebr <jirkavebr@gmail.com>
2021-07-30 19:36:29 +0200

    Test babel runtime: transform jsx and import.meta.env vars

--- build/getBabelJestConfig.cjs
+++ build/getBabelJestConfig.cjs
@@ -1,3 +1,8 @@
 module.exports = () => ({
-	presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-react', '@babel/preset-typescript'],
+	presets: [
+		['@babel/preset-env', { targets: { node: 'current' } }],
+		['@babel/preset-react', { runtime: 'automatic' }],
+		'@babel/preset-typescript',
+	],
+	plugins: ['./../../build/babelImportMeta.cjs'],
 })

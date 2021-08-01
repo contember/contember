cf4ed89a8f87a8cf8e524573880ab33d3546341f
Jirka Vebr <jirkavebr@gmail.com>
2021-08-01 15:10:34 +0200

    MessageFormatter: fix an accidental typing dependency

--- packages/admin/src/i18n/MessageFormatter.ts
+++ packages/admin/src/i18n/MessageFormatter.ts
@@ -1,12 +1,15 @@
-import type { PrimitiveType } from 'intl-messageformat'
 import type { ReactNode } from 'react'
 import type { MessageDictionary, MessageDictionaryKeys } from './MessageDictionary'
 
-export type FormatJSXChunk = (parts: ReactNode) => ReactNode
+export type MessageFormatterFormatJSXChunk = (parts: ReactNode) => ReactNode
+export type MessageFormatterPrimitiveType = string | number | boolean | null | undefined | Date
 
 export interface MessageFormatter<Dict extends MessageDictionary> {
-	(key: MessageDictionaryKeys<Dict>, values?: Record<string, PrimitiveType>): string
-	(key: MessageDictionaryKeys<Dict>, values?: Record<string, PrimitiveType | FormatJSXChunk>): ReactNode
+	(key: MessageDictionaryKeys<Dict>, values?: Record<string, MessageFormatterPrimitiveType>): string
+	(
+		key: MessageDictionaryKeys<Dict>,
+		values?: Record<string, MessageFormatterPrimitiveType | MessageFormatterFormatJSXChunk>,
+	): ReactNode
 	(userSpecifiedKey: string | undefined, fallbackKey: MessageDictionaryKeys<Dict>, values?: Record<string, any>): string
 	<U extends ReactNode>(userSpecifiedKey: U, fallbackKey: MessageDictionaryKeys<Dict>, values?: Record<string, any>):
 		| U

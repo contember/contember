26fe46af697e32c6d4caadaa2289b355ff379e07
Filip Chalupa <chalupa.filip@gmail.com>
2021-08-02 14:08:01 +0200

    Add SoundCloud embed handler

--- packages/admin/src/components/bindingFacade/richText/blockEditor/embed/handlers/index.ts
+++ packages/admin/src/components/bindingFacade/richText/blockEditor/embed/handlers/index.ts
@@ -1,3 +1,4 @@
 export { GoogleFormEmbedHandler as GoogleForm } from './GoogleFormEmbedHandler'
+export { SoundCloudEmbedHandler as SoundCloud } from './SoundCloudEmbedHandler'
 export { VimeoEmbedHandler as Vimeo } from './VimeoEmbedHandler'
 export { YouTubeEmbedHandler as YouTube } from './YouTubeEmbedHandler'

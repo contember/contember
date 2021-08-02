7426aa5da047f100f9f49899a72f2ced3a41db27
Jirka Vebr <jirkavebr@gmail.com>
2021-08-02 17:04:16 +0200

    TS BC break: fix i18n typings

--- packages/admin/src/i18n/useMessageFormatter.ts
+++ packages/admin/src/i18n/useMessageFormatter.ts
@@ -3,7 +3,7 @@ import { createElement, Fragment, ReactElement, ReactNode, useCallback, useMemo 
 import { DictionaryCache } from './DictionaryCache'
 import { I18nError } from './I18nError'
 import type { I18nMetadata } from './I18nMetadata'
-import type { MessageDictionary, MessageDictionaryKeys } from './MessageDictionary'
+import type { MessageDictionary } from './MessageDictionary'
 import type { MessageFormatter } from './MessageFormatter'
 import { useI18n } from './useI18n'
 
@@ -86,19 +86,15 @@ export const useMessageFormatter = <Dict extends MessageDictionary>(
 	return useCallback<MessageFormatter<Dict>>(
 		<U extends ReactNode>(
 			...args:
-				| [key: MessageDictionaryKeys<Dict>, values?: Record<string, any>]
-				| [userSpecifiedKey: U, fallbackKey: MessageDictionaryKeys<Dict>, values?: Record<string, any>]
+				| [key: string, values?: Record<string, any>]
+				| [userSpecifiedKey: U, fallbackKey: string, values?: Record<string, any>]
 		): U | string => {
 			if (args.length === 1 || (args.length === 2 && typeof args[1] !== 'string')) {
-				const [key, values] = args as [MessageDictionaryKeys<Dict>, Record<string, any> | undefined]
+				const [key, values] = args as [string, Record<string, any> | undefined]
 
-				return formatMessage(i18n, fallbackDictionaryCache, key, values) as any
+				return formatMessage(i18n, fallbackDictionaryCache, key, values) as U | string
 			} else {
-				const [userSpecifiedKey, fallbackKey, values] = args as [
-					U,
-					MessageDictionaryKeys<Dict>,
-					Record<string, unknown> | undefined,
-				]
+				const [userSpecifiedKey, fallbackKey, values] = args as [U, string, Record<string, unknown> | undefined]
 
 				if (userSpecifiedKey !== undefined) {
 					// The user-specified key isn't mandatory
@@ -119,7 +115,7 @@ export const useMessageFormatter = <Dict extends MessageDictionary>(
 					// It's probably a ReactNode that handles strings through other means.
 					return userSpecifiedKey
 				}
-				return formatMessage(i18n, fallbackDictionaryCache, fallbackKey, values) as any
+				return formatMessage(i18n, fallbackDictionaryCache, fallbackKey, values) as U | string
 			}
 		},
 		[fallbackDictionaryCache, i18n],

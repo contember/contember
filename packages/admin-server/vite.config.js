e8a8b597c45cf11a22640917570273f85c657509
Jan Tvrdik <jan.tvrdik@gmail.com>
2021-08-05 16:44:13 +0200

    admin-server: simplify vite config

--- packages/admin-server/vite.config.js
+++ packages/admin-server/vite.config.js
@@ -1,21 +1,16 @@
 import { defineConfig } from 'vite'
-import { resolve } from "path";
-import { rootDirectory } from "../../build/rootDirectory";
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { resolve } from 'path';
+import { rootDirectory } from '../../build/rootDirectory'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 import { packageList } from "../../build/packageList";
 
-export default defineConfig(async ({ command, mode}) => ({
+export default defineConfig(async ({ command, mode }) => ({
 	build: {
-		emptyOutDir: false,
 		minify: mode === 'development' ? false : 'terser',
 		outDir: resolve(rootDirectory, `packages/admin-server/dist`),
 		assetsDir: '_static',
-		sourcemap: mode === 'development' ? 'inline' : false,
-		target: mode === 'development' ? 'esnext' : 'es2020',
-	},
-	esbuild: {
-		jsxInject: `import * as React from 'react'`,
-		target: 'esnext',
+		sourcemap: true,
+		target: 'es2020',
 	},
 	plugins: [reactRefresh()],
 	resolve: {
@@ -24,11 +19,6 @@ export default defineConfig(async ({ command, mode}) => ({
 				find: `@contember/${packageName}`,
 				replacement: resolve(rootDirectory, `packages/${packageName}/src/index.ts`),
 			})),
-			{
-				find: 'attr-accept',
-				replacement: resolve(rootDirectory, `packages/admin/node_modules/attr-accept/src/index.js`),
-			},
 		],
-		dedupe: packageList.map(packageName => `@contember/${packageName}`),
 	},
 }))

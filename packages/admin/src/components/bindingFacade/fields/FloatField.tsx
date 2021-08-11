ab1b61ee1764aa3d7e8479892a4839de1b7d3910
David Matejka <matej21@matej21.cz>
2021-08-11 15:36:09 +0200

    float field: fix value parsing

--- packages/admin/src/components/bindingFacade/fields/FloatField.tsx
+++ packages/admin/src/components/bindingFacade/fields/FloatField.tsx
@@ -1,17 +1,31 @@
 import { SingleLineTextInputProps, TextInput } from '@contember/ui'
 import { SimpleRelativeSingleField, SimpleRelativeSingleFieldProps } from '../auxiliary'
 import { useTextInput } from './useTextInput'
+import { useState } from 'react'
 
 export type FloatFieldProps = SimpleRelativeSingleFieldProps &
 	Omit<SingleLineTextInputProps, 'value' | 'onChange' | 'validationState' | 'allowNewlines'>
 
 export const FloatField = SimpleRelativeSingleField<FloatFieldProps, number>(
 	(fieldMetadata, { defaultValue, onBlur, ...props }) => {
+		const [innerValue, setInnerValue] = useState('')
 		const inputProps = useTextInput<number>({
 			fieldMetadata,
 			onBlur,
-			parse: parseFloat,
-			format: value => (typeof value === 'number' ? value.toString(10) : '0'),
+			parse: val => {
+				const normalizedValue = (val || '0')
+					.replaceAll(',', '.')
+					.replace(/([^0-9.]|(?<=\.\d*)\.)/g, '')
+					.replace(/^0*(?=\d)/, '')
+				setInnerValue(normalizedValue)
+				return parseFloat(normalizedValue)
+			},
+			format: value =>
+				innerValue && parseFloat(innerValue) === value
+					? innerValue
+					: typeof value === 'number'
+					? value.toString(10)
+					: '0',
 		})
 		return <TextInput {...inputProps} {...props} />
 	},

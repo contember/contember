1fcc35418ba9cc44651e9dc141aac950a91ef1ee
Martin Adamko <attitude@users.noreply.github.com>
2021-08-19 13:56:21 +0200

    Update PersistControl component

--- packages/ui/src/components/PersistControl.tsx
+++ packages/ui/src/components/PersistControl.tsx
@@ -1,27 +1,49 @@
-import cn from 'classnames'
-import { ContentStatus, SaveControl } from '.'
+import classNames from 'classnames'
+import { ReactNode } from 'react'
+import { Icon, SaveControl, Spinner } from '.'
 import { useClassNamePrefix } from '../auxiliary'
+import { toEnumStateClass } from '../utils'
 
 export interface PersistControlProps {
+	label?: ReactNode
+	labelSaved ?: ReactNode
 	isMutating: boolean
 	isDirty: boolean
 	onSave: () => void
 }
 
-export function PersistControl({ isMutating, isDirty, onSave }: PersistControlProps) {
+export function PersistControl({
+	isDirty,
+	isMutating,
+	label,
+	labelSaved,
+	onSave,
+}: PersistControlProps) {
 	const prefix = useClassNamePrefix()
-
 	const isDisabled = isMutating || !isDirty
+	const classList = classNames(
+		`${prefix}persistControl`,
+		toEnumStateClass(isDirty ? 'dirty' : undefined),
+		toEnumStateClass(isDisabled ? 'disabled' : undefined),
+		toEnumStateClass(isMutating ? 'mutating' : undefined),
+	)
 
-	const message = isMutating ? 'Saving…' : !isDirty ? 'All saved' : 'Unsaved changes'
+	const labelSavedText = labelSaved ?? 'Saved'
+	const labelSaveText = label ?? 'Save'
+
+	const isSaved = isDirty || isMutating
 
 	return (
-		<div className={`${prefix}persistControl`}>
-			<div className={`${prefix}persistControl-label`}>
-				<ContentStatus label={message} />
-			</div>
-			<SaveControl isPrimaryDisabled={isDisabled} primaryAction="Save" onPrimary={isDisabled ? undefined : onSave} />
-		</div>
+		<SaveControl
+			isPrimaryDisabled={isDisabled}
+			onPrimary={isDisabled ? undefined : onSave}
+			primaryAction={<span className={classList}>
+				<span className={`${prefix}persistControl-label--saved`} aria-hidden={isSaved}>{labelSavedText}</span>
+				<span className={`${prefix}persistControl-label--dirty`} aria-hidden={!isSaved}>{labelSaveText}</span>
+				<span className={`${prefix}persistControl-icon`} aria-hidden={isSaved}><Icon blueprintIcon={'tick'} /></span>
+				{isMutating && <Spinner />}
+			</span>}
+		/>
 	)
 }
 PersistControl.displayName = 'PersistControl'

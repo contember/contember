c1c8482e8410220420b1e88e62515c1129477b96
David Matejka <matej21@matej21.cz>
2021-08-23 16:32:30 +0200

    switch project link: minor refactoring

--- packages/admin/src/components/SwitchProjectLink.tsx
+++ packages/admin/src/components/SwitchProjectLink.tsx
@@ -1,19 +1,19 @@
-import { ComponentType } from 'react'
+import { AnchorHTMLAttributes, ComponentType } from 'react'
 import { useSelector } from 'react-redux'
 import type State from '../state'
-import { InnerLinkProps } from './Link'
+
+export type PublicAnchorProps = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'>
 
 interface SwitchProjectLinkProps {
-	Component: ComponentType<InnerLinkProps>
+	Component: ComponentType<AnchorHTMLAttributes<HTMLAnchorElement>>
 }
 
-export const SwitchProjectLink = (props: SwitchProjectLinkProps) => {
+export const SwitchProjectLink = ({ Component, ...props }: SwitchProjectLinkProps & PublicAnchorProps) => {
 	const hasMoreProjects = useSelector<State, boolean>(state => !!(state.auth.identity && state.auth.identity.projects.length > 1))
 
 	if (!hasMoreProjects) {
 		return null
 	}
 
-	// TODO: better routing to loginPage?
-	return <props.Component href="/" isActive={false} />
+	return <Component href="/" {...props}/>
 }

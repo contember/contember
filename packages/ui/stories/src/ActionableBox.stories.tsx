f6679a5c8e22307a49390429fac18df25d7dd939
Martin Adamko <attitude@users.noreply.github.com>
2021-08-25 11:33:14 +0200

    Add `ActionableBox` to Storybook and fix focus within

---
+++ packages/ui/stories/src/ActionableBox.stories.tsx
@@ -0,0 +1,35 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react'
+import * as React from 'react'
+import { ActionableBox, Aether } from '../../src'
+
+export default {
+	title: 'ActionableBox',
+	component: ActionableBox,
+	decorators: [
+		Story => (
+			<Aether style={{ alignItems: 'flex-start', display: 'flex', flexDirection: 'column', gap: '1em', padding: '2em' }}>
+				<button>focusable before</button>
+				<Story />
+				<button>focusable after</button>
+			</Aether>
+		),
+	],
+} as ComponentMeta<typeof ActionableBox>
+
+const Template: ComponentStory<typeof ActionableBox> = args => <ActionableBox {...args} />
+
+const loremIpsum =
+	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur provident, quis? Ad adipisci dolore ipsam\n' +
+	'magnam modi nostrum optio quia velit! Ab aperiam consequatur consequuntur deleniti iusto libero possimus\n' +
+	'tpraesentium.'
+
+export const Simple = Template.bind({})
+
+Simple.args = {
+	editContents: <div style={{ display: 'flex', flexDirection: 'column' }}>
+		<input />
+		<input />
+		<button>Click</button>
+	</div>,
+	children: loremIpsum,
+}

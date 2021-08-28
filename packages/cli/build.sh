2a90b5acfa85e21291382319e285d6c80d84e1f2
David Matejka <matej21@matej21.cz>
2021-08-28 10:20:09 +0200

    CLI: fix bundled build

--- packages/cli/build.sh
+++ packages/cli/build.sh
@@ -5,8 +5,8 @@ mkdir dist
 yarn install
 yarn esbuild ./packages/cli/src/run.ts \
 	--bundle --platform=node --sourcemap=external \
-	--external:pg-native --external:electron \
+	--external:pg-native --external:electron --external:typescript --external:ts-node \
 	--outfile=./dist/run.js
 cd dist
 echo "{}" > package.json
-yarn add typescript
+yarn add typescript@^4.3.5 add ts-node@^10.2.0

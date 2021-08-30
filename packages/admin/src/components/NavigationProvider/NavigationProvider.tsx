484d32e8b4aed907fabb07f6baf24fdaa5809135
Jan Tvrdik <jan.tvrdik@gmail.com>
2021-08-30 15:05:25 +0200

    simplify NavigationProvider

--- packages/admin/src/components/NavigationProvider/NavigationProvider.tsx
+++ packages/admin/src/components/NavigationProvider/NavigationProvider.tsx
@@ -1,12 +1,12 @@
 import { NavigationContext } from '@contember/ui'
-import { memo, ReactNode } from 'react'
+import { ReactNode } from 'react'
 import { isRoutingLinkTarget, PageNotFound, useRoutingLinkFactory } from '../../routing'
 
 export interface NavigationProviderProps {
 	children?: ReactNode
 }
 
-export const NavigationProvider = memo(function NavigationProvider(props: NavigationProviderProps) {
+export const NavigationProvider = (props: NavigationProviderProps) => {
 	const linkFactory = useRoutingLinkFactory()
 	return (
 		<NavigationContext.Provider
@@ -14,11 +14,10 @@ export const NavigationProvider = memo(function NavigationProvider(props: Naviga
 				if (!isRoutingLinkTarget(val)) {
 					throw new PageNotFound('Invalid request')
 				}
-				return linkFactory(val as any)
+				return linkFactory(val)
 			}}
 		>
 			{props.children}
 		</NavigationContext.Provider>
 	)
-})
-NavigationProvider.displayName = 'NavigationProvider'
+}

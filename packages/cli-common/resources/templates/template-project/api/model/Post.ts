b464fa12ff44f448837f59d16dc134c572616b23
Honza Sládek <me@honzasladek.com>
2021-09-13 14:29:31 +0200

    Change def to d so it's consistent across template files

--- packages/cli-common/resources/templates/template-project/api/model/Post.ts
+++ packages/cli-common/resources/templates/template-project/api/model/Post.ts
@@ -1,8 +1,8 @@
-import { SchemaDefinition as def } from '@contember/schema-definition'
+import { SchemaDefinition as d } from '@contember/schema-definition'
 import { Image } from './Image'
 
 export class Post {
-	title = def.stringColumn().notNull()
-	content = def.stringColumn().notNull()
-	image = def.manyHasOne(Image)
+	title = d.stringColumn().notNull()
+	content = d.stringColumn().notNull()
+	image = d.manyHasOne(Image)
 }

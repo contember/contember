7d613bd7653a04f191ca9e392192e90769294daf
David Matejka <matej21@matej21.cz>
2021-09-15 14:18:18 +0200

    page link by id: add shortcut for the link target

--- packages/admin/src/components/bindingFacade/buttons/PageLinkById.tsx
+++ packages/admin/src/components/bindingFacade/buttons/PageLinkById.tsx
@@ -1,21 +1,50 @@
 import { EntityAccessor, useEntity } from '@contember/binding'
-import { ComponentType, memo, ReactNode } from 'react'
+import { ComponentType, memo, ReactNode, useMemo } from 'react'
 import { PageConfig, PageLink } from '../../pageRouting'
 import { InnerRoutingLinkProps } from '../../../routing'
 
-interface PageLinkByIdProps {
-	change: (id: string, entity: EntityAccessor) => PageConfig
+interface BasePageLinkByIdProps {
 	Component?: ComponentType<InnerRoutingLinkProps>
 	children?: ReactNode
 }
 
+interface SimplePageLinkByIdTarget {
+	to: string
+	idParameterName?: string
+}
+
+interface CustomPageLinkByIdTarget {
+	change: (id: string, entity: EntityAccessor) => PageConfig
+}
+
+type PageLinkByIdProps =
+	& BasePageLinkByIdProps
+	& (
+		| CustomPageLinkByIdTarget
+		| SimplePageLinkByIdTarget
+	)
+
 export const PageLinkById = memo(function (props: PageLinkByIdProps) {
 	const parentEntity = useEntity()
 	const id = parentEntity.id
+	const target = useMemo(() => {
+		if (typeof id !== 'string') {
+			return null
+		}
+		if ('to' in props) {
+			return {
+				pageName: props.to,
+				parameters: {
+					[props.idParameterName ?? 'id']: id,
+				},
+			}
+		}
+		return () => props.change(id, parentEntity)
+	}, [id, parentEntity, props])
 
-	if (typeof id === 'string') {
+	if (target !== null) {
 		return (
-			<PageLink to={() => props.change(id, parentEntity)} Component={props.Component}>
+			<PageLink to={target} Component={props.Component}>
 				{props.children}
 			</PageLink>
 		)

4b8cdb74c4481c876e104d9c20e4135383a70eb2
Martin Adamko <attitude@users.noreply.github.com>
2022-01-06 12:11:59 +0100

    Update `FieldContainer` stories

--- packages/ui/stories/src/FieldContainer.stories.tsx
+++ packages/ui/stories/src/FieldContainer.stories.tsx
@@ -1,7 +1,7 @@
 import type { ComponentMeta, ComponentStory } from '@storybook/react'
 import * as React from 'react'
 import { FieldContainer } from '../../src'
-import { disabledControlsForAttributes } from './Helpers'
+import { Block, disabledControlsForAttributes } from './Helpers'
 
 export default {
 	title: 'FieldContainer',
@@ -17,5 +17,10 @@ Defaut.args = {
 	label: 'Label',
 	labelDescription: 'Hint to fill valid value',
 	description: 'Description comes here',
-	children: '[input component]',
+	children: <Block />,
+	errors: [
+		{ message: 'Error message lorem ipsum' },
+		{ message: 'Dolor error message' },
+		{ message: 'Error message sit amet' },
+	],
 }

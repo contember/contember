8efff43f36c4b179e866f83d06837db1916c456f
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-01-17 21:01:32 +0100

    init template: mount minio as directory instead of as files

--- packages/cli-common/resources/templates/template-workspace/docker/minio/s3-entrypoint.sh
+++ packages/cli-common/resources/templates/template-workspace/docker/minio/s3-entrypoint.sh
@@ -3,6 +3,6 @@ set -ex
 
 mkdir -p /data/contember
 mkdir -p /data/.minio.sys/buckets/contember/
-cp /bucket-policy.json /data/.minio.sys/buckets/contember/policy.json
+cp /minio/s3-policy.json /data/.minio.sys/buckets/contember/policy.json
 
 minio "$@"

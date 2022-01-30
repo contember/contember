35a053eb497b79c8ec83c7e76c74c07a4d36954d
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-01-30 14:11:23 +0100

    NavigationProvider: support link DSL

--- packages/admin/src/components/NavigationProvider/NavigationProvider.tsx
+++ packages/admin/src/components/NavigationProvider/NavigationProvider.tsx
@@ -1,20 +1,23 @@
 import { NavigationContext } from '@contember/ui'
 import { ReactNode } from 'react'
-import { isRoutingLinkTarget, PageNotFound, useRoutingLinkFactory } from '../../routing'
+import { isRoutingLinkTarget, PageNotFound, parseLinkTarget, useRoutingLinkFactory } from '../../routing'
+import { useEnvironment } from '@contember/binding'
 
 export interface NavigationProviderProps {
 	children?: ReactNode
 }
 
 export const NavigationProvider = (props: NavigationProviderProps) => {
+	const env = useEnvironment()
 	const linkFactory = useRoutingLinkFactory()
+
 	return (
 		<NavigationContext.Provider
 			value={val => {
 				if (!isRoutingLinkTarget(val)) {
 					throw new PageNotFound('Invalid request')
 				}
-				return linkFactory(val)
+				return linkFactory(parseLinkTarget(val, env))
 			}}
 		>
 			{props.children}

13b2d8a908a65ee1f40a263e92a996a095bb1700
Filip Chalupa <chalupa.filip@gmail.com>
2022-02-21 17:19:10 +0100

    admin: make TimeField more similar to DateField by using DateTimeInput

--- packages/admin/src/components/bindingFacade/fields/TimeField.tsx
+++ packages/admin/src/components/bindingFacade/fields/TimeField.tsx
@@ -1,18 +1,50 @@
-import { SingleLineTextInputProps, TextInput } from '@contember/ui'
-import { SimpleRelativeSingleField, SimpleRelativeSingleFieldProps } from '../auxiliary'
-import { stringFieldParser, useTextInput } from './useTextInput'
+import type { FieldAccessor } from '@contember/binding'
+import { DateTimeInput, SingleLineTextInputProps } from '@contember/ui'
+import { forwardRef, memo, Ref } from 'react'
+import { dateToTimeValue } from '../../../../../ui/src/components/Forms/DateTimeInput/Serializer'
+import {
+	SimpleRelativeSingleField,
+	SimpleRelativeSingleFieldMetadata,
+	SimpleRelativeSingleFieldProps,
+} from '../auxiliary'
 
 export type TimeFieldProps = SimpleRelativeSingleFieldProps &
-	Omit<SingleLineTextInputProps, 'value' | 'onChange' | 'validationState' | 'allowNewlines'>
+	Omit<SingleLineTextInputProps, 'value' | 'onChange' | 'validationState' | 'max' | 'min'> & {
+		max?: string
+		min?: string
+	}
 
 export const TimeField = SimpleRelativeSingleField<TimeFieldProps, string>(
-	(fieldMetadata, { defaultValue, name, label, onBlur, ...props }) => {
-		const inputProps = useTextInput({
-			fieldMetadata,
-			onBlur,
-			parse: stringFieldParser,
-		})
-		return <TextInput type="time" {...inputProps} {...props} />
-	},
+	(fieldMetadata, props) => <TimeFieldInner fieldMetadata={fieldMetadata} {...props} />,
 	'TimeField',
+)
+
+export interface TimeFieldInnerProps
+	extends Omit<TimeFieldProps, 'field' | 'label' | 'isNonbearing' | 'defaultValue' | 'max' | 'min'> {
+	fieldMetadata: SimpleRelativeSingleFieldMetadata<string>
+	max?: string
+	min?: string
+}
+
+// Dummy date 1-1-1970 is used to benefit from Date class time validation
+const deserializeTime = (time: string | null): string => (time ? dateToTimeValue(new Date(`1-1-1970 ${time}`)) : '')
+
+const serialize = (time: string | null) => time
+
+const generateOnChange = (data: FieldAccessor<string>) => (value: string | null) => {
+	data.updateValue(serialize(value ? value : null))
+}
+
+export const TimeFieldInner = memo(
+	forwardRef(({ fieldMetadata, ...props }: TimeFieldInnerProps, suppliedRef: Ref<any>) => {
+		return (
+			<DateTimeInput
+				{...props}
+				onChange={generateOnChange(fieldMetadata.field)}
+				ref={suppliedRef}
+				value={deserializeTime(fieldMetadata.field.value)}
+				type="time"
+			/>
+		)
+	}),
 )

d9717018af1d82fced2ce74e6dfe2373212c7022
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-04-05 15:48:27 +0200

    eslint: enable check for react keys

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,8 +2,8 @@
 
 module.exports = {
 	parser: '@typescript-eslint/parser',
-	extends: ['plugin:@typescript-eslint/recommended'],
-	plugins: ['react-hooks', '@typescript-eslint'],
+	extends: ['plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime'],
+	plugins: ['react', 'react-hooks', '@typescript-eslint'],
 	parserOptions: {
 		ecmaVersion: 2018,
 		sourceType: 'module',
@@ -34,6 +34,10 @@ module.exports = {
 		'@typescript-eslint/prefer-interface': 'off',
 
 		'prefer-const': 'off',
+		'react/no-children-prop': 'off',
+		'react/no-render-return-value': 'off',
+		'react/display-name': 'off',
+		'react/prop-types': 'off',
 		'react-hooks/rules-of-hooks': 'error',
 		'react-hooks/exhaustive-deps': 'error',
 

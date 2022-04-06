9781b07a6df068d17345500187ffee96cfc46a06
Martin Adamko <attitude@users.noreply.github.com>
2022-04-06 09:01:28 +0200

    toThemeClass()`: Add suffix

--- packages/ui/src/utils/toThemeClass.ts
+++ packages/ui/src/utils/toThemeClass.ts
@@ -2,16 +2,16 @@ import { Intent } from '../types'
 
 const REQUIRED_THEME_CLASS = 'cui-theme'
 
-export const toThemeClass = <T extends string = Intent>(contentTheme: T | null | undefined, controlsTheme: T | null | undefined): string | undefined => {
+export const toThemeClass = <T extends string = Intent>(contentTheme: T | null | undefined, controlsTheme: T | null | undefined, suffix: string = ''): string | undefined => {
   if (contentTheme === controlsTheme) {
     const both = contentTheme
 
-    return both ? `${REQUIRED_THEME_CLASS} theme-${both}` : undefined
+    return both ? `${REQUIRED_THEME_CLASS} theme-${both}${suffix}` : undefined
   }
 
   return [
     REQUIRED_THEME_CLASS,
-    contentTheme ? `theme-${contentTheme}-content` : undefined,
-    controlsTheme ? `theme-${controlsTheme}-controls` : undefined,
+    contentTheme ? `theme-${contentTheme}-content${suffix}` : undefined,
+    controlsTheme ? `theme-${controlsTheme}-controls${suffix}` : undefined,
   ].filter(Boolean).join(' ')
 }

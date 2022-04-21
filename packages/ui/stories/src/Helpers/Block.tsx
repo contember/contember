71ff42be2fc122973b9d7c0b4a7312c56cae87e8
Martin Adamko <attitude@users.noreply.github.com>
2022-04-21 11:11:36 +0200

    Update Storybook `Block` helper component

--- packages/ui/stories/src/Helpers/Block.tsx
+++ packages/ui/stories/src/Helpers/Block.tsx
@@ -1,13 +1,18 @@
-export const Block = () => <div style={{
+import { DetailedHTMLProps, HTMLAttributes } from 'react'
+
+export const Block = ({ style }: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => <div style={{
 	background: 'var(--cui-toned-control-background-color)',
 	borderRadius: '0.25em',
 	display: 'flex',
 	color: 'var(--cui-toned-color)',
 	fontSize: '0.8125em',
+	flexBasis: 0,
+	flexGrow: 1,
+	flexShrink: 1,
 	lineHeight: '1em',
 	justifyContent: 'space-between',
 	padding: '0.5em',
-	width: '100%',
+	...style,
 }}>
 	<span>&lt;ReactNode</span>
 	<span>/&gt;</span>

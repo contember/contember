2c19eb8a3618c6c6ffa193de771df8f7e4a98fc4
David Matejka <matej21@matej21.cz>
2022-04-25 15:21:21 +0200

    sandbox: add select or create example

--- packages/admin-sandbox/admin/pages/tags.tsx
+++ packages/admin-sandbox/admin/pages/tags.tsx
@@ -4,8 +4,9 @@ export default () => (
 	<MultiEditPage entities="Tag" rendererProps={{ title: 'Abcd' }}>
 		<TextField field={'name'} label={'Name'} />
 		<Repeater field={'locales'} label={'Locales'} sortableBy={'order'}>
+			<SelectField label={'Locale'} options={'Locale.code'} field={'locale'}
+									 createNewForm={<TextField field={'code'} label={'Locale code'} />} />
 			<TextField field={'name'} label={'Name'} />
-			<SelectField label={'Locale'} options={'Locale.code'} field={'locale'} />
 		</Repeater>
 	</MultiEditPage>
 )

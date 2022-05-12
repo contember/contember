6ef7c6ed0835335c39a3438ce744127b6243a826
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-05-12 16:06:42 +0200

    admin-server: fix proxy request detection

--- ee/admin-server/src/utils/forwared.ts
+++ ee/admin-server/src/utils/forwared.ts
@@ -1,9 +1,19 @@
 import { IncomingMessage } from 'http'
-import ipaddr from 'ipaddr.js'
+import ipaddr, { IPv4 } from 'ipaddr.js'
 import { TLSSocket } from 'tls'
 
 export const isProxyRequest = (req: IncomingMessage): boolean => {
-	return ipaddr.parse(req.socket.remoteAddress ?? '0.0.0.0').range() === 'private'
+	const ip = ipaddr.parse(req.socket.remoteAddress ?? '0.0.0.0')
+
+	if (ip instanceof IPv4) {
+		return ip.range() === 'private'
+
+	} else if (ip.range() === 'ipv4Mapped') {
+		return ip.toIPv4Address().range() === 'private'
+
+	} else {
+		return false
+	}
 }
 
 export const isRequestSecure = (req: IncomingMessage): boolean => {

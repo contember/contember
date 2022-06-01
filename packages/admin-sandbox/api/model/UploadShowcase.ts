ac290e8fa339446e651113e908ff1a6380a0ea82
Martin Adamko <attitude@users.noreply.github.com>
2022-06-01 09:45:34 +0200

    Update model to test multiple images

--- packages/admin-sandbox/api/model/UploadShowcase.ts
+++ packages/admin-sandbox/api/model/UploadShowcase.ts
@@ -1,4 +1,4 @@
-import { SchemaDefinition as d, InputValidation as val } from '@contember/schema-definition'
+import { InputValidation as val, SchemaDefinition as d } from '@contember/schema-definition'
 import {
 	BasicImage,
 	ComplexFileList,
@@ -17,10 +17,17 @@ export class UploadShowcase {
 	@val.required('required UploadShowcase - singleBasicImage')
 	singleBasicImage = d.oneHasOne(BasicImage)
 	singleComplexImage = d.oneHasOne(ComplexImage)
+	multipleBasicImageList = d.oneHasMany(UploadShowcaseImage, 'page')
 
 	@val.required('required UploadShowcase - discriminatedAttachment')
 	discriminatedAttachment = d.oneHasOne(DiscriminatedAttachment)
 
 	imageList = d.oneHasOne(ComplexImageList)
 	fileList = d.oneHasOne(ComplexFileList)
+}
+
+export class UploadShowcaseImage {
+	page = d.manyHasOne(UploadShowcase, 'multipleBasicImageList')
+	image = d.manyHasOne(BasicImage)
+	order = d.intColumn()
 }

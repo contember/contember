2773291a948045cf6ca0dabca5dc1da0464070ae
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-04 21:20:07 +0200

    feat(transfer-api): initial support for streaming import / export

--- packages/engine-http/src/koa/types.ts
+++ packages/engine-http/src/koa/types.ts
@@ -1,5 +1,5 @@
 import * as Koa from 'koa'
 
-export type KoaContext<State> = Koa.ExtendableContext & { state: State }
+export type KoaContext<State> = Koa.ExtendableContext & { state: State, compress?: boolean }
 type KoaNext = () => Promise<any>
 export type KoaMiddleware<State> = (context: KoaContext<State>, next: KoaNext) => any

938c0594f69dea2b58a24c3df54abcb5fdd02cfc
David Matejka <matej21@matej21.cz>
2022-06-08 14:56:33 +0200

    useTopLevelOptionAccessor: use useEntityListSubTree hook

--- packages/admin/src/components/bindingFacade/fields/ChoiceField/hooks/useTopLevelOptionAccessor.ts
+++ packages/admin/src/components/bindingFacade/fields/ChoiceField/hooks/useTopLevelOptionAccessor.ts
@@ -1,19 +1,12 @@
-import { useCallback, useMemo } from 'react'
-import {
-	SugaredQualifiedEntityList,
-	TreeRootId,
-	useAccessorUpdateSubscription,
-	useGetEntityListSubTree,
-} from '@contember/binding'
+import { useMemo } from 'react'
+import { SugaredQualifiedEntityList, TreeRootId, useEntityListSubTree } from '@contember/binding'
 import { DesugaredOptionPath } from './useDesugaredOptionPath'
 
 export const useTopLevelOptionAccessors = (desugaredOptionPath: DesugaredOptionPath, treeRootId: TreeRootId | undefined) => {
-	const getSubTree = useGetEntityListSubTree()
 	const entityList = useMemo<SugaredQualifiedEntityList>(
 		() => ({ entities: desugaredOptionPath, ...desugaredOptionPath }),
 		[desugaredOptionPath],
 	)
-	const getSubTreeData = useCallback(() => getSubTree(entityList, treeRootId), [entityList, getSubTree, treeRootId])
-	const subTreeData = useAccessorUpdateSubscription(getSubTreeData)
+	const subTreeData = useEntityListSubTree(entityList, treeRootId)
 	return useMemo(() => Array.from(subTreeData), [subTreeData]) // Preserve ref equality if possible.
 }

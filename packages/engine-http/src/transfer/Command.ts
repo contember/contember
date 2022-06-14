19e74e3cd1cfdd9a5acaa27693e18d7ce9c7f98c
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-14 11:39:24 +0200

    perf(transfer-api): use discriminatedTupleUnion for commands

--- packages/engine-http/src/transfer/Command.ts
+++ packages/engine-http/src/transfer/Command.ts
@@ -1,58 +1,34 @@
 import * as Typesafe from '@contember/typesafe'
 
-export type ImportTenantSchemaBegin = ReturnType<typeof ImportTenantSchemaBegin>
-export const ImportTenantSchemaBegin = Typesafe.tuple(
-	Typesafe.literal('importTenantSchemaBegin'),
-)
+const tuple = <T extends any[]>(...values: T) => values
 
-export type ImportSystemSchemaBegin = ReturnType<typeof ImportSystemSchemaBegin>
-export const ImportSystemSchemaBegin = Typesafe.tuple(
-	Typesafe.literal('importSystemSchemaBegin'),
-	Typesafe.object({
-		project: Typesafe.string,
-		tables: Typesafe.array(Typesafe.string),
-	}),
-)
-
-export type ImportContentSchemaBegin = ReturnType<typeof ImportContentSchemaBegin>
-export const ImportContentSchemaBegin = Typesafe.tuple(
-	Typesafe.literal('importContentSchemaBegin'),
-	Typesafe.object({
-		project: Typesafe.string,
-		stage: Typesafe.string,
-		schemaVersion: Typesafe.string,
-		tables: Typesafe.array(Typesafe.string),
-	}),
-)
-
-export type InsertBeginCommand = ReturnType<typeof InsertBeginCommand>
-export const InsertBeginCommand = Typesafe.tuple(
-	Typesafe.literal('insertBegin'),
-	Typesafe.object({
-		table: Typesafe.string,
-		columns: Typesafe.array(Typesafe.string),
-	}),
-)
-
-export type InsertRowCommand = ReturnType<typeof InsertRowCommand>
-export const InsertRowCommand = Typesafe.tuple(
-	Typesafe.literal('insertRow'),
-	Typesafe.array(Typesafe.anyJson), // values
-)
-
-export type InsertEndCommand = ReturnType<typeof InsertEndCommand>
-export const InsertEndCommand = Typesafe.tuple(
-	Typesafe.literal('insertEnd'),
-)
-
-export const Command = Typesafe.union(
-	ImportTenantSchemaBegin,
-	ImportSystemSchemaBegin,
-	ImportContentSchemaBegin,
-	InsertBeginCommand,
-	InsertRowCommand,
-	InsertEndCommand,
-)
+export const Command = Typesafe.discriminatedTupleUnion({
+	importTenantSchemaBegin: tuple(),
+	importSystemSchemaBegin: tuple(
+		Typesafe.object({
+			project: Typesafe.string,
+			tables: Typesafe.array(Typesafe.string),
+		}),
+	),
+	importContentSchemaBegin: tuple(
+		Typesafe.object({
+			project: Typesafe.string,
+			stage: Typesafe.string,
+			schemaVersion: Typesafe.string,
+			tables: Typesafe.array(Typesafe.string),
+		}),
+	),
+	insertBegin: tuple(
+		Typesafe.object({
+			table: Typesafe.string,
+			columns: Typesafe.array(Typesafe.string),
+		}),
+	),
+	insertRow: tuple(
+		Typesafe.array(Typesafe.anyJson), // values
+	),
+	insertEnd: tuple(),
+})
 
 export type Command = ReturnType<typeof Command>
 export type CommandName = Command[0]

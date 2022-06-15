085ce5bcfee0d238bd34a3bbfaff0bec60845e97
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-06-15 16:57:33 +0200

    feat(engine-http): timer middleware supports stream response

--- packages/engine-http/src/common/TimerMiddleware.ts
+++ packages/engine-http/src/common/TimerMiddleware.ts
@@ -1,4 +1,5 @@
 import { KoaMiddleware } from '../koa'
+import { Readable } from 'stream'
 
 type EventTime = { label: string; start: number; duration?: number }
 
@@ -25,17 +26,37 @@ export const createTimerMiddleware = (): KoaMiddleware<TimerMiddlewareState> => 
 			time.duration = new Date().getTime() - start
 			return res as any
 		}
+
 		await next()
 
-		const eventsDescription = times
-			.map(it => `${it.label} T+${it.start}ms ${it.duration !== undefined ? it.duration : '[unknown]'}ms`)
-			.join('; ')
-		const total = new Date().getTime() - globalStart
-		const timeLabel = total > 500 ? 'TIME_SLOW' : 'TIME_OK'
-		// eslint-disable-next-line no-console
-		console.log(
-			`${ctx.request.method} ${ctx.request.url} [${ctx.response.status}] ${ctx.req.connection.remoteAddress} ${timeLabel} ${total}ms (${eventsDescription})`,
-		)
+		const emit = () => {
+			const eventsDescription = times
+				.map(it => `${it.label} T+${it.start}ms ${it.duration !== undefined ? it.duration : '[unknown]'}ms`)
+				.join('; ')
+
+			const total = new Date().getTime() - globalStart
+			const timeLabel = total > 500 ? 'TIME_SLOW' : 'TIME_OK'
+			// eslint-disable-next-line no-console
+			console.log(
+				`${ctx.request.method} ${ctx.request.url} [${ctx.response.status}] ${ctx.req.connection.remoteAddress} ${timeLabel} ${total}ms (${eventsDescription})`,
+			)
+		}
+
+		if (ctx.response.body instanceof Readable) {
+			const body = ctx.response.body
+			const streamTimer = ctx.state.timer('Stream', () => {
+				return new Promise(resolve => {
+					body.addListener('close', () => {
+						resolve(null)
+					})
+				})
+			})
+
+			streamTimer.then(emit).catch(emit)
+
+		} else {
+			emit()
+		}
 	}
 	return timer
 }

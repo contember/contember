contember/interface-archive

radio field: fix and drop select-related props

packages/admin/src/components/bindingFacade/fields/ChoiceField/BaseDynamicChoiceField.tsx
@@ -24,17 +24,18 @@ export type SearchByFields =
 	| SugaredRelativeSingleField['field']
 	| SugaredRelativeSingleField['field'][]
 
+export type BaseDynamicChoiceFieldOptions =
+	| {
+		options: OptionsAsEntityList
+		optionLabel: ReactElement
+	}
+	| {
+		options: OptionsAsFieldList
+	}
+	| LegacyChoiceFieldWithOptionRenderer
+
 export type BaseDynamicChoiceField =
-	& (
-		| {
-				options: OptionsAsEntityList
-				optionLabel: ReactElement
-			}
-		| {
-				options: OptionsAsFieldList
-			}
-		| LegacyChoiceFieldWithOptionRenderer
-	)
+	& BaseDynamicChoiceFieldOptions
 	& SelectFuseOptionsProps<EntityAccessor>
 	& {
 		searchByFields?: SearchByFields

contember/contember

packages/admin/src/components/bindingFacade/fields/ChoiceField/BaseDynamicChoiceField.tsx
@@ -24,17 +24,18 @@ export type SearchByFields =
 	| SugaredRelativeSingleField['field']
 	| SugaredRelativeSingleField['field'][]
 
+export type BaseDynamicChoiceFieldOptions =
+	| {
+		options: OptionsAsEntityList
+		optionLabel: ReactNode
+	}
+	| {
+		options: OptionsAsFieldList
+	}
+	| LegacyChoiceFieldWithOptionRenderer
+
 export type BaseDynamicChoiceField =
-	& (
-		| {
-				options: OptionsAsEntityList
-				optionLabel: ReactNode
-			}
-		| {
-				options: OptionsAsFieldList
-			}
-		| LegacyChoiceFieldWithOptionRenderer
-	)
+	& BaseDynamicChoiceFieldOptions
 	& SelectFuseOptionsProps<EntityAccessor>
 	& {
 		searchByFields?: SearchByFields
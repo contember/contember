aeb9c41b8c2016c9f820bea41fbb07cf80ee716e
David Matejka <matej21@matej21.cz>
2022-07-01 15:27:32 +0200

    feat(system-api): use advisory lock in migrate mutation

--- packages/engine-system-api/src/resolvers/mutation/MigrateMutationResolver.ts
+++ packages/engine-system-api/src/resolvers/mutation/MigrateMutationResolver.ts
@@ -4,6 +4,9 @@ import { MutationResolver } from '../Resolver'
 import { MigrateResponse, MutationMigrateArgs } from '../../schema'
 import { Migration } from '@contember/schema-migrations'
 import { AuthorizationActions, MigrationError, ProjectMigrator, StagesQuery } from '../../model'
+import { withDatabaseAdvisoryLock } from '@contember/database'
+
+const pg_lock_id = 1597474138739147
 
 export class MigrateMutationResolver implements MutationResolver<'migrate'> {
 	constructor(private readonly projectMigrator: ProjectMigrator) {}
@@ -25,39 +28,40 @@ export class MigrateMutationResolver implements MutationResolver<'migrate'> {
 		force = false,
 	): Promise<MigrateResponse> {
 		const migrations = args.migrations as readonly Migration[]
-
 		return context.db.transaction(async trx => {
-			const stages = await trx.queryHandler.fetch(new StagesQuery())
-			for (const stage of stages) {
-				await context.requireAccess(AuthorizationActions.PROJECT_MIGRATE, stage.slug)
-			}
-			try {
-				await this.projectMigrator.migrate(trx, stages, migrations, {
-					logger: () => null,
-					ignoreOrder: force,
-					skipExecuted: true,
-				})
-			} catch (e) {
-				if (e instanceof MigrationError) {
-					await trx.client.connection.rollback()
-					const error = {
-						code: e.code,
-						migration: e.version,
-						developerMessage: e.message,
-					}
-					return {
-						ok: false,
-						errors: [error],
-						error,
-					}
-				} else {
-					throw e
+			return await withDatabaseAdvisoryLock(trx.client, pg_lock_id, async () => {
+				const stages = await trx.queryHandler.fetch(new StagesQuery())
+				for (const stage of stages) {
+					await context.requireAccess(AuthorizationActions.PROJECT_MIGRATE, stage.slug)
 				}
-			}
-			return {
-				ok: true,
-				errors: [],
-			}
+				try {
+					await this.projectMigrator.migrate(trx, stages, migrations, {
+						logger: () => null,
+						ignoreOrder: force,
+						skipExecuted: true,
+					})
+				} catch (e) {
+					if (e instanceof MigrationError) {
+						await trx.client.connection.rollback()
+						const error = {
+							code: e.code,
+							migration: e.version,
+							developerMessage: e.message,
+						}
+						return {
+							ok: false,
+							errors: [error],
+							error,
+						}
+					} else {
+						throw e
+					}
+				}
+				return {
+					ok: true,
+					errors: [],
+				}
+			})
 		})
 	}
 }

d8091bcb30b8353d8c279cf8e6e4623547deca85
David Matejka <matej21@matej21.cz>
2022-07-13 13:21:34 +0200

    tests(database): improvements

--- packages/database/tests/cases/unit/connection.test.ts
+++ packages/database/tests/cases/unit/connection.test.ts
@@ -1,11 +1,9 @@
 import { expect, it } from 'vitest'
-import { Connection, Pool } from '../../../src'
-import { PgClient } from '../../../src/client/PgClient'
-import EventEmitter from 'events'
 import { MutexDeadlockError } from '../../../src/utils'
+import { createConnectionMockAlt } from './createConnectionMockAlt'
 
 it('support nested scope', async () => {
-	const [connection, end] = createConnectionMock(
+	const [connection, end] = createConnectionMockAlt(
 		[{ sql: 'SELECT 1' }],
 	)
 
@@ -17,7 +15,7 @@ it('support nested scope', async () => {
 })
 
 it('another scope acquires new connection', async () => {
-	const [connection, end] = createConnectionMock(
+	const [connection, end] = createConnectionMockAlt(
 		[{ sql: 'SELECT 1', timeout: 5 }, { sql: 'SELECT 2', timeout: 5 }],
 		[{ sql: 'SELECT 3', timeout: 5 }],
 	)
@@ -34,7 +32,7 @@ it('another scope acquires new connection', async () => {
 
 
 it('another nested scope waits for connection', async () => {
-	const [connection, end] = createConnectionMock(
+	const [connection, end] = createConnectionMockAlt(
 		[{ sql: 'SELECT 1', timeout: 5 }, { sql: 'SELECT 2', timeout: 5 }, { sql: 'SELECT 3', timeout: 5 }],
 	)
 	await connection.scope(async c1 => {
@@ -51,7 +49,7 @@ it('another nested scope waits for connection', async () => {
 
 
 it('another nested scope waits for connection in a transaction', async () => {
-	const [connection, end] = createConnectionMock(
+	const [connection, end] = createConnectionMockAlt(
 		[
 			{ sql: 'BEGIN', timeout: 5 },
 			{ sql: 'SELECT 1', timeout: 5 },
@@ -74,7 +72,7 @@ it('another nested scope waits for connection in a transaction', async () => {
 
 
 it('unscoped queries can pe out of order', async () => {
-	const [connection, end] = createConnectionMock(
+	const [connection, end] = createConnectionMockAlt(
 		[
 			{ sql: 'SELECT 1', timeout: 5 },
 			{ sql: 'SELECT 3' },
@@ -96,7 +94,7 @@ it('unscoped queries can pe out of order', async () => {
 
 
 it('support nested transaction / savepoints', async () => {
-	const [connection, end] = createConnectionMock([
+	const [connection, end] = createConnectionMockAlt([
 		{ sql: 'BEGIN' },
 		{ sql: 'SELECT 1' },
 		{ sql: 'SAVEPOINT "savepoint_1"' },
@@ -148,49 +146,9 @@ it('support nested transaction / savepoints', async () => {
 })
 
 it('detects deadlock', async () => {
-	const [connection, end] = createConnectionMock([])
+	const [connection, end] = createConnectionMockAlt([])
 
 	await expect(async () => await connection.scope(async c1 => c1.scope(async () => await c1.query('SELECT 1')))).rejects.toThrow(MutexDeadlockError)
 	end()
 })
 
-
-const createConnectionMock = (...queries: { sql: string; timeout?: number }[][]): [Connection, () => void] => {
-	const connectionMocks: (PgClient & { assertEmpty: () => void })[] = []
-	for (const queriesSet of queries) {
-		connectionMocks.push(new class extends EventEmitter {
-			connect() {
-				return Promise.resolve()
-			}
-
-			end() {
-				return Promise.resolve()
-			}
-
-			async query(sql: string) {
-				const query = queriesSet.shift()
-				expect(query).toBeDefined()
-				expect(sql).toEqual(query?.sql)
-				await new Promise<void>(resolve => setTimeout(resolve, query?.timeout ?? 1))
-
-				return sql as any
-			}
-
-			assertEmpty() {
-				expect(queriesSet).deep.eq([])
-			}
-		})
-	}
-	const allMocks = [...connectionMocks]
-	const pool = new Pool(() => {
-		return connectionMocks.shift() ?? (() => {
-			throw new Error('No connection')
-		})()
-	}, {})
-	return [
-		new Connection(pool, {}),
-		() => {
-			allMocks.forEach(it => it.assertEmpty())
-		},
-	]
-}
--- packages/database/tests/cases/unit/eventManager.test.ts
+++ packages/database/tests/cases/unit/eventManager.test.ts
@@ -1,17 +1,11 @@
 import { EventManager } from '../../../src'
-import { createConnectionMock } from '@contember/database-tester'
-import { test, assert } from 'vitest'
-
-const simpleQuery = (sql: string) => ({
-	sql,
-	parameters: [],
-	response: { rows: [] },
-})
+import { assert, test } from 'vitest'
+import { createConnectionMockAlt } from './createConnectionMockAlt'
 
 test('event manager: connection and client', async () => {
-	const connection = createConnectionMock([
-		simpleQuery('SELECT 1'),
-		simpleQuery('SELECT 2'),
+	const [connection, end] = createConnectionMockAlt([
+		{ sql: 'SELECT 1' },
+		{ sql: 'SELECT 2' },
 	])
 	const events: { source: string; sql: string }[] = []
 	const client = connection.createClient('public', {})
@@ -25,16 +19,17 @@ test('event manager: connection and client', async () => {
 		{ sql: 'SELECT 1', source: 'connection' },
 		{ sql: 'SELECT 2', source: 'connection' },
 	])
+	end()
 })
 
 
 test('event manager: connection and client with transaction', async () => {
-	const connection = createConnectionMock([
-		simpleQuery('BEGIN;'),
-		simpleQuery('SELECT 1'),
-		simpleQuery('COMMIT;'),
-		simpleQuery('SELECT 2'),
-		simpleQuery('SELECT 3'),
+	const [connection, end] = createConnectionMockAlt([
+		{ sql: 'BEGIN' },
+		{ sql: 'SELECT 1' },
+		{ sql: 'COMMIT' },
+		{ sql: 'SELECT 2' },
+		{ sql: 'SELECT 3' },
 	])
 	const events: { source: string; sql: string }[] = []
 	const client = connection.createClient('public', {})
@@ -49,13 +44,100 @@ test('event manager: connection and client with transaction', async () => {
 	await client.query('SELECT 2')
 	await connection.query('SELECT 3')
 	assert.deepStrictEqual(events, [
-		{ sql: 'BEGIN;', source: 'connection' },
+		{ sql: 'BEGIN', source: 'client' },
+		{ sql: 'BEGIN', source: 'connection' },
 		{ sql: 'SELECT 1', source: 'transaction' },
 		{ sql: 'SELECT 1', source: 'client' },
 		{ sql: 'SELECT 1', source: 'connection' },
-		{ sql: 'COMMIT;', source: 'connection' },
+		{ sql: 'COMMIT', source: 'client' },
+		{ sql: 'COMMIT', source: 'connection' },
 		{ sql: 'SELECT 2', source: 'client' },
 		{ sql: 'SELECT 2', source: 'connection' },
 		{ sql: 'SELECT 3', source: 'connection' },
 	])
+	end()
+})
+
+
+test('event manager: connection and client with scopes', async () => {
+	const [connection, end] = createConnectionMockAlt([
+		{ sql: 'SELECT 1' },
+		{ sql: 'SELECT 2' },
+		{ sql: 'SELECT 3' },
+	])
+	const events: { source: string; sql: string }[] = []
+	const client = connection.createClient('public', {})
+
+	connection.eventManager.on(EventManager.Event.queryStart, ({ sql }) => events.push({ sql, source: 'connection' }))
+	client.eventManager.on(EventManager.Event.queryStart, ({ sql }) => events.push({ sql, source: 'client' }))
+	await client.scope(async conn => {
+		conn.eventManager.on(EventManager.Event.queryStart, ({ sql }) => events.push({ sql, source: 'scoped' }))
+		await conn.query('SELECT 1')
+
+	})
+	await client.query('SELECT 2')
+	await connection.query('SELECT 3')
+	assert.deepStrictEqual(events, [
+		{ sql: 'SELECT 1', source: 'scoped' },
+		{ sql: 'SELECT 1', source: 'client' },
+		{ sql: 'SELECT 1', source: 'connection' },
+		{ sql: 'SELECT 2', source: 'client' },
+		{ sql: 'SELECT 2', source: 'connection' },
+		{ sql: 'SELECT 3', source: 'connection' },
+	])
+	end()
+})
+
+
+test('event manager: connection and client with transaction and savepoint', async () => {
+	const [connection, end] = createConnectionMockAlt([
+		{ sql: 'BEGIN' },
+		{ sql: 'SELECT 1' },
+		{ sql: 'SAVEPOINT "savepoint_1"' },
+		{ sql: 'SELECT 2' },
+		{ sql: 'RELEASE SAVEPOINT "savepoint_1"' },
+		{ sql: 'COMMIT' },
+		{ sql: 'SELECT 3' },
+		{ sql: 'SELECT 4' },
+	])
+	const events: { source: string; sql: string }[] = []
+	const client = connection.createClient('public', {})
+
+	connection.eventManager.on(EventManager.Event.queryStart, ({ sql }) => events.push({ sql, source: 'connection' }))
+	client.eventManager.on(EventManager.Event.queryStart, ({ sql }) => events.push({ sql, source: 'client' }))
+	await client.transaction(async trx => {
+		trx.eventManager.on(EventManager.Event.queryStart, ({ sql }) => events.push({ sql, source: 'transaction' }))
+		await trx.query('SELECT 1')
+		await trx.transaction(async savepoint => {
+			savepoint.eventManager.on(EventManager.Event.queryStart, ({ sql }) => events.push({ sql, source: 'savepoint' }))
+			await savepoint.query('SELECT 2')
+
+		})
+
+	})
+	await client.query('SELECT 3')
+	await connection.query('SELECT 4')
+	assert.deepStrictEqual(events, [
+		{ sql: 'BEGIN', source: 'client' },
+		{ sql: 'BEGIN', source: 'connection' },
+		{ sql: 'SELECT 1', source: 'transaction' },
+		{ sql: 'SELECT 1', source: 'client' },
+		{ sql: 'SELECT 1', source: 'connection' },
+		{ sql: 'SAVEPOINT "savepoint_1"', source: 'transaction' },
+		{ sql: 'SAVEPOINT "savepoint_1"', source: 'client' },
+		{ sql: 'SAVEPOINT "savepoint_1"', source: 'connection' },
+		{ sql: 'SELECT 2', source: 'savepoint' },
+		{ sql: 'SELECT 2', source: 'transaction' },
+		{ sql: 'SELECT 2', source: 'client' },
+		{ sql: 'SELECT 2', source: 'connection' },
+		{ sql: 'RELEASE SAVEPOINT "savepoint_1"', source: 'transaction' },
+		{ sql: 'RELEASE SAVEPOINT "savepoint_1"', source: 'client' },
+		{ sql: 'RELEASE SAVEPOINT "savepoint_1"', source: 'connection' },
+		{ sql: 'COMMIT', source: 'client' },
+		{ sql: 'COMMIT', source: 'connection' },
+		{ sql: 'SELECT 3', source: 'client' },
+		{ sql: 'SELECT 3', source: 'connection' },
+		{ sql: 'SELECT 4', source: 'connection' },
+	])
+	end()
 })

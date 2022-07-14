d05145db7479ba148ca5249658ec60f1f557a54a
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-07-14 15:50:09 +0200

    fix(database): always close transaction in asyncIterableTransaction

--- packages/database/src/utils/asyncIterableTransaction.ts
+++ packages/database/src/utils/asyncIterableTransaction.ts
@@ -7,15 +7,27 @@ interface AsyncIterableX<T, TReturn = any, TNext = undefined> {
 export async function* asyncIterableTransaction<T, TReturn = any, TNext = undefined>(db: Client, cb: (db2: Client<Connection.TransactionLike>) => AsyncIterableX<T, TReturn, TNext>): AsyncIterableX<T, TReturn, TNext> {
 	// eslint-disable-next-line promise/param-names
 	return yield* await new Promise(async resolveOuter => {
-		await db.transaction((db2: Client<Connection.TransactionLike>) => {
-			// eslint-disable-next-line promise/param-names
-			return new Promise<void>(resolveInner => {
-				resolveOuter((async function* () {
-					const result = yield* cb(db2)
-					resolveInner()
-					return result
-				})())
+		try {
+			await db.transaction((db2: Client<Connection.TransactionLike>) => {
+				// eslint-disable-next-line promise/param-names
+				return new Promise<void>((resolveInner, rejectInner) => {
+					resolveOuter((async function* () {
+						try {
+							const result = yield* cb(db2)
+							resolveInner()
+							return result
+
+						} finally { // required to close the transaction when the generator is externally closed, or exceptions are thrown
+							if (!db2.connection.isClosed) {
+								rejectInner()
+							}
+						}
+					})())
+				})
 			})
-		})
+
+		} catch (e) {
+			// suppress the error caused by rejectInner(), it was already propagated out of asyncIterableTransaction()
+		}
 	})
 }

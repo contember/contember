646457f5f6895b6a3d1f22bb9049c46511207f29
David Matejka <matej21@matej21.cz>
2022-07-20 13:05:47 +0200

    feat(template): add import to blank schema definition

--- packages/cli-common/resources/templates/template-project/api/model/index.ts
+++ packages/cli-common/resources/templates/template-project/api/model/index.ts
@@ -1,3 +1,5 @@
+import { SchemaDefinition as def } from '@contember/schema-definition'
+
 // export your model definition here
 
 export { } // you can delete this line once you export your first entity

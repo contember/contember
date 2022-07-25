96801631dca95a7a33844f520b16d4e06413bdaf
David Matejka <matej21@matej21.cz>
2022-07-25 16:43:20 +0200

    feat(authorization): custom verifier

--- packages/authorization/src/Permissions.ts
+++ packages/authorization/src/Permissions.ts
@@ -3,24 +3,24 @@ import Authorizator from './Authorizator'
 class Permissions {
 	private permissions: Permissions.PermissionsMap = {}
 
-	public allow(role: string, { resource, privilege }: Authorizator.Action) {
-		if (!this.permissions[role]) {
-			this.permissions[role] = {}
-		}
-		if (!this.permissions[role][resource]) {
-			this.permissions[role][resource] = {}
-		}
-		this.permissions[role][resource][privilege] = true
+	public allow<Meta = undefined>(
+		role: string,
+		{ resource, privilege }: Authorizator.Action<Meta>,
+		verifier: (meta: Meta) => boolean = () => true,
+	) {
+		this.permissions[role] ??= {}
+		this.permissions[role][resource] ??= {}
+		this.permissions[role][resource][privilege] = verifier
 	}
 
-	public isAllowed(role: string, resource: string, action: string): boolean {
+	public isAllowed(role: string, resource: string, action: string, meta: any): boolean {
 		const rolePermissions = this.permissions[role]
 		if (!rolePermissions) {
 			return false
 		}
 		for (let tmpResource of [resource, Permissions.ALL]) {
 			for (let tmpAction of [action, Permissions.ALL]) {
-				if (rolePermissions[tmpResource] && rolePermissions[tmpResource][tmpAction] === true) {
+				if (rolePermissions[tmpResource]?.[tmpAction]?.(meta) === true) {
 					return true
 				}
 			}
@@ -31,7 +31,13 @@ class Permissions {
 
 namespace Permissions {
 	export const ALL = '*'
-	export type PermissionsMap = { [role: string]: { [resource: string]: { [privilege: string]: true } } }
+	export type PermissionsMap = {
+		[role: string]: {
+			[resource: string]: {
+				[privilege: string]: (meta: any) => boolean
+			}
+		}
+	}
 }
 
 export default Permissions

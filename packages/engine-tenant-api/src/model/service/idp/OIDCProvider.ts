94e0bb468a3b5a8d38b9fbed1e946ed62e24acc4
David Matejka <matej21@matej21.cz>
2022-07-25 11:02:07 +0200

    feat(tenant-api): support scope configuration in oidc provider

--- packages/engine-tenant-api/src/model/service/idp/OIDCProvider.ts
+++ packages/engine-tenant-api/src/model/service/idp/OIDCProvider.ts
@@ -24,6 +24,7 @@ const OIDCConfigurationSchema = Typesafe.intersection(
 	}),
 	Typesafe.partial({
 		responseType: Typesafe.enumeration<ResponseType>('code', 'code id_token', 'code id_token token', 'code token', 'id_token', 'id_token token', 'none'),
+		claims: Typesafe.string,
 	}),
 )
 
@@ -39,7 +40,7 @@ export class OIDCProvider implements IdentityProviderHandler<SessionData, OIDCCo
 		const state = generators.state()
 		const url = client.authorizationUrl({
 			redirect_uri: redirectUrl,
-			scope: 'openid email',
+			scope: configuration.claims ?? 'openid email',
 			nonce,
 			state,
 		})

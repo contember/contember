8ebf7c3cd99d0fc419ec836bba0daf6d2593254f
David Matejka <matej21@matej21.cz>
2022-07-29 10:22:32 +0200

    tests(content-api): add test for filtering a has-many relation

--- packages/engine-content-api/tests/cases/integration/graphQlRequests/query/relations/manyHasManyOwning.test.ts
+++ packages/engine-content-api/tests/cases/integration/graphQlRequests/query/relations/manyHasManyOwning.test.ts
@@ -4,6 +4,83 @@ import { SchemaBuilder } from '@contember/schema-definition'
 import { GQL, SQL } from '../../../../../src/tags'
 import { testUuid } from '../../../../../src/testUuid'
 
+
+test('Posts with filtered tags', async () => {
+	await execute({
+		schema: new SchemaBuilder()
+			.entity('Post', entity => entity.manyHasMany('tags', relation => relation.target('Tag')))
+			.entity('Tag', entity => entity.column('name'))
+			.buildSchema(),
+		query: GQL`
+        query {
+          listPost {
+            id
+            tags(filter: {name: {eq: "Foo"}}) {
+				name
+            }
+          }
+        }
+			`,
+		executes: [
+			{
+				sql: SQL`select "root_"."id" as "root_id",
+					         "root_"."id" as "root_id"
+				         from "public"."post" as "root_"`,
+				response: {
+					rows: [{ root_id: testUuid(1) }, { root_id: testUuid(2) }],
+				},
+			},
+			{
+				sql: SQL`select "junction_"."tag_id", "junction_"."post_id"  from "public"."post_tags" as "junction_" inner join  "public"."tag" as "root_" on  "junction_"."tag_id" = "root_"."id"  where "junction_"."post_id" in (?, ?) and "root_"."name" = ?`,
+				parameters: [testUuid(1), testUuid(2), 'Foo'],
+				response: {
+					rows: [
+						{ post_id: testUuid(1), tag_id: testUuid(3) },
+						{ post_id: testUuid(2), tag_id: testUuid(3) },
+					],
+				},
+			},
+			{
+				sql: SQL`
+					select "root_"."name" as "root_name",
+						"root_"."id" as "root_id"
+					from "public"."tag" as "root_"
+					where "root_"."id" in (?)`,
+				parameters: [testUuid(3)],
+				response: {
+					rows: [
+						{ root_id: testUuid(3), root_name: 'foo' },
+					],
+				},
+			},
+		],
+		return: {
+			data: {
+				listPost: [
+					{
+						id: testUuid(1),
+						tags: [
+							{
+								name: 'foo',
+							},
+						],
+					},
+					{
+						id: testUuid(2),
+						tags: [
+							{
+								name: 'foo',
+							},
+						],
+					},
+				],
+			},
+		},
+	})
+})
+
+
+
 test('Posts with paginated tags (many has many owning)', async () => {
 	await execute({
 		schema: new SchemaBuilder()

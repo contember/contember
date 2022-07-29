aff5c31e569728a2571bd799abf9ecfd7bac8338
David Matejka <matej21@matej21.cz>
2022-07-29 10:22:01 +0200

    feat(content-api): add where optimizer to eliminate "always" and "never" branches

---
+++ packages/engine-content-api/src/mapper/select/optimizer/ConditionOptimizer.ts
@@ -0,0 +1,45 @@
+import { Input } from '@contember/schema'
+import { optimizeJunction, ResultCollector } from './helpers'
+
+
+export class ConditionOptimizer {
+	public optimize(condition: Input.Condition): Input.Condition | boolean {
+		const result = this.optimizeCondition(condition)
+
+		if (typeof result === 'boolean') {
+			return result
+		}
+
+		return result.length > 1 ? { and: result } : (result?.[0] ?? {})
+	}
+
+	private optimizeCondition(condition: Input.Condition): Input.Condition[] | boolean {
+		if (condition.never) {
+			return false
+		}
+
+		const resultCollector = new ResultCollector<Input.Condition>(true)
+
+		for (const [key, value] of Object.entries(condition)) {
+			if (value === undefined || value === null) {
+				// do nothing
+			} else if (key === 'always') {
+				resultCollector.add(true, 'and')
+
+			} else if (key === 'or' || key === 'and') {
+				const parts = (value as readonly Input.Condition[]).map(it => this.optimizeCondition(it))
+				resultCollector.add(optimizeJunction(key, parts), key)
+
+			} else if (key === 'not') {
+				const resolved = this.optimizeCondition(value as Input.Condition)
+				resultCollector.add(resolved, 'not')
+
+			} else {
+				resultCollector.add([{ [key]: value } as Input.Condition], 'and')
+
+			}
+		}
+
+		return resultCollector.getResult()
+	}
+}
---
+++ packages/engine-content-api/tests/cases/unit/whereOptimizer.test.ts
@@ -0,0 +1,77 @@
+import { describe, it, assert } from 'vitest'
+import { ConditionOptimizer } from '../../../src/mapper/select/optimizer/ConditionOptimizer'
+import { SchemaDefinition as def } from '@contember/schema-definition'
+import { WhereOptimizer } from '../../../src/mapper/select/optimizer/WhereOptimizer'
+
+namespace TestModel {
+	export class Author {
+		name = def.stringColumn()
+		isPublic = def.boolColumn().notNull()
+		image = def.manyHasOne(Image, 'authors')
+	}
+
+	export class Article {
+		title = def.stringColumn()
+		isPublic = def.boolColumn().notNull()
+		image = def.manyHasOne(Image, 'articles')
+	}
+
+	export class Image {
+		url = def.stringColumn().notNull()
+
+		articles = def.oneHasMany(Article, 'image')
+		authors = def.oneHasMany(Author, 'image')
+	}
+}
+
+describe('where optimized', () => {
+	const conditionOptimizer = new ConditionOptimizer()
+	const model = def.createModel(TestModel)
+	const whereOptimizer = new WhereOptimizer(model, conditionOptimizer)
+
+
+	it('removes unnecessary condition', () => {
+		assert.deepStrictEqual(whereOptimizer.optimize({
+			or: [
+				{
+					authors: { isPublic: { eq: true } },
+				},
+				{
+					articles: { isPublic: { always: true } },
+				},
+			],
+		}, model.entities.Image), {
+			id: { always: true },
+		})
+	})
+
+	it('keep condition', () => {
+		assert.deepStrictEqual(whereOptimizer.optimize({
+			or: [
+				{
+					authors: { isPublic: { eq: true } },
+				},
+			],
+		}, model.entities.Image), {
+			authors: { isPublic: { eq: true } },
+		})
+	})
+
+	it('do not swap AND and a field', () => {
+		assert.deepStrictEqual(whereOptimizer.optimize({
+			authors: {
+				and: [
+					{ isPublic: { eq: true } },
+					{ name: { eq: 'bar' } },
+				],
+			},
+		}, model.entities.Image), {
+			authors: {
+				and: [
+					{ isPublic: { eq: true } },
+					{ name: { eq: 'bar' } },
+				],
+			},
+		})
+	})
+})

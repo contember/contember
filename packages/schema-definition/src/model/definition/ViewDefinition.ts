6a9eb17e4dec0f4594c640d3513d0b7d3c849ea8
David Matejka <matej21@matej21.cz>
2022-08-10 14:34:06 +0200

    feat(schema-definition): support function returning dependencies array to avoid circular import issues

--- packages/schema-definition/src/model/definition/ViewDefinition.ts
+++ packages/schema-definition/src/model/definition/ViewDefinition.ts
@@ -1,11 +1,24 @@
 import { extendEntity } from './extensions'
 import { EntityConstructor } from './types'
 
-export const View = (sql: string, { dependencies }: { dependencies?: EntityConstructor[] } = {}) =>
-	extendEntity(({ entity, registry }) => ({
-		...entity,
-		view: {
-			sql,
-			...(dependencies ? { dependencies: dependencies?.map(it => registry.getName(it)) } : {}),
-		},
-	}))
+export const View = (sql: string, { dependencies }: { dependencies?: (() => EntityConstructor[]) | EntityConstructor[] } = {}) =>
+	extendEntity(({ entity, registry }) => {
+		const dependenciesResolved = typeof dependencies === 'function' ? dependencies() : dependencies
+		if (dependenciesResolved?.some(it => it === undefined)) {
+			throw `"undefined" value detected in dependencies of view entity ${entity.name}. This is possibly caused by circular imports.
+Please wrap the array of dependencies into a function returning this array.
+
+Instead of:
+dependencies: [MyEntity]
+Use:
+dependencies: () => [MyEntity]
+`
+		}
+		return ({
+			...entity,
+			view: {
+				sql,
+				...(dependenciesResolved ? { dependencies: dependenciesResolved.map(it => registry.getName(it)) } : {}),
+			},
+		})
+	})

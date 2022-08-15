fe5b9828d175ae12fc3c879d7bc560c955319a44
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-08-15 11:05:05 +0200

    SchemaColumn: fix defaultValue type

--- packages/binding/src/core/schema/SchemaColumn.ts
+++ packages/binding/src/core/schema/SchemaColumn.ts
@@ -4,7 +4,7 @@ import type { SchemaEnumName } from './SchemaEnums'
 
 export interface SchemaColumn {
 	__typename: '_Column'
-	defaultValue: string | null // JSON
+	defaultValue: string | number | boolean | null // JSON
 	name: FieldName
 	nullable: boolean
 	type: SchemaColumnType

08584113b471fac90d4bfb6bdce04618f79bfe78
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-08-31 10:38:09 +0200

    server: add socket timeout to inner proxy request

--- ee/admin-server/src/controllers/ApiController.ts
+++ ee/admin-server/src/controllers/ApiController.ts
@@ -82,6 +82,11 @@ export class ApiController extends BaseController<ApiParams> {
 			res.end(JSON.stringify(jsonBody))
 		})
 
+		innerReq.setTimeout(15 * 60 * 1000, () => {
+			console.log('ApiController: request has timed out')
+			innerReq.destroy(new Error('ApiController: request has timed out'))
+		})
+
 		req.pipe(innerReq)
 
 		return new Promise((resolve, reject) => {

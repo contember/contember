2164300b71c84d7053c2f0e83bddaaf285c85665
David Matejka <matej21@matej21.cz>
2022-10-11 17:06:21 +0200

    refactor(engine-http): resolve operation type in graphql executor

--- packages/engine-http/src/graphql/state.ts
+++ packages/engine-http/src/graphql/state.ts
@@ -1,22 +1,12 @@
-import { DocumentNode, OperationTypeNode } from 'graphql'
 import { GraphQLListener } from './execution'
 import { KoaContext } from '../koa'
-
-const getOperationFromDocument = (document: DocumentNode): OperationTypeNode | undefined => {
-	for (const node of document.definitions) {
-		if (node.kind === 'OperationDefinition') {
-			return node.operation
-		}
-	}
-	return undefined
-}
 
 export const createGraphqlRequestInfoProviderListener = (): GraphQLListener<{
 	koaContext: KoaContext<GraphQLKoaState>
 }> => ({
-	onExecute: ({ context, document }) => {
+	onExecute: ({ context, operation }) => {
 		context.koaContext.state.graphql = {
-			operationName: getOperationFromDocument(document),
+			operationName: operation,
 		}
 	},
 })

917c63465a2d4c362d48ee58b04c4c08f8de3fea
David Matejka <matej21@matej21.cz>
2022-10-17 13:37:29 +0200

    feat(database): support lock modifiers

--- packages/database/src/builders/Compiler.ts
+++ packages/database/src/builders/Compiler.ts
@@ -139,18 +139,8 @@ class Compiler {
 		if (!lock) {
 			return Literal.empty
 		}
-		switch (lock) {
-			case LockType.forUpdate:
-				return new Literal(' for update')
-			case LockType.forNoKeyUpdate:
-				return new Literal(' for no key update')
-			case LockType.forShare:
-				return new Literal(' for share')
-			case LockType.forKeyShare:
-				return new Literal(' for key share')
-			default:
-				return assertNever(lock)
-		}
+		const modifier = lock.modifier ? ` ${lock.modifier}` : ''
+		return new Literal(` ${lock.type}` + modifier)
 	}
 
 	private compileJoin(join: SelectBuilder.Options['join'], namespaceContext: Compiler.Context): Literal {

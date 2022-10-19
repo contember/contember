96116e4669742d34257cd1ad11d3192c318b42a0
David Matejka <matej21@matej21.cz>
2022-10-19 16:22:17 +0200

    refactor(graphql-utils): extract UuidType from content-api

--- packages/engine-content-api/src/schema/CustomTypesProvider.ts
+++ packages/engine-content-api/src/schema/CustomTypesProvider.ts
@@ -1,34 +1,9 @@
 import { GraphQLScalarType, Kind } from 'graphql'
-import { JSONType } from '@contember/graphql-utils'
+import { JSONType, UuidType } from '@contember/graphql-utils'
 
-
-const uuidPattern = /^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}$/i
-/** dashes are optional */
-const isUuid = (value: string): boolean => {
-	return (
-		value.length <= 36 &&
-		value.length >= 32 &&
-		value.match(uuidPattern) !== null
-	)
-}
 
 export class CustomTypesProvider {
-	public readonly uuidType: GraphQLScalarType = new GraphQLScalarType({
-		name: 'UUID',
-		serialize: str => String(str),
-		parseValue: str => {
-			if (typeof str !== 'string' || !isUuid(str)) {
-				return undefined
-			}
-			return str
-		},
-		parseLiteral: function parseLiteral(ast) {
-			if (ast.kind !== Kind.STRING || !isUuid(ast.value)) {
-				return undefined
-			}
-			return ast.value
-		},
-	})
+	public readonly uuidType: GraphQLScalarType = UuidType
 
 	//todo: implement serialize, parseValue and parseLiteral properly
 	public readonly dateType: GraphQLScalarType = new GraphQLScalarType({

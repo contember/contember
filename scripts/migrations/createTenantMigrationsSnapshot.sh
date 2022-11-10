f7658df970260922699e445b2e77fb064809efa1
David Matejka <matej21@matej21.cz>
2022-11-10 14:39:03 +0100

    feat(tenant-api): dispatch pg notification after changing project config

--- scripts/migrations/createTenantMigrationsSnapshot.sh
+++ scripts/migrations/createTenantMigrationsSnapshot.sh
@@ -13,6 +13,6 @@ cat ./packages/engine-tenant-api/src/migrations/snapshot-template-start.txt > ./
 
 docker-compose exec -T db \
 	bash -c "pg_dump -v --serializable-deferrable --no-owner --no-acl --dbname=tenant_migrations --username=\${POSTGRES_USER} --schema-only --schema=tenant --exclude-table=tenant.migrations --quote-all-identifiers  --exclude-table=tenant.migrations_id_seq" \
-	| sed -e '/^--/d' -e 's/"tenant"\.//g' -e '1,32d' -e '/^$/d' >> ./packages/engine-tenant-api/src/migrations/snapshot.ts
+	| sed  -e '1,32d' -e '/^--/d' -e 's/"tenant"\.//g' -e '/^SET /d' -e '/SET "search_path"/d' -e '/^$/d' >> ./packages/engine-tenant-api/src/migrations/snapshot.ts
 
 cat ./packages/engine-tenant-api/src/migrations/snapshot-template-end.txt >> ./packages/engine-tenant-api/src/migrations/snapshot.ts

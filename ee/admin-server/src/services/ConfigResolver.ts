fa0ff30735d2aab4476290851ff74675f95ed6a6
Jan Tvrdik <jan.tvrdik@gmail.com>
2022-12-16 15:47:31 +0100

    server: read base config from CONTEMBER_CUSTOM_CONFIG

--- ee/admin-server/src/services/ConfigResolver.ts
+++ ee/admin-server/src/services/ConfigResolver.ts
@@ -4,22 +4,36 @@ import { CustomConfig, customConfig } from '../config'
 export class ConfigResolver {
 	constructor(
 		private s3Manager: S3Manager,
+		private baseConfig: CustomConfig,
 	) {
 	}
 
 	async getConfig(projectGroup: string | undefined): Promise<CustomConfig> {
-		let config = {}
+		let config: CustomConfig = {}
+
 		try {
 			const configContent = await this.s3Manager.getObjectContent({
 				projectGroup,
 				path: 'config.json',
 			})
+
 			config = customConfig(JSON.parse(configContent))
+
 		} catch (e) {
 			if (!(typeof e === 'object' && e !== null && 'name' in e && (e as { name?: unknown }).name === 'NoSuchKey')) {
 				throw e
 			}
 		}
+
+		config = {
+			...this.baseConfig,
+			...config,
+			login: {
+				...this.baseConfig.login,
+				...config.login,
+			},
+		}
+
 		return config
 	}
 }

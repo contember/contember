037f3e4d8ea920c8cfec41537fd863f3bd473671
David Matejka <matej21@matej21.cz>
2023-03-03 14:30:04 +0100

    feat(actions): actions worker

--- ee/engine-actions/src/index.ts
+++ ee/engine-actions/src/index.ts
@@ -42,24 +42,33 @@ export default class ActionsPlugin implements Plugin {
 	getMasterContainerHook() {
 		const hook: MasterContainerHook = (builder: MasterContainerBuilder) => {
 			return builder
-				.setupService('application', (it, { projectContextResolver, debugMode }) => {
-					const handlerFactory = new ActionsGraphQLHandlerFactory()
+				.addService('actions_eventDispatcher', () => {
 					const eventsRepository = new EventsRepository()
 					const webhookTargetHandler = new WebhookTargetHandler()
 					const targetHandlerResolver = new TargetHandlerResolver(webhookTargetHandler)
-					const eventDispatcher = new EventDispatcher(eventsRepository, targetHandlerResolver)
+					return new EventDispatcher(eventsRepository, targetHandlerResolver)
+				})
+				.addService('actions_dispatchWorkerSupervisorFactory', ({ actions_eventDispatcher }) => {
+					const projectDispatcherFactory = new ProjectDispatcherFactory(actions_eventDispatcher)
+					return new DispatchWorkerSupervisorFactory(projectDispatcherFactory)
+				})
+				.setupService('application', (it, { projectContextResolver, debugMode, actions_eventDispatcher, actions_dispatchWorkerSupervisorFactory }) => {
+					const handlerFactory = new ActionsGraphQLHandlerFactory()
 					const eventsQueryResolver = new EventsQueryResolver()
-					const processBatchMutationResolver = new ProcessBatchMutationResolver(eventDispatcher)
-					const projectDispatcherFactory = new ProjectDispatcherFactory(eventDispatcher)
-					const dispatchWorkerSupervisorFactory = new DispatchWorkerSupervisorFactory(projectDispatcherFactory)
+					const processBatchMutationResolver = new ProcessBatchMutationResolver(actions_eventDispatcher)
 					const resolversFactory = new ResolversFactory(eventsQueryResolver, processBatchMutationResolver)
 
 					const actionsContextResolver = new ActionsContextResolver(debugMode, projectContextResolver)
 					const actionsMiddlewareFactory = new ActionsApiMiddlewareFactory(actionsContextResolver, handlerFactory.create(resolversFactory))
-					const actionsWebsocketMiddlewareFactory = new ActionsWebsocketControllerFactory(debugMode, actionsContextResolver, dispatchWorkerSupervisorFactory)
+					const actionsWebsocketMiddlewareFactory = new ActionsWebsocketControllerFactory(debugMode, actionsContextResolver, actions_dispatchWorkerSupervisorFactory)
 					it.addRoute('actions', '/actions/:projectSlug', actionsMiddlewareFactory.create())
 					it.addWebsocketRoute('actions', '/actions/_worker', actionsWebsocketMiddlewareFactory.create())
 				})
+				.setupService('applicationWorkers', (it, { actions_dispatchWorkerSupervisorFactory, projectGroupContainer, serverConfig }) => {
+					if (!(serverConfig as any).projectGroup) {
+						it.registerWorker(this.name, actions_dispatchWorkerSupervisorFactory.create(projectGroupContainer))
+					}
+				}) as unknown as MasterContainerBuilder
 
 		}
 		return hook

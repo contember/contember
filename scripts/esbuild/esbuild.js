60c5384e949525f8590ab61205ca3a2d1859225b
David Matejka <matej21@matej21.cz>
2023-03-14 12:56:45 +0100

    chore: build fix

--- scripts/esbuild/esbuild.js
+++ scripts/esbuild/esbuild.js
@@ -2,20 +2,20 @@ const path = require('path')
 const esbuild = require('esbuild')
 
 const root = path.dirname(path.dirname(__dirname))
+
+const eePackages = [
+	'engine-actions',
+	'engine-server-ee',
+]
+
 const resolvePlugin = {
 	name: 'resolve contember',
 	setup(build) {
 		build.onResolve({ filter: /^@contember\/.+$/ }, args => {
 			const pkg = args.path.match(/@contember\/(.+)/)[1]
-			return { path: path.join(root, '/packages/', pkg, pkg.includes('/') ? 'index.ts' : 'src/index.ts') }
+			const baseDir = eePackages.includes(pkg) ? 'ee' : 'packages'
+			return { path: path.join(root, baseDir, pkg, pkg.includes('/') ? 'index.ts' : 'src/index.ts') }
 		})
-		build.onResolve({ filter: /\.\.$/ }, args => {
-			if (args.resolveDir.endsWith('/migrations')) {
-				return { path: path.join(path.dirname(args.resolveDir), '/src/index.ts') }
-			}
-			return undefined
-		})
-
 	},
 }
 

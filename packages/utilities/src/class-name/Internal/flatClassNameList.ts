bb3b7da4c23cd5de52d39765f9ee0d2bda409126
Martin Adamko <attitude@users.noreply.github.com>
2023-05-11 13:56:45 +0200

    fix(utilities): Handle `false` case

--- packages/utilities/src/class-name/Internal/flatClassNameList.ts
+++ packages/utilities/src/class-name/Internal/flatClassNameList.ts
@@ -1,13 +1,17 @@
 import { NestedClassName } from '../Types'
 
 export function flatClassNameList(className: NestedClassName): string[] {
-	if (className == null) {
+	if (!className) {
 		return []
 	} else if (typeof className === 'string') {
 		return className.split(' ').filter(Boolean)
 	} else if (Array.isArray(className)) {
-		return className.map(flatClassNameList).flat(1)
+		return className.flatMap(flatClassNameList)
 	} else {
+		if (import.meta.env.DEV) {
+			console.error(`Unexpected className: ${JSON.stringify(className)}`)
+		}
+
 		throw new Error(`Unexpected className: ${JSON.stringify(className)}`)
 	}
 }

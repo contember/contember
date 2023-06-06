8bf12b3cebcb222adecb781e838b8f4a765cb96a
Martin Adamko <attitude@users.noreply.github.com>
2023-06-06 12:19:11 +0200

    feat(admin-sandbox): Add example bare HTML layout

--- packages/admin-sandbox/admin/components/Layouts/index.ts
+++ packages/admin-sandbox/admin/components/Layouts/index.ts
@@ -1,9 +1,11 @@
+import { Layout as bare } from './Layout.bare'
 import { Layout as cms } from './Layout.cms'
 import { Layout as _default } from './Layout.default'
 
 export const Layouts = {
 	cms,
 	default: _default,
+	bare,
 } as const
 
 export const layoutTypeList = Object.keys(Layouts) as ReadonlyArray<keyof typeof Layouts>

4d2263797b35f2ca5276e989a24ad81b6fd4cf3c
David Matejka <matej21@matej21.cz>
2023-06-06 15:20:26 +0200

    chore: fix npm publish

--- scripts/npm/publish.sh
+++ scripts/npm/publish.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-echo "registry=https://registry.npmjs.org/
-//registry.npmjs.org/:_authToken=$NPM_AUTH_TOKEN
-access=public" > ~/.npmrc
 
-yarn workspaces foreach -pt --no-private exec "yarn npm publish --tag $NPM_TAG --access public"
+yarn config set npmAuthToken "$NPM_AUTH_TOKEN"
+yarn workspaces foreach -pt --no-private npm publish --tag $NPM_TAG  --access public

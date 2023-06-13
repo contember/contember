838af47d7ef0230138b7eb012973a079fc1ea88f
David Matejka <matej21@matej21.cz>
2023-06-13 14:39:50 +0200

    feat(sandbox): add delete button to blockeditor

--- packages/admin-sandbox/admin/pages/homepage.tsx
+++ packages/admin-sandbox/admin/pages/homepage.tsx
@@ -1,8 +1,24 @@
-import { EditScope, PersistButton } from '@contember/admin'
+import { Button, EditScope, PersistButton, useEntity } from '@contember/admin'
 import { AddContent } from '../components/AddContent'
 import { ContentField } from '../components/ContentField'
 import { Title } from '../components/Directives'
 import { Slots } from '../components/Slots'
+
+const DeleteBlocks = () => {
+	const entity = useEntity()
+	return <Button onClick={() => {
+		Array.from(entity.getEntityList('content.blocks')).forEach(it => {
+			it.getField('json').updateValue(JSON.stringify({
+				'formatVersion': 1,
+				'children': [{ 'type': 'paragraph', 'children': [{ text: '' }] }],
+			}))
+		})
+		setTimeout(() => {
+			entity.getEntityList('content.blocks').deleteAll()
+		}, 10)
+
+	}}>delete all</Button>
+}
 
 export default (
 	<EditScope entity="Homepage(unique = One)" setOnCreate="(unique = One)">
@@ -15,6 +31,7 @@ export default (
 		<Slots.ContentStack>
 			<ContentField field="content" />
 			<AddContent field="content" />
+			<DeleteBlocks/>
 		</Slots.ContentStack>
 	</EditScope>
 )

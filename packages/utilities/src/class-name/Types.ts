e34d3ae8f20417cca60c2e9b05496a187d61fdc0
Martin Adamko <attitude@users.noreply.github.com>
2023-06-25 21:23:08 +0200

    feat(utilities): Add reusable `ComponentClassNameProps` type

--- packages/utilities/src/class-name/Types.ts
+++ packages/utilities/src/class-name/Types.ts
@@ -1,2 +1,9 @@
+import { PropsWithChildren } from 'react'
+
 export type NestedClassName = string | false | null | undefined | (string | false | null | undefined)[] | NestedClassName[]
 export type ClassNameStateMap = { [key: string]: string | number | boolean | null | undefined }
+
+export type ComponentClassNameProps = PropsWithChildren<{
+	className?: NestedClassName;
+	componentClassName?: string | string[];
+}>

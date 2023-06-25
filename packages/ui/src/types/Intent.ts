4b6e04dc531d17b42d410077a282dc823501087b
Martin Adamko <attitude@users.noreply.github.com>
2023-06-25 21:52:49 +0200

    revert(ui): Remove unused `*-above` and `*-below` schemes and lower future complexity
    BREAKING CHANGE: Affected users should copy removed custom CSS properties to their projects before updating.

--- packages/ui/src/types/Intent.ts
+++ packages/ui/src/types/Intent.ts
@@ -2,4 +2,4 @@ import type { Default } from './Default'
 
 export type Intent = Default | 'primary' | 'secondary' | 'tertiary' | 'positive' | 'success' | 'warn' | 'danger'
 
-export type Scheme = 'system' | 'light' | 'light-above' | 'light-below' | 'dark' | 'dark-above' | 'dark-below'
+export type Scheme = 'system' | 'light' | 'dark'

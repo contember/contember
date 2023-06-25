388af27610f7af88af39362721fe72af482c079c
Martin Adamko <attitude@users.noreply.github.com>
2023-06-25 22:40:28 +0200

    chore(react-utils): Improve documentation of `useExpectSameValueReference()`

--- packages/react-utils/src/hooks/useExpectSameValueReference.ts
+++ packages/react-utils/src/hooks/useExpectSameValueReference.ts
@@ -1,7 +1,28 @@
 import deepEqual from 'deep-equal'
 import { useRef } from 'react'
 
-// TODO: Unify error behavior with useConstantValueInvariant() and maybe merge into one hook.
+/**
+ * Checks whether the next value is the same reference as the previous one
+ *
+ * If not, it checks whether the values are deeply equal. If not, it logs an error.
+ *
+ * @param next - Value to be checked.
+ * @param shouldThrow - If true, it throws an error instead of logging it. Defaults to false.
+ * @returns void
+ * -
+ * @example
+ * ```ts
+ * useExpectSameValueReference(style)
+ * ```
+ * @example
+ * ```ts
+ * useExpectSameValueReference(style, true)
+ * ```
+ * @example
+ * ```ts
+ * useExpectSameValueReference(style, import.meta.env.DEV)
+ * ```
+ */
 export function useExpectSameValueReference<T>(next: T, shouldThrow: boolean = false) {
 	const previous = useRef(next)
 
@@ -11,7 +32,11 @@ export function useExpectSameValueReference<T>(next: T, shouldThrow: boolean = f
 		&& deepEqual(next, previous.current)
 	) {
 		if (import.meta.env.DEV) {
-			console.error('Previous and next next do not match in reference although match in value', {
+			console.error(
+				'Previous and next values do not match in reference although match in value. ' +
+				'Try declaring constants outside of the component scope. ' +
+				'Otherwise try to memoize callbacks with useCallback, ' +
+				'useReferentiallyStableCallback useEvent, useRef or useMemo.', {
 				previous: previous.current,
 				next,
 			})

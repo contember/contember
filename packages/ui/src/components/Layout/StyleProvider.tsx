eaa36b2cf28954528ed4330b9c8afe173540e463
Martin Adamko <attitude@users.noreply.github.com>
2023-06-27 14:30:41 +0200

    chore(ui): Move `StyleProvider` outside of Layout

--- packages/ui/src/components/Layout/StyleProvider.tsx
+++
@@ -1,6 +0,0 @@
-import { useClassName } from '@contember/utilities'
-import { ReactNode } from 'react'
-
-export const StyleProvider = ({ children }: { children: ReactNode }) => (
-	<div className={useClassName('root')}>{children}</div>
-)

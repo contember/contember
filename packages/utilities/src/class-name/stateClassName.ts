830f9bce7907c0322f480de7b1df9bbb1fa4d7b2
Martin Adamko <attitude@users.noreply.github.com>
2023-06-29 09:03:27 +0200

    feat(utilities): Change argument to options & improve

--- packages/utilities/src/class-name/stateClassName.ts
+++ packages/utilities/src/class-name/stateClassName.ts
@@ -6,6 +6,11 @@ function toKebabCase(value: string) {
 	return value.replace(UPPER_CASE_REG_EXP, '-$1').toLowerCase()
 }
 
+export interface StateClassNameOptions {
+	glue?: string;
+	removeFalsy?: boolean;
+}
+
 /**
  * Combines a state map of class names into a single string
  *
@@ -13,12 +18,17 @@ function toKebabCase(value: string) {
  * stateClassName({ foo: true, bar: false, baz: 1 }) // => 'foo baz-1'
  *
  * @param state - State map object with class names as keys and literals as values.
- * @param glue - Glue to use between the class name and the value, default is '-'. Set to '' to disable.
+ * @param options - Options object
+ * @param options.glue - Glue to use between the class name and the value, default is '-'. Set to '' to disable.
+ * @param options.removeFalsy - Whether to remove falsy class names, default is true.
  * @returns Combined class names as string
  */
-export function stateClassName(state: ClassNameStateMap | null = null, glue = '-'): string[] {
+export function stateClassName(
+	state: ClassNameStateMap | null = null,
+	{ glue = '-', removeFalsy = true }: StateClassNameOptions = {},
+): string[] {
 	const entries = Object.entries(state ?? []).map(
-		([className, value]) => value || value === 0
+		([className, value]) => value || value === 0 || !removeFalsy
 			? (value === true ? toKebabCase(className) : `${toKebabCase(className)}${glue}${value}`)
 			: '',
 	).filter(Boolean)

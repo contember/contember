d42aabd755fb1c14094dfcbf08bdc1d75b49064d
Martin Adamko <attitude@users.noreply.github.com>
2023-06-30 16:24:19 +0200

    feat(ui): Update `StyleProvider` to be more configurable

--- packages/ui/src/components/StyleProvider/StyleProvider.tsx
+++ packages/ui/src/components/StyleProvider/StyleProvider.tsx
@@ -1,7 +1,43 @@
-import { useClassName } from '@contember/utilities'
+import { NonOptional, stateDataAttributes, useClassName } from '@contember/utilities'
 import { PropsWithChildren } from 'react'
+import { mergeProps } from 'react-aria'
+import { Intent, Scheme } from '../../types'
+import { toSchemeClass, toThemeClass } from '../../utils'
 
-export const StyleProvider = ({ children }: PropsWithChildren) => (
-	<div className={useClassName('root')}>{children}</div>
-)
+export type StyleProviderProps = {
+	transparent?: boolean;
+	displayContents?: boolean;
+	scheme?: Scheme;
+	themeContent?: Intent;
+	themeControls?: Intent;
+	overridesLucideIcons?: boolean;
+}
+
+const initialValues: NonOptional<StyleProviderProps> = {
+	displayContents: true,
+	overridesLucideIcons: true,
+	scheme: 'light',
+	themeContent: 'default',
+	themeControls: 'primary',
+	transparent: true,
+}
+
+export const StyleProvider = ({
+	children,
+	...props
+}: PropsWithChildren<StyleProviderProps>) => {
+	const { scheme, themeContent, themeControls, ...state } = mergeProps(initialValues, props)
+
+	return (
+		<div
+			{...stateDataAttributes(state)}
+			className={useClassName('root', [
+				toThemeClass(themeContent, themeControls),
+				toSchemeClass(scheme),
+			])}
+		>
+			{children}
+		</div>
+	)
+}
 StyleProvider.displayName = 'Interface.StyleProvider'

1c3d10ca14995f190e32742eca1dc5121b598957
Martin Adamko <attitude@users.noreply.github.com>
2023-06-30 16:25:48 +0200

    feat(ui): Remove `StyleProvider` from `Portal`

--- packages/ui/src/components/Portal.tsx
+++ packages/ui/src/components/Portal.tsx
@@ -1,13 +1,15 @@
 import { memo, ReactNode } from 'react'
 import { createPortal } from 'react-dom'
-import { StyleProvider } from './StyleProvider'
 
 export interface PortalProps {
 	to?: HTMLElement
 	children: ReactNode
 }
 
-export const Portal = memo((props: PortalProps) => createPortal(<StyleProvider>{props.children}</StyleProvider>, props.to ?? document.getElementById('portal-root') ?? document.body))
+export const Portal = memo((props: PortalProps) => createPortal(
+	props.children,
+	props.to ?? document.getElementById('portal-root') ?? document.body,
+))
 Portal.displayName = 'Portal'
 
 export function getPortalRoot(): HTMLElement {

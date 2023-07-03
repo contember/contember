a8d300c24b2e671366ff5da220c1af1bdf965ece
Martin Adamko <attitude@users.noreply.github.com>
2023-07-03 19:43:40 +0200

    refactor(ui): Remove custom theme/scheme context from TitleBar

--- packages/ui/src/components/TitleBar/TitleBar.tsx
+++ packages/ui/src/components/TitleBar/TitleBar.tsx
@@ -1,9 +1,7 @@
 import { useClassNameFactory } from '@contember/utilities'
-import { memo, ReactNode } from 'react'
-import { toSchemeClass, toThemeClass } from '../../utils'
+import { ReactNode, memo } from 'react'
 import { ButtonList } from '../Forms'
 import { LayoutPageStickyContainer } from '../Layout/LayoutPageStickyContainer'
-import { useThemeScheme, useTitleThemeScheme } from '../Layout/ThemeSchemeContext'
 import type { ThemeScheme } from '../Layout/Types'
 import { Heading, HeadingProps } from '../Typography/Heading'
 
@@ -19,24 +17,9 @@ export interface TitleBarProps extends ThemeScheme {
  */
 export const TitleBar = memo(({ after, navigation, children, actions, ...props }: TitleBarProps) => {
 	const componentClassName = useClassNameFactory('titleBar')
-	const {
-		scheme,
-		theme,
-		themeContent,
-		themeControls,
-	} = useTitleThemeScheme(props)
-
-	const { scheme: layoutScheme } = useThemeScheme({})
 
 	return (
-		<LayoutPageStickyContainer
-			top={0}
-			className={componentClassName(null, [
-				toThemeClass(themeContent ?? theme, themeControls ?? theme),
-				toSchemeClass(scheme),
-				scheme !== layoutScheme ? 'is-global-theme' : undefined,
-			])}
-		>
+		<LayoutPageStickyContainer top={0} className={componentClassName()}>
 			{navigation && (
 				<nav className={componentClassName('navigation')}>
 					<ButtonList>{navigation}</ButtonList>

4829f757086157132dc295532a3a3d7e5e4badbf
Martin Adamko <attitude@users.noreply.github.com>
2023-07-13 12:29:12 +0200

    feat(ui): Allow `Spacer` to grow and shrink

--- packages/ui/src/components/Spacer/Spacer.tsx
+++ packages/ui/src/components/Spacer/Spacer.tsx
@@ -1,10 +1,13 @@
 import { useClassName } from '@contember/react-utils'
+import { dataAttribute } from '@contember/utilities'
 import { memo } from 'react'
 import { HTMLDivElementProps, Size } from '../../types'
 import { toEnumViewClass } from '../../utils'
 
 export type SpacerProps =
 	& {
+		shrink?: boolean
+		grow?: boolean
 		gap?: Size | 'xlarge' | 'none'
 	}
 	& Omit<HTMLDivElementProps, 'ref'>
@@ -13,8 +16,11 @@ export type SpacerProps =
 /**
  * @group UI
  */
-export const Spacer = memo(({ className, gap, ...rest }: SpacerProps) => {
+export const Spacer = memo(({ className, gap, grow, shrink, ...rest }: SpacerProps) => {
 	return <div
+		data-gap={dataAttribute(gap)}
+		data-grow={dataAttribute(grow)}
+		data-shrink={dataAttribute(shrink)}
 		className={useClassName('spacer', [
 			toEnumViewClass(gap),
 			className,

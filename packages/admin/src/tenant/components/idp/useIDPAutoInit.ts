dd52595734f5a048042e11a518e2000acc7aacfa
David Matejka <matej21@matej21.cz>
2023-07-18 15:16:08 +0200

    fix(admin): idp init in backlink param

--- packages/admin/src/tenant/components/idp/useIDPAutoInit.ts
+++ packages/admin/src/tenant/components/idp/useIDPAutoInit.ts
@@ -11,9 +11,21 @@ export const useIDPAutoInit = ({ onError, providers }: UseIDPAutoInitProps) => {
 	const onInitIDP = useInitIDPRedirect({ onError })
 
 	useEffect(() => {
-		const params = new URLSearchParams(window.location.search)
-		const idp = params.get('idp')
-		if (idp === null) {
+		const idp = (() => {
+			const params = new URLSearchParams(window.location.search)
+			const idp = params.get('idp')
+			if (idp !== null) {
+				return idp
+			}
+			const backlink = params.get('backlink')
+
+			if (backlink !== null) {
+				const resolvedBacklink = new URL(backlink, window.location.href)
+				return resolvedBacklink.searchParams.get('idp')
+			}
+		})()
+
+		if (!idp) {
 			return
 		}
 

746b02225c7e982ec43e158c39297c52c1a3605e
Martin Adamko <attitude@users.noreply.github.com>
2023-08-01 10:50:06 +0200

    feat(ui): Add missing Lucide table icons

---
+++ packages/ui/src/components/LucideIcons/index.ts
@@ -0,0 +1,21 @@
+import { createLucideIcon } from 'lucide-react'
+
+/**
+ * @internal Missing Lucide Icon
+ * TODO: PR to Lucide
+ */
+export const TableRowsIcon = createLucideIcon('TableRowsIcon', [
+	['rect', { key: 'rect', x: '3', y: '3', width: '18', height: '18' }],
+	['line', { key: 'line1', x1: '3', x2: '21', y1: '9', y2: '9' }],
+	['line', { key: 'line2', x1: '3', x2: '21', y1: '15', y2: '15' }],
+])
+
+/**
+ * @internal Missing Lucide Icon
+ * TODO: PR to Lucide
+ */
+export const TableColumnsIcon = createLucideIcon('TableColumnsIcon', [
+	['rect', { key: 'rect', x: '3', y: '3', width: '18', height: '18' }],
+	['line', { key: 'line1', x1: '9', x2: '9', y1: '3', y2: '21' }],
+	['line', { key: 'line2', x1: '15', x2: '15', y1: '3', y2: '21' }],
+])

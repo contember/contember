32e251cfcefca99d1f50919abc2deea76742d7cc
Martin Adamko <attitude@users.noreply.github.com>
2023-08-15 16:26:08 +0200

    chore(admin-sandbox): Add example of stacked locales without border-radiuses

--- packages/admin-sandbox/admin/components/CategoryForm.tsx
+++ packages/admin-sandbox/admin/components/CategoryForm.tsx
@@ -1,8 +1,10 @@
 import { Component, Repeater, SelectField, TextField } from '@contember/admin'
 
+const categoriesLocalesContainerProps = { gap: false }
+
 export const CategoryForm = Component(() => <>
 	<TextField field={'name'} label={'Name'} />
-	<Repeater field={'locales'} label={'Locales'} orderBy={'id'}>
+	<Repeater field={'locales'} label={'Locales'} orderBy={'id'} containerComponentExtraProps={categoriesLocalesContainerProps}>
 		<SelectField label={'Locale'} options={'Locale.code'} field={'locale'} createNewForm={<TextField field={'code'} label={'Locale code'} />} />
 		<TextField field={'name'} label={'Name'} />
 	</Repeater>

35df9baf8f3da98a5d73315a5ce8b653209bf50c
Martin Adamko <attitude@users.noreply.github.com>
2023-08-25 13:19:51 +0200

    refactor(ui): Finer threshold of `IntersectionObserver` in `SectionTabs`

--- packages/ui/src/components/SectionTabs/SectionTabs.tsx
+++ packages/ui/src/components/SectionTabs/SectionTabs.tsx
@@ -1,4 +1,5 @@
 import { useClassNameFactory } from '@contember/react-utils'
+import { range } from '@contember/utilities'
 import { memo, useCallback, useEffect, useMemo, useReducer, useRef, useState } from 'react'
 import { TabButton } from '../Tabs'
 import { ActiveSectionsTabsContext, SectionTabsContext, SectionTabsRegistrationContext, useActiveSectionsTabs, useSectionTabs } from './Context'
@@ -25,7 +26,7 @@ export const SectionTabsProvider = memo(({ children }: SectionTabsProps) => {
 			}
 		})
 	}, {
-		threshold: [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0],
+		threshold: range(0, 1, 0.002),
 	}))
 
 	const registerTab = useCallback((newTab: SectionTabProps) => {

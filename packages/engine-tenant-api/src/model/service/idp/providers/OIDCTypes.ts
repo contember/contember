aff67e07095486a303fd33a047b4b222ab056189
David Matejka <matej21@matej21.cz>
2023-08-28 11:32:32 +0200

    feat(tenant-api): support additional AZP in OIDC client

--- packages/engine-tenant-api/src/model/service/idp/providers/OIDCTypes.ts
+++ packages/engine-tenant-api/src/model/service/idp/providers/OIDCTypes.ts
@@ -9,6 +9,7 @@ export interface OIDCSessionData {
 export const OIDCConfigurationOptions = Typesafe.partial({
 	responseType: Typesafe.enumeration<ResponseType>('code', 'code id_token', 'code id_token token', 'code token', 'id_token', 'id_token token', 'none'),
 	claims: Typesafe.string,
+	additionalAuthorizedParties: Typesafe.array(Typesafe.string),
 })
 export const BaseOIDCConfiguration = Typesafe.intersection(
 	Typesafe.object({

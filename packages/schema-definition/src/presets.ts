9f4b950188d71ea43e2c62bfe04f65ad4bce4785
David Matejka <matej21@matej21.cz>
2023-08-31 17:19:00 +0200

    feat(schema-definition): introduce 1.4 settings preset

--- packages/schema-definition/src/presets.ts
+++ packages/schema-definition/src/presets.ts
@@ -7,6 +7,18 @@ const v1_3preset: Settings.Schema = {
 	},
 }
 
+
+const v1_4preset: Settings.Schema = {
+	tenant: {
+		inviteExpirationMinutes: 60 * 24 * 7, // 7 days
+	},
+	content: {
+		shortDateResponse: true,
+		useExistsInHasManyFilter: true,
+	},
+}
+
 export const settingsPresets = {
 	'v1.3': v1_3preset,
+	'v1.4': v1_4preset,
 }

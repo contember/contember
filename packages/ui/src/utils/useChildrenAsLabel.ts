d88a5bb692c5f560da33907198d30617fff6e6c4
Martin Adamko <attitude@users.noreply.github.com>
2023-09-13 09:21:07 +0200

    feat(ui): Improve `useChildrenAsLabel()`
    - Allow to reuse of the logic with `getLabelFromChildren()`
    - Add test coverage

--- packages/ui/src/utils/useChildrenAsLabel.ts
+++ packages/ui/src/utils/useChildrenAsLabel.ts
@@ -1,15 +1,6 @@
-import { Children, ReactNode, useMemo } from 'react'
+import { useChildrenAsLabel as _useChildrenAsLabel } from '@contember/react-utils'
+import { ReactNode } from 'react'
 
 export function useChildrenAsLabel(children: ReactNode): string | undefined {
-	return useMemo(() => {
-		let label: string[] = []
-
-		Children.map(children, child => {
-			if (typeof child === 'string') {
-				label.push(child)
-			}
-		})
-
-		return label.length > 0 ? label.join(' ') : undefined
-	}, [children])
+	return _useChildrenAsLabel(children)
 }

c70be39ae3c0cfe972b57e4dfd25d61ddfdafc14
Martin Adamko <attitude@users.noreply.github.com>
2023-09-15 09:15:32 +0200

    feat(ui): Supoort aria props on `Divider`

--- packages/ui/src/components/Divider/Divider.tsx
+++ packages/ui/src/components/Divider/Divider.tsx
@@ -1,6 +1,7 @@
 import { useClassName } from '@contember/react-utils'
 import { ComponentClassNameProps, dataAttribute } from '@contember/utilities'
 import { memo } from 'react'
+import { useSeparator } from 'react-aria'
 import { HTMLDivElementProps } from '../../types'
 import { StackOwnProps } from '../Stack'
 
@@ -37,11 +38,21 @@ export type DividerProps = Omit<HTMLDivElementProps, keyof DividerOwnProps> & Di
 export const Divider = memo(({ className, componentClassName = 'divider', gap = true, padding, ...rest }: DividerProps) => {
 	return (
 		<div
+			{...useSeparator(separatorConfigProps).separatorProps}
+			{...rest}
 			data-gap={dataAttribute(gap)}
 			data-padding={dataAttribute(padding)}
 			className={useClassName(componentClassName, className)}
-			{...rest}
 		/>
 	)
 })
 Divider.displayName = 'Interface.Divider'
+
+// NOTE:
+// 1. We don't use `orientation` prop because we adapt to the parent's
+//    container orientation and we don't know it here. We could use
+//    `useLayoutEffect` or React context to get the orientation
+//    of the parent, but that might be a little bit unnecessary.
+// 2. We hardcode the `elementType` to `div` because we don't use
+//    polymorphic component here.
+const separatorConfigProps = { elementType: 'div', orientation: undefined }

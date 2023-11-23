93cd5a50d5f1d8860dd2fe113dcd3be50bd7f67f
David Matejka <matej21@matej21.cz>
2023-11-23 17:27:29 +0100

    refactor(client): remove unused types

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,33 +1,16 @@
 import * as CrudQueryBuilderTmp from './crudQueryBuilder'
 
 import * as GraphQlBuilderTmp from './graphQlBuilder'
+
 export { GraphQlLiteral } from './graphQlBuilder'
 
 export namespace GraphQlBuilder {
-	export import GraphqlLiteral = GraphQlBuilderTmp.GraphQlLiteral
-	export import GraphQlLiteral = GraphQlBuilderTmp.GraphQlLiteral
+	export import GraphqlLiteral = GraphQlBuilderTmp.GraphQlLiteral;
+	export import GraphQlLiteral = GraphQlBuilderTmp.GraphQlLiteral;
 }
 
 export namespace CrudQueryBuilder {
-	export type CreateMutationArguments = CrudQueryBuilderTmp.CreateMutationArguments
-	export type CreateMutationFields = CrudQueryBuilderTmp.CreateMutationFields
-	export type DeleteMutationArguments = CrudQueryBuilderTmp.DeleteMutationArguments
-	export type DeleteMutationFields = CrudQueryBuilderTmp.DeleteMutationFields
-	export type GetQueryArguments = CrudQueryBuilderTmp.GetQueryArguments
-	export type HasManyArguments = CrudQueryBuilderTmp.HasManyArguments
-	export type HasOneArguments = CrudQueryBuilderTmp.HasOneArguments
-	export type ListQueryArguments = CrudQueryBuilderTmp.ListQueryArguments
-	export type Mutations = CrudQueryBuilderTmp.Mutations
 	export type OrderDirection = CrudQueryBuilderTmp.OrderDirection
-	export type PaginateQueryArguments = CrudQueryBuilderTmp.PaginateQueryArguments
-	export type Queries = CrudQueryBuilderTmp.Queries
-	export type ReadArguments = CrudQueryBuilderTmp.ReadArguments
-	export type ReductionArguments = CrudQueryBuilderTmp.ReductionArguments
-	export type UpdateMutationArguments = CrudQueryBuilderTmp.UpdateMutationArguments
-	export type UpdateMutationFields = CrudQueryBuilderTmp.UpdateMutationFields
-	export type WriteArguments = CrudQueryBuilderTmp.WriteArguments
-	export type WriteFields = CrudQueryBuilderTmp.WriteFields
-	export type WriteRelationOps = CrudQueryBuilderTmp.WriteRelationOps
 }
 
 export * from './builder'

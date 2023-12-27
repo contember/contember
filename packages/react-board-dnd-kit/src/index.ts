contember/interface-archive

feat: add react board

packages/react-board-dnd-kit/src/index.ts
@@ -1,0 +1,21 @@
+import { BoardProps, createBoard } from '@contember/react-board'
+import { BoardColumnRendererProps, BoardItemRendererProps, createBoardDndKitRenderer } from './BoardDndKitRenderer'
+import React, { FunctionComponent, ReactNode } from 'react'
+
+export * from './BoardDndKitRenderer'
+export * from './types'
+
+export const createBoardDndKit = <ColumnExtraProps extends {}, ItemExtraProps extends {}>({ Column, Item, Wrapper, usePortalProvider }: {
+	Wrapper: React.ComponentType<{ children: ReactNode }>
+	Column: React.ComponentType<BoardColumnRendererProps & ColumnExtraProps>
+	Item: React.ComponentType<BoardItemRendererProps & ItemExtraProps>
+	usePortalProvider?: () => Element | null
+}): FunctionComponent<BoardProps<ColumnExtraProps & ItemExtraProps>> => {
+	const renderer = createBoardDndKitRenderer({ Column, Item, Wrapper, usePortalProvider })
+
+	return createBoard<ColumnExtraProps & ItemExtraProps>({
+		Renderer: renderer,
+		ItemStaticRender: Item as any,
+		ColumnStaticRender: Column as any,
+	})
+}

packages/react-board/src/Board.tsx
@@ -1,0 +1,156 @@
+import { Component, EntityListSubTree, Field, HasMany, HasOne, useEntityList, useEntityListSubTree } from '@contember/react-binding'
+import { ComponentType } from 'react'
+import { useDynamicBoard } from './useDynamicBoard'
+import { useStaticBoard } from './useStaticBoard'
+import { BoardBindingProps } from './BoardBindingProps'
+import {
+	BoardProps,
+	BoardQualifiedDynamicProps,
+	BoardQualifiedStaticProps,
+	BoardRelativeDynamicProps,
+	BoardRelativeStaticProps,
+} from './types'
+
+export type CreateBoardArgs<T extends {}> = {
+	Renderer: ComponentType<BoardBindingProps<any> & T>
+	ItemStaticRender?: ComponentType<T>
+	ColumnStaticRender?: ComponentType<T>
+}
+
+export const createBoard = <RendererExtraProps extends {}>({ Renderer, ItemStaticRender, ColumnStaticRender }: CreateBoardArgs<RendererExtraProps>) => {
+
+	const TopLevelDynamicBoard = Component<BoardQualifiedDynamicProps & RendererExtraProps>(({ entities, orderBy, limit, offset, columns, columnsSortableBy, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityListSubTree({ entities, orderBy, limit, offset })
+		const columnsAccessor = useEntityListSubTree({ entities: columns })
+		const dynamicBoardProps = useDynamicBoard({
+			columnEntities: columnsAccessor,
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			columnsSortableBy,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...dynamicBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ entities, orderBy, limit, offset, columns, discriminationField, sortableBy, columnsSortableBy, ...props }) => {
+		return (<>
+			<EntityListSubTree entities={entities} orderBy={orderBy} limit={limit} offset={offset}>
+				<HasOne {...typeof discriminationField === 'string' ? { field: discriminationField } : discriminationField} />
+				{sortableBy && <Field {...typeof sortableBy === 'string' ? { field: sortableBy } : sortableBy} />}
+				{ItemStaticRender && <ItemStaticRender {...props as RendererExtraProps} />}
+			</EntityListSubTree>
+			<EntityListSubTree entities={columns}>
+				{columnsSortableBy && <Field {...typeof columnsSortableBy === 'string' ? { field: columnsSortableBy } : columnsSortableBy} />}
+				{ColumnStaticRender && <ColumnStaticRender {...props as RendererExtraProps} />}
+			</EntityListSubTree>
+		</>)
+	})
+
+
+
+	const RelativeDynamicBoard = Component<BoardRelativeDynamicProps & RendererExtraProps>(({ field, orderBy, limit, offset, columns, columnsSortableBy, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityList({ field, orderBy, limit, offset })
+		const columnsAccessor = useEntityListSubTree({ entities: columns })
+		const dynamicBoardProps = useDynamicBoard({
+			columnEntities: columnsAccessor,
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			columnsSortableBy,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...dynamicBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ field, orderBy, limit, offset, columns, discriminationField, sortableBy, columnsSortableBy, ...props }) => {
+		return (<>
+			<HasMany field={field} orderBy={orderBy} limit={limit} offset={offset}>
+				<HasOne {...typeof discriminationField === 'string' ? { field: discriminationField } : discriminationField} />
+				{sortableBy && <Field {...typeof sortableBy === 'string' ? { field: sortableBy } : sortableBy} />}
+				{ItemStaticRender && <ItemStaticRender {...props as RendererExtraProps} />}
+			</HasMany>
+			<EntityListSubTree entities={columns}>
+				{columnsSortableBy && <Field {...typeof columnsSortableBy === 'string' ? { field: columnsSortableBy } : columnsSortableBy} />}
+				{ColumnStaticRender && <ColumnStaticRender {...props as RendererExtraProps} />}
+			</EntityListSubTree>
+		</>)
+	})
+
+
+	const staticBoardItemInnerRender = ({ discriminationField, sortableBy, ...props }: Pick<BoardQualifiedStaticProps, 'discriminationField' | 'sortableBy'> & RendererExtraProps) => <>
+		<Field {...typeof discriminationField === 'string' ? { field: discriminationField } : discriminationField} />
+		{sortableBy && <Field {...typeof sortableBy === 'string' ? { field: sortableBy } : sortableBy} />}
+		{ItemStaticRender && <ItemStaticRender {...props as unknown as RendererExtraProps} />}
+	</>
+
+	const TopLevelStaticBoard = Component<BoardQualifiedStaticProps & RendererExtraProps>(({ entities, orderBy, limit, offset, columns, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityListSubTree({ entities, orderBy, limit, offset })
+		const staticBoardProps = useStaticBoard({
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...staticBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ entities, orderBy, limit, offset, columns, discriminationField, sortableBy, ...props }) => {
+		return (<>
+			<EntityListSubTree entities={entities} orderBy={orderBy} limit={limit} offset={offset}>
+				{staticBoardItemInnerRender({ sortableBy, discriminationField, ...(props as RendererExtraProps) })}
+			</EntityListSubTree>
+		</>)
+	})
+
+
+	const RelativeStaticBoard = Component<BoardRelativeStaticProps & RendererExtraProps>(({ field, orderBy, limit, offset, columns, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityList({ field, orderBy, limit, offset })
+		const staticBoardProps = useStaticBoard({
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...staticBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ field, orderBy, limit, offset, columns, discriminationField, sortableBy, ...props }) => {
+		return (<>
+			<HasMany field={field} orderBy={orderBy} limit={limit} offset={offset}>
+				{staticBoardItemInnerRender({ sortableBy, discriminationField, ...(props as RendererExtraProps) })}
+			</HasMany>
+		</>)
+	})
+
+
+
+	return Component<BoardProps<RendererExtraProps>>(props => {
+		if ('entities' in props) {
+			if (Array.isArray(props.columns)) {
+				return <TopLevelStaticBoard {...(props as BoardQualifiedStaticProps & RendererExtraProps)} />
+			} else {
+				return <TopLevelDynamicBoard {...(props as BoardQualifiedDynamicProps & RendererExtraProps)} />
+			}
+		} else {
+			if (Array.isArray(props.columns)) {
+				return <RelativeStaticBoard {...(props as BoardRelativeStaticProps & RendererExtraProps)} />
+			} else {
+				return <RelativeDynamicBoard {...(props as BoardRelativeDynamicProps & RendererExtraProps)} />
+			}
+		}
+	})
+
+}

packages/react-board/src/types.ts
@@ -1,0 +1,64 @@
+import { ReactNode } from 'react'
+import { SugaredQualifiedEntityList, SugaredRelativeEntityList, SugaredRelativeSingleEntity, SugaredRelativeSingleField } from '@contember/react-binding'
+
+export type BoardDynamicColumnsBindingProps = {
+	columns: SugaredQualifiedEntityList['entities']
+	columnsSortableBy?: string | SugaredRelativeSingleField
+	discriminationField: string | SugaredRelativeSingleEntity
+}
+
+export type BoardStaticColumnsBindingProps = {
+	columns: {
+		value: string,
+		label: ReactNode
+	}[]
+	discriminationField: string | SugaredRelativeSingleField
+}
+
+export type BoardNullBehaviourProps = {
+	nullColumn?: 'never' | 'always' | 'auto'
+	nullColumnPlacement?: 'start' | 'end'
+}
+export type BoardCommonProps =
+	& BoardNullBehaviourProps
+	& {
+		children: ReactNode
+		sortableBy?: string | SugaredRelativeSingleField
+		sortScope?: 'column' | 'board'
+	}
+
+export type BoardQualifiedItemsProps =
+	& Pick<SugaredQualifiedEntityList, 'entities' | 'orderBy' | 'limit' | 'offset'>
+
+export type BoardRelativeItemsProps =
+	& Pick<SugaredRelativeEntityList, 'field' | 'orderBy' | 'limit' | 'offset'>
+
+
+export type BoardQualifiedDynamicProps =
+	& BoardCommonProps
+	& BoardDynamicColumnsBindingProps
+	& BoardQualifiedItemsProps
+
+export type BoardRelativeDynamicProps =
+	& BoardCommonProps
+	& BoardDynamicColumnsBindingProps
+	& BoardRelativeItemsProps
+
+export type BoardQualifiedStaticProps =
+	& BoardCommonProps
+	& BoardStaticColumnsBindingProps
+	& BoardQualifiedItemsProps
+
+export type BoardRelativeStaticProps =
+	& BoardCommonProps
+	& BoardStaticColumnsBindingProps
+	& BoardRelativeItemsProps
+
+export type BoardProps<RendererExtraProps> =
+	& (
+		| BoardQualifiedDynamicProps
+		| BoardRelativeDynamicProps
+		| BoardQualifiedStaticProps
+		| BoardRelativeStaticProps
+	)
+	& RendererExtraProps

contember/contember

packages/react-board-dnd-kit/src/index.ts
@@ -1,0 +1,23 @@
+import { BoardBaseProps, createBoard } from '@contember/react-board'
+import { BoardColumnRendererProps, BoardItemRendererProps, createBoardDndKitRenderer } from './BoardDndKitRenderer'
+import React, { FunctionComponent, ReactNode } from 'react'
+
+export * from './BoardDndKitRenderer'
+export * from './types'
+
+export const createBoardDndKit = <ColumnExtraProps extends {}, ItemExtraProps extends {}>({ Column, Item, Wrapper, usePortalProvider }: {
+	Wrapper: React.ComponentType<{ children: ReactNode }>
+	Column: React.ComponentType<BoardColumnRendererProps & ColumnExtraProps>
+	Item: React.ComponentType<BoardItemRendererProps & ItemExtraProps>
+	usePortalProvider?: () => Element | null
+}): FunctionComponent<BoardBaseProps<ColumnExtraProps & ItemExtraProps>> => {
+	const renderer = createBoardDndKitRenderer({ Column, Item, Wrapper, usePortalProvider })
+
+	return createBoard<ColumnExtraProps & ItemExtraProps>({
+		Renderer: renderer,
+		ItemStaticRender: Item as any,
+		ColumnStaticRender: Column as any,
+	})
+}
+
+export * from '@contember/react-board'

packages/react-board/src/Board.tsx
@@ -1,0 +1,156 @@
+import { Component, EntityListSubTree, Field, HasMany, HasOne, useEntityList, useEntityListSubTree } from '@contember/react-binding'
+import { ComponentType } from 'react'
+import { useDynamicBoard } from './useDynamicBoard'
+import { useStaticBoard } from './useStaticBoard'
+import { BoardBindingProps } from './BoardBindingProps'
+import {
+	BoardBaseProps,
+	BoardQualifiedDynamicProps,
+	BoardQualifiedStaticProps,
+	BoardRelativeDynamicProps,
+	BoardRelativeStaticProps,
+} from './types'
+
+export type CreateBoardArgs<T extends {}> = {
+	Renderer: ComponentType<BoardBindingProps<any> & T>
+	ItemStaticRender?: ComponentType<T>
+	ColumnStaticRender?: ComponentType<T>
+}
+
+export const createBoard = <RendererExtraProps extends {}>({ Renderer, ItemStaticRender, ColumnStaticRender }: CreateBoardArgs<RendererExtraProps>) => {
+
+	const TopLevelDynamicBoard = Component<BoardQualifiedDynamicProps & RendererExtraProps>(({ entities, orderBy, limit, offset, columns, columnsSortableBy, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityListSubTree({ entities, orderBy, limit, offset })
+		const columnsAccessor = useEntityListSubTree({ entities: columns })
+		const dynamicBoardProps = useDynamicBoard({
+			columnEntities: columnsAccessor,
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			columnsSortableBy,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...dynamicBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ entities, orderBy, limit, offset, columns, discriminationField, sortableBy, columnsSortableBy, ...props }) => {
+		return (<>
+			<EntityListSubTree entities={entities} orderBy={orderBy} limit={limit} offset={offset}>
+				<HasOne {...typeof discriminationField === 'string' ? { field: discriminationField } : discriminationField} />
+				{sortableBy && <Field {...typeof sortableBy === 'string' ? { field: sortableBy } : sortableBy} />}
+				{ItemStaticRender && <ItemStaticRender {...props as RendererExtraProps} />}
+			</EntityListSubTree>
+			<EntityListSubTree entities={columns}>
+				{columnsSortableBy && <Field {...typeof columnsSortableBy === 'string' ? { field: columnsSortableBy } : columnsSortableBy} />}
+				{ColumnStaticRender && <ColumnStaticRender {...props as RendererExtraProps} />}
+			</EntityListSubTree>
+		</>)
+	})
+
+
+
+	const RelativeDynamicBoard = Component<BoardRelativeDynamicProps & RendererExtraProps>(({ field, orderBy, limit, offset, columns, columnsSortableBy, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityList({ field, orderBy, limit, offset })
+		const columnsAccessor = useEntityListSubTree({ entities: columns })
+		const dynamicBoardProps = useDynamicBoard({
+			columnEntities: columnsAccessor,
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			columnsSortableBy,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...dynamicBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ field, orderBy, limit, offset, columns, discriminationField, sortableBy, columnsSortableBy, ...props }) => {
+		return (<>
+			<HasMany field={field} orderBy={orderBy} limit={limit} offset={offset}>
+				<HasOne {...typeof discriminationField === 'string' ? { field: discriminationField } : discriminationField} />
+				{sortableBy && <Field {...typeof sortableBy === 'string' ? { field: sortableBy } : sortableBy} />}
+				{ItemStaticRender && <ItemStaticRender {...props as RendererExtraProps} />}
+			</HasMany>
+			<EntityListSubTree entities={columns}>
+				{columnsSortableBy && <Field {...typeof columnsSortableBy === 'string' ? { field: columnsSortableBy } : columnsSortableBy} />}
+				{ColumnStaticRender && <ColumnStaticRender {...props as RendererExtraProps} />}
+			</EntityListSubTree>
+		</>)
+	})
+
+
+	const staticBoardItemInnerRender = ({ discriminationField, sortableBy, ...props }: Pick<BoardQualifiedStaticProps, 'discriminationField' | 'sortableBy'> & RendererExtraProps) => <>
+		<Field {...typeof discriminationField === 'string' ? { field: discriminationField } : discriminationField} />
+		{sortableBy && <Field {...typeof sortableBy === 'string' ? { field: sortableBy } : sortableBy} />}
+		{ItemStaticRender && <ItemStaticRender {...props as unknown as RendererExtraProps} />}
+	</>
+
+	const TopLevelStaticBoard = Component<BoardQualifiedStaticProps & RendererExtraProps>(({ entities, orderBy, limit, offset, columns, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityListSubTree({ entities, orderBy, limit, offset })
+		const staticBoardProps = useStaticBoard({
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...staticBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ entities, orderBy, limit, offset, columns, discriminationField, sortableBy, ...props }) => {
+		return (<>
+			<EntityListSubTree entities={entities} orderBy={orderBy} limit={limit} offset={offset}>
+				{staticBoardItemInnerRender({ sortableBy, discriminationField, ...(props as RendererExtraProps) })}
+			</EntityListSubTree>
+		</>)
+	})
+
+
+	const RelativeStaticBoard = Component<BoardRelativeStaticProps & RendererExtraProps>(({ field, orderBy, limit, offset, columns, discriminationField, sortableBy, sortScope, nullColumn, nullColumnPlacement, ...props }) => {
+		const itemsAccessor = useEntityList({ field, orderBy, limit, offset })
+		const staticBoardProps = useStaticBoard({
+			itemEntities: itemsAccessor,
+			columns,
+			sortableBy,
+			sortScope,
+			discriminationField,
+			nullColumn,
+			nullColumnPlacement,
+		})
+		return (
+			<Renderer {...staticBoardProps} {...props as RendererExtraProps} />
+		)
+	}, ({ field, orderBy, limit, offset, columns, discriminationField, sortableBy, ...props }) => {
+		return (<>
+			<HasMany field={field} orderBy={orderBy} limit={limit} offset={offset}>
+				{staticBoardItemInnerRender({ sortableBy, discriminationField, ...(props as RendererExtraProps) })}
+			</HasMany>
+		</>)
+	})
+
+
+
+	return Component<BoardBaseProps<RendererExtraProps>>(props => {
+		if ('entities' in props) {
+			if (Array.isArray(props.columns)) {
+				return <TopLevelStaticBoard {...(props as BoardQualifiedStaticProps & RendererExtraProps)} />
+			} else {
+				return <TopLevelDynamicBoard {...(props as BoardQualifiedDynamicProps & RendererExtraProps)} />
+			}
+		} else {
+			if (Array.isArray(props.columns)) {
+				return <RelativeStaticBoard {...(props as BoardRelativeStaticProps & RendererExtraProps)} />
+			} else {
+				return <RelativeDynamicBoard {...(props as BoardRelativeDynamicProps & RendererExtraProps)} />
+			}
+		}
+	})
+
+}

packages/react-board/src/types.ts
@@ -1,0 +1,64 @@
+import { ReactNode } from 'react'
+import { SugaredQualifiedEntityList, SugaredRelativeEntityList, SugaredRelativeSingleEntity, SugaredRelativeSingleField } from '@contember/react-binding'
+
+export type BoardDynamicColumnsBindingProps = {
+	columns: SugaredQualifiedEntityList['entities']
+	columnsSortableBy?: string | SugaredRelativeSingleField
+	discriminationField: string | SugaredRelativeSingleEntity
+}
+
+export type BoardStaticColumnsBindingProps = {
+	columns: {
+		value: string,
+		label: ReactNode
+	}[]
+	discriminationField: string | SugaredRelativeSingleField
+}
+
+export type BoardNullBehaviourProps = {
+	nullColumn?: 'never' | 'always' | 'auto'
+	nullColumnPlacement?: 'start' | 'end'
+}
+export type BoardCommonProps =
+	& BoardNullBehaviourProps
+	& {
+		children: ReactNode
+		sortableBy?: string | SugaredRelativeSingleField
+		sortScope?: 'column' | 'board'
+	}
+
+export type BoardQualifiedItemsProps =
+	& Pick<SugaredQualifiedEntityList, 'entities' | 'orderBy' | 'limit' | 'offset'>
+
+export type BoardRelativeItemsProps =
+	& Pick<SugaredRelativeEntityList, 'field' | 'orderBy' | 'limit' | 'offset'>
+
+
+export type BoardQualifiedDynamicProps =
+	& BoardCommonProps
+	& BoardDynamicColumnsBindingProps
+	& BoardQualifiedItemsProps
+
+export type BoardRelativeDynamicProps =
+	& BoardCommonProps
+	& BoardDynamicColumnsBindingProps
+	& BoardRelativeItemsProps
+
+export type BoardQualifiedStaticProps =
+	& BoardCommonProps
+	& BoardStaticColumnsBindingProps
+	& BoardQualifiedItemsProps
+
+export type BoardRelativeStaticProps =
+	& BoardCommonProps
+	& BoardStaticColumnsBindingProps
+	& BoardRelativeItemsProps
+
+export type BoardBaseProps<RendererExtraProps> =
+	& (
+		| BoardQualifiedDynamicProps
+		| BoardRelativeDynamicProps
+		| BoardQualifiedStaticProps
+		| BoardRelativeStaticProps
+	)
+	& RendererExtraProps
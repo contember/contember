8e8d9c847f8cca04fdc9bf1f3a4d7c6f18c23318
David Matejka <matej21@matej21.cz>
2023-12-29 15:44:39 +0100

    refactor(layout): extract react-slots package plus some refactoring, simplify public api

---
+++ packages/react-slots/src/components/SlotSource.tsx
@@ -0,0 +1,22 @@
+import { memo, ReactNode } from 'react'
+import { createPortal } from 'react-dom'
+import { useSlotTargetElement } from '../hooks'
+
+export type SlotSourceProps = {
+	children: ReactNode;
+	name: string;
+}
+
+/**
+ * @group Layout
+ */
+export const SlotSource = memo<SlotSourceProps>(({ name, children }) => {
+	const el = useSlotTargetElement(name)
+	if (el) {
+		return createPortal(children, el)
+	} else if (el === undefined) {
+		return null
+	}
+	return <>{children}</>
+})
+SlotSource.displayName = 'Layout.Slots.Source'
--- packages/utilities/src/functional/setHasOneOf.ts
+++
@@ -1,3 +0,0 @@
-export function setHasOneOf<T>(set: Set<T>, values: T[]): boolean {
-  return values.some(value => set.has(value))
-}

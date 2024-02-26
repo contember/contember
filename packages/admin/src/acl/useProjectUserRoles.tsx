098476d8fa3f5ea824ab994f3da76bb26cdb2bbd
David Matejka <matej21@matej21.cz>
2024-02-26 13:57:01 +0100

    refactor(admin): reexport useProjectUserROles

--- packages/admin/src/acl/useProjectUserRoles.tsx
+++ packages/admin/src/acl/useProjectUserRoles.tsx
@@ -1,21 +1,2 @@
-import { useIdentity } from '../components'
-import { useMemo } from 'react'
-import { useProjectSlug } from '@contember/react-client'
-
-export type ProjectUserRoles = Set<string>
-
-export const useProjectUserRoles = (): ProjectUserRoles => {
-	const identity = useIdentity()
-	const projectSlug = useProjectSlug()
-	return useMemo(() => {
-		if (!projectSlug) {
-			return new Set()
-		}
-		const targetProject = identity.projects.find(project => project.slug === projectSlug)
-		if (targetProject === undefined) {
-			return new Set()
-		}
-
-		return new Set(targetProject.roles)
-	}, [identity.projects, projectSlug])
-}
+export { useProjectUserRoles } from '@contember/react-identity'
+export type { ProjectUserRoles } from '@contember/react-identity'

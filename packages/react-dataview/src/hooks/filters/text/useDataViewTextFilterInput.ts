bdf03fc6c4c0c8537d43b8ed6739031f48ae9e32
David Matejka <matej21@matej21.cz>
2024-03-14 16:10:17 +0100

    feat(react-dataview): debounce input

--- packages/react-dataview/src/hooks/filters/text/useDataViewTextFilterInput.ts
+++ packages/react-dataview/src/hooks/filters/text/useDataViewTextFilterInput.ts
@@ -1,4 +1,4 @@
-import { ChangeEvent, useCallback } from 'react'
+import { ChangeEvent, useCallback, useRef, useState } from 'react'
 import { useDataViewFilter } from '../../useDataViewFilter'
 import { TextFilterArtifacts } from '../../../filterTypes'
 
@@ -7,17 +7,33 @@ export interface UseDataViewTextFilterInputResult {
 	onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-export const useDataViewTextFilterInput = (name: string): UseDataViewTextFilterInputResult => {
+export const useDataViewTextFilterInput = ({ name, debounceMs = 500 }: { name: string, debounceMs?: number }): UseDataViewTextFilterInputResult => {
 	const [state, setFilter] = useDataViewFilter<TextFilterArtifacts>(name)
+	const [value, setValue] = useState(state?.query ?? '')
+	const timerRef = useRef<ReturnType<typeof setTimeout>>()
+
 	const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-		setFilter(it => ({
-			...it,
-			query: e.target.value,
-		}))
-	}, [setFilter])
+		if (debounceMs && e.target.value) {
+			timerRef.current && clearTimeout(timerRef.current)
+			timerRef.current = setTimeout(() => {
+				setFilter(it => ({
+					...it,
+					query: e.target.value,
+				}))
+			}, debounceMs)
+
+		} else {
+			setFilter(it => ({
+				...it,
+				query: e.target.value,
+			}))
+		}
+		setValue(e.target.value)
+
+	}, [debounceMs, setFilter])
 
 	return {
-		value: state?.query ?? '',
+		value,
 		onChange,
 	}
 }

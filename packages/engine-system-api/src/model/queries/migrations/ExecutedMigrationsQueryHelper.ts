bd56721df9a67f8a42e7411214033660ff14439b
David Matejka <matej21@matej21.cz>
2024-03-20 16:13:26 +0100

    fix(system-api): fix executed migrations query when running on old schema [fixes #523]

--- packages/engine-system-api/src/model/queries/migrations/ExecutedMigrationsQueryHelper.ts
+++ packages/engine-system-api/src/model/queries/migrations/ExecutedMigrationsQueryHelper.ts
@@ -1,16 +1,11 @@
-import { SelectBuilder } from '@contember/database'
+import { Literal, SelectBuilder } from '@contember/database'
 import { ExecutedMigration } from '../../dtos'
 import { VERSION_INITIAL } from '@contember/schema-migrations'
 
 type ExecutedMigrationRow = { id: number; name: string; migration: any; checksum: string | null; executed_at: Date; type: 'content' | 'schema' }
 export const createExecutedMigrationQueryBuilder = () =>
 	SelectBuilder.create<ExecutedMigrationRow>()
-		.select('id')
-		.select('name')
-		.select('migration')
-		.select('checksum')
-		.select('executed_at')
-		.select('type')
+		.select(new Literal('*'))
 		.from('schema_migration')
 		.orderBy('version')
 
@@ -22,5 +17,5 @@ export const createExecutedMigrationDto = (row: ExecutedMigrationRow) =>
 		row.migration.modifications,
 		row.executed_at,
 		row.checksum,
-		row.type,
+		row.type ?? 'schema',
 	)

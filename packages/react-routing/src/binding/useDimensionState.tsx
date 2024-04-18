624149379f96a711920275580270d223e65a3247
David Matejka <matej21@matej21.cz>
2024-04-18 16:40:20 +0200

    feat(react-routing): add useDimensionState hook

---
+++ packages/react-routing/src/binding/useDimensionState.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react'
+import { useCurrentRequest } from '../RequestContext'
+import { useRedirect } from './useRedirect'
+import { StateStorageOrName, useStoredState } from '@contember/react-utils'
+
+
+const emptyDim = [] as string[]
+
+export const useDimensionState = ({ dimension, defaultValue, storage = 'null' }: {
+	dimension: string,
+	defaultValue: string | string[],
+	storage?: StateStorageOrName
+}) => {
+	const currentDimensionValue = useCurrentRequest()?.dimensions[dimension] ?? emptyDim
+
+	const [storedState, setStoredState] = useStoredState<string[]>(storage, ['', `dimension.${dimension}`], it => {
+		const valuesArray = Array.isArray(defaultValue) ? defaultValue : [defaultValue]
+		return it ?? valuesArray
+	})
+	const [initialStoredState] = useState(storedState)
+	const redirect = useRedirect()
+
+	useEffect(() => {
+		setStoredState(currentDimensionValue)
+	}, [currentDimensionValue, setStoredState])
+
+	useEffect(() => {
+		if (currentDimensionValue.length === 0 && initialStoredState.length > 0) {
+			redirect(it => it ? {
+				...it,
+				dimensions: {
+					...it.dimensions,
+					[dimension]: initialStoredState,
+				},
+			} : null)
+		}
+	}, [currentDimensionValue.length, dimension, initialStoredState, redirect])
+
+	return currentDimensionValue
+}

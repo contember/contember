1226811a769e20c892e257cf0da13a6bb6d47315
David Matejka <matej21@matej21.cz>
2024-05-29 10:51:25 +0200

    chore(playground): update vite build to get project name from dsn

--- packages/playground/admin/vite.config.ts
+++ packages/playground/admin/vite.config.ts
@@ -5,30 +5,37 @@ import { defineConfig } from 'vite'
 import { resolveConfig } from '../../../build/createViteConfig.js'
 import { resolve } from 'node:path'
 
-export default defineConfig({
-	base: '/',
-	plugins: [
-		react(),
-		{
-			name: 'rewrite-middleware',
-			configureServer(serve) {
-				serve.middlewares.use((req, res, next) => {
-					if (req.url === '/app' || req.url?.startsWith('/app/') && !req.url?.match(/\.\w+($|\?)/)) {
-						req.url = '/app/'
-					}
-					next()
-				})
+export default defineConfig(() => {
+	const dsn = process.argv.find(it => it.includes('://'))
+	const projectName = dsn ? new URL(dsn).username : null
+	return ({
+		define: projectName ? {
+			'import.meta.env.VITE_CONTEMBER_ADMIN_PROJECT_NAME': JSON.stringify(projectName),
+		} : {},
+		base: '/',
+		plugins: [
+			react(),
+			{
+				name: 'rewrite-middleware',
+				configureServer(serve) {
+					serve.middlewares.use((req, res, next) => {
+						if (req.url === '/app' || req.url?.startsWith('/app/') && !req.url?.match(/\.\w+($|\?)/)) {
+							req.url = '/app/'
+						}
+						next()
+					})
+				},
+			},
+		],
+		resolve: resolveConfig,
+		build: {
+			rollupOptions: {
+				input: {
+					root: resolve(__dirname, './index.html'),
+					app: resolve(__dirname, './app/index.html'),
+				},
 			},
 		},
-	],
-	resolve: resolveConfig,
-	build: {
-		rollupOptions: {
-			input: {
-				root: resolve(__dirname, './index.html'),
-				app: resolve(__dirname, './app/index.html'),
-			},
-		},
-	},
 
+	})
 })

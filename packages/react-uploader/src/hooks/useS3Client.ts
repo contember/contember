b9eb164c7fe9cb3610ec3360690be44fd6d46e71
David Matejka <matej21@matej21.cz>
2024-06-18 14:56:24 +0200

    refactor(react-uploader): s3 client accepts url signer function

--- packages/react-uploader/src/hooks/useS3Client.ts
+++ packages/react-uploader/src/hooks/useS3Client.ts
@@ -1,4 +1,5 @@
 import { S3UploadClient } from '../uploadClient/S3UploadClient'
 import { useCurrentContentGraphQlClient } from '@contember/react-client'
+import { createBatchSignedUrlGenerator } from '../internal/utils/urlSigner'
 
-export const useS3Client = () => new S3UploadClient(useCurrentContentGraphQlClient())
+export const useS3Client = () => new S3UploadClient(createBatchSignedUrlGenerator(useCurrentContentGraphQlClient()))

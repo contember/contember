d601464d6beeb1d2939724cd375a409d4bc55018
Jonáš Nobile <jonas.nobile@icloud.com>
2024-06-19 18:44:30 +0200

    feat(vite-plugin): add build options and middleware

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -1,13 +1,51 @@
 import { createHash } from 'node:crypto'
+import { resolve } from 'node:path'
 import { Plugin } from 'vite'
 
 type ContemberOptions = {
 	buildVersion?: boolean
+	disableMiddleware?: boolean,
+	appPath?: string,
 }
 
 export function contember(options?: ContemberOptions): Plugin {
+	const appPath = options?.appPath ?? '/app'
+	const contemberDsn = process.argv.find(it => it.includes('contember://'))
+	const projectName = contemberDsn ? new URL(contemberDsn).username : null
+
+	const defineConfig = projectName ? {
+		'import.meta.env.VITE_CONTEMBER_ADMIN_PROJECT_NAME': JSON.stringify(projectName),
+	} : {}
+
 	return ({
 		name: 'contember',
+		config(config) {
+			return {
+				define: defineConfig,
+				base: '/',
+				...config,
+				build: {
+					...config.build,
+					rollupOptions: {
+						...config.build?.rollupOptions,
+						input: config.build?.rollupOptions?.input ?? {
+							root: resolve(__dirname, './index.html'),
+							app: resolve(__dirname, `.${appPath}/index.html`),
+						},
+					},
+				},
+			}
+		},
+		configureServer(serve) {
+			if (!options?.disableMiddleware) {
+				return () => serve.middlewares.use((req, res, next) => {
+					if (req.url === appPath || req.url?.startsWith(`${appPath}/`) && !req.url?.match(/\.\w+($|\?)/)) {
+						req.url = `${appPath}/`
+					}
+					next()
+				})
+			}
+		},
 		transformIndexHtml: options?.buildVersion === false
 			? undefined
 			: {

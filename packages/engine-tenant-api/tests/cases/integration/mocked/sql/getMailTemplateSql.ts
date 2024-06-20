5c8df5fa07a029c4519da82216348660501dbeae
David Matejka <matej21@matej21.cz>
2024-06-20 16:01:26 +0200

    tests(tenant-api): fix sql snapshot

--- packages/engine-tenant-api/tests/cases/integration/mocked/sql/getMailTemplateSql.ts
+++ packages/engine-tenant-api/tests/cases/integration/mocked/sql/getMailTemplateSql.ts
@@ -3,7 +3,7 @@ import { ExpectedQuery } from '@contember/database-tester'
 export const getMailTemplateSql = (
 	args: { type: string; projectId: string | null },
 ): ExpectedQuery => args.projectId ? {
-	sql: `SELECT "id", "subject", "content", "use_layout" AS "uselayout"
+	sql: `select "mail_template"."id", "subject", "content", "use_layout" as "useLayout", "reply_to" as "replyTo", "project_id" as "projectId", "mail_type" as "type", "variant"
 			  FROM "tenant"."mail_template"
 			  WHERE "project_id" = ?
 				AND "mail_type" = ?
@@ -14,7 +14,7 @@ export const getMailTemplateSql = (
 	},
 } :
 	{
-		sql: `SELECT "id", "subject", "content", "use_layout" AS "uselayout"
+		sql: `select "mail_template"."id", "subject", "content", "use_layout" as "useLayout", "reply_to" as "replyTo", "project_id" as "projectId", "mail_type" as "type", "variant"
 			  FROM "tenant"."mail_template"
 			  WHERE "project_id" IS NULL
 				AND "mail_type" = ?

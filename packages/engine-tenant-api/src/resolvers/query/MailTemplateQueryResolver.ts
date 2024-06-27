22ea52f0845f527a81f3ee93fe5af1b827741ea1
David Matejka <matej21@matej21.cz>
2024-06-27 17:22:01 +0200

    fix(tenant-api): fix permission check in mail template query

--- packages/engine-tenant-api/src/resolvers/query/MailTemplateQueryResolver.ts
+++ packages/engine-tenant-api/src/resolvers/query/MailTemplateQueryResolver.ts
@@ -1,18 +1,12 @@
 import { MailTemplateData, QueryResolvers } from '../../schema'
-import { MailTemplateListQuery, MailTemplateManager, mailTypeFromDbToSchema, PermissionActions } from '../../model'
+import { MailTemplateListQuery, mailTypeFromDbToSchema, PermissionActions } from '../../model'
 import { TenantResolverContext } from '../TenantResolverContext'
 
 export class MailTemplateQueryResolver implements QueryResolvers {
-
-	constructor(
-		private readonly mailTemplateManager: MailTemplateManager,
-	) {
-	}
-
 	async mailTemplates(parent: unknown, args: unknown, context: TenantResolverContext): Promise<MailTemplateData[]> {
 		await context.requireAccess({
-			action: PermissionActions.IDP_LIST,
-			message: 'You are not allowed to list IDP',
+			action: PermissionActions.MAIL_TEMPLATE_LIST,
+			message: 'You are not allowed to list mail templates',
 		})
 		return (await context.db.queryHandler.fetch(new MailTemplateListQuery())).map(it => ({
 			...it,

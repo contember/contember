395612b27bbe3549059a2584ec5303df8c32f745
David Matejka <matej21@matej21.cz>
2024-07-01 14:17:10 +0200

    tests(react-binding): add tests for connectEntityAtField

--- packages/react-binding/tests/cases/unit/core/entityOperations.test.tsx
+++ packages/react-binding/tests/cases/unit/core/entityOperations.test.tsx
@@ -32,4 +32,49 @@ describe('entity operations', () => {
 		entity.getAccessor().getField('fooField').updateValue(null)
 		expect(entity.unpersistedChangesCount).eq(0)
 	})
+
+	it('fails when relation not defined in static render', () => {
+		const { treeStore, environment } = createBinding({
+			node: (<>
+				<EntitySubTree entity="Article(id = 'cfb8d0ae-c892-4047-acfb-a89adab2371d')" alias="article">
+				</EntitySubTree>
+				<EntitySubTree entity="Category(id = '89560cfa-f874-42b6-ace3-35a8ebcbba15')" alias="category">
+				</EntitySubTree>
+			</>),
+			schema: convertModelToAdminSchema(createSchema(ModelWithRelation).model),
+		})
+		const article = treeStore.getSubTreeState('entity', undefined, 'article', environment)
+		const category = treeStore.getSubTreeState('entity', undefined, 'category', environment)
+		expect(() => {
+			article.getAccessor().connectEntityAtField('category', category.getAccessor())
+		}).toThrowError('Cannot connect at field \'category\' as it wasn\'t registered during static render.')
+	})
+
+	it('ok when relation defined in static render', () => {
+		const { treeStore, environment } = createBinding({
+			node: (<>
+				<EntitySubTree entity="Article(id = 'cfb8d0ae-c892-4047-acfb-a89adab2371d')" alias="article">
+					<HasOne field="category" />
+				</EntitySubTree>
+				<EntitySubTree entity="Category(id = '89560cfa-f874-42b6-ace3-35a8ebcbba15')" alias="category">
+				</EntitySubTree>
+			</>),
+			schema: convertModelToAdminSchema(createSchema(ModelWithRelation).model),
+		})
+		const article = treeStore.getSubTreeState('entity', undefined, 'article', environment)
+		const category = treeStore.getSubTreeState('entity', undefined, 'category', environment)
+		article.getAccessor().connectEntityAtField('category', category.getAccessor())
+	})
 })
+
+
+
+namespace ModelWithRelation {
+	export class Category {
+		articles = c.oneHasMany(Article, 'category')
+	}
+
+	export class Article {
+		category = c.manyHasOne(Category, 'articles')
+	}
+}

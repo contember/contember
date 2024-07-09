e8a46e6836db606790934aaab907655da8651e9b
David Matejka <matej21@matej21.cz>
2024-07-09 14:57:25 +0200

    feat(tenant-api): add changeProfile mutation to allow admin change any person profile by personId

--- packages/engine-tenant-api/src/resolvers/mutation/person/ChangeProfileMutationResolver.ts
+++ packages/engine-tenant-api/src/resolvers/mutation/person/ChangeProfileMutationResolver.ts
@@ -1,13 +1,51 @@
-import { ChangeMyProfileResponse, MutationChangeMyProfileArgs, MutationResolvers } from '../../../schema'
+import {
+	ChangeMyProfileResponse,
+	ChangeProfileResponse,
+	MutationChangeMyProfileArgs,
+	MutationChangeProfileArgs,
+	MutationResolvers,
+} from '../../../schema'
 import { GraphQLResolveInfo } from 'graphql'
 import { TenantResolverContext } from '../../TenantResolverContext'
-import { PermissionActions, PersonManager, PersonQuery } from '../../../model'
+import { IdentityScope, PermissionActions, PersonManager, PersonQuery } from '../../../model'
 import { createErrorResponse } from '../../errorUtils'
 
-export class ChangeProfileMutationResolver implements MutationResolvers {
+export class ChangeProfileMutationResolver implements Pick<MutationResolvers, 'changeMyProfile' | 'changeProfile'> {
 	constructor(
 		private readonly personManager: PersonManager,
 	) { }
+
+	async changeProfile(
+		parent: unknown,
+		args: MutationChangeProfileArgs,
+		context: TenantResolverContext,
+		info: GraphQLResolveInfo,
+	): Promise<ChangeProfileResponse> {
+		const person = await context.db.queryHandler.fetch(PersonQuery.byId(args.personId))
+		if (!person) {
+			return createErrorResponse('PERSON_NOT_FOUND', 'Person not found')
+		}
+		if (args.email === null) {
+			return createErrorResponse('INVALID_EMAIL_FORMAT', 'E-mail address cannot be null.')
+		}
+
+		await context.requireAccess({
+			scope: new IdentityScope(person.identity_id),
+			action: PermissionActions.PERSON_CHANGE_PROFILE,
+			message: 'You are not allowed to change a profile',
+		})
+		const result = await this.personManager.changeProfile(context.db, person, {
+			email: args.email,
+			name: args.name === '' ? null : args.name,
+		})
+
+		if (!result.ok) {
+			return createErrorResponse(result.error, result.errorMessage)
+		}
+
+		return { ok: true }
+	}
+
 
 	async changeMyProfile(
 		parent: unknown,
@@ -28,7 +66,7 @@ export class ChangeProfileMutationResolver implements MutationResolvers {
 			message: 'You are not allowed to change profile',
 		})
 
-		const result = await this.personManager.changeMyProfile(context.db, person, {
+		const result = await this.personManager.changeProfile(context.db, person, {
 			email: args.email,
 			name: args.name === '' ? null : args.name,
 		})

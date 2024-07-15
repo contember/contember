ba69d6729437e478597ee6ea708f45af79f50221
David Matejka <matej21@matej21.cz>
2024-07-15 18:43:43 +0200

    fix(create): fix resourcesDir path

--- packages/create/src/lib/pathUtils.ts
+++ packages/create/src/lib/pathUtils.ts
@@ -1,4 +1,4 @@
 import { join } from 'node:path'
 
-export const packageRoot = process.env.CONTEMBER_CLI_PACKAGE_ROOT || join(__dirname, '../../')
+export const packageRoot = process.env.CONTEMBER_CLI_PACKAGE_ROOT || join(__dirname, __dirname.endsWith('dist/src/lib') ? '../../../' : '../../')
 export const resourcesDir = join(packageRoot, './resources')

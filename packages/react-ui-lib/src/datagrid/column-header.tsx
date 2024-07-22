9e7a243a80f672b87818fa63891947faca01ecee
David Matejka <matej21@matej21.cz>
2024-07-22 18:34:25 +0200

    feat(react-ui-lib): ability to show filters in column header popover

--- packages/react-ui-lib/src/datagrid/column-header.tsx
+++ packages/react-ui-lib/src/datagrid/column-header.tsx
@@ -1,25 +1,34 @@
 import { cn } from '../utils'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown'
 import { Button } from '../ui/button'
 import { ReactNode } from 'react'
-import { ArrowDownAZIcon, ArrowUpDownIcon, ArrowUpZaIcon, EyeOffIcon } from 'lucide-react'
-import { DataViewSortingSwitch, DataViewSortingTrigger, DataViewVisibilityTrigger } from '@contember/react-dataview'
+import { ArrowDownAZIcon, ArrowUpDownIcon, ArrowUpZaIcon, EyeOffIcon, FilterIcon } from 'lucide-react'
+import { DataViewSortingSwitch, DataViewSortingTrigger, DataViewVisibilityTrigger, useDataViewFilter } from '@contember/react-dataview'
 import { dict } from '../dict'
+import { Component } from '@contember/interface'
+import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
 
 
-export function DataGridColumnHeader<TData, TValue>({ sortingField, hidingName, children }: {
+export const DataGridColumnHeader = Component(({ sortingField, hidingName, children, filter, filterName }: {
 	sortingField?: string
 	hidingName?: string
+	filterName?: string
+	filter?: ReactNode
 	children: ReactNode
-}) {
-	if (!sortingField && !hidingName) {
+}) => {
+	let hasFilter = false
+	if (filterName) {
+		const [, , { isEmpty }] = useDataViewFilter(filterName)
+		hasFilter = !isEmpty
+	}
+
+	if (!sortingField && !hidingName && !filter) {
 		return <div>{children}</div>
 	}
 
 	return (
 		<div className={cn('flex items-center space-x-2')}>
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
+			<Popover>
+				<PopoverTrigger asChild>
 					<Button
 						variant="ghost"
 						size="sm"
@@ -29,37 +38,52 @@ export function DataGridColumnHeader<TData, TValue>({ sortingField, hidingName, 
 
 						{sortingField && <DataViewSortingSwitch
 							field={sortingField}
-							asc={<ArrowDownAZIcon className={'h-4 w-4'} />}
-							desc={<ArrowUpZaIcon className={'h-4 w-4'} />}
+							asc={<ArrowDownAZIcon className={'h-4 w-4 text-blue-600'} />}
+							desc={<ArrowUpZaIcon className={'h-4 w-4 text-blue-600'} />}
 							none={<ArrowUpDownIcon className={'h-4 w-4'} />}
 						/>}
+						{hasFilter && <FilterIcon className="h-4 w-4 text-blue-600"/>}
 					</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent align="start">
-					{sortingField && <>
-						<DataViewSortingTrigger field={sortingField} action="toggleAsc">
-							<DropdownMenuItem className={'data-[active]:text-gray-500 cursor-pointer'}>
-								<ArrowDownAZIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-								{dict.datagrid.columnAsc}
-							</DropdownMenuItem>
-						</DataViewSortingTrigger>
-						<DataViewSortingTrigger field={sortingField} action="toggleDesc">
-							<DropdownMenuItem className={'data-[active]:text-gray-500 cursor-pointer'}>
-								<ArrowUpZaIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-								{dict.datagrid.columnDesc}
-							</DropdownMenuItem>
-						</DataViewSortingTrigger>
-					</>}
-					{hidingName &&
-						<DataViewVisibilityTrigger name={hidingName} value={false}>
-							<DropdownMenuItem>
-								<EyeOffIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
-								{dict.datagrid.columnHide}
-							</DropdownMenuItem>
-						</DataViewVisibilityTrigger>
-					}
-				</DropdownMenuContent>
-			</DropdownMenu>
+				</PopoverTrigger>
+				<PopoverContent className="p-3">
+					<div className="flex flex-col gap-1">
+
+						{(sortingField || hidingName) && <>
+							<div className="flex gap-2 border rounded p-1">
+								{sortingField && <><DataViewSortingTrigger field={sortingField} action="toggleAsc">
+									<Button variant="ghost" className={'data-[active]:text-blue-600 cursor-pointer flex-1'} size="sm">
+										<ArrowDownAZIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+										{dict.datagrid.columnAsc}
+									</Button>
+								</DataViewSortingTrigger>
+								<DataViewSortingTrigger field={sortingField} action="toggleDesc">
+									<Button variant="ghost" className={'data-[active]:text-blue-600 cursor-pointer flex-1'} size="sm">
+										<ArrowUpZaIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+										{dict.datagrid.columnDesc}
+									</Button>
+								</DataViewSortingTrigger>
+								</>}
+								{hidingName &&
+									<DataViewVisibilityTrigger name={hidingName} value={false}>
+										<Button variant="ghost" size="sm">
+											<EyeOffIcon className="mr-2 h-3.5 w-3.5 text-muted-foreground/70" />
+											{dict.datagrid.columnHide}
+										</Button>
+									</DataViewVisibilityTrigger>
+								}
+							</div>
+						</>}
+
+						{filter && <>
+							<div className="py-2">
+								{filter}
+							</div>
+						</>}
+					</div>
+				</PopoverContent>
+			</Popover>
 		</div>
 	)
-}
+}, ({ filter }) => {
+	return <>{filter}</>
+})

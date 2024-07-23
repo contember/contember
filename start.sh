2b68b63e4724c76917bc87975fb6631b2c9358af
David Matejka <matej21@matej21.cz>
2024-07-23 15:01:29 +0200

    chore: assume-unchanged for generated client files

--- start.sh
+++ start.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
-
+set -e
 yarn install
 yarn pre-build
+
+git update-index --assume-unchanged packages/graphql-client-system/src/generated/Async.ts
+git update-index --assume-unchanged packages/graphql-client-system/src/generated/CommonTypes.ts
+git update-index --assume-unchanged packages/graphql-client-system/src/generated/enums/index.ts
+git update-index --assume-unchanged packages/graphql-client-system/src/generated/fetchers/index.ts
+git update-index --assume-unchanged packages/graphql-client-system/src/generated/inputs/index.ts
+git update-index --assume-unchanged packages/graphql-client-tenant/src/generated/Async.ts
+git update-index --assume-unchanged packages/graphql-client-tenant/src/generated/CommonTypes.ts
+git update-index --assume-unchanged packages/graphql-client-tenant/src/generated/enums/index.ts
+git update-index --assume-unchanged packages/graphql-client-tenant/src/generated/fetchers/index.ts
+git update-index --assume-unchanged packages/graphql-client-tenant/src/generated/inputs/index.ts
+
 test -f docker-compose.override.yaml || cp docker-compose.override.dist.yaml docker-compose.override.yaml
 yarn contember migrations:execute --yes
 docker-compose up -d

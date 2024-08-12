8aeb79aeda507ba4c8016149d9fc8deecfaca084
David Matejka <matej21@matej21.cz>
2024-08-12 10:57:04 +0200

    fixup! feat(binding): introduce fetchData on binding operations

--- packages/binding-common/src/types/BindingOperations.ts
+++ packages/binding-common/src/types/BindingOperations.ts
@@ -7,16 +7,20 @@ import type { Environment } from '../environment'
 import { ReceivedDataTree } from './QueryRequestResponse'
 import { MarkerTreeRoot } from '../markers'
 
+export type FetchData<Node> = (fragment: Node, options?: { signal?: AbortSignal; environment?: Environment }) => Promise<{
+	data: ReceivedDataTree
+	markerTreeRoot: MarkerTreeRoot
+} | undefined>
+
+export type BatchDeferredUpdates = (performUpdates: (bindingOperations: BatchUpdatesOptions) => void) => void
+
 export interface BindingOperations<Node> extends AsyncBatchUpdatesOptions {
 	// addEventListener: ...
 	getTreeFilters: () => TreeFilter[]
 
 	extendTree: ExtendTree<Node>
-	fetchData: (fragment: Node, options?: { signal?: AbortSignal; environment?: Environment }) => Promise<{
-		data: ReceivedDataTree
-		markerTreeRoot: MarkerTreeRoot
-	} | undefined>
-	batchDeferredUpdates: (performUpdates: (bindingOperations: BatchUpdatesOptions) => void) => void
+	fetchData: FetchData<Node>
+	batchDeferredUpdates: BatchDeferredUpdates
 	// discardSubTree: ...
 	persist: Persist
 }

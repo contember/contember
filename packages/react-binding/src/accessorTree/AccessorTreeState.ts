f6b53973a2bbb08710dc76abdfaa84dccda1a184
David Matejka <matej21@matej21.cz>
2024-08-12 11:14:36 +0200

    refactor(react-binding): simplify accessor state management

--- packages/react-binding/src/accessorTree/AccessorTreeState.ts
+++ packages/react-binding/src/accessorTree/AccessorTreeState.ts
@@ -1,25 +1,22 @@
 import type { TreeRootAccessor } from '@contember/binding'
-import { DataBinding, Environment } from '@contember/binding'
+import { Environment } from '@contember/binding'
 import { ReactNode } from 'react'
 import { GraphQlClientError } from '@contember/react-client'
 
 export interface InitializingAccessorTreeState {
 	name: 'initializing'
 	environment: Environment
-	binding?: DataBinding<ReactNode>
 }
 
 export interface InitializedAccessorTreeState {
 	name: 'initialized'
 	environment: Environment
-	binding: DataBinding<ReactNode>
 	data: TreeRootAccessor<ReactNode>
 }
 
 export interface ErrorAccessorTreeState {
 	name: 'error'
 	environment: Environment
-	binding: DataBinding<ReactNode>
 	error: GraphQlClientError
 }
 

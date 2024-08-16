0914ed6ab3e55f5758a095ef920f828d082e3db0
David Matejka <matej21@matej21.cz>
2024-08-16 12:43:34 +0200

    feat(react-routing): introduce useAddRequestChangeListener which allows to intercept redirect

--- packages/react-routing/src/RequestContext.tsx
+++ packages/react-routing/src/RequestContext.tsx
@@ -1,7 +1,8 @@
-import { createContext, FC, ReactNode, useCallback, useContext, useEffect, useState } from 'react'
+import { createContext, FC, ReactNode, useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { PageRequest, RequestParameters, RequestState } from './types'
 import { RoutingContextValue, useRouting } from './RoutingContext'
 import { pathToRequestState, requestStateToPath } from './urlMapper'
+import { createRequiredContext } from '@contember/react-utils'
 
 
 export const requestChangeFactory = <P extends RequestParameters>(pageName: string, parameters?: P) =>
@@ -18,18 +19,70 @@ CurrentRequestContext.displayName = 'CurrentRequestContext'
 export const PushRequestContext = createContext<(req: RequestState) => void>(() => {})
 PushRequestContext.displayName = 'PushRequestContext'
 
+export class RequestChangeEvent {
+	constructor(
+		public request: RequestState | undefined,
+	) {}
+}
+export type RequestChangeHandler = (event: RequestChangeEvent) => void
+
+const [AddRequestListenerContext, useAddRequestListenerContext] = createRequiredContext<(handler: RequestChangeHandler) => () => void>('AddRequestListenerContext')
+
 export const useCurrentRequest = () => useContext(CurrentRequestContext)
 export const usePushRequest = () => useContext(PushRequestContext)
+
+export const useAddRequestChangeListener = (listener: RequestChangeHandler) => {
+	const add = useAddRequestListenerContext()
+	useEffect(() => {
+		const remove = add(listener)
+		return remove
+	}, [add, listener])
+}
 
 export const RequestProvider: FC<{ children: ReactNode }> = ({ children }) => {
 	const routing = useRouting()
 	const [request, setRequest] = useState<RequestState>(() => populateRequest(routing, window.location))
+	const [listeners, setListeners] = useState<RequestChangeHandler[]>([])
+
+	const listenersRef = useRef(listeners)
+	listenersRef.current = listeners
+
+	const requestRef = useRef(request)
+	requestRef.current = request
+
+	const fireListeners = useCallback(async (request: RequestState) => {
+		const event = new RequestChangeEvent(request)
+		for (const listener of listenersRef.current) {
+			await listener(event)
+			if (event.request === undefined) {
+				return
+			}
+		}
+		return event.request
+	}, [])
+
+	const pushHistoryState = useCallback((request: RequestState) => {
+		const newUrl = requestStateToPath(routing, request)
+		if (newUrl === window.location.pathname + window.location.search) {
+			return
+		}
+		window.history.pushState({}, document.title, newUrl)
+	}, [routing])
 
 	useEffect(
 		() => {
-			const onPopState = (e: PopStateEvent) => {
+			const onPopState = async (e: PopStateEvent) => {
 				e.preventDefault()
-				setRequest(populateRequest(routing, window.location))
+				const newRequest = populateRequest(routing, window.location)
+				const finalRequest = await fireListeners(newRequest)
+				if (finalRequest === undefined) {
+					pushHistoryState(requestRef.current)
+				} else if (finalRequest !== newRequest) {
+					pushHistoryState(finalRequest)
+					setRequest(finalRequest)
+				} else {
+					setRequest(newRequest)
+				}
 			}
 
 			window.addEventListener('popstate', onPopState)
@@ -38,25 +91,34 @@ export const RequestProvider: FC<{ children: ReactNode }> = ({ children }) => {
 				window.removeEventListener('popstate', onPopState)
 			}
 		},
-		[routing],
+		[fireListeners, pushHistoryState, routing],
 	)
 
 	const pushRequest = useCallback(
-		(request: RequestState) => {
-			setRequest(request)
-			const newUrl = requestStateToPath(routing, request)
-			if (newUrl === window.location.pathname + window.location.search) {
+		async (request: RequestState) => {
+			const newRequest = await fireListeners(request)
+			if (newRequest === undefined) {
 				return
 			}
-			window.history.pushState({}, document.title, newUrl)
+			setRequest(newRequest)
+			pushHistoryState(newRequest)
 		},
-		[routing],
+		[fireListeners, pushHistoryState],
 	)
+
+	const addListener = useCallback((handler: RequestChangeHandler) => {
+		setListeners(current => [...current, handler])
+		return () => {
+			setListeners(current => current.filter(h => h !== handler))
+		}
+	}, [])
 
 	return (
 		<CurrentRequestContext.Provider value={request}>
 			<PushRequestContext.Provider value={pushRequest}>
-				{children}
+				<AddRequestListenerContext.Provider value={addListener}>
+					{children}
+				</AddRequestListenerContext.Provider>
 			</PushRequestContext.Provider>
 		</CurrentRequestContext.Provider>
 	)

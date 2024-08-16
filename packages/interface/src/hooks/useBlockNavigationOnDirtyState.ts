c3449b628451f85cbb86c66b518d22a652bebcf1
David Matejka <matej21@matej21.cz>
2024-08-16 14:00:32 +0200

    feat(interface): add useBlockNavigationOnDirtyState hook

---
+++ packages/interface/src/hooks/useBlockNavigationOnDirtyState.ts
@@ -0,0 +1,41 @@
+import { ErrorPersistResult, SuccessfulPersistResult, useDirtinessState, useMutationState, usePersist } from '@contember/react-binding'
+import { useCallback, useRef } from 'react'
+import { useRegisterRequestChangeListener } from '@contember/react-routing'
+
+export type BlockNavigationOnDirtyStateResult = 'save' | 'discard' | 'cancel'
+
+export const useBlockNavigationOnDirtyState = (handler: () => Promise<BlockNavigationOnDirtyStateResult>, options?: {
+	onPersistSuccess?: (result: SuccessfulPersistResult) => void
+	onPersistError?: (result: ErrorPersistResult) => void
+}) => {
+	const isDirty = useDirtinessState()
+	const isDirtyRef = useRef(isDirty)
+	isDirtyRef.current = isDirty
+	const isMutating = useMutationState()
+	const isMutatingRef = useRef(isMutating)
+	isMutatingRef.current = isMutating
+	const persist = usePersist()
+
+	useRegisterRequestChangeListener(useCallback(async event => {
+		if (!isDirtyRef.current || isMutatingRef.current) {
+			return
+		}
+		const result = await handler()
+		if (result === 'cancel') {
+			event.request = undefined
+			return
+		}
+
+		if (result === 'save') {
+			try {
+				const result = await persist()
+				options?.onPersistSuccess?.(result)
+			} catch (e: any) {
+				options?.onPersistError?.(e)
+
+				event.request = undefined
+				return
+			}
+		}
+	}, [handler, options, persist]))
+}

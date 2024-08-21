efc636ee9f51c8b12f0fd7d4c4b2f7406f123396
David Matejka <matej21@matej21.cz>
2024-08-21 14:34:15 +0200

    refactor(react-binding): remove legacy entity sorting utils

--- packages/react-binding/src/accessorSorting/index.ts
+++ packages/react-binding/src/accessorSorting/index.ts
@@ -1,9 +1,7 @@
 export * from './addEntityAtIndex'
 // Deliberately not exporting errors
-export * from './moveEntity'
 export * from './repairEntitiesOrder'
 export * from './SetOrderFieldOnCreate'
 export * from './sortEntities'
-export * from './useSortedEntities'
 
 export type { SortedEntities } from './SortedEntities'

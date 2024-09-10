36fe964ce6b5de0967aa8407dc4c5f346e1d3f02
David Matejka <matej21@matej21.cz>
2024-09-10 13:47:27 +0200

    refactor(react-binding): utililze useEntity(List)SubTree hooks in Entity(List)SubTree components

--- packages/react-binding/src/coreComponents/EntitySubTree.tsx
+++ packages/react-binding/src/coreComponents/EntitySubTree.tsx
@@ -1,18 +1,11 @@
 import { useConstantValueInvariant } from '@contember/react-utils'
-import { ReactElement, ReactNode, useCallback } from 'react'
-import { useAccessorUpdateSubscription, useEntitySubTreeParameters, useGetEntitySubTree } from '../accessorPropagation'
-import { PRIMARY_KEY_NAME } from '@contember/binding'
-import { Environment } from '@contember/binding'
-import { MarkerFactory } from '@contember/binding'
-import type {
-	SugaredQualifiedSingleEntity,
-	SugaredUnconstrainedQualifiedSingleEntity,
-	TreeRootId,
-} from '@contember/binding'
+import { ReactElement, ReactNode } from 'react'
+import { useEntitySubTree } from '../accessorPropagation'
+import type { SugaredQualifiedSingleEntity, SugaredUnconstrainedQualifiedSingleEntity, TreeRootId } from '@contember/binding'
+import { Environment, MarkerFactory, PRIMARY_KEY_NAME, TreeNodeEnvironmentFactory } from '@contember/binding'
 import { Component } from './Component'
 import { Entity } from './Entity'
 import { Field } from './Field'
-import { TreeNodeEnvironmentFactory } from '@contember/binding'
 import { SetOrderFieldOnCreate, SetOrderFieldOnCreateOwnProps } from '../accessorSorting'
 
 export interface EntitySubTreeAdditionalProps {
@@ -45,15 +38,7 @@ export type EntitySubTreeProps<EntityProps> =
 export const EntitySubTree = Component(
 	<EntityProps extends {}>(props: EntitySubTreeProps<EntityProps>) => {
 		useConstantValueInvariant(props.isCreating, 'EntitySubTree: cannot update isCreating')
-
-		const getSubTree = useGetEntitySubTree()
-		const parameters = useEntitySubTreeParameters(props)
-		const getAccessor = useCallback(
-			() => getSubTree(parameters, props.treeRootId),
-			[getSubTree, parameters, props.treeRootId],
-		)
-		const accessor = useAccessorUpdateSubscription(getAccessor)
-
+		const accessor = useEntitySubTree(props, ...props.treeRootId ? [props.treeRootId] : [])
 		// TODO revive top-level hasOneRelationPath
 		// {parameters.value.hasOneRelationPath.length ? (
 		// 	<HasOne field={parameters.value.hasOneRelationPath}>{props.children}</HasOne>

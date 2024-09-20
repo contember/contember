921a0f1cda6c2a721ef907a8e6ef04852833e08f
jonasnobile <mesons_rates0p@icloud.com>
2024-09-20 10:50:25 +0200

    fix(react-ui-lib): enable scroll in sheet

--- packages/react-ui-lib/src/ui/sheet.tsx
+++ packages/react-ui-lib/src/ui/sheet.tsx
@@ -17,7 +17,7 @@ export const SheetOverlay = uic(SheetPrimitive.Overlay, {
 })
 
 export const SheetContent = uic(SheetPrimitive.Content, {
-	baseClass: 'fixed z-50 gap-4 bg-background p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+	baseClass: 'fixed z-50 gap-4 bg-background p-6 shadow-lg overflow-auto transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
 	variants: {
 		side: {
 			top: 'inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',

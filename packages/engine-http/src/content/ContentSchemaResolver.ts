ef3a018716d81b23f693540a9aa314d0329e32c1
David Matejka <matej21@matej21.cz>
2024-10-03 14:29:22 +0200

    perf(engine-http): handle concurrency in ContentSchemaResolver by serializing buildSchema calls

--- packages/engine-http/src/content/ContentSchemaResolver.ts
+++ packages/engine-http/src/content/ContentSchemaResolver.ts
@@ -3,26 +3,47 @@ import { filterSchemaByStage } from '@contember/schema-utils'
 
 
 export class ContentSchemaResolver {
-	private baseSchemaCache: VersionedSchema | undefined
+	private prevSchema: VersionedSchema | undefined
+	private schemaPromise: Promise<VersionedSchema> | undefined
 	private stageSchemaCache: { [stage: string]: VersionedSchema } = {}
 
 	constructor(private readonly schemaVersionBuilder: SchemaVersionBuilder) {
 	}
 
 	public clearCache() {
-		this.baseSchemaCache = undefined
+		this.prevSchema = undefined
+		this.schemaPromise = undefined
 		this.stageSchemaCache = {}
 	}
 
 	public async getSchema(db: DatabaseContext, stage?: string): Promise<VersionedSchema> {
-		const prevBaseSchema = this.baseSchemaCache
-		this.baseSchemaCache = await this.schemaVersionBuilder.buildSchema(db, prevBaseSchema)
-		if (prevBaseSchema !== this.baseSchemaCache) {
+		const schema = await this.getBaseSchema(db)
+		if (!stage) {
+			return schema
+		}
+		return this.stageSchemaCache[stage] ??= filterSchemaByStage(schema, stage)
+	}
+
+	private async getBaseSchema(db: DatabaseContext): Promise<VersionedSchema> {
+		const prevSchema = this.prevSchema
+
+		if (!this.schemaPromise) {
+			this.schemaPromise = (async () => {
+				try {
+					const newSchema = await this.schemaVersionBuilder.buildSchema(db, prevSchema)
+					this.prevSchema = newSchema
+					return newSchema
+				} finally {
+					this.schemaPromise = undefined
+				}
+			})()
+		}
+
+		const newSchema = await this.schemaPromise
+
+		if (prevSchema !== newSchema) {
 			this.stageSchemaCache = {}
 		}
-		if (!stage) {
-			return this.baseSchemaCache
-		}
-		return this.stageSchemaCache[stage] ??= filterSchemaByStage(this.baseSchemaCache, stage)
+		return newSchema
 	}
 }

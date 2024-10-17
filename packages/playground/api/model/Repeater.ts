8c2519b7aed3306c6309528889d1885df377a718
David Matejka <matej21@matej21.cz>
2024-10-17 17:17:44 +0200

    feat(playground): add more repeater examples

--- packages/playground/api/model/Repeater.ts
+++ packages/playground/api/model/Repeater.ts
@@ -1,6 +1,16 @@
 import { c } from '@contember/schema-definition'
 
+export class RepeaterRoot {
+	unique = c.enumColumn(c.createEnum('unique')).unique().notNull()
+	items = c.oneHasMany(RepeaterItem, 'root').orderBy('order')
+}
+
 export class RepeaterItem {
+	root = c.manyHasOne(RepeaterRoot, 'items')
 	title = c.stringColumn().notNull()
+	relation = c.manyHasOne(RepeaterRelation)
 	order = c.intColumn()
+}
+export class RepeaterRelation {
+	name = c.stringColumn().notNull()
 }

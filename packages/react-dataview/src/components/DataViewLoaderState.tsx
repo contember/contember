af99152edfefc8924c8186fba2832b013509d8ce
David Matejka <matej21@matej21.cz>
2024-11-12 16:38:53 +0100

    fix(react-dataview): fix wrong props interface name

--- packages/react-dataview/src/components/DataViewLoaderState.tsx
+++ packages/react-dataview/src/components/DataViewLoaderState.tsx
@@ -1,14 +1,14 @@
 import React, { ReactNode } from 'react'
 import { useDataViewLoaderState } from '../contexts'
 
-export interface DataViewEachRowProps {
+export interface DataViewLoaderStateProps {
 	children: ReactNode
 	loaded?: boolean
 	refreshing?: boolean
 	initial?: boolean
 }
 
-export const DataViewLoaderState = ({ children, ...props }: DataViewEachRowProps) => {
+export const DataViewLoaderState = ({ children, ...props }: DataViewLoaderStateProps) => {
 	const state = useDataViewLoaderState()
 	return props[state] ? <>{children}</> : null
 }

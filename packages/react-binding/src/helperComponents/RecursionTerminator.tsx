5cad2480836bea1f0e78ece3bbec3736354bb19b
David Matejka <matej21@matej21.cz>
2024-11-15 17:33:28 +0100

    feat(react-binding): introduce recursion terminator component

---
+++ packages/react-binding/src/helperComponents/RecursionTerminator.tsx
@@ -0,0 +1,73 @@
+import {
+	Environment,
+	SugarableHasOneRelation,
+	SugaredRelativeEntityList,
+	SugaredRelativeSingleEntity,
+	TokenRegExps,
+} from '@contember/binding-common'
+import { ReactNode, useMemo } from 'react'
+import { Component } from '../coreComponents'
+
+export interface RecursionTerminatorOptions {
+	shouldTerminate?: (args: { node: Environment.AnyNode; field: string; environment: Environment }) => boolean | undefined
+}
+
+export const recursionTerminatorEnvironmentExtension = Environment.createExtension((options: RecursionTerminatorOptions | undefined) => {
+	return options
+})
+
+export interface RecursionTerminatorProps {
+	field:
+		| { kind: 'hasOne'; field: SugaredRelativeSingleEntity['field'] }
+		| { kind: 'hasMany'; field: SugaredRelativeEntityList['field'] }
+	children: ReactNode
+}
+
+export const RecursionTerminator = Component<RecursionTerminatorProps>(({ field, children }, env) => {
+	const isRecursion = useMemo(() => {
+		return recursionDetector(field, env)
+	}, [field, env])
+
+	return !isRecursion ? children : null
+}, ({ field, children }, env) => {
+	const isRecursion = recursionDetector(field, env)
+	return !isRecursion ? children : null
+})
+
+const recursionDetector = (field: RecursionTerminatorProps['field'], env: Environment) => {
+	const firstPathItem = getFirstPathItem(field)
+	if (!firstPathItem) {
+		return false
+	}
+	const shouldTerminate = env.getExtension(recursionTerminatorEnvironmentExtension)?.shouldTerminate
+	const shouldTerminateResult = shouldTerminate?.({ node: env.getSubTreeNode(), field: firstPathItem, environment: env })
+	if (shouldTerminateResult !== undefined) {
+		return shouldTerminateResult
+	}
+
+	const node = env.getSubTreeNode()
+	const otherSide = node.type === 'entity-list' || node.type === 'entity' ? (node.field.ownedBy ?? node.field.inversedBy) : undefined
+	if (!otherSide) {
+		return false
+	}
+	if (!firstPathItem) {
+		return false
+	}
+	return otherSide === firstPathItem
+}
+
+const getFirstPathItem = (field: RecursionTerminatorProps['field']) => {
+	if (typeof field.field === 'string') {
+		return field.field.match(TokenRegExps.identifier)?.[0]
+	}
+	const getFromHasOne = (relation: SugarableHasOneRelation[] | SugarableHasOneRelation | undefined) => {
+		if (Array.isArray(relation)) {
+			return relation?.[0].field
+		}
+		return relation?.field
+	}
+	if (field.kind === 'hasMany') {
+		return getFromHasOne(field.field.hasOneRelationPath) ?? field.field.hasManyRelation.field
+	}
+	return getFromHasOne(field.field)
+}
---
+++ packages/react-binding/src/helperComponents/RecursionTerminatorPortal.tsx
@@ -0,0 +1,35 @@
+import { BindingError, Environment, TreeNodeEnvironmentFactory } from '@contember/binding-common'
+import { Component } from '../coreComponents'
+import { recursionTerminatorEnvironmentExtension, RecursionTerminatorProps } from './RecursionTerminator'
+import { EnvironmentMiddleware } from '../accessorPropagation'
+
+export const RecursionTerminatorPortal = Component<RecursionTerminatorProps>(({ field, children }) => {
+	return <EnvironmentMiddleware args={[field]} create={createEnvWithExtension} >{children}</EnvironmentMiddleware>
+})
+
+const createEnvWithExtension = (env: Environment, [{ field, kind }]: [field: RecursionTerminatorProps['field']]) => {
+	const envForField = kind === 'hasOne'
+		? TreeNodeEnvironmentFactory.createEnvironmentForEntity(env, { field })
+		: TreeNodeEnvironmentFactory.createEnvironmentForEntityList(env, { field })
+
+	const parentNode = envForField.getSubTreeNode()
+	if (!(parentNode.type === 'entity-list' || parentNode.type === 'entity')) {
+		throw new BindingError()
+	}
+	return env.withExtension(recursionTerminatorEnvironmentExtension, {
+		shouldTerminate: args => {
+			if (args.environment.getParent().getSubTreeNode() !== env.getSubTreeNode()) {
+				return undefined
+			}
+			const currentNode = args.node
+			if (currentNode.type !== 'subtree-entity' || currentNode.entity.name !== parentNode.field.targetEntity) {
+				return undefined
+			}
+			const inverseField = parentNode.field.inversedBy ?? parentNode.field.ownedBy
+			if (!inverseField) {
+				return false
+			}
+			return args.field === inverseField
+		},
+	})
+}

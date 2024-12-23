a0c1963d7a8a1bc2eea0d58c6c2f07e542d55f7d
jonasnobile <mesons_rates0p@icloud.com>
2024-12-23 16:55:57 +0100

    feat(react-ui-lib): add pre-defined data grid date ranges

--- packages/react-ui-lib/src/datagrid/filters/date.tsx
+++ packages/react-ui-lib/src/datagrid/filters/date.tsx
@@ -1,18 +1,29 @@
+import { Component } from '@contember/interface'
+import {
+	DataViewDateFilter,
+	DataViewDateFilterInput,
+	DataViewDateFilterProps,
+	DataViewDateFilterResetTrigger,
+	DataViewNullFilterTrigger,
+	DateRangeFilterArtifacts,
+	useDataViewFilter,
+	useDataViewFilterName,
+} from '@contember/react-dataview'
+import { XIcon } from 'lucide-react'
 import * as React from 'react'
 import { ReactNode, useId } from 'react'
-import { DataViewDateFilter, DataViewDateFilterInput, DataViewDateFilterProps, DataViewDateFilterResetTrigger, DataViewNullFilterTrigger, DateRangeFilterArtifacts, useDataViewFilter, useDataViewFilterName } from '@contember/react-dataview'
-import { Component } from '@contember/interface'
-import { Popover, PopoverContent, PopoverTrigger } from '../../ui/popover'
-import { DataGridActiveFilterUI, DataGridFilterSelectTriggerUI, DataGridSingleFilterUI } from '../ui'
-import { Input } from '../../ui/input'
-import { formatDate } from '../../formatting/formatting'
+import { Temporal } from 'temporal-polyfill'
 import { dict } from '../../dict'
+import { formatDate } from '../../formatting'
+import { Button } from '../../ui/button'
+import { Input } from '../../ui/input'
+import { Label } from '../../ui/label'
+import { Popover, PopoverContent, PopoverTrigger } from '../../ui/popover'
+import { cn } from '../../utils'
+import { DataViewFieldLabel } from '../labels'
+import { DataGridActiveFilterUI, DataGridFilterSelectTriggerUI, DataGridSingleFilterUI } from '../ui'
 import { DataGridNullFilter } from './common'
 import { DataGridFilterMobileHiding } from './mobile'
-import { Button } from '../../ui/button'
-import { Label } from '../../ui/label'
-import { XIcon } from 'lucide-react'
-import { DataViewFieldLabel } from '../labels'
 
 export type DataGridPredefinedDateRange = { start: string; end: string; label: ReactNode }
 
@@ -97,6 +108,27 @@ export const createDataGridDateRange = (label: ReactNode, dayDeltaStart: number,
 		start: `${start.getFullYear()}-${String(start.getMonth() + 1).padStart(2, '0')}-${String(start.getDate()).padStart(2, '0')}`,
 		end: `${end.getFullYear()}-${String(end.getMonth() + 1).padStart(2, '0')}-${String(end.getDate()).padStart(2, '0')}`,
 	}
+}
+
+const now = Temporal.Now.plainDateISO()
+const lastMonth = now.subtract({ months: 1 })
+const nextMonth = now.add({ months: 1 })
+const lastYear = now.subtract({ years: 1 })
+const nextYear = now.add({ years: 1 })
+
+export const dataGridRanges = {
+	yesterday: createDataGridDateRange(dict.datagrid.yesterday, -1, -1),
+	today: createDataGridDateRange(dict.datagrid.today, 0, 0),
+	tomorrow: createDataGridDateRange(dict.datagrid.tomorrow, 1, 1),
+	thisWeek: createDataGridDateRange(dict.datagrid.thisWeek, 1 - now.dayOfWeek, 6 - now.dayOfWeek),
+	lastWeek: createDataGridDateRange(dict.datagrid.lastWeek, 1 - now.dayOfWeek - 7, 6 - now.dayOfWeek - 7),
+	nextWeek: createDataGridDateRange(dict.datagrid.nextWeek, 1 - now.dayOfWeek + 7, 6 - now.dayOfWeek + 7),
+	thisMonth: createDataGridDateRange(dict.datagrid.thisMonth, 1 - now.day, now.daysInMonth - now.day),
+	lastMonth: createDataGridDateRange(dict.datagrid.lastMonth, 1 - now.day - lastMonth.daysInMonth, -now.day),
+	nextMonth: createDataGridDateRange(dict.datagrid.nextMonth, 1 - now.day + nextMonth.daysInMonth, now.daysInMonth - now.day + nextMonth.daysInMonth),
+	thisYear: createDataGridDateRange(dict.datagrid.thisYear, 1 - now.dayOfYear, now.daysInYear - now.dayOfYear),
+	lastYear: createDataGridDateRange(dict.datagrid.lastYear, 1 - now.dayOfYear - lastYear.daysInYear, -now.dayOfYear),
+	nextYear: createDataGridDateRange(dict.datagrid.nextYear, 1 - now.dayOfYear + nextYear.daysInYear, now.daysInYear - now.dayOfYear + nextYear.daysInYear),
 }
 
 const defaultRanges = [createDataGridDateRange(dict.datagrid.today, 0, 0)]

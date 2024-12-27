80cb7c92f7a1c0182d01bfd3b9d003c62c5f190b
David Matejka <matej21@matej21.cz>
2024-12-27 14:25:35 +0100

    feat(binding): add global persistSuccess and persistError events

---
+++ packages/react-binding/src/entityEvents/useDataBindingEvent.ts
@@ -0,0 +1,16 @@
+import { DataBindingEventListenerMap } from '@contember/binding'
+import { useEffect } from 'react'
+import { useBindingOperations } from '../accessorPropagation'
+import { useReferentiallyStableCallback } from '@contember/react-utils'
+
+export const useDataBindingEvent = <Type extends keyof DataBindingEventListenerMap>(
+	event: Type,
+	listener: DataBindingEventListenerMap[Type],
+): void => {
+	const { addEventListener } = useBindingOperations()
+	const listenerStable = useReferentiallyStableCallback(listener)
+	useEffect(() => {
+		const unsubscribe = addEventListener(event, listenerStable)
+		return () => unsubscribe()
+	}, [addEventListener, event, listenerStable])
+}

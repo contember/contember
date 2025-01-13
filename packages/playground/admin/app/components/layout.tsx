1eb12cf400b50f479c5505c20b9700b4f43aaadf
jonasnobile <mesons_rates0p@icloud.com>
2025-01-13 15:25:23 +0100

    feat(playground): use new layout with update new navigation

--- packages/playground/admin/app/components/layout.tsx
+++ packages/playground/admin/app/components/layout.tsx
@@ -1,33 +1,39 @@
-import * as React from 'react'
+import { Link } from '@contember/interface'
 import { memo, PropsWithChildren } from 'react'
 import { IdentityLoader } from '~/lib/binding'
-import { LayoutComponent, Slots } from '~/lib/layout'
-import { Navigation } from './navigation'
-import { Link } from '@contember/interface'
+import { Slots } from '~/lib/layout'
+import { LayoutComponent } from '~/lib/layout/layout'
 import { Logo } from './logo'
+import { Navigation, UserNavigation } from './navigation'
 
-export const Layout = memo(({ children }: PropsWithChildren) => (
-	<IdentityLoader>
-		<LayoutComponent>
+export const Layout = memo(({ children }: PropsWithChildren) => {
 
-			<Slots.Logo>
-				<Link to="index">
-					<Logo />
-				</Link>
-			</Slots.Logo>
+	return (
+		<IdentityLoader>
+			<LayoutComponent>
+				<Slots.Logo>
+					<Link to="index">
+						<Logo />
+					</Link>
+				</Slots.Logo>
 
-			<Slots.Navigation>
-				<Navigation />
-			</Slots.Navigation>
+				<Slots.Navigation>
+					<Navigation />
+				</Slots.Navigation>
 
-			<Slots.Footer>
-				<p>
-					<small>Created with <a className="content-link" href="https://www.contember.com/">AI-assisted Contember Studio</a></small>
-				</p>
-			</Slots.Footer>
+				<Slots.UserNavigation>
+					<UserNavigation />
+				</Slots.UserNavigation>
 
-			{children}
-		</LayoutComponent>
-	</IdentityLoader>
-))
+				<Slots.Footer>
+					<p>
+						<small>Created with <a className="content-link" href="https://www.contember.com/">AI-assisted Contember Studio</a></small>
+					</p>
+				</Slots.Footer>
+
+				{children}
+			</LayoutComponent>
+		</IdentityLoader>
+	)
+})
 Layout.displayName = 'Layout'

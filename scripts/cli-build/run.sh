84ac205a7ba3a9d5931f33c1250548a1dfdab0e0
David Matejka <matej21@matej21.cz>
2025-01-28 17:18:18 +0100

    build: set NODE_ENV to production when running bun build

--- scripts/cli-build/run.sh
+++ scripts/cli-build/run.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir --parent dist
 bun run --filter=@contember/cli build
-bun build --target=node --conditions=typescript --external=esbuild --sourcemap=linked --env=disable ./packages/cli/src/run.ts --outdir=dist
+NODE_ENV=production bun build --target=node --conditions=typescript --external=esbuild --sourcemap=linked --env=disable ./packages/cli/src/run.ts --outdir=dist
 cd dist
 echo "{}" > package.json
 
--- scripts/server-build/run.sh
+++ scripts/server-build/run.sh
@@ -2,5 +2,5 @@
 set -e
 
 mkdir --parent dist
-bun build --target=node --conditions=typescript --external=esbuild --sourcemap=linked --env=disable ./packages/engine-server/src/start.ts --outdir=dist
+NODE_ENV=production bun build --target=node --conditions=typescript --external=esbuild --sourcemap=linked --env=disable ./packages/engine-server/src/start.ts --outdir=dist
 #bun workspaces focus --production @contember/engine-server

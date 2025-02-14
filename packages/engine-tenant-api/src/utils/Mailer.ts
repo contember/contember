c1840b08ed87428fca5ca856fed5acdfbad50439
David Matejka <matej21@matej21.cz>
2025-02-14 15:16:11 +0100

    feat(tenant-api): send mails using webhooks

--- packages/engine-tenant-api/src/utils/Mailer.ts
+++ packages/engine-tenant-api/src/utils/Mailer.ts
@@ -1,12 +1,20 @@
 import { createTransport, Transporter, TransportOptions } from 'nodemailer'
-import { Options as MailMessage } from 'nodemailer/lib/mailer'
 import * as SMTPTransport from 'nodemailer/lib/smtp-transport'
 import * as SMTPPool from 'nodemailer/lib/smtp-pool'
 import * as SendmailTransport from 'nodemailer/lib/sendmail-transport'
 import * as SESTransport from 'nodemailer/lib/ses-transport'
 import { logger } from '@contember/logger'
+import { MailTemplateIdentifier } from '../model'
 
-export { type MailMessage }
+export interface MailMessage {
+	to: string
+	subject: string
+	html: string
+	from?: string
+	replyTo?: string
+	variables?: Record<string, any>
+	template?: MailTemplateIdentifier
+}
 
 export interface SentInfo {
 	error?: any
@@ -17,11 +25,20 @@ export interface SentInfo {
 	pending?: string[]
 }
 
-export type MailerOptions = Partial<
-	(SMTPTransport.Options | SMTPPool.Options | SendmailTransport.Options | SESTransport.Options | TransportOptions) & {
+export type MailerOptions =
+	& (
+		SMTPTransport.Options | SMTPPool.Options | SendmailTransport.Options | SESTransport.Options | TransportOptions
+		| {
+			webhook: string | {
+				url: string
+				headers?: Record<string, string>
+			}
+		}
+	)
+	& {
 		from?: string
 	}
->
+
 
 export interface Mailer {
 	send(message: MailMessage): Promise<SentInfo>
@@ -30,7 +47,7 @@ export interface Mailer {
 export class NodeMailer implements Mailer {
 	constructor(private readonly from: string | undefined, private readonly transport: Transporter) {}
 
-	public async send(message: MailMessage): Promise<SentInfo> {
+	public async send({ variables, template, ...message }: MailMessage): Promise<SentInfo> {
 		try {
 			return await this.transport.sendMail({ ...message, from: message.from || this.from })
 		} catch (e) {
@@ -40,7 +57,40 @@ export class NodeMailer implements Mailer {
 	}
 }
 
+export class WebhookMailer implements Mailer {
+
+	constructor(
+		private readonly from: string | undefined,
+		private readonly url: string,
+		private readonly headers: Record<string, string> = {},
+	) {
+	}
+
+	public async send(message: MailMessage): Promise<SentInfo> {
+		const response = await fetch(this.url, {
+			method: 'POST',
+			body: JSON.stringify({ ...message, from: message.from || this.from }),
+			headers: {
+				...this.headers,
+				'Content-Type': 'application/json',
+			},
+		})
+		if (!response.ok) {
+			logger.error('Mail sending failed', { mail: message, response })
+			return { error: response.statusText }
+		}
+		await response.text()
+		return {}
+	}
+}
+
+
 export const createMailer = (options: MailerOptions): Mailer => {
 	const { from, ...transportOptions } = options
+	if ('webhook' in transportOptions) {
+		const config = typeof transportOptions.webhook === 'string' ? { url: transportOptions.webhook } : transportOptions.webhook
+		return new WebhookMailer(from, config.url, config.headers)
+	}
+
 	return new NodeMailer(from, createTransport(transportOptions as any))
 }

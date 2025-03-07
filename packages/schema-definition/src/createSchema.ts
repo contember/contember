1faaaa78a978b464d0a98eecf665b472a2226749
David Matejka <matej21@matej21.cz>
2025-03-07 17:40:29 +0100

    feat(schema): support column collation

--- packages/schema-definition/src/createSchema.ts
+++ packages/schema-definition/src/createSchema.ts
@@ -2,17 +2,21 @@ import { SchemaDefinition } from './model'
 import * as InputValidation from './validation'
 import * as AclDefinition from './acl/definition'
 import * as ActionsDefinition from './actions/definition'
-import { Schema } from '@contember/schema'
+import { Model, Schema } from '@contember/schema'
 import { emptySchema } from '@contember/schema-utils'
 import { allStrict, StrictOptions, StrictDefinitionValidator } from './strict'
 
 export const createSchema = (definitions: Record<string, any>, modifyCallback?: (schema: Schema) => Schema, options?: {
 	strict?: boolean | StrictOptions
+	defaultCollation?: Model.Collation
 }): Schema => {
 	const strictOptions = options?.strict === true ? allStrict : (options?.strict || {})
 	const strictDefinitionValidator = new StrictDefinitionValidator(strictOptions)
 
-	const model = SchemaDefinition.createModel(definitions, { strictDefinitionValidator: strictDefinitionValidator })
+	const model = SchemaDefinition.createModel(definitions, {
+		strictDefinitionValidator: strictDefinitionValidator,
+		defaultCollation: options?.defaultCollation,
+	})
 	const validation = InputValidation.parseDefinition(definitions)
 	const acl = AclDefinition.createAcl(definitions, model)
 	const actions = ActionsDefinition.createActions(definitions)

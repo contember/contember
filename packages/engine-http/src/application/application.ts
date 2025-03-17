938f42e49e7badd7b5bfdb1f68264f8274e63fa2
David Matejka <matej21@matej21.cz>
2025-03-17 17:25:55 +0100

    refactor(engine-http): use performance.now instead of new Date

--- packages/engine-http/src/application/application.ts
+++ packages/engine-http/src/application/application.ts
@@ -15,6 +15,7 @@ import { ProjectGroupResolver } from '../projectGroup/ProjectGroupResolver'
 import { ProjectGroupContainer } from '../projectGroup/ProjectGroupContainer'
 import { URL } from 'node:url'
 import { cpuUsage, memoryUsage } from 'node:process'
+import { performance } from 'node:perf_hooks'
 
 type Route<C> = { match: RequestMatcher; controller: C; module: string }
 export class Application {
@@ -140,7 +141,7 @@ export class Application {
 			const ws = await new Promise<WebSocket>(resolve => wss.handleUpgrade(req, socket, head, (ws, request) => {
 				resolve(ws)
 			}))
-			const wsEstablished = new Date().getTime()
+			const wsEstablished = performance.now()
 			webSocketContext = {
 				ws,
 				abortSignal,
@@ -156,14 +157,14 @@ export class Application {
 			requestLogger.debug('Websocket connection established')
 			ws.on('error', e => {
 				requestLogger.error(e, {
-					websocketOpenMs: new Date().getTime() - wsEstablished,
+					websocketOpenMs: performance.now() - wsEstablished,
 				})
 			})
 
 			return new Promise<void>(resolve => {
 				ws.on('close', () => {
 					requestLogger.debug('Websocket connection closed', {
-						websocketOpenMs: new Date().getTime() - wsEstablished,
+						websocketOpenMs: performance.now() - wsEstablished,
 					})
 					resolve()
 				})
@@ -332,12 +333,12 @@ export class Application {
 
 	private createTimer() {
 		const times: EventTime[] = []
-		const globalStart = new Date().getTime()
+		const globalStart = performance.now()
 		const cpuUsageStart = cpuUsage()
 		const memoryUsageStart = memoryUsage()
 		const timer: Timer = (name: string, cb) => {
-			const start = new Date().getTime()
-			const time: EventTime = { label: name, start: start - globalStart }
+			const start = performance.now()
+			const time: EventTime = { label: name, start: Math.round(start - globalStart) }
 			times.push(time)
 			const res = cb()
 
@@ -347,7 +348,7 @@ export class Application {
 						await res
 					} catch {
 					} finally {
-						time.duration = new Date().getTime() - start
+						time.duration = Math.round(performance.now() - start)
 					}
 				})()
 			}
@@ -360,7 +361,7 @@ export class Application {
 			}
 
 			const emit = () => {
-				const total = new Date().getTime() - globalStart
+				const total = performance.now() - globalStart
 				const timeLabel = total > 500 ? 'TIME_SLOW' : 'TIME_OK'
 				const shouldSuppress = this.suppressAccessLog === true || !ctx.req.url || (this.suppressAccessLog !== false && this.suppressAccessLog.test(ctx.req.url))
 
@@ -369,12 +370,11 @@ export class Application {
 
 				const cpuUsageDiffMs = (cpuUsageEnd.user - cpuUsageStart.user) / 1000
 				const memoryUsageDiff = memoryUsageEnd.heapUsed - memoryUsageStart.heapUsed
-
 				const level = shouldSuppress ? 'debug' : 'info'
 				ctx.logger.log(level, !ctx.response ? 'Connection established' : ctx.response.statusCode < 400 ? `Request successful` : 'Request failed', {
 					status: ctx.response?.statusCode,
 					timeLabel: timeLabel,
-					totalTimeMs: total,
+					totalTimeMs: Math.round(total),
 					events: times,
 					cpuUsageDiffMs,
 					memoryUsageDiff,

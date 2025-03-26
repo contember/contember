5a7d93dbee0bbab3404cefe0ce3b1cf0a65bf34d
David Matejka <matej21@matej21.cz>
2025-03-26 16:41:02 +0100

    fix(react-select): disable auto orderBy in sortable multi selet

--- packages/react-select/src/components/SortableMultiSelect.tsx
+++ packages/react-select/src/components/SortableMultiSelect.tsx
@@ -71,7 +71,7 @@ export const SortableMultiSelect = Component(({ field, children, sortableBy, con
 	}, [selectedEntities])
 
 	return (
-		<Repeater field={field} sortableBy={sortableBy} initialEntityCount={0}>
+		<Repeater field={field} sortableBy={sortableBy} initialEntityCount={0} orderBy={false}>
 			<SelectCurrentEntitiesContext.Provider value={entitiesArr}>
 				<SelectIsSelectedContext.Provider value={isSelected}>
 					<SelectHandleSelectContext.Provider value={handler}>
@@ -85,7 +85,7 @@ export const SortableMultiSelect = Component(({ field, children, sortableBy, con
 	)
 }, ({ children, field, sortableBy, connectAt }) => {
 	return (
-		<Repeater field={field} sortableBy={sortableBy} initialEntityCount={0}>
+		<Repeater field={field} sortableBy={sortableBy} initialEntityCount={0} orderBy={false}>
 			<HasOne field={connectAt} expectedMutation="connectOrDisconnect">
 				{children}
 			</HasOne>

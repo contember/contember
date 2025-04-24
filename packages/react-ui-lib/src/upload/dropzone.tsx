15e4222b547e1dc5c8bbcdebb35afa4a80e57f82
jonasnobile <mesons_rates0p@icloud.com>
2025-04-24 11:51:19 +0200

    docs: add new generated ui components

--- packages/react-ui-lib/src/upload/dropzone.tsx
+++ packages/react-ui-lib/src/upload/dropzone.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import { UploaderDropzoneArea, UploaderDropzoneRoot } from '@contember/react-uploader-dropzone'
 import { useUploaderStateFiles } from '@contember/react-uploader'
 import { UploaderDropzoneAreaUI, UploaderDropzoneWrapperUI, UploaderInactiveDropzoneUI } from './ui'

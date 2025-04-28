f4e21bd3945131d56c791c9d2a291822918bdfb1
David Matejka <matej21@matej21.cz>
2025-04-28 11:57:11 +0200

    fix(schema-definition): fix allow custom primary

--- packages/schema-definition/src/acl/definition/customPrimary.ts
+++ packages/schema-definition/src/acl/definition/customPrimary.ts
@@ -5,7 +5,7 @@ import { extendEntityAcl } from './aclExtensions'
 export const allowCustomPrimary = (role?: Role | Role[]): DecoratorFunction<any> => {
 	return extendEntityAcl(({ permissions, role: currentRole }) => {
 		if (!role || role === currentRole || (Array.isArray(role) && role.includes(currentRole))) {
-			return { ...permissions, operations: { customPrimary: true } }
+			return { ...permissions, operations: { ...permissions.operations, customPrimary: true } }
 		}
 		return permissions
 	})

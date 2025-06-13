825df4aa8bb1cc8930d9df1dbf8e904cc92fc852
David Matejka <matej21@matej21.cz>
2025-06-13 14:52:24 +0200

    feat(schema): index method (gin, gist etc.)

--- packages/schema-definition/src/model/definition/IndexDefinition.ts
+++ packages/schema-definition/src/model/definition/IndexDefinition.ts
@@ -1,17 +1,21 @@
 import { extendEntity } from './extensions'
 import { DecoratorFunction } from '../../utils'
+import { Model } from '@contember/schema'
 
-export type IndexOptions<T> = { fields: (keyof T)[] }
+export type IndexOptions<T> = {
+	fields: (keyof T)[]
+	method?: Model.IndexMethod
+}
 export function Index<T>(options: IndexOptions<T>): DecoratorFunction<T>
 export function Index<T>(...fields: (keyof T)[]): DecoratorFunction<T>
 export function Index<T>(options: IndexOptions<T> | keyof T, ...args: (keyof T)[]): DecoratorFunction<T> {
 	return extendEntity(({ entity }) => {
-		const fields = (typeof options !== 'object' ? [options, ...args] : options.fields) as string[]
+		const indexDef = (typeof options !== 'object' ? { fields: [options, ...args] } : options) as { fields: string[]; method?: Model.IndexMethod }
 		return {
 			...entity,
 			indexes: [
 				...entity.indexes,
-				{ fields },
+				indexDef,
 			],
 		}
 	})

c7d2d9ff1c20ee2fee880e742d0bef78117124de
David Matejka <matej21@matej21.cz>
2025-06-17 13:29:36 +0200

    refactor(react-client)!: remove use*ApiRequest hooks

--- packages/react-client/src/hooks/tenant/useTenantApiRequest.ts
+++
@@ -1,6 +0,0 @@
-import type { UseApiRequestResult } from '../apiRequest'
-import { useApiRequest } from '../apiRequest'
-import { useTenantGraphQlClient } from './useTenantGraphQlClient'
-
-export const useTenantApiRequest = <SuccessData>(): UseApiRequestResult<SuccessData> =>
-	useApiRequest<SuccessData>(useTenantGraphQlClient())
--- packages/react-client/src/hooks/useTenantGraphQlClient.ts
+++ packages/react-client/src/hooks/useTenantGraphQlClient.ts
@@ -1,3 +1,3 @@
-import { useGraphQlClient } from '../useGraphQlClient'
+import { useGraphQlClient } from './useGraphQlClient'
 
 export const useTenantGraphQlClient = () => useGraphQlClient('/tenant')

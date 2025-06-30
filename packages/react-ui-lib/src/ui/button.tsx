e941d7e7013347f943ee7ce3af76f379f39f0582
jonasnobile <mesons_rates0p@icloud.com>
2025-06-30 20:30:28 +0200

    feat(react-ui-lib): add page components

--- packages/react-ui-lib/src/ui/button.tsx
+++ packages/react-ui-lib/src/ui/button.tsx
@@ -1,10 +1,10 @@
 import { uic, uiconfig } from '../utils'
 
 export const buttonConfig = uiconfig({
-	baseClass: 'inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 transition-all',
+	baseClass: 'inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 transition-all',
 	variants: {
 		variant: {
-			default: 'bg-primary text-primary-foreground shadow-sm hover:bg-primary/90',
+			primary: 'bg-primary text-primary-foreground shadow-sm hover:bg-primary/90',
 			destructive: 'bg-destructive text-destructive-foreground shadow-xs hover:bg-destructive/90',
 			outline: 'border border-input bg-background-upper shadow-xs hover:bg-accent hover:text-accent-foreground',
 			secondary: 'bg-secondary border border-input text-secondary-foreground shadow-xs hover:bg-secondary/80',
@@ -12,7 +12,7 @@ export const buttonConfig = uiconfig({
 			link: 'text-primary underline-offset-4 hover:underline',
 		},
 		size: {
-			default: 'h-9 px-4 py-2',
+			md: 'h-9 px-4 py-2',
 			xs: 'h-6 rounded-sm px-2 text-xs',
 			sm: 'h-8 rounded-md px-3 text-xs',
 			lg: 'h-10 rounded-md px-8',
@@ -20,8 +20,8 @@ export const buttonConfig = uiconfig({
 		},
 	},
 	defaultVariants: {
-		variant: 'default',
-		size: 'default',
+		variant: 'primary',
+		size: 'md',
 	},
 })
 

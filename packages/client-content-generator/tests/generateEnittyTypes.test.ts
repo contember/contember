3a7ac4571bdb1f57183c2f3e97d5efe5a2ac938b
jonasnobile <mesons_rates0p@icloud.com>
2025-07-01 18:54:57 +0200

    feat(client-content-generator): implement inlcude deprecated flag

--- packages/client-content-generator/tests/generateEnittyTypes.test.ts
+++ packages/client-content-generator/tests/generateEnittyTypes.test.ts
@@ -11,13 +11,30 @@ describe('generate entities', () => {
 		expect(entityGenerator.generate(schemas.scalarsSchema.model)).toMatchSnapshot()
 	})
 
+	test('generate for scalars with deprecated', () => {
+
+		expect(entityGenerator.generate(schemas.scalarsSchema.model, { includeDeprecated: true })).toMatchSnapshot()
+	})
+
 	test('generate for has enum', () => {
 
 		expect(entityGenerator.generate(schemas.enumSchema.model)).toMatchSnapshot()
 	})
+
+	test('generate for has enum deprecated', () => {
+
+		expect(entityGenerator.generate(schemas.enumSchema.model, { includeDeprecated: true })).toMatchSnapshot()
+	})
+
+
 	test('generate one has one', () => {
 
 		expect(entityGenerator.generate(schemas.oneHasOneSchema.model)).toMatchSnapshot()
+	})
+
+	test('generate one has one deprecated', () => {
+
+		expect(entityGenerator.generate(schemas.oneHasOneSchema.model, { includeDeprecated: true })).toMatchSnapshot()
 	})
 
 	test('generate one has many', () => {
@@ -25,14 +42,29 @@ describe('generate entities', () => {
 		expect(entityGenerator.generate(schemas.oneHasManySchema.model)).toMatchSnapshot()
 	})
 
+	test('generate one has many deprecated', () => {
+
+		expect(entityGenerator.generate(schemas.oneHasManySchema.model, { includeDeprecated: true })).toMatchSnapshot()
+	})
+
 	test('generate many has many', () => {
 
 		expect(entityGenerator.generate(schemas.manyHasManySchema.model)).toMatchSnapshot()
 	})
 
+	test('generate many has many deprecated', () => {
+
+		expect(entityGenerator.generate(schemas.manyHasManySchema.model, { includeDeprecated: true })).toMatchSnapshot()
+	})
+
 	test('generate reduced has by', () => {
 
 		expect(entityGenerator.generate(schemas.reducedHasManySchema.model)).toMatchSnapshot()
+	})
+
+	test('generate reduced has by deprecated', () => {
+
+		expect(entityGenerator.generate(schemas.reducedHasManySchema.model, { includeDeprecated: true })).toMatchSnapshot()
 	})
 
 })
